87f2394a02396cf7e4a844c10826aaae2c8bde44
Kishan-Dhakan <kishandhakan17@gmail.com>
2021-08-25 14:50:27 +0530

    refactored using t.Run()

--- zboxcli_tests/registerwallet_test.go
+++ zboxcli_tests/registerwallet_test.go
@@ -8,17 +8,18 @@ import (
 )
 
 func TestZboxCliRegisterWallet(t *testing.T) {
-
-	// Run the "./zbox register" command from the latest build
-	// fetched dynamically using the getLatestBuild.sh script
-	err := exec.Command("sh", "-c", "../getLatestBuild.sh").Run()
-
-	if assert.Nil(t, err) {
-		cmd := exec.Command("./zbox", "register")
-		output, err := cmd.Output()
+	t.Run("Checks terminal output", func(t *testing.T) {
+		// Run the "./zbox register" command from the latest build
+		// fetched dynamically using the getLatestBuild.sh script
+		err := exec.Command("sh", "-c", "../getLatestBuild.sh").Run()
 
 		if assert.Nil(t, err) {
-			assert.Equal(t, "Wallet registered\n", string(output))
+			cmd := exec.Command("./zbox", "register")
+			output, err := cmd.Output()
+
+			if assert.Nil(t, err) {
+				assert.Equal(t, "Wallet registered\n", string(output))
+			}
 		}
-	}
+	})
 }
