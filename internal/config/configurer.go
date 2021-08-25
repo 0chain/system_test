c5d7d699ed9a94ecb7ea0d9dda23efffd64e7f73
Kush Trivedi <44091822+kushthedude@users.noreply.github.com>
2021-08-25 20:41:57 +0530

    fix: add validator on the configuration package (#9)
    Signed-off-by: kushthedude <kushthedude@gmail.com>

--- internal/config/configurer.go
+++ internal/config/configurer.go
@@ -1,6 +1,7 @@
 package config
 
 import (
+	"github.com/go-playground/validator/v10"
 	"github.com/pkg/errors"
 	log "github.com/sirupsen/logrus"
 	"github.com/spf13/viper"
@@ -36,6 +37,12 @@ func NewConfigurer(fileLocation string) (*Configurer, error) {
 	if err != nil {
 		log.Errorf("failed to unmarshal configuration %v", err)
 		return nil, errors.Wrap(err, "failed to unmarshal configuration")
+	}
+
+	validate := validator.New()
+
+	if err := validate.Struct(requiredConfig); err!=nil {
+		return nil, errors.Wrap(err, "missing configuration values")
 	}
 
 	return &Configurer{
