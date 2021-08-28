aa8fdbb45c4aa57bf5971647b4790a196c1bb90b
Kishan-Dhakan <kishandhakan17@gmail.com>
2021-08-28 10:40:57 +0530

    added zwallet script and faucet test commit

---
+++ getLatestZwalletBuild.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+## Get Latest Build's URL from master
+URL=$(curl -s https://api.github.com/repos/0chain/zwalletcli/releases/latest \
+| grep "browser_download_url.*zip" \
+| cut -d : -f 2,3 \
+| tr -d \")
+
+# Download, extract the zip to get the exe and delete the zip
+curl -LO ${URL}
+unzip -o zwallet-windows.zip && rm zwallet-windows.zip
