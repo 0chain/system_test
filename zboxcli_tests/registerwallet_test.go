918e6fe700f7e20dc1980d104c1b9cd33abc9ada
Kishan-Dhakan <kishandhakan17@gmail.com>
2021-08-28 12:15:22 +0530

    integrating with workflow

--- zboxcli_tests/registerwallet_test.go
+++ zboxcli_tests/registerwallet_test.go
@@ -12,13 +12,13 @@ func TestShouldReturnWalletRegisteredOnRunningRegisterCommand(t *testing.T) {
 	t.Run("Checks terminal output", func(t *testing.T) {
 		// Run the "./zbox register" command from the latest build
 		// fetched dynamically using the getLatestBuild.sh script
-		err := exec.Command("sh", "-c", "../getLatestZboxBuild.sh").Run()
+		//err := exec.Command("sh", "-c", "../getLatestZboxBuild.sh").Run()
 
-		if err != nil {
-			t.Errorf(err.Error())
-		}
+		// if err != nil {
+		// 	t.Errorf(err.Error())
+		// }
 
-		cmd := exec.Command("./zbox", "register")
+		cmd := exec.Command("./zbox.exe", "register")
 		output, err := cmd.Output()
 
 		if err != nil {
@@ -31,13 +31,13 @@ func TestShouldReturnWalletRegisteredOnRunningRegisterCommand(t *testing.T) {
 	t.Run("Execute faucet against Wallet", func(t *testing.T) {
 		// Get faucet tokens on zwallet by running the fuacet method on
 		// latest release of zwalletcli
-		err := exec.Command("sh", "-c", "../getLatestZwalletBuild.sh").Run()
+		// err := exec.Command("sh", "-c", "../getLatestZwalletBuild.sh").Run()
 
-		if err != nil {
-			t.Errorf(err.Error())
-		}
+		// if err != nil {
+		// 	t.Errorf(err.Error())
+		// }
 
-		cmd := exec.Command("./zwallet", "faucet", "--methodName", "pour", "--input", "\"{Pay day}\"")
+		cmd := exec.Command("./zwallet.exe", "faucet", "--methodName", "pour", "--input", "\"{Pay day}\"")
 		output, err := cmd.Output()
 
 		if err != nil {
