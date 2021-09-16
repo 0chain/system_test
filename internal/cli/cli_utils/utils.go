84bf071d16708bef4bedafa42d93c142e47070db
Mohsen Mirghafouri <mohsen.no1@gmail.com>
2021-09-16 22:43:07 +0300

    Create allocations with small expiry test

--- internal/cli/cli_utils/utils.go
+++ internal/cli/cli_utils/utils.go
@@ -1,11 +1,17 @@
 package cli_utils
 
 import (
+	"encoding/json"
+	"fmt"
 	"math/rand"
 	"os/exec"
 	"regexp"
 	"strings"
+	"testing"
 	"time"
+
+	"github.com/0chain/system_test/internal/cli/cli_model"
+	"github.com/stretchr/testify/assert"
 )
 
 func RunCommand(command string) ([]string, error) {
@@ -33,4 +39,56 @@ func RandomAlphaNumericString(n int) string {
 		b[i] = letterRunes[rand.Intn(len(letterRunes))]
 	}
 	return string(b)
+}
+
+func RegisterWallet(cliConfigFilename string) ([]string, error) {
+	return RunCommand("./zbox register --silent --configDir ./config --config " + cliConfigFilename)
+}
+
+func GetBalance(cliConfigFilename string) ([]string, error) {
+	return RunCommand("./zwallet getbalance --silent --configDir ./config --config " + cliConfigFilename)
+}
+
+func GetWallet(t *testing.T, cliConfigFilename string) (*cli_model.Wallet, error) {
+	output, err := RunCommand("./zbox getwallet --json --silent --configDir ./config --config " + cliConfigFilename)
+
+	if err != nil {
+		return nil, err
+	}
+
+	assert.Equal(t, 1, len(output))
+
+	var wallet *cli_model.Wallet
+
+	err = json.Unmarshal([]byte(output[0]), &wallet)
+	if err != nil {
+		t.Errorf("failed to unmarshal the result into wallet")
+		return nil, err
+	}
+
+	return wallet, err
+}
+
+func ExecuteFaucet(cliConfigFilename string) ([]string, error) {
+	return RunCommand("./zwallet faucet --methodName pour --tokens 1 --input {} --silent --configDir ./config --config " + cliConfigFilename)
+}
+
+func VerifyTransaction(cliConfigFilename string, txn string) ([]string, error) {
+	return RunCommand("./zwallet verify --silent --hash " + txn + " --configDir ./config --config " + cliConfigFilename)
+}
+
+func NewAllocation(cliConfigFilename string, lock float64, options map[string]string) ([]string, error) {
+	cmd := fmt.Sprintf("./zbox newallocation --lock %v --silent --configDir ./config --config %s ", lock, cliConfigFilename)
+	for key, option := range options {
+		cmd += fmt.Sprintf(" --%s %s ", key, option)
+	}
+	return RunCommand(cmd)
+}
+
+func WritePoolInfo() ([]string, error) {
+	return RunCommand("./zbox wp-info --silent")
+}
+
+func ChallengePoolInfo(allocationID string) ([]string, error) {
+	return RunCommand("./zbox cp-info --allocation " + allocationID + " --silent")
 }
