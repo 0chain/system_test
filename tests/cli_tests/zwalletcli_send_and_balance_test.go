76263aede43987e84022fdd4fa6aa0969afaef4b
Tyrone Villaluna <avanaur@gmail.com>
2021-09-16 23:29:27 +1000

    fix: update tests

--- tests/cli_tests/zwalletcli_send_and_balance_test.go
+++ tests/cli_tests/zwalletcli_send_and_balance_test.go
@@ -4,6 +4,7 @@ import (
 	"github.com/0chain/system_test/internal/cli/cli_model"
 	"github.com/0chain/system_test/internal/cli/cli_utils"
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 	"regexp"
 	"strings"
 	"testing"
@@ -15,64 +16,41 @@ func TestSendAndBalance(t *testing.T) {
 
 		target, err := registerSenderAndTargetWallets(t)
 		if err != nil {
+			t.Errorf("Registering sender and target wallets failed: %v", err)
 			return
 		}
 
-		output, err := executeFaucet(t, configPath)
-		if err != nil {
-			t.Errorf("Faucet execution failed due to error: %v", err)
-			return
-		}
+		requireFaucetSuccess(t)
 
 		// Before send balance checks
-		output, err = getBalance(t, configPath)
-		if err != nil {
-			t.Error(err)
-		}
+		assertGetBalanceSuccess(t, senderWallet(t), "1.000")
+		assertGetBalanceFailure(t, targetWallet(t))
 
-		assert.Equal(t, 1, len(output))
-		assert.Regexp(t, regexp.MustCompile("Balance: 1.000 ZCN \\([0-9.]+ USD\\)$"), output[0])
-
-		output, err = getBalanceForWallet(configPath, targetWallet(t))
-		if err == nil {
-			t.Errorf("Expected balance check to fail for target wallet: Output %v", strings.Join(output, "\n"))
-			return
-		}
-
-		assert.Equal(t, 1, len(output))
-		assert.Equal(t, "Failed to get balance:", output[0])
-
-		output, err = send(t, configPath, target.ClientId, "1", "")
-		if err != nil {
-			t.Errorf("Send failed due to error: Output %v", strings.Join(output, "\n"))
-			return
-		}
-
-		assert.Equal(t, "Send tokens success", output[len(output)-1])
+		// Transfer ZCN from sender to target wallet
+		assertSendSuccess(t, target.ClientId, "1", "transfer")
 
 		// After send balance checks
-		output, err = getBalance(t, configPath)
-		if err == nil {
-			t.Errorf("Expected balance check to fail for sender wallet: Output %v", strings.Join(output, "\n"))
+		assertGetBalanceFailure(t, senderWallet(t))
+		assertGetBalanceSuccess(t, targetWallet(t), "1.000")
+	})
+
+	t.Run("Send with and without description", func(t *testing.T) {
+		target, err := registerSenderAndTargetWallets(t)
+		if err != nil {
+			t.Errorf("Registering sender and target wallets failed: %v", err)
 			return
 		}
 
-		assert.Equal(t, 1, len(output))
-		assert.Equal(t, "Failed to get balance:", output[0])
+		requireFaucetSuccess(t)
 
-		output, err = getBalanceForWallet(configPath, targetWallet(t))
-		if err != nil {
-			t.Error(err)
-		}
+		// no desc
+		assertSendSuccess(t, target.ClientId, "0.5", "")
 
-		assert.Equal(t, 1, len(output))
-		assert.Regexp(t, regexp.MustCompile("Balance: 1.000 ZCN \\([0-9.]+ USD\\)$"), output[0])
+		// with desc
+		assertSendSuccess(t, target.ClientId, "0.5", "rich description")
+
+		// cannot verify transaction payload at this moment due to transaction hash not being printed.
 	})
-
-	// TODO
-	//t.Run("Send with and without description", func(t *testing.T) {
-	// TODO verify send transaction and message
-	//})
 
 	t.Run("Send attempt on zero ZCN wallet", func(t *testing.T) {
 		t.Parallel()
@@ -82,36 +60,36 @@ func TestSendAndBalance(t *testing.T) {
 			return
 		}
 
-		output, err := send(t, configPath, target.ClientId, "1", "")
-		if err == nil {
-			t.Errorf("Expected send to fail due to zero ZCN wallet: Output %v", strings.Join(output, "\n"))
-			return
-		}
-
-		assert.Equal(t, "Send tokens failed. {\"error\": \"verify transaction failed\"}", output[len(output)-1])
+		wantFailureMsg := "Send tokens failed. {\"error\": \"verify transaction failed\"}"
+		assertSendFailure(t, target.ClientId, "1", "", wantFailureMsg)
 	})
 
 	t.Run("Send attempt to invalid address", func(t *testing.T) {
 		t.Parallel()
 
-		if _, err := registerWallet(t, configPath); err != nil {
-			t.Errorf("An error occured registering a wallet due to error: %v", err)
-			return
-		}
+		output, err := registerWallet(t, configPath)
+		require.NoError(t, err, "Unexpected register wallet failure: Output %v", strings.Join(output, "\n"))
 
-		output, err := executeFaucet(t, configPath)
+		requireFaucetSuccess(t)
+
+		invalidClientID := "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaabb" // more than 64 chars
+
+		wantFailureMsg := "Send tokens failed. submit transaction failed. {\"code\":\"invalid_request\"," +
+			"\"error\":\"invalid_request: Invalid request (to client id must be a hexadecimal hash)\"}"
+		assertSendFailure(t, invalidClientID, "1", "invalid address", wantFailureMsg)
+	})
+
+	t.Run("Send with zero token", func(t *testing.T) {
+		t.Parallel()
+
+		target, err := registerSenderAndTargetWallets(t)
 		if err != nil {
-			t.Errorf("Faucet execution failed due to error: %v", err)
 			return
 		}
 
-		output, err = send(t, configPath, "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa", "1", "")
-		if err == nil {
-			t.Errorf("Expected send to fail due to invalid target wallet: Output %v", strings.Join(output, "\n"))
-			return
-		}
+		requireFaucetSuccess(t)
 
-		assert.Equal(t, "Send tokens failed. {\"error\": \"verify transaction failed\"}", output[len(output)-1])
+		assertSendSuccess(t, target.ClientId, "0", "negative token")
 	})
 
 	t.Run("Send attempt to exceeding balance", func(t *testing.T) {
@@ -122,32 +100,77 @@ func TestSendAndBalance(t *testing.T) {
 			return
 		}
 
-		output, err := executeFaucet(t, configPath)
+		requireFaucetSuccess(t)
+
+		wantFailureMsg := "Send tokens failed. {\"error\": \"verify transaction failed\"}"
+		assertSendFailure(t, target.ClientId, "1.5", "exceed bal", wantFailureMsg)
+	})
+
+	t.Run("Send attempt with negative token", func(t *testing.T) {
+		t.Parallel()
+
+		target, err := registerSenderAndTargetWallets(t)
 		if err != nil {
-			t.Errorf("Faucet execution failed due to error: %v", err)
 			return
 		}
 
-		output, err = send(t, configPath, target.ClientId, "1.5", "")
-		if err == nil {
-			t.Errorf("Expected send to fail due to low ZCN balance: Output %v", strings.Join(output, "\n"))
+		requireFaucetSuccess(t)
+
+		wantFailureMsg := "Send tokens failed. submit transaction failed. {\"code\":\"invalid_request\"," +
+			"\"error\":\"invalid_request: Invalid request (value must be greater than or equal to zero)\"}"
+		assertSendFailure(t, target.ClientId, "-1", "negative token", wantFailureMsg)
+	})
+
+	t.Run("Send attempt with very long description", func(t *testing.T) {
+		t.Parallel()
+
+		target, err := registerSenderAndTargetWallets(t)
+		if err != nil {
 			return
 		}
 
-		assert.Equal(t, "Send tokens failed. {\"error\": \"verify transaction failed\"}", output[len(output)-1])
+		requireFaucetSuccess(t)
+
+		b := make([]rune, 100000)
+		for i := range b {
+			b[i] = 'a'
+		}
+		longDesc := string(b)
+
+		wantFailureMsg := "Send tokens failed. submit transaction failed. {\"code\":\"txn_exceed_max_payload\"," +
+			"\"error\":\"txn_exceed_max_payload: transaction payload exceeds the max payload (98304)\"}"
+		assertSendFailure(t, target.ClientId, "1", longDesc, wantFailureMsg)
+	})
+
+	t.Run("Send attempt to self", func(t *testing.T) {
+		t.Parallel()
+
+		output, err := registerWallet(t, configPath)
+		require.NoError(t, err, "Unexpected register wallet failure: Output %v", strings.Join(output, "\n"))
+
+		wallet, err := getWallet(t, configPath)
+		if err != nil {
+			t.Errorf("Error occured when retrieving target wallet due to error: %v", err)
+		}
+
+		requireFaucetSuccess(t)
+
+		wantFailureMsg := "Send tokens failed. submit transaction failed. {\"code\":\"invalid_request\"," +
+			"\"error\":\"invalid_request: Invalid request (from and to client should be different)\"}"
+		assertSendFailure(t, wallet.ClientId, "1", "send self", wantFailureMsg)
 	})
 }
 
 func registerSenderAndTargetWallets(t *testing.T) (target *cli_model.Wallet, err error) {
 	// Sender Wallet
-	if _, err := registerWallet(t, configPath); err != nil {
-		t.Errorf("An error occured registering a wallet due to error: %v", err)
+	if output, err := registerWallet(t, configPath); err != nil {
+		require.NoError(t, err, "Unexpected register wallet failure: Output %v", strings.Join(output, "\n"))
 		return nil, err
 	}
 
 	// Target Wallet
-	if _, err := registerWalletForName(configPath, targetWallet(t)); err != nil {
-		t.Errorf("An error occured registering a wallet due to error: %v", err)
+	if output, err := registerWalletForName(configPath, targetWallet(t)); err != nil {
+		require.NoError(t, err, "Unexpected register wallet failure: Output %v", strings.Join(output, "\n"))
 		return nil, err
 	}
 
@@ -156,7 +179,57 @@ func registerSenderAndTargetWallets(t *testing.T) (target *cli_model.Wallet, err
 		t.Errorf("Error occured when retrieving target wallet due to error: %v", err)
 	}
 
-	return target, err
+	return target, nil
+}
+
+func assertGetBalanceFailure(t *testing.T, walletName string) {
+	output, err := getBalanceForWallet(configPath, walletName)
+	if err == nil {
+		t.Errorf("Missing expected balance check failure for wallet %s: Output %v", walletName, strings.Join(output, "\n"))
+		return
+	}
+
+	assert.Equal(t, 1, len(output))
+	assert.Equal(t, "Failed to get balance:", output[0])
+}
+
+func assertGetBalanceSuccess(t *testing.T, walletName string, balance string) {
+	output, err := getBalanceForWallet(configPath, walletName)
+	if err != nil {
+		t.Errorf("Unexpected balance check failure for wallet %s: Output %v", walletName, strings.Join(output, "\n"))
+	}
+
+	assert.Equal(t, 1, len(output))
+	assert.Regexp(t, regexp.MustCompile("Balance: "+balance+" ZCN \\([0-9.]+ USD\\)$"), output[0])
+}
+
+func assertSendFailure(t *testing.T, toClientID string, tokens string, desc string, failureMsg string) {
+	output, err := send(t, configPath, toClientID, tokens, desc)
+	if err == nil {
+		t.Errorf("Missing expected send failure: Output %v", strings.Join(output, "\n"))
+		return
+	}
+
+	assert.Equal(t, failureMsg, output[len(output)-1])
+}
+
+func assertSendSuccess(t *testing.T, toClientID string, tokens string, desc string) {
+	output, err := send(t, configPath, toClientID, tokens, desc)
+	if err != nil {
+		t.Errorf("Unexpected send failure: Output %v", strings.Join(output, "\n"))
+		return
+	}
+
+	assert.Equal(t, "Send tokens success", output[len(output)-1])
+}
+
+func requireFaucetSuccess(t *testing.T) {
+	output, err := executeFaucet(t, configPath)
+	require.NoError(t, err, "Unexpected faucet failure: Output %v", strings.Join(output, "\n"))
+}
+
+func senderWallet(t *testing.T) string {
+	return escapedTestName(t)
 }
 
 func targetWallet(t *testing.T) string {
@@ -164,7 +237,7 @@ func targetWallet(t *testing.T) string {
 }
 
 func send(t *testing.T, cliConfigFilename string, toClientID string, tokens string, desc string) ([]string, error) {
-	return cli_utils.RunCommand("./zwallet send --tokens " + tokens +
+	return cli_utils.RunCommand("./zwallet send --silent --tokens " + tokens +
 		" --desc \"" + desc + "\"" +
 		" --to_client_id " + toClientID +
 		" --wallet " + escapedTestName(t) + "_wallet.json --configDir ./config --config " + cliConfigFilename)
