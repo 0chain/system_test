fa165078176df5a937c9707516bc31d1a74678cd
Mohsen Mirghafouri <mohsen.no1@gmail.com>
2021-09-17 00:54:06 +0300

    Create allocation with smallest possible size

--- tests/cli_tests/zboxcli_create_allocation_test.go
+++ tests/cli_tests/zboxcli_create_allocation_test.go
@@ -10,39 +10,62 @@ import (
 )
 
 func TestCreateAllocation(t *testing.T) {
-
-	t.Run("Create allocations with small expiry", func(t *testing.T) {
+	t.Run("Create allocation with small expiry", func(t *testing.T) {
 		t.Parallel()
 
 		output, err := cli_utils.RegisterWallet(configPath)
-		assert.NotNil(t, err)
-
-		if len(output) == 1 {
-			assert.Equal(t, "Wallet registered", output[0])
-		} else {
-			// This is true for the first round only since the wallet is created here
-			assert.Equal(t, "ZCN wallet created!!", output[1])
-			assert.Equal(t, "Creating related read pool for storage smart-contract...", output[2])
-			assert.Equal(t, "Read pool created successfully", output[3])
-		}
+		assert.Nil(t, err)
 
 		_, err = cli_utils.ExecuteFaucet(configPath)
-		assert.NotNil(t, err)
+		assert.Nil(t, err)
 
 		_, err = cli_utils.GetBalance(configPath)
-		assert.NotNil(t, err)
+		assert.Nil(t, err)
 
 		var lock float64 = 0.5
-		options := map[string]string{"expire": "300s"}
+		options := map[string]string{"expire": "1h"}
 		output, err = cli_utils.NewAllocation(configPath, lock, options)
-		assert.NotNil(t, err)
+		assert.Nil(t, err)
 
 		assert.Regexp(t, regexp.MustCompile("^Allocation created: [0-9a-fA-F]{64}$"), output[0])
 
 	})
 
-	t.Run("Create allocations with largest possible expiry", func(t *testing.T) {
+	t.Run("Create allocation with invalid expiry", func(t *testing.T) {
 		t.Parallel()
+		output, err := cli_utils.RegisterWallet(configPath)
+		assert.Nil(t, err)
 
+		_, err = cli_utils.ExecuteFaucet(configPath)
+		assert.Nil(t, err)
+
+		_, err = cli_utils.GetBalance(configPath)
+		assert.Nil(t, err)
+
+		var lock float64 = 0.5
+		options := map[string]string{"expire": "-1"}
+		output, err = cli_utils.NewAllocation(configPath, lock, options)
+		assert.NotNil(t, err)
+
+		assert.Equal(t, "invalid argument \"-1\" for \"--expire\" flag: time: missing unit in duration -1", output[len(output)-1])
+	})
+
+	t.Run("Create allocation with smallest possible size", func(t *testing.T) {
+		t.Parallel()
+		output, err := cli_utils.RegisterWallet(configPath)
+		assert.Nil(t, err)
+
+		_, err = cli_utils.ExecuteFaucet(configPath)
+		assert.Nil(t, err)
+
+		_, err = cli_utils.GetBalance(configPath)
+		assert.Nil(t, err)
+
+		var lock float64 = 0.5
+		options := map[string]string{"expire": "-1"}
+		output, err = cli_utils.NewAllocation(configPath, lock, options)
+		assert.NotNil(t, err)
+
+		assert.Equal(t, "invalid argument \"-1\" for \"--expire\" flag: time: missing unit in duration -1", output[len(output)-1])
 	})
 }
