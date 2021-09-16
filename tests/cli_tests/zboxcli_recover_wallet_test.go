9250b18cf2f91464406143242fd7a6c41408e840
Kush Trivedi <kushthedude@gmail.com>
2021-09-16 20:56:57 +0530

    feat: add parallel execution in tests
    Signed-off-by: Kush Trivedi <kushthedude@gmail.com>

--- tests/cli_tests/zboxcli_recover_wallet_test.go
+++ tests/cli_tests/zboxcli_recover_wallet_test.go
@@ -1,50 +1,52 @@
 package cli_tests
 
 import (
-	"github.com/0chain/system_test/internal/cli/cli_utils"
+	"github.com/0chain/system_test/internal/cli/util"
 	"github.com/stretchr/testify/assert"
 	"testing"
 )
 
 func TestWalletRecoveryUsingMnemonics(t *testing.T) {
 
-	t.Run("Recover wallet valid mnemonic", func(t *testing.T) {
-		t.Parallel()
-		validMnemonic := "pull floor crop best weasel suit solid gown filter kitten loan absent noodle nation potato planet demise online ten affair rich panel rent sell"
-		output, err := recoverWalletFromMnemonic(t, configPath, validMnemonic)
-		if err != nil {
-			t.Errorf("An error occured recovering a wallet due to error: %v", err)
-		}
+	t.Run("parallel", func(t *testing.T) {
+		t.Run("Recover wallet valid mnemonic", func(t *testing.T) {
+			t.Parallel()
+			validMnemonic := "pull floor crop best weasel suit solid gown filter kitten loan absent noodle nation potato planet demise online ten affair rich panel rent sell"
+			output, err := recoverWalletFromMnemonic(t, configPath, validMnemonic)
+			if err != nil {
+				t.Errorf("An error occured recovering a wallet due to error: %v", err)
+			}
 
-		message := output[len(output)-1]
-		assert.Equal(t, "Wallet recovered!!", message)
-	})
+			message := output[len(output)-1]
+			assert.Equal(t, "Wallet recovered!!", message)
+		})
 
-	//FIXME: POSSIBLE BUG: Blank wallet created if mnemonic is invalid
-	t.Run("Recover wallet invalid mnemonic", func(t *testing.T) {
-		t.Parallel()
-		inValidMnemonic := "floor crop best weasel suit solid gown filter kitten loan absent noodle nation potato planet demise online ten affair rich panel rent sell"
-		output, err := recoverWalletFromMnemonic(t, configPath, inValidMnemonic)
-		assert.NotNil(t, err)
-		assert.Equal(t, 5, len(output))
-		assert.Equal(t, "No wallet in path  ./config/TestWalletRecoveryUsingMnemonics-Recover_wallet_invalid_mnemonic_wallet.json found. Creating wallet...", output[0])
-		assert.Equal(t, "ZCN wallet created!!", output[1])
-		assert.Equal(t, "Creating related read pool for storage smart-contract...", output[2])
-		assert.Equal(t, "Read pool created successfully", output[3])
-		assert.Equal(t, "Error: Invalid mnemonic", output[4])
-	})
+		//FIXME: POSSIBLE BUG: Blank wallet created if mnemonic is invalid
+		t.Run("Recover wallet invalid mnemonic", func(t *testing.T) {
+			t.Parallel()
+			inValidMnemonic := "floor crop best weasel suit solid gown filter kitten loan absent noodle nation potato planet demise online ten affair rich panel rent sell"
+			output, err := recoverWalletFromMnemonic(t, configPath, inValidMnemonic)
+			assert.NotNil(t, err)
+			assert.Equal(t, 5, len(output))
+			assert.Equal(t, "No wallet in path  ./config/TestWalletRecoveryUsingMnemonics-Recover_wallet_invalid_mnemonic_wallet.json found. Creating wallet...", output[0])
+			assert.Equal(t, "ZCN wallet created!!", output[1])
+			assert.Equal(t, "Creating related read pool for storage smart-contract...", output[2])
+			assert.Equal(t, "Read pool created successfully", output[3])
+			assert.Equal(t, "Error: Invalid mnemonic", output[4])
+		})
 
-	//FIXME: POSSIBLE BUG: Blank wallet created if mnemonic is missing
-	t.Run("Recover wallet no mnemonic", func(t *testing.T) {
-		t.Parallel()
-		output, err := cli_utils.RunCommand("./zwallet recoverwallet --silent --wallet " + escapedTestName(t) + "_wallet.json" + " --configDir ./config --config " + configPath)
-		assert.NotNil(t, err)
-		assert.Equal(t, 5, len(output))
-		assert.Equal(t, "No wallet in path  ./config/TestWalletRecoveryUsingMnemonics-Recover_wallet_no_mnemonic_wallet.json found. Creating wallet...", output[0])
-		assert.Equal(t, "ZCN wallet created!!", output[1])
-		assert.Equal(t, "Creating related read pool for storage smart-contract...", output[2])
-		assert.Equal(t, "Read pool created successfully", output[3])
-		assert.Equal(t, "Error: Mnemonic not provided", output[4])
+		//FIXME: POSSIBLE BUG: Blank wallet created if mnemonic is missing
+		t.Run("Recover wallet no mnemonic", func(t *testing.T) {
+			t.Parallel()
+			output, err := cli_utils.RunCommand("./zwallet recoverwallet --silent --wallet " + escapedTestName(t) + "_wallet.json" + " --configDir ./config --config " + configPath)
+			assert.NotNil(t, err)
+			assert.Equal(t, 5, len(output))
+			assert.Equal(t, "No wallet in path  ./config/TestWalletRecoveryUsingMnemonics-Recover_wallet_no_mnemonic_wallet.json found. Creating wallet...", output[0])
+			assert.Equal(t, "ZCN wallet created!!", output[1])
+			assert.Equal(t, "Creating related read pool for storage smart-contract...", output[2])
+			assert.Equal(t, "Read pool created successfully", output[3])
+			assert.Equal(t, "Error: Mnemonic not provided", output[4])
+		})
 	})
 }
 
--- tests/cli_tests/zboxcli_register_wallet_test.go
+++ tests/cli_tests/zboxcli_register_wallet_test.go
@@ -2,8 +2,8 @@ package cli_tests
 
 import (
 	"encoding/json"
-	"github.com/0chain/system_test/internal/cli/cli_model"
-	"github.com/0chain/system_test/internal/cli/cli_utils"
+	"github.com/0chain/system_test/internal/cli/model"
+	"github.com/0chain/system_test/internal/cli/util"
 	"github.com/stretchr/testify/assert"
 	"regexp"
 	"strings"
@@ -11,92 +11,94 @@ import (
 )
 
 func TestWalletRegisterAndBalanceOperations(t *testing.T) {
-	t.Run("Register wallet outputs expected", func(t *testing.T) {
-		t.Parallel()
-		output, err := registerWallet(t, configPath)
-		if err != nil {
-			t.Errorf("An error occured registering a wallet due to error: %v", err)
-		}
+	t.Run("parallel", func(t *testing.T) {
+		t.Run("Register wallet outputs expected", func(t *testing.T) {
+			t.Parallel()
+			output, err := registerWallet(t, configPath)
+			if err != nil {
+				t.Errorf("An error occured registering a wallet due to error: %v", err)
+			}
 
-		assert.Equal(t, 4, len(output))
-		assert.Equal(t, "ZCN wallet created", output[0])
-		assert.Equal(t, "Creating related read pool for storage smart-contract...", output[1])
-		assert.Equal(t, "Read pool created successfully", output[2])
-		assert.Equal(t, "Wallet registered", output[3])
-	})
+			assert.Equal(t, 4, len(output))
+			assert.Equal(t, "ZCN wallet created", output[0])
+			assert.Equal(t, "Creating related read pool for storage smart-contract...", output[1])
+			assert.Equal(t, "Read pool created successfully", output[2])
+			assert.Equal(t, "Wallet registered", output[3])
+		})
 
-	t.Run("Get wallet outputs expected", func(t *testing.T) {
-		t.Parallel()
-		_, err := registerWallet(t, configPath)
-		if err != nil {
-			t.Errorf("An error occured registering a wallet due to error: %v", err)
-		}
+		t.Run("Get wallet outputs expected", func(t *testing.T) {
+			t.Parallel()
+			_, err := registerWallet(t, configPath)
+			if err != nil {
+				t.Errorf("An error occured registering a wallet due to error: %v", err)
+			}
 
-		wallet, err := getWallet(t, configPath)
+			wallet, err := getWallet(t, configPath)
 
-		if err != nil {
-			t.Errorf("Error occured when retreiving wallet due to error: %v", err)
-		}
+			if err != nil {
+				t.Errorf("Error occured when retreiving wallet due to error: %v", err)
+			}
 
-		assert.NotNil(t, wallet.ClientId)
-		assert.NotNil(t, wallet.ClientPublicKey)
-		assert.NotNil(t, wallet.EncryptionPublicKey)
-	})
+			assert.NotNil(t, wallet.ClientId)
+			assert.NotNil(t, wallet.ClientPublicKey)
+			assert.NotNil(t, wallet.EncryptionPublicKey)
+		})
 
-	t.Run("Balance call fails due to zero ZCN in wallet", func(t *testing.T) {
-		t.Parallel()
-		_, err := registerWallet(t, configPath)
-		if err != nil {
-			t.Errorf("An error occured registering a wallet due to error: %v", err)
-		}
+		t.Run("Balance call fails due to zero ZCN in wallet", func(t *testing.T) {
+			t.Parallel()
+			_, err := registerWallet(t, configPath)
+			if err != nil {
+				t.Errorf("An error occured registering a wallet due to error: %v", err)
+			}
 
-		output, err := getBalance(t, configPath)
-		if err == nil {
-			t.Errorf("Expected initial getBalance operation to fail but was successful with output %v", strings.Join(output, "\n"))
-		}
+			output, err := getBalance(t, configPath)
+			if err == nil {
+				t.Errorf("Expected initial getBalance operation to fail but was successful with output %v", strings.Join(output, "\n"))
+			}
 
-		assert.Equal(t, 1, len(output))
-		assert.Equal(t, "Failed to get balance:", output[0])
-	})
+			assert.Equal(t, 1, len(output))
+			assert.Equal(t, "Failed to get balance:", output[0])
+		})
 
-	t.Run("Balance of 1 is returned after faucet execution", func(t *testing.T) {
-		t.Parallel()
-		_, err := registerWallet(t, configPath)
-		if err != nil {
-			t.Errorf("An error occured registering a wallet due to error: %v", err)
-		}
+		t.Run("Balance of 1 is returned after faucet execution", func(t *testing.T) {
+			t.Parallel()
+			_, err := registerWallet(t, configPath)
+			if err != nil {
+				t.Errorf("An error occured registering a wallet due to error: %v", err)
+			}
 
-		output, err := executeFaucet(t, configPath)
+			output, err := executeFaucet(t, configPath)
 
-		if err != nil {
-			t.Errorf("Faucet execution failed due to error: %v", err)
-		}
+			if err != nil {
+				t.Errorf("Faucet execution failed due to error: %v", err)
+			}
 
-		assert.Equal(t, 1, len(output))
-		matcher := regexp.MustCompile("Execute faucet smart contract success with txn : {2}([a-f0-9]{64})$")
+			assert.Equal(t, 1, len(output))
+			matcher := regexp.MustCompile("Execute faucet smart contract success with txn : {2}([a-f0-9]{64})$")
 
-		assert.Regexp(t, matcher, output[0], "Faucet execution output did not match expected")
+			assert.Regexp(t, matcher, output[0], "Faucet execution output did not match expected")
 
-		txnId := matcher.FindAllStringSubmatch(output[0], 1)[0][1]
-		output, err = verifyTransaction(t, configPath, txnId)
+			txnId := matcher.FindAllStringSubmatch(output[0], 1)[0][1]
+			output, err = verifyTransaction(t, configPath, txnId)
 
-		if err != nil {
-			t.Errorf("Faucet verification failed due to error: %v", err)
-		}
+			if err != nil {
+				t.Errorf("Faucet verification failed due to error: %v", err)
+			}
 
-		assert.Equal(t, 1, len(output))
-		assert.Equal(t, "Transaction verification success", output[0])
+			assert.Equal(t, 1, len(output))
+			assert.Equal(t, "Transaction verification success", output[0])
 
-		t.Log("Faucet executed successful with txn id [" + txnId + "]")
+			t.Log("Faucet executed successful with txn id [" + txnId + "]")
 
-		output, err = getBalance(t, configPath)
+			output, err = getBalance(t, configPath)
 
-		if err != nil {
-			t.Error(err)
-		}
+			if err != nil {
+				t.Error(err)
+			}
 
-		assert.Equal(t, 1, len(output))
-		assert.Regexp(t, regexp.MustCompile("Balance: 1.000 ZCN \\([0-9.]+ USD\\)$"), output[0])
+			assert.Equal(t, 1, len(output))
+			assert.Regexp(t, regexp.MustCompile("Balance: 1.000 ZCN \\([0-9.]+ USD\\)$"), output[0])
+		})
 	})
 }
 
