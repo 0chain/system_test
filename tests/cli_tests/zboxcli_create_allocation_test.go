5816ffd3ef851cb662d0996924cffcf8cca7d282
Mohsen Mirghafouri <mohsen.no1@gmail.com>
2021-09-20 22:50:33 +0300

    more scenarios added

--- tests/cli_tests/zboxcli_create_allocation_test.go
+++ tests/cli_tests/zboxcli_create_allocation_test.go
@@ -2,83 +2,128 @@ package cli_tests
 
 import (
 	"fmt"
-	"github.com/0chain/system_test/internal/cli/util"
-	"github.com/stretchr/testify/assert"
 	"regexp"
+	"strings"
 	"testing"
+
+	cli_utils "github.com/0chain/system_test/internal/cli/util"
+	"github.com/stretchr/testify/require"
 )
 
 func TestCreateAllocation(t *testing.T) {
-	// networkConfig, err := cli_utils.GetNetworkConfiguration(configPath)
-	// if err != nil {
-	// 	t.Fatal(err)
-	// }
+	const parallel bool = true
 
-	// var miners []interface{} = (networkConfig["miners"]).([]interface{})
-
-	// if len(miners) < 1 {
-	// 	t.Fatal(err)
-	// }
+	type AssertFunction func(t *testing.T, output []string, err error)
 
 	type Test struct {
-		lock    float64
-		name    string
-		options map[string]interface{}
+		name           string
+		options        map[string]interface{}
+		assertFunction AssertFunction
+	}
+
+	var allocationCreatedAssertFunction = func(t *testing.T, output []string, err error) {
+		require.Nil(t, err)
+		require.True(t, len(output) > 0, "expected output length be at least 1", strings.Join(output, "\n"))
+		require.Regexp(t, regexp.MustCompile("^Allocation created: [0-9a-fA-F]{64}$"), output[0])
 	}
 
 	var successScenarioTests = []Test{
-		{name: "Create allocation with smallest expiry (5m)", options: map[string]interface{}{"expire": "5m", "size": "256000", "lock": "0.5"}},
-		{name: "Create allocation with smallest possible size (1024)", options: map[string]interface{}{"expire": "1h", "size": "1024", "lock": "0.5"}},
-		{name: "Create allocation with parity 1", options: map[string]interface{}{"expire": "1h", "size": "1024", "parity": "1", "lock": "0.5"}},
-		{name: "Create allocation with data shard 20", options: map[string]interface{}{"expire": "1h", "size": "128000", "data": "20", "lock": "0.5"}},
-		{name: "Create allocation with read price range 0-0.03", options: map[string]interface{}{"expire": "1h", "size": "128000", "read_price": "0-0.03", "lock": "0.5"}},
-		{name: "Create allocation with write price range 0-0.03", options: map[string]interface{}{"expire": "1h", "size": "128000", "write_price": "0-0.03", "lock": "0.5"}},
+		{
+			name:           "Create allocation with smallest expiry (5m)",
+			options:        map[string]interface{}{"expire": "5m", "size": "256000", "lock": "0.5"},
+			assertFunction: allocationCreatedAssertFunction,
+		},
+		{
+			name:           "Create allocation with smallest possible size (1024)",
+			options:        map[string]interface{}{"expire": "1h", "size": "1024", "lock": "0.5"},
+			assertFunction: allocationCreatedAssertFunction,
+		},
+		{
+			name:           "Create allocation with parity 1",
+			options:        map[string]interface{}{"expire": "1h", "size": "1024", "parity": "1", "lock": "0.5"},
+			assertFunction: allocationCreatedAssertFunction,
+		},
+		{
+			name:           "Create allocation with data shard 20",
+			options:        map[string]interface{}{"expire": "1h", "size": "128000", "data": "20", "lock": "0.5"},
+			assertFunction: allocationCreatedAssertFunction,
+		},
+		{
+			name:           "Create allocation with read price range 0-0.03",
+			options:        map[string]interface{}{"expire": "1h", "size": "128000", "read_price": "0-0.03", "lock": "0.5"},
+			assertFunction: allocationCreatedAssertFunction,
+		},
+		{
+			name:           "Create allocation with write price range 0-0.03",
+			options:        map[string]interface{}{"expire": "1h", "size": "128000", "write_price": "0-0.03", "lock": "0.5"},
+			assertFunction: allocationCreatedAssertFunction,
+		},
 	}
 
-	for _, tt := range successScenarioTests {
-		t.Run(tt.name, func(t *testing.T) {
+	var failureScenarioTests = []Test{
+		{
+			name:    "Create allocation with invalid expiry",
+			options: map[string]interface{}{"expire": "-1", "lock": "0.5"},
+			assertFunction: func(t *testing.T, output []string, err error) {
+				require.NotNil(t, err)
+				require.True(t, len(output) > 0, "expected output length be at least 1", strings.Join(output, "\n"))
+				require.Equal(t, "invalid argument \"-1\" for \"--expire\" flag: time: missing unit in duration -1", output[len(output)-1])
+			},
+		},
+		{
+			name:    "Create allocation with too large parity",
+			options: map[string]interface{}{"parity": "7", "lock": "0.5", "size": 1024, "expire": "1h"},
+			assertFunction: func(t *testing.T, output []string, err error) {
+				require.NotNil(t, err)
+				require.True(t, len(output) > 0, "expected output length be at least 1", strings.Join(output, "\n"))
+				require.Equal(t, "Error creating allocation: [txn] too less sharders to confirm it: min_confirmation is 50%, but got 0/2 sharders", output[0])
+			},
+		},
+		{
+			name:    "Create allocation with read price range 0-0.01",
+			options: map[string]interface{}{"read_price": "0-0.01", "lock": "0.5", "size": 1024, "expire": "1h"},
+			assertFunction: func(t *testing.T, output []string, err error) {
+				require.NotNil(t, err)
+				require.True(t, len(output) > 0, "expected output length be at least 1", strings.Join(output, "\n"))
+				require.Equal(t, "Error creating allocation: [txn] too less sharders to confirm it: min_confirmation is 50%, but got 0/2 sharders", output[0])
+			},
+		},
+	}
+
+	t.Run("Parallel", func(t *testing.T) {
+		if parallel {
 			t.Parallel()
+		}
 
-			_, err := setupWallet(t, configPath)
-			assert.Nil(t, err)
+		for _, tt := range successScenarioTests {
+			t.Run(tt.name+" Should Work", func(t *testing.T) {
+				if parallel {
+					t.Parallel()
+				}
 
-			output, err := createNewAllocation(t, configPath, createParams(tt.options))
-			assert.Nil(t, err)
+				_, err := setupWallet(t, configPath)
+				require.Nil(t, err)
 
-			assert.Regexp(t, regexp.MustCompile("^Allocation created: [0-9a-fA-F]{64}$"), output[0])
-		})
-	}
+				output, err := createNewAllocation(t, configPath, createParams(tt.options))
+				tt.assertFunction(t, output, err)
+			})
+		}
 
-	t.Run("Create allocation with invalid expiry", func(t *testing.T) {
-		t.Parallel()
-		_, err := setupWallet(t, configPath)
-		assert.Nil(t, err)
+		for _, tt := range failureScenarioTests {
+			t.Run(tt.name+" Should Fail", func(t *testing.T) {
+				if parallel {
+					t.Parallel()
+				}
 
-		var lock = 0.5
-		options := map[string]interface{}{"expire": "-1", "size": "1024", "lock": lock}
-		output, err := createNewAllocation(t, configPath, createParams(options))
-		assert.NotNil(t, err)
+				_, err := setupWallet(t, configPath)
+				require.Nil(t, err)
 
-		assert.Equal(t, "invalid argument \"-1\" for \"--expire\" flag: time: missing unit in duration \"-1\"", output[len(output)-1])
+				output, err := createNewAllocation(t, configPath, createParams(tt.options))
+				tt.assertFunction(t, output, err)
+			})
+		}
+
 	})
-
-	// t.Run("Create allocation with invalid expiry", func(t *testing.T) {
-	// 	// t.Parallel()
-	// 	_, err := setupWallet(t, configPath)
-	// 	assert.Nil(t, err)
-
-	// 	var lock float64 = 0.5
-	// 	options := map[string]string{"expire": "-1", "size": "1024"}
-	// 	output, err := cli_utils.NewAllocation(configPath, lock, options)
-	// 	assert.NotNil(t, err)
-
-	// 	assert.Equal(t, "invalid argument \"-1\" for \"--expire\" flag: time: missing unit in duration -1", output[len(output)-1])
-	// })
-
-	// var failureScenarioTests = []Test {
-	// 	{lock: 0.5, name: "Create allocation with invalid parity bigger than", options: map[string]string{"expire": "1h", "size": "1024", "parity": strconv.Itoa(len(miners) + 1)}},
-	// 	{lock: 0.5, name: "Create allocation with invalid parity bigger than", options: map[string]string{"expire": "1h", "size": "1024", "parity": strconv.Itoa(len(miners) + 1)}},
-	// }
 
 }
 
