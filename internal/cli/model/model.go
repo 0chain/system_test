39b2f21777374bdbd5baad7cb4fe19f0ac564b53
Tyrone <avanaur@gmail.com>
2021-10-05 02:46:08 +1100

    Tests for Lock and Unlock Tokens for Interest  (#59)
    * feat: lock and unlock interest
    
    * feat: lock and unlock interest (complete)
    
    * feat: lock and unlock interest (with success scenarios)
    
    * feat: lock and unlock interest (all other tests)
    
    * feat: added extra test
    
    * feat: add token account on success tests
    
    * fix: from code reviews
    
    * fix: incorrect
    
    Co-authored-by: Kush Trivedi <44091822+kushthedude@users.noreply.github.com>
    Co-authored-by: stewartie4 <ryanstewart456@gmail.com>

--- internal/cli/model/model.go
+++ internal/cli/model/model.go
@@ -149,3 +149,18 @@ type CommitResponse struct {
 		Attributes      Attributes      `json:"Attributes"`
 	} `json:"MetaData"`
 }
+
+type LockedInterestPoolStats struct {
+	Stats []LockedInterestPoolStat `json:"stats"`
+}
+
+type LockedInterestPoolStat struct {
+	ID           string        `json:"pool_id"`
+	StartTime    int64         `json:"start_time"`
+	Duration     time.Duration `json:"duration"`
+	TimeLeft     time.Duration `json:"time_left"`
+	Locked       bool          `json:"locked"`
+	APR          float64       `json:"apr"`
+	TokensEarned int64         `json:"tokens_earned"`
+	Balance      int64         `json:"balance"`
+}
