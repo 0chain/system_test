ea5582913b05c66d9dd200f24cffba9de7b10e71
Kishan-Dhakan <kishandhakan17@gmail.com>
2021-10-12 01:04:48 +0530

    cannot lock more write tokens than balance

--- tests/cli_tests/zboxcli_wp_lock_unlock_test.go
+++ tests/cli_tests/zboxcli_wp_lock_unlock_test.go
@@ -123,6 +123,50 @@ func TestWritePoolLockUnlock(t *testing.T) {
 		require.Nil(t, err, "Error fetching balance", strings.Join(output, "\n"))
 		require.Regexp(t, regexp.MustCompile(`Balance: 1.500 ZCN \(\d*\.?\d+ USD\)$`), output[0])
 	})
+
+	t.Run("Should not be able to lock more tokens than wallet balance", func(t *testing.T) {
+		t.Parallel()
+
+		output, err := registerWallet(t, configPath)
+		require.Nil(t, err, "registering wallet failed", strings.Join(output, "\n"))
+
+		output, err = executeFaucetWithTokens(t, configPath, 1.0)
+		require.Nil(t, err, "faucet execution failed", strings.Join(output, "\n"))
+
+		// Lock 0.5 token for allocation
+		allocParams := createParams(map[string]interface{}{
+			"expire": "5m",
+			"size":   "1024",
+			"lock":   "0.5",
+		})
+		output, err = createNewAllocation(t, configPath, allocParams)
+		require.Nil(t, err, "Failed to create new allocation", strings.Join(output, "\n"))
+
+		require.Len(t, output, 1)
+		require.Regexp(t, regexp.MustCompile("Allocation created: ([a-f0-9]{64})"), output[0], "Allocation creation output did not match expected")
+		allocationID := strings.Fields(output[0])[2]
+
+		// Wallet balance before lock should be 0.5 ZCN
+		output, err = getBalance(t, configPath)
+		require.Nil(t, err, "Error fetching balance", strings.Join(output, "\n"))
+		require.Regexp(t, regexp.MustCompile(`Balance: 500.000 mZCN \(\d*\.?\d+ USD\)$`), output[0])
+
+		// Lock 1 token in write pool distributed amongst all blobbers
+		params := createParams(map[string]interface{}{
+			"allocation": allocationID,
+			"tokens":     1,
+			"duration":   "5m",
+		})
+		output, err = writePoolLock(t, configPath, params)
+		require.NotNil(t, err, "Locked more tokens than in wallet", strings.Join(output, "\n"))
+		require.True(t, len(output) > 0, "expected output length be at least 1")
+		require.Equal(t, "Failed to lock tokens in write pool: [txn] too less sharders to confirm it: min_confirmation is 50%, but got 0/2 sharders", output[0], strings.Join(output, "\n"))
+
+		// Wallet balance should remain same
+		output, err = getBalance(t, configPath)
+		require.Nil(t, err, "Error fetching balance", strings.Join(output, "\n"))
+		require.Regexp(t, regexp.MustCompile(`Balance: 500.000 mZCN \(\d*\.?\d+ USD\)$`), output[0])
+	})
 }
 
 func writePoolLock(t *testing.T, cliConfigFilename, params string) ([]string, error) {
