a36c079121be3e536e80f9eb2226eae54b9267bc
Kishan-Dhakan <kishandhakan17@gmail.com>
2021-10-11 23:03:33 +0530

    unlock before expire fails

--- tests/cli_tests/zboxcli_rp_lock_unlock_test.go
+++ tests/cli_tests/zboxcli_rp_lock_unlock_test.go
@@ -317,6 +317,58 @@ func TestReadPoolLockUnlock(t *testing.T) {
 		require.Nil(t, err, "Error fetching balance", strings.Join(output, "\n"))
 		require.Regexp(t, regexp.MustCompile(`Balance: 500.000 mZCN \(\d*\.?\d+ USD\)$`), output[0])
 	})
+
+	t.Run("Should not be able to unlock unexpired tokens", func(t *testing.T) {
+		t.Parallel()
+
+		output, err := registerWallet(t, configPath)
+		require.Nil(t, err, "registering wallet failed", strings.Join(output, "\n"))
+
+		output, err = executeFaucetWithTokens(t, configPath, 2.0)
+		require.Nil(t, err, "faucet execution failed", strings.Join(output, "\n"))
+
+		// Lock 0.5 token for allocation
+		allocParams := createParams(map[string]interface{}{
+			"expire": "5m",
+			"size":   "1024",
+			"lock":   "0.5",
+		})
+		output, err = createNewAllocation(t, configPath, allocParams)
+		require.Nil(t, err, "Failed to create new allocation", strings.Join(output, "\n"))
+
+		require.Len(t, output, 1)
+		require.Regexp(t, regexp.MustCompile("Allocation created: ([a-f0-9]{64})"), output[0], "Allocation creation output did not match expected")
+		allocationID := strings.Fields(output[0])[2]
+
+		// Lock 1 token in read pool distributed amongst all blobbers
+		params := createParams(map[string]interface{}{
+			"allocation": allocationID,
+			"tokens":     1,
+			"duration":   "1m",
+		})
+		output, err = readPoolLock(t, configPath, params)
+		require.Nil(t, err, "Tokens could not be locked", strings.Join(output, "\n"))
+
+		require.Len(t, output, 1)
+		require.Equal(t, "locked", output[0])
+
+		output, err = readPoolInfo(t, configPath, allocationID)
+		require.Nil(t, err, "Error fetching read pool", strings.Join(output, "\n"))
+
+		readPool := []climodel.ReadPoolInfo{}
+		err = json.Unmarshal([]byte(output[0]), &readPool)
+		require.Nil(t, err, "Error unmarshalling read pool", strings.Join(output, "\n"))
+
+		// Unlock without waiting till expiration should result in error
+		params = createParams(map[string]interface{}{
+			"pool_id": readPool[0].Id,
+		})
+		output, err = readPoolUnlock(t, configPath, params)
+		require.NotNil(t, err, "Read pool tokens unlocked before expired", strings.Join(output, "\n"))
+
+		require.True(t, len(output) > 0, "expected output length be at least 1")
+		require.Equal(t, "Failed to unlock tokens in read pool: [txn] too less sharders to confirm it: min_confirmation is 50%, but got 0/2 sharders", output[0])
+	})
 }
 
 func readPoolUnlock(t *testing.T, cliConfigFilename, params string) ([]string, error) {
