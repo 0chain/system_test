c205974b3bd07457934a9f1353106911e424b624
Kishan-Dhakan <kishandhakan17@gmail.com>
2021-10-11 19:11:33 +0530

    error on locking more tokens than in wallet

--- tests/cli_tests/zboxcli_rp_lock_unlock_test.go
+++ tests/cli_tests/zboxcli_rp_lock_unlock_test.go
@@ -28,6 +28,7 @@ func TestReadPoolLockUnlock(t *testing.T) {
 		require.Nil(t, err, "Error fetching balance", strings.Join(output, "\n"))
 		require.Regexp(t, regexp.MustCompile(`Balance: 2.000 ZCN \(\d*\.?\d+ USD\)$`), output[0])
 
+		// Lock 0.5 token for allocation
 		allocParams := createParams(map[string]interface{}{
 			"expire": "5m",
 			"size":   "1024",
@@ -40,6 +41,7 @@ func TestReadPoolLockUnlock(t *testing.T) {
 		require.Regexp(t, regexp.MustCompile("Allocation created: ([a-f0-9]{64})"), output[0], "Allocation creation output did not match expected")
 		allocationID := strings.Fields(output[0])[2]
 
+		// Lock 1 token in read pool distributed amongst all blobbers
 		params := createParams(map[string]interface{}{
 			"allocation": allocationID,
 			"tokens":     1,
@@ -70,10 +72,54 @@ func TestReadPoolLockUnlock(t *testing.T) {
 		require.Less(t, 0, len(readPool[0].Blobber))
 		require.Equal(t, true, readPool[0].Locked)
 
-		// Blobber read pool balance should be 0.25 ZCN each
+		// Blobber read pool balance should be 1/num(blobbers) ZCN in each
 		for i := 0; i < len(readPool[0].Blobber); i++ {
 			require.Regexp(t, regexp.MustCompile("([a-f0-9]{64})"), readPool[0].Blobber[i].BlobberID)
-			require.InEpsilon(t, 0.25, intToZCN(readPool[0].Blobber[i].Balance), epsilon)
+			require.InEpsilon(t, 1.0/float64(len(readPool[0].Blobber)), intToZCN(readPool[0].Blobber[i].Balance), epsilon)
 		}
+	})
+
+	t.Run("Should not be able to lock more tokens than wallet balance", func(t *testing.T) {
+		t.Parallel()
+
+		output, err := registerWallet(t, configPath)
+		require.Nil(t, err, "registering wallet failed", strings.Join(output, "\n"))
+
+		output, err = executeFaucetWithTokens(t, configPath, 1.0)
+		require.Nil(t, err, "faucet execution failed", strings.Join(output, "\n"))
+
+		// Lock 0.5 token for allocation
+		allocParams := createParams(map[string]interface{}{
+			"expire": "5m",
+			"size":   "1024",
+			"lock":   "0.5",
+		})
+		output, err = createNewAllocation(t, configPath, allocParams)
+		require.Nil(t, err, "Failed to create new allocation", strings.Join(output, "\n"))
+
+		require.Len(t, output, 1)
+		require.Regexp(t, regexp.MustCompile("Allocation created: ([a-f0-9]{64})"), output[0], "Allocation creation output did not match expected")
+		allocationID := strings.Fields(output[0])[2]
+
+		// Wallet balance before lock should be 0.5 ZCN
+		output, err = getBalance(t, configPath)
+		require.Nil(t, err, "Error fetching balance", strings.Join(output, "\n"))
+		require.Regexp(t, regexp.MustCompile(`Balance: 500.000 mZCN \(\d*\.?\d+ USD\)$`), output[0])
+
+		// Lock 1 token in read pool distributed amongst all blobbers
+		params := createParams(map[string]interface{}{
+			"allocation": allocationID,
+			"tokens":     1,
+			"duration":   "5m",
+		})
+		output, err = readPoolLock(t, configPath, params)
+		require.NotNil(t, err, "Locked more tokens than in wallet", strings.Join(output, "\n"))
+		require.True(t, len(output) > 0, "expected output length be at least 1")
+		require.Equal(t, "Failed to lock tokens in read pool: [txn] too less sharders to confirm it: min_confirmation is 50%, but got 0/2 sharders", output[0], strings.Join(output, "\n"))
+
+		// Wallet balance should remain same
+		output, err = getBalance(t, configPath)
+		require.Nil(t, err, "Error fetching balance", strings.Join(output, "\n"))
+		require.Regexp(t, regexp.MustCompile(`Balance: 500.000 mZCN \(\d*\.?\d+ USD\)$`), output[0])
 	})
 }
