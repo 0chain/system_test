dd0ab483198142744294dc09f7b2a03b18dba95f
Kishan-Dhakan <kishandhakan17@gmail.com>
2021-10-19 16:38:03 +0530

    added staking, unstaking, wallet and pool checks

--- tests/cli_tests/zboxcli_stake_unstake_token_test.go
+++ tests/cli_tests/zboxcli_stake_unstake_token_test.go
@@ -1,15 +1,137 @@
 package cli_tests
 
 import (
+	"encoding/json"
 	"fmt"
+	"regexp"
 	"strings"
 	"testing"
+	"time"
 
+	climodel "github.com/0chain/system_test/internal/cli/model"
 	cliutils "github.com/0chain/system_test/internal/cli/util"
 	"github.com/stretchr/testify/require"
 )
 
+func TestStakeUnstakeTokens(t *testing.T) {
+	t.Parallel()
+
+	t.Run("Staked tokens should move from wallet to Blobber's stake pool info, unstaking should move them back to wallet", func(t *testing.T) {
+		t.Parallel()
+
+		output, err := registerWallet(t, configPath)
+		require.Nil(t, err, "registering wallet failed", strings.Join(output, "\n"))
+
+		wallet, err := getWallet(t, configPath)
+		require.Nil(t, err, "Error getting wallet", strings.Join(output, "\n"))
+
+		output, err = executeFaucetWithTokens(t, configPath, 1.0)
+		require.Nil(t, err, "faucet execution failed", strings.Join(output, "\n"))
+
+		blobbers := []climodel.BlobberInfo{}
+		output, err = listBlobbers(t, configPath, "--json")
+		require.Nil(t, err, "Error listing blobbers", strings.Join(output, "\n"))
+
+		err = json.Unmarshal([]byte(output[0]), &blobbers)
+		require.Nil(t, err, "Error unmarshalling blobber list", strings.Join(output, "\n"))
+		require.True(t, len(blobbers) > 0, "No blobbers found in blobber list")
+
+		// Pick a random blobber
+		blobber := blobbers[time.Now().Unix()%int64(len(blobbers))]
+
+		// Stake tokens against this blobber
+		output, err = stakeTokens(t, configPath, createParams(map[string]interface{}{
+			"blobber_id": blobber.Id,
+			"tokens":     0.5,
+		}))
+		require.Nil(t, err, "Error staking tokens", strings.Join(output, "\n"))
+		require.Len(t, output, 1)
+		require.Regexp(t, regexp.MustCompile("tokens locked, pool id: ([a-f0-9]{64})"), output[0])
+		stakePoolID := strings.Fields(output[0])[4]
+		require.Nil(t, err, "Error extracting pool Id from sp-lock output", strings.Join(output, "\n"))
+
+		// Wallet balance should decrease by locked amount
+		output, err = getBalance(t, configPath)
+		require.Nil(t, err, "Error fetching balance", strings.Join(output, "\n"))
+		require.Regexp(t, regexp.MustCompile(`Balance: 500.000 mZCN \(\d*\.?\d+ USD\)$`), output[0])
+
+		// Use sp-info to check the staked tokens in blobber's stake pool
+		output, err = stakePoolInfo(t, configPath, createParams(map[string]interface{}{
+			"blobber_id": blobber.Id,
+			"json":       "",
+		}))
+		require.Nil(t, err, "Error fetching stake pool info", strings.Join(output, "\n"))
+
+		stakePool := climodel.StakePoolInfo{}
+		err = json.Unmarshal([]byte(output[0]), &stakePool)
+		require.Nil(t, err, "Error unmarshalling stake pool info", strings.Join(output, "\n"))
+
+		delegates := stakePool.Delegate
+
+		// Pool Id returned by sp-lock should be found in sp-info with correct balance
+		found := false
+		for _, delegate := range delegates {
+			if delegate.ID == stakePoolID {
+				found = true
+				require.Equal(t, int64(5000000000), delegate.Balance)
+				require.Equal(t, wallet.ClientID, delegate.DelegateID)
+			}
+		}
+		require.True(t, found, "Pool id returned by sp-lock not found in blobber's sp-info", strings.Join(output, "\n"))
+
+		// Unstake the tokens
+		output, err = unstakeTokens(t, configPath, createParams(map[string]interface{}{
+			"blobber_id": blobber.Id,
+			"pool_id":    stakePoolID,
+		}))
+		require.Nil(t, err, "Error unstaking tokens from stake pool", strings.Join(output, "\n"))
+		require.Len(t, output, 1)
+		require.Equal(t, "tokens has unlocked, pool deleted", output[0])
+
+		// Wallet balance should increase by unlocked amount
+		output, err = getBalance(t, configPath)
+		require.Nil(t, err, "Error fetching balance", strings.Join(output, "\n"))
+		require.Regexp(t, regexp.MustCompile(`Balance: 1.000 ZCN \(\d*\.?\d+ USD\)$`), output[0])
+
+		// Pool Id must be deleted from stake pool now
+		output, err = stakePoolInfo(t, configPath, createParams(map[string]interface{}{
+			"blobber_id": blobber.Id,
+			"json":       "",
+		}))
+		require.Nil(t, err, "Error fetching stake pool info", strings.Join(output, "\n"))
+
+		err = json.Unmarshal([]byte(output[0]), &stakePool)
+		require.Nil(t, err, "Error unmarshalling stake pool info", strings.Join(output, "\n"))
+
+		delegates = stakePool.Delegate
+
+		// Pool Id returned by sp-lock should be found in sp-info with correct balance
+		found = false
+		for _, delegate := range delegates {
+			if delegate.ID == stakePoolID {
+				found = true
+			}
+		}
+		require.False(t, found, "Pool id found in blobber's sp-info even after unlocking tokens", strings.Join(output, "\n"))
+	})
+}
+
 func listBlobbers(t *testing.T, cliConfigFilename, params string) ([]string, error) {
 	t.Log("Requesting blobber list...")
-	return cliutils.RunCommand(fmt.Sprintf("./zbox ls-blobbers %s --json --silent --wallet %s_wallet.json --configDir ./config --config %s", params, escapedTestName(t), cliConfigFilename))
+	return cliutils.RunCommand(fmt.Sprintf("./zbox ls-blobbers %s --silent --wallet %s_wallet.json --configDir ./config --config %s", params, escapedTestName(t), cliConfigFilename))
+}
+
+func stakeTokens(t *testing.T, cliConfigFilename, params string) ([]string, error) {
+	t.Log("Staking tokens...")
+	return cliutils.RunCommand(fmt.Sprintf("./zbox sp-lock %s --silent --wallet %s_wallet.json --configDir ./config --config %s", params, escapedTestName(t), cliConfigFilename))
+}
+
+func stakePoolInfo(t *testing.T, cliConfigFilename, params string) ([]string, error) {
+	t.Log("Fetching stake pool info...")
+	return cliutils.RunCommand(fmt.Sprintf("./zbox sp-info %s --silent --wallet %s_wallet.json --configDir ./config --config %s", params, escapedTestName(t), cliConfigFilename))
+}
+
+func unstakeTokens(t *testing.T, cliConfigFilename, params string) ([]string, error) {
+	t.Log("Unlocking tokens from stake pool...")
+	return cliutils.RunCommand(fmt.Sprintf("./zbox sp-unlock %s --silent --wallet %s_wallet.json --configDir ./config --config %s", params, escapedTestName(t), cliConfigFilename))
 }
