f623e1a884fb7cfb2acdcad66c05599713e7958f
Tyrone Villaluna <avanaur@gmail.com>
2021-10-28 19:58:41 +1100

    fix: failing test due to change of tmp directorafy used (3rd attempt)

--- tests/cli_tests/zboxcli_download_test.go
+++ tests/cli_tests/zboxcli_download_test.go
@@ -758,7 +758,7 @@ func TestDownload(t *testing.T) {
 
 		expected := fmt.Sprintf(
 			"Download failed. Local file already exists '%s'",
-			filepath.Dir(os.TempDir())+"/"+filepath.Base(filename),
+			strings.TrimSuffix(os.TempDir(), "/")+"/"+filepath.Base(filename),
 		)
 		require.Equal(t, expected, output[0])
 	})
