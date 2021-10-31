1b16198c3444932702440f2aeda8567f9e53076d
Kishan-Dhakan <42718091+Kishan-Dhakan@users.noreply.github.com>
2021-11-01 05:24:07 +0530

    Common user functions (#89)
    * TestCommonUserFunctions
    
    * config
    
    * Lock token interest test
    
    * Lock must've been withdrown from user wallet
    
    * update allocation stack pool test
    
    * File Update Tests
    
    * FileUploadTest
    
    * Small Fixes
    
    * Change challenge pool balance type to float64
    
    * Blobber Payment For Write Markers
    
    * Syntax error
    
    * Send ZCN between wallets added
    
    * added createDir if does not exists
    
    * removed top t.Run('parallel')
    
    * corrected file update blobber should be paid case
    
    * removed unused functions
    
    * removed unused import
    
    * Send ZCN between wallets test
    
    * Refactoring and Modification
    
    * Small Fixes
    
    * remove duplicate struct
    
    * blobber payment on update
    
    * Refactored file update with different size case
    
    * removed incorrect update case
    
    * refactored update same size case
    
    * refactored send ZCN b/w wallets test
    
    * removed blobbers should pay for rename case
    
    * refactored rename case, removed blobber payment case
    
    * refactored move case, removed blobber payment case
    
    * renamed stack to stake
    
    * removed unused functions
    
    * refactored new allocation case to check all blobbers stake correctly
    
    * removed unused functions
    
    * refactored create alloc reduces wallet balance case
    
    * reordered tests
    
    * refactored update allocation cases
    
    * removed unused functions
    
    * increased wait times
    
    * corrected model.go and references
    
    * linting fixes
    
    * linting fix
    
    * fix failing tests
    
    * lint fix
    
    * Increase wating time
    
    * Add Debuging Info
    
    * Add more debug info
    
    * Debug send ZCN
    
    * change search criteria
    
    * Change Criteria
    
    * Criteria change
    
    * check by transaction type
    
    * revert back
    
    * More Logs
    
    * Change for better assertion
    
    * added stake clause
    
    * reduced file size for fewer requests
    
    * smaller sized file
    
    * chaging timeout
    
    * fix assertion
    
    * fix assertion
    
    * removed redundant int64 conversion
    
    * removed commented block workers
    
    * corrected comments mentioning 2 mb
    
    * added explicit data and parity
    
    * refactored getUploadCostInUnit and references
    
    * added assertion inside writePoolInfo
    
    * added user friendly messages
    
    * updated 'tmp' dir to os.TempDir()
    
    * moved single test case to send_and_balance.go
    
    * removed unused setupTransferWallets func
    
    * documented miner fees test case
    
    * lint fix
    
    * fee not found fixed
    
    * Merge branch 'master' of github.com:0chain/system_test into common-user-functions
    
    # Conflicts:
    #	tests/cli_tests/config/zbox_config.yaml
    
    * more lenient on command output
    
    * fix bug that expects wp info command to always be successful
    
    * fix json expected output sie to be 1
    
    Co-authored-by: Mohsen Mirghafouri <mohsen.no1@gmail.com>
    Co-authored-by: stewartie4 <ryanstewart456@gmail.com>

--- tests/cli_tests/zboxcli_upload_token_test.go
+++ tests/cli_tests/zboxcli_upload_token_test.go
@@ -75,8 +75,7 @@ func TestFileUploadTokenMovement(t *testing.T) {
 
 		allocationID := strings.Fields(output[0])[2]
 
-		output, err = writePoolInfo(t, configPath)
-		require.Nil(t, err, "Failed to fetch Write Pool info", strings.Join(output, "\n"))
+		output, _ = writePoolInfo(t, configPath)
 
 		writePool := []climodel.WritePoolInfo{}
 		err = json.Unmarshal([]byte(output[0]), &writePool)
@@ -99,14 +98,17 @@ func TestFileUploadTokenMovement(t *testing.T) {
 }
 
 func writePoolInfo(t *testing.T, cliConfigFilename string) ([]string, error) {
-	time.Sleep(5 * time.Second) // TODO replace with poller
+	time.Sleep(10 * time.Second) // TODO replace with poller
 	t.Logf("Getting write pool info...")
 	return cliutils.RunCommand("./zbox wp-info --json --silent --wallet " + escapedTestName(t) + "_wallet.json" + " --configDir ./config --config " + cliConfigFilename)
 }
 
 func getUploadCostInUnit(t *testing.T, cliConfigFilename, allocationID, localpath string) ([]string, error) {
 	t.Logf("Getting upload cost...")
-	return cliutils.RunCommand("./zbox get-upload-cost --allocation " + allocationID + " --localpath " + localpath + " --silent --wallet " + escapedTestName(t) + "_wallet.json" + " --configDir ./config --config " + cliConfigFilename)
+	output, err := cliutils.RunCommand("./zbox get-upload-cost --allocation " + allocationID + " --localpath " + localpath + " --silent --wallet " + escapedTestName(t) + "_wallet.json" + " --configDir ./config --config " + cliConfigFilename)
+	require.Len(t, output, 1)
+	require.Nil(t, err, "error getting upload cost in unit", strings.Join(output, "\n"))
+	return output, err
 }
 
 func challengePoolInfo(t *testing.T, cliConfigFilename, allocationID string) ([]string, error) {
