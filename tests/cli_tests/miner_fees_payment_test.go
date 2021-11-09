299a1a4fff75611f168fc3a164c2d28f541f907d
Kishan-Dhakan <kishandhakan17@gmail.com>
2021-11-10 01:02:51 +0530

    added wp-lock and replaced wait with polling

--- tests/cli_tests/miner_fees_payment_test.go
+++ tests/cli_tests/miner_fees_payment_test.go
@@ -6,7 +6,6 @@ import (
 	"regexp"
 	"strings"
 	"testing"
-	"time"
 
 	apimodel "github.com/0chain/system_test/internal/api/model"
 	climodel "github.com/0chain/system_test/internal/cli/model"
@@ -16,7 +15,7 @@ import (
 func TestMinerFeesPayment(t *testing.T) {
 	t.Parallel()
 
-	t.Run("rp-Lock and rp-unlock command with fees flag - fees must be paid to the miners", func(t *testing.T) {
+	t.Run("rp-Lock and rp-unlock command with fee flag - fees must be paid to the miners", func(t *testing.T) {
 		t.Parallel()
 
 		output, err := registerWallet(t, configPath)
@@ -70,8 +69,10 @@ func TestMinerFeesPayment(t *testing.T) {
 		err = json.Unmarshal([]byte(output[0]), &readPool)
 		require.Nil(t, err, "Error unmarshalling read pool", strings.Join(output, "\n"))
 
-		wait(t, time.Minute)
 		endBalance := getNodeBalanceFromASharder(t, miner.ID)
+		for endBalance.Round <= startBalance.Round {
+			endBalance = getNodeBalanceFromASharder(t, miner.ID)
+		}
 
 		require.Greater(t, endBalance.Round, startBalance.Round, "Round of balance is unexpectedly unchanged since last balance check: last %d, retrieved %d", startBalance.Round, endBalance.Round)
 		require.Greater(t, endBalance.Balance, startBalance.Balance, "Balance is unexpectedly unchanged since last balance check: last %d, retrieved %d", startBalance.Balance, endBalance.Balance)
@@ -148,6 +149,9 @@ func TestMinerFeesPayment(t *testing.T) {
 		require.Equal(t, "unlocked", output[0])
 
 		endBalance = getNodeBalanceFromASharder(t, miner.ID)
+		for endBalance.Round <= startBalance.Round {
+			endBalance = getNodeBalanceFromASharder(t, miner.ID)
+		}
 
 		require.Greater(t, endBalance.Round, startBalance.Round, "Round of balance is unexpectedly unchanged since last balance check: last %d, retrieved %d", startBalance.Round, endBalance.Round)
 		require.Greater(t, endBalance.Balance, startBalance.Balance, "Balance is unexpectedly unchanged since last balance check: last %d, retrieved %d", startBalance.Balance, endBalance.Balance)
@@ -187,6 +191,129 @@ func TestMinerFeesPayment(t *testing.T) {
 				} else {
 					// Search for the fee payment to generator miner in "payFee" transaction output
 					if strings.ContainsAny(txn.TransactionData, "read_pool_unlock") && strings.ContainsAny(txn.TransactionData, fmt.Sprintf("%d", transactionRound)) {
+						var transfers []apimodel.Transfer
+						err = json.Unmarshal([]byte(fmt.Sprintf("[%s]", strings.Replace(txn.TransactionOutput, "}{", "},{", -1))), &transfers)
+						require.Nil(t, err, "Cannot unmarshal the transfers from transaction output")
+
+						for _, transfer := range transfers {
+							// Transfer needs to be from Miner Smart contract to Generator miner
+							if transfer.From != MINER_SC_ADDRESS || transfer.To != block_miner_id {
+								continue
+							} else {
+								t.Logf("--- FOUND IN ROUND: %d ---", block.Block.Round)
+								require.NotNil(t, transfer, "The transfer of fee to miner could not be found")
+								// Transfer fee must be equal to miner fee
+								require.InEpsilon(t, expectedMinerFee, transfer.Amount, 0.00000001, "Transfer fee must be equal to miner fee")
+								return // test passed
+							}
+						}
+					}
+				}
+			}
+		}
+	})
+
+	t.Run("wp-lock and wp-unlock command with fee flag - fee must be paid to the miners", func(t *testing.T) {
+		t.Parallel()
+
+		output, err := registerWallet(t, configPath)
+		require.Nil(t, err, "registering wallet failed", strings.Join(output, "\n"))
+
+		wallet, err := getWallet(t, configPath)
+		require.Nil(t, err, "Error occurred when retrieving target wallet")
+
+		output, err = executeFaucetWithTokens(t, configPath, 2)
+		require.Nil(t, err, "faucet execution failed", strings.Join(output, "\n"))
+
+		mconfig := getMinerSCConfiguration(t)
+		minerShare := mconfig["share_ratio"]
+
+		miners := getMinersList(t)
+		minerNode := miners.Nodes[0].SimpleNode
+		miner := getMinersDetail(t, minerNode.ID).SimpleNode
+
+		// Create an allocation to use rp-lock on
+		allocParams := createParams(map[string]interface{}{
+			"expire": "5m",
+			"size":   "1024",
+			"lock":   "0.5",
+		})
+		output, err = createNewAllocation(t, configPath, allocParams)
+		require.Nil(t, err, "Failed to create new allocation", strings.Join(output, "\n"))
+
+		require.Len(t, output, 1)
+		require.Regexp(t, regexp.MustCompile("Allocation created: ([a-f0-9]{64})"), output[0], "Allocation creation output did not match expected")
+		allocationID := strings.Fields(output[0])[2]
+		fee := 0.1
+
+		startBalance := getNodeBalanceFromASharder(t, miner.ID)
+
+		// Lock 1 token in Write pool amongst all blobbers
+		params := createParams(map[string]interface{}{
+			"allocation": allocationID,
+			"duration":   "2m",
+			"tokens":     1,
+			"fee":        fee,
+		})
+		output, err = writePoolLock(t, configPath, params)
+		require.Nil(t, err, "Failed to lock write tokens", strings.Join(output, "\n"))
+		require.Equal(t, "locked", output[0])
+
+		output, err = writePoolInfo(t, configPath)
+		require.Nil(t, err, "error fetching write pool info", strings.Join(output, "\n"))
+
+		writePool := []climodel.WritePoolInfo{}
+		err = json.Unmarshal([]byte(output[0]), &writePool)
+		require.Nil(t, err, "error unmarshalling write pool", strings.Join(output, "\n"))
+
+		endBalance := getNodeBalanceFromASharder(t, miner.ID)
+		for endBalance.Round <= startBalance.Round {
+			endBalance = getNodeBalanceFromASharder(t, miner.ID)
+		}
+
+		require.Greater(t, endBalance.Round, startBalance.Round, "Round of balance is unexpectedly unchanged since last balance check: last %d, retrieved %d", startBalance.Round, endBalance.Round)
+		require.Greater(t, endBalance.Balance, startBalance.Balance, "Balance is unexpectedly unchanged since last balance check: last %d, retrieved %d", startBalance.Balance, endBalance.Balance)
+
+		var block_miner *climodel.Node
+		var block_miner_id string
+		var transactionRound int64
+		var expectedMinerFee int64
+
+		for round := startBalance.Round + 1; round <= endBalance.Round; round++ {
+			block := getRoundBlockFromASharder(t, round)
+
+			for i := range block.Block.Transactions {
+				txn := block.Block.Transactions[i]
+				// Find the generator miner of the block on which this transaction was recorded
+				if block_miner_id == "" {
+					if txn.ToClientId == MINER_SC_ADDRESS && txn.ClientId == wallet.ClientID && strings.Contains(txn.TransactionData, "write_pool_lock") {
+						block_miner_id = block.Block.MinerId // Generator miner
+						transactionRound = block.Block.Round
+						block_miner = getMinersDetail(t, minerNode.ID)
+
+						// Expected miner fee is calculating using this formula:
+						// Fee * minerShare * miner.ServiceCharge
+						// Stakeholders' reward is:
+						// Fee * minerShare * (1 - miner.ServiceCharge)
+						// In case of no stakeholders, miner gets:
+						// Fee * minerShare
+						minerFee := ConvertToValue(fee * minerShare)
+						minerServiceCharge := int64(float64(minerFee) * block_miner.SimpleNode.ServiceCharge)
+						expectedMinerFee = minerServiceCharge
+						minerFeeRemaining := minerFee - minerServiceCharge
+
+						// If there is no stake, the miner gets entire fee.
+						// Else "Remaining" portion would be distributed to stake holders
+						// And hence not go the miner
+						if miner.TotalStake == 0 {
+							expectedMinerFee += minerFeeRemaining
+						}
+						t.Logf("Expected miner fee: %v", expectedMinerFee)
+						t.Logf("Miner ID: %v", block_miner_id)
+					}
+				} else {
+					// Search for the fee payment to generator miner in "payFee" transaction output
+					if strings.ContainsAny(txn.TransactionData, "write_pool_lock") && strings.ContainsAny(txn.TransactionData, fmt.Sprintf("%d", transactionRound)) {
 						var transfers []apimodel.Transfer
 						err = json.Unmarshal([]byte(fmt.Sprintf("[%s]", strings.Replace(txn.TransactionOutput, "}{", "},{", -1))), &transfers)
 						require.Nil(t, err, "Cannot unmarshal the transfers from transaction output")
