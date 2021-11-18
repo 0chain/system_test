39f46196eb33b661d859318845c019a871d789e8
stewartie4 <ryanstewart456@gmail.com>
2021-11-18 02:33:09 +0000

    move cancel allocation tests to flaky tests

--- tests/cli_tests/flaky___unhealthy_blobber_test.go
+++ tests/cli_tests/flaky___unhealthy_blobber_test.go
@@ -1,47 +1,12 @@
 package cli_tests
 
 import (
-	"regexp"
-	"strings"
 	"testing"
-
-	"github.com/stretchr/testify/require"
 )
 
 /*
 Tests in here are skipped until we set up a byzantime chain/investigate conductor tests
 */
 func Test___FlakyUnhealthyBlobberScenarios(t *testing.T) {
-	t.Run("Cancel Allocation Should Work when blobber fails challenges", func(t *testing.T) {
-		t.Skip("allocations can only be canceled if 20% of blobbers fail 20 challenges, which we can't force on a vanilla chain as a black box test")
-		t.Parallel()
 
-		allocationID := setupAllocation(t, configPath)
-
-		output, err := cancelAllocation(t, configPath, allocationID, true)
-
-		require.Nil(t, err, "error canceling allocation", strings.Join(output, "\n"))
-		require.Len(t, output, 1)
-		assertOutputMatchesAllocationRegex(t, regexp.MustCompile(`^Allocation canceled with txId : [a-f0-9]{64}$`), output[0])
-	})
-
-	t.Run("Cancel Other's Allocation Should Fail", func(t *testing.T) {
-		t.Skip("allocations can only be canceled if 20% of blobbers fail 20 challenges, which we can't force on a vanilla chain as a black box test")
-		t.Parallel()
-
-		var otherAllocationID string
-		// This test creates a separate wallet and allocates there, test nesting needed to create other wallet json
-		t.Run("Get Other Allocation ID", func(t *testing.T) {
-			otherAllocationID = setupAllocation(t, configPath)
-		})
-
-		// otherAllocationID should not be cancelable from this level
-		output, err := cancelAllocation(t, configPath, otherAllocationID, false)
-
-		require.NotNil(t, err, "expected error canceling allocation", strings.Join(output, "\n"))
-		require.True(t, len(output) > 0, "expected output length be at least 1", strings.Join(output, "\n"))
-		//FIXME: POSSIBLE BUG: Error message shows error in creating instead of error in canceling
-		require.Equal(t, "Error creating allocation:[txn] too less sharders to confirm it: min_confirmation is 50%, "+
-			"but got 0/2 sharders", output[len(output)-1])
-	})
 }
