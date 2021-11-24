8981bb44581dd3ec6ef01b715ebce576ec9a7dbb
Artem Bogomaz <artembogomaz@gmail.com>
2021-11-24 23:43:57 +0700

    :hammer: repair allocation test

---
+++ tests/cli_tests/zboxcli_repair_allocation_test.go
@@ -0,0 +1,66 @@
+package cli_tests
+
+import (
+	"fmt"
+	cliutils "github.com/0chain/system_test/internal/cli/util"
+	"github.com/stretchr/testify/require"
+	"os"
+	"path/filepath"
+	"strings"
+	"testing"
+	"time"
+)
+
+func TestRepairAllocation(t *testing.T) {
+	t.Parallel()
+
+	t.Run("Create allocation and repair it", func(t *testing.T) {
+		walletOwner := escapedTestName(t)
+		allocationID, _ := registerAndCreateAllocation(t, configPath, walletOwner)
+
+		// upload file
+		file := generateRandomTestFileName(t)
+		fileSize := int64(256)
+		err := createFileWithSize(file, fileSize)
+		require.Nil(t, err)
+
+		uploadParams := map[string]interface{}{
+			"allocation": allocationID,
+			"localpath":  file,
+			"remotepath": file,
+		}
+		output, err := uploadFile(t, configPath, uploadParams, true)
+		require.Nil(t, err, strings.Join(output, "\n"))
+		require.Len(t, output, 2)
+		require.Equal(t, fmt.Sprintf("Status completed callback. Type = application/octet-stream. Name = %s", filepath.Base(file)), output[1])
+
+		// now try to repair allocation to different folder
+		// Create a folder to keep all the generated files to be uploaded
+		err = os.MkdirAll("tmp_repair", os.ModePerm)
+		require.Nil(t, err)
+
+		params := createParams(map[string]interface{}{
+			"allocation": allocationID,
+			"repairpath": "/",
+			"rootpath":   "tmp_repair/",
+		})
+
+		output, err = repairAllocation(t, walletOwner, configPath, params, false)
+		require.Nil(t, err, strings.Join(output, "\n"))
+		require.Equal(t, fmt.Sprintf("Repair file completed, Total files repaired:  %s", "1"), output[1])
+
+		fileBase := filepath.Base(file)
+		_, err = os.Stat("tmp_repair/"+fileBase)
+		require.Nil(t, err)
+	})
+}
+
+func repairAllocation(t *testing.T, wallet, cliConfigFilename, params string, retry bool) ([]string, error) {
+	t.Logf("Repairing allocation...")
+	cmd := fmt.Sprintf("./zbox start-repair %s --silent --wallet %s_wallet.json --configDir ./config --config %s", params, wallet, cliConfigFilename)
+	if retry {
+		return cliutils.RunCommand(t, cmd, 3, time.Second*2)
+	} else {
+		return cliutils.RunCommandWithoutRetry(cmd)
+	}
+}
