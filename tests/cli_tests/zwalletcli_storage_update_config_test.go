82998460b2fdfc04119d4ffe8f4a111a8c8e55ac
stewartie4 <ryanstewart456@gmail.com>
2021-12-06 15:21:42 +0000

    move additional flaky tests

--- tests/cli_tests/zwalletcli_storage_update_config_test.go
+++ tests/cli_tests/zwalletcli_storage_update_config_test.go
@@ -15,100 +15,6 @@ import (
 func TestStorageUpdateConfig(t *testing.T) {
 	t.Parallel()
 
-	t.Run("should allow update of max_read_price", func(t *testing.T) {
-		t.Parallel()
-
-		if _, err := os.Stat("./config/" + scOwnerWallet + "_wallet.json"); err != nil {
-			t.Skipf("SC owner wallet located at %s is missing", "./config/"+scOwnerWallet+"_wallet.json")
-		}
-
-		configKey := "max_read_price"
-		newValue := "110"
-
-		// unused wallet, just added to avoid having the creating new wallet outputs
-		output, err := registerWallet(t, configPath)
-		require.Nil(t, err, "Failed to register wallet", strings.Join(output, "\n"))
-
-		// register SC owner wallet
-		output, err = registerWalletForName(t, configPath, scOwnerWallet)
-		require.Nil(t, err, "Failed to register wallet", strings.Join(output, "\n"))
-
-		output, err = getStorageSCConfig(t, configPath, true)
-		require.Nil(t, err, strings.Join(output, "\n"))
-		require.Greater(t, len(output), 0, strings.Join(output, "\n"))
-
-		cfgBefore := map[string]string{}
-		for _, o := range output {
-			configPair := strings.Split(o, "\t")
-			cfgBefore[strings.TrimSpace(configPair[0])] = strings.TrimSpace(configPair[1])
-		}
-
-		// ensure revert in config is run regardless of test result
-		defer func() {
-			oldValue := cfgBefore[configKey]
-			output, err = updateStorageSCConfig(t, scOwnerWallet, map[string]interface{}{
-				"keys":   configKey,
-				"values": oldValue,
-			}, true)
-			require.Nil(t, err, strings.Join(output, "\n"))
-			require.Len(t, output, 2, strings.Join(output, "\n"))
-			require.Equal(t, "storagesc smart contract settings updated", output[0], strings.Join(output, "\n"))
-			require.Regexp(t, `Hash: [0-9a-f]+`, output[1], strings.Join(output, "\n"))
-		}()
-
-		output, err = updateStorageSCConfig(t, scOwnerWallet, map[string]interface{}{
-			"keys":   configKey,
-			"values": newValue,
-		}, true)
-		require.Nil(t, err, strings.Join(output, "\n"))
-		require.Len(t, output, 2, strings.Join(output, "\n"))
-		require.Equal(t, "storagesc smart contract settings updated", output[0], strings.Join(output, "\n"))
-		require.Regexp(t, `Hash: [0-9a-f]+`, output[1], strings.Join(output, "\n"))
-
-		output, err = getStorageSCConfig(t, configPath, true)
-		require.Nil(t, err, strings.Join(output, "\n"))
-		require.Greater(t, len(output), 0, strings.Join(output, "\n"))
-
-		cfgAfter := map[string]string{}
-		for _, o := range output {
-			configPair := strings.Split(o, "\t")
-			cfgAfter[strings.TrimSpace(configPair[0])] = strings.TrimSpace(configPair[1])
-		}
-
-		require.Equal(t, newValue, cfgAfter[configKey], "new value %s for config was not set", newValue, configKey)
-
-		// test transaction to verify chain is still working
-		output, err = executeFaucetWithTokens(t, configPath, 1)
-		require.Nil(t, err, "faucet execution failed", strings.Join(output, "\n"))
-	})
-
-	t.Run("update max_read_price to invalid value should fail", func(t *testing.T) {
-		t.Parallel()
-
-		if _, err := os.Stat("./config/" + scOwnerWallet + "_wallet.json"); err != nil {
-			t.Skipf("SC owner wallet located at %s is missing", "./config/"+scOwnerWallet+"_wallet.json")
-		}
-
-		configKey := "max_read_price"
-		newValue := "x"
-
-		// unused wallet, just added to avoid having the creating new wallet outputs
-		output, err := registerWallet(t, configPath)
-		require.Nil(t, err, "Failed to register wallet", strings.Join(output, "\n"))
-
-		// register SC owner wallet
-		output, err = registerWalletForName(t, configPath, scOwnerWallet)
-		require.Nil(t, err, "Failed to register wallet", strings.Join(output, "\n"))
-
-		output, err = updateStorageSCConfig(t, scOwnerWallet, map[string]interface{}{
-			"keys":   configKey,
-			"values": newValue,
-		}, false)
-		require.NotNil(t, err, strings.Join(output, "\n"))
-		require.Len(t, output, 1, strings.Join(output, "\n"))
-		require.Equal(t, "fatal:{\"error\": \"verify transaction failed\"}", output[0], strings.Join(output, "\n"))
-	})
-
 	t.Run("update by non-smartcontract owner should fail", func(t *testing.T) {
 		t.Parallel()
 
@@ -204,6 +110,7 @@ func TestStorageUpdateConfig(t *testing.T) {
 }
 
 func getStorageSCConfig(t *testing.T, cliConfigFilename string, retry bool) ([]string, error) {
+	cliutils.Wait(t, 5*time.Second)
 	t.Logf("Retrieving storage config...")
 
 	cmd := "./zwallet sc-config --silent --wallet " + escapedTestName(t) + "_wallet.json --configDir ./config --config " + cliConfigFilename
