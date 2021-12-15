6d2445fb801bb1c91eaf9dc1767a00572cfa1b6f
Kishan-Dhakan <kishandhakan17@gmail.com>
2021-12-15 12:34:35 +0530

    add windows setpgid

---
+++ internal/cli/util/win_utils.go
@@ -0,0 +1,12 @@
+//go:build windows
+
+package cliutils
+
+import (
+	"os/exec"
+	"syscall"
+)
+
+func Setpgid(cmd *exec.Cmd) {
+	cmd.SysProcAttr = &syscall.SysProcAttr{CreationFlags: syscall.CREATE_NEW_PROCESS_GROUP}
+}
