d8300dc455570fd8d1c3d71681b6b9f7479bf546
Kishan-Dhakan <kishandhakan17@gmail.com>
2021-12-24 13:43:20 +0530

    list another wallet's vesting pools

--- tests/cli_tests/zwalletcli_vesting_pool_test.go
+++ tests/cli_tests/zwalletcli_vesting_pool_test.go
@@ -818,6 +818,49 @@ func TestVestingPool(t *testing.T) {
 		require.Len(t, output, 1)
 		require.Equal(t, "-  "+poolId, output[0])
 	})
+
+	// FIXME: Is this expected behavior that any wallet can list any wallet's vesting pools?
+	t.Run("Listing pools for someone else's client-id should fail", func(t *testing.T) {
+		t.Parallel()
+
+		output, err := registerWallet(t, configPath)
+		require.Nil(t, err, "error registering wallet", strings.Join(output, "\n"))
+
+		foreignWalletName := "foreignWallet" + escapedTestName(t)
+		output, err = registerWalletForName(t, configPath, foreignWalletName)
+		require.Nil(t, err, "error registering new wallet", strings.Join(output, "\n"))
+
+		foreignWallet, err := getWalletForName(t, configPath, foreignWalletName)
+		require.Nil(t, err, "error fetching wallet")
+
+		output, err = executeFaucetWithTokensForWallet(t, foreignWalletName, configPath, 1.0)
+		require.Nil(t, err, "error requesting tokens from faucet", strings.Join(output, "\n"))
+
+		targetWalletName := "targetWallet" + escapedTestName(t)
+		output, err = registerWalletForName(t, configPath, targetWalletName)
+		require.Nil(t, err, "error registering target wallet", strings.Join(output, "\n"))
+
+		targetWallet, err := getWalletForName(t, configPath, targetWalletName)
+		require.Nil(t, err, "error fetching destination wallet")
+
+		output, err = vestingPoolAddForWallet(t, configPath, createParams(map[string]interface{}{
+			"d":        targetWallet.ClientID + ":0.1",
+			"lock":     0.1,
+			"duration": validDuration,
+		}), true, foreignWalletName)
+		require.Nil(t, err, "error adding a new vesting pool")
+		require.Len(t, output, 1)
+		require.Regexp(t, regexp.MustCompile("Vesting pool added successfully: [a-z0-9]{64}:vestingpool:[a-z0-9]{64}"), output[0], "output did not match expected vesting pool pattern")
+		poolId := regexp.MustCompile("[a-z0-9]{64}:vestingpool:[a-z0-9]{64}").FindString(output[0])
+		require.NotEmpty(t, poolId, "expected pool ID as output to vp-add command")
+
+		output, err = vestingPoolList(t, configPath, createParams(map[string]interface{}{
+			"client_id": foreignWallet.ClientID,
+		}), true)
+		require.Nil(t, err, "error listing vesting pools")
+		require.Len(t, output, 1)
+		require.Equal(t, "-  "+poolId, output[0])
+	})
 }
 
 func vestingPoolAdd(t *testing.T, cliConfigFilename, params string, retry bool) ([]string, error) {
