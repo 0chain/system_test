42f158ee0ad7c67f85027e81b374a20784e9927a
Kishan-Dhakan <kishandhakan17@gmail.com>
2021-12-29 22:29:47 +0530

    token accounting for vp-stop multiple dest

--- tests/cli_tests/zwalletcli_vesting_pool_test.go
+++ tests/cli_tests/zwalletcli_vesting_pool_test.go
@@ -1080,6 +1080,27 @@ func TestVestingPool(t *testing.T) {
 		}), true)
 		require.Nil(t, err, "error fetching pool-info")
 		require.Len(t, output, 23, "expected output of length 23")
+		canUnlockAmount, err := strconv.ParseFloat(regexp.MustCompile(`\d+\.?\d*`).FindString(output[2]), 64)
+		require.Nil(t, err, "error parsing float from vp-info")
+		canUnlockUnit := regexp.MustCompile("[um]?ZCN").FindString(output[2])
+		canUnlockAmount = unitToZCN(canUnlockAmount, canUnlockUnit)
+
+		output, err = vestingPoolUnlock(t, configPath, createParams(map[string]interface{}{
+			"pool_id": poolId,
+		}), true)
+		require.Nil(t, err, "error unlocking tokens from vesting pool")
+		require.Len(t, output, 1)
+		require.Equal(t, "Tokens unlocked successfully.", output[0])
+
+		output, err = getBalance(t, configPath)
+		require.Nil(t, err, "error fetching balance for target wallet")
+		require.Len(t, output, 1)
+		require.Regexp(t, regexp.MustCompile(`Balance: \d+\.?\d* [um]?ZCN \(\d+\.?\d* USD\)`), output[0])
+		newBalance := regexp.MustCompile(`\d+\.?\d* [um]?ZCN`).FindString(output[0])
+		newBalanceValue, err := strconv.ParseFloat(strings.Fields(newBalance)[0], 64)
+		require.Nil(t, err, "error parsing float from balance")
+		newBalanceInZCN := unitToZCN(newBalanceValue, strings.Fields(newBalance)[1])
+		require.InEpsilonf(t, 0.4+canUnlockAmount, newBalanceInZCN, 0.00000000001, "expected balance to be [%v] but was [%v]", 0.9+canUnlockAmount, newBalanceInZCN)
 	})
 
 	t.Run("Vesting pool stop for someone else's pool must fail", func(t *testing.T) {
