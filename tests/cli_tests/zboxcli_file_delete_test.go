711b8315aee8e1e8ace7773369e086ee361482dc
littlestar642 <avinashjaiswal642@gmail.com>
2022-01-04 21:01:32 +0530

    add new tests for delete file

--- tests/cli_tests/zboxcli_file_delete_test.go
+++ tests/cli_tests/zboxcli_file_delete_test.go
@@ -156,14 +156,15 @@ func TestFileDelete(t *testing.T) {
 	t.Run("delete existing file with encryption should work", func(t *testing.T) {
 		t.Parallel()
 
-		allocationID := setupAllocation(t, configPath)
+		allocationID := setupAllocation(t, configPath, map[string]interface{}{
+			"size": 10000,
+		})
 		defer createAllocationTestTeardown(t, allocationID)
 
 		remotepath := "/"
-		filesize := int64(1 * KB)
 
 		filename := generateRandomTestFileName(t)
-		err = createFileWithSize(filename, filesize)
+		err = createFileWithSize(filename, 10)
 		require.Nil(t, err)
 
 		output, err := uploadFile(t, configPath, map[string]interface{}{
@@ -195,7 +196,7 @@ func TestFileDelete(t *testing.T) {
 		require.Equal(t, "null", output[0], strings.Join(output, "\n"))
 	})
 
-	t.Run("delete shared file should fail", func(t *testing.T) {
+	t.Run("delete shared file by owner should work", func(t *testing.T) {
 		t.Parallel()
 
 		collaboratorWalletName := escapedTestName(t) + "_collaborator"
@@ -226,7 +227,90 @@ func TestFileDelete(t *testing.T) {
 			"allocation": allocationID,
 			"remotepath": remotepath,
 		}), false)
+		require.Nil(t, err, strings.Join(output, "\n"))
+		require.Len(t, output, 1)
+
+		output, err = listFilesInAllocation(t, configPath, createParams(map[string]interface{}{
+			"allocation": allocationID,
+			"remotepath": remotepath,
+			"json":       "",
+		}), false)
+		require.Nil(t, err, "List files failed", err, strings.Join(output, "\n"))
+		require.Len(t, output, 1)
+		require.Equal(t, "null", output[0], strings.Join(output, "\n"))
+	})
+
+	t.Run("delete shared file by collaborator should fail", func(t *testing.T) {
+		t.Parallel()
+
+		collaboratorWalletName := escapedTestName(t) + "_collaborator"
+
+		output, err := registerWalletForName(t, configPath, collaboratorWalletName)
+		require.Nil(t, err, "Unexpected register wallet failure", strings.Join(output, "\n"))
+
+		collaboratorWallet, err := getWalletForName(t, configPath, collaboratorWalletName)
+		require.Nil(t, err, "Error occurred when retrieving curator wallet")
+
+		allocationID := setupAllocation(t, configPath, map[string]interface{}{"size": 2 * MB})
+		defer createAllocationTestTeardown(t, allocationID)
+
+		localpath := uploadRandomlyGeneratedFile(t, allocationID, "/", 128*KB)
+		remotepath := "/" + filepath.Base(localpath)
+
+		output, err = addCollaborator(t, createParams(map[string]interface{}{
+			"allocation": allocationID,
+			"collabid":   collaboratorWallet.ClientID,
+			"remotepath": remotepath,
+		}), true)
+		require.Nil(t, err, "error in adding collaborator", strings.Join(output, "\n"))
+		require.Len(t, output, 1, strings.Join(output, "\n"))
+		expectedOutput := fmt.Sprintf("Collaborator %s added successfully for the file %s", collaboratorWallet.ClientID, remotepath)
+		require.Equal(t, expectedOutput, output[0], strings.Join(output, "\n"))
+
+		output, err = deleteFile(t, collaboratorWalletName, createParams(map[string]interface{}{
+			"allocation": allocationID,
+			"remotepath": remotepath,
+		}), false)
 		require.NotNil(t, err, strings.Join(output, "\n"))
-		require.Len(t, output, 2)
+		require.Len(t, output, 1)
+
+		output, err = listFilesInAllocation(t, configPath, createParams(map[string]interface{}{
+			"allocation": allocationID,
+			"remotepath": remotepath,
+			"json":       "",
+		}), false)
+		require.Nil(t, err, "List files failed", err, strings.Join(output, "\n"))
+		require.Len(t, output, 1)
+
+		// FIXME: Deleting the file even though the collaborator is not the owner and delete prior failed
+		require.Equal(t, "null", output[0], strings.Join(output, "\n"))
+	})
+
+	t.Run("delete existing directory should fail", func(t *testing.T) {
+		t.Parallel()
+
+		allocationID := setupAllocation(t, configPath)
+		defer createAllocationTestTeardown(t, allocationID)
+
+		remotepath := "/"
+		filesize := int64(1 * KB)
+		generateFileAndUpload(t, allocationID, remotepath, filesize)
+
+		output, err := deleteFile(t, escapedTestName(t), createParams(map[string]interface{}{
+			"allocation": allocationID,
+			"remotepath": remotepath,
+		}), false)
+		require.NotNil(t, err, strings.Join(output, "\n"))
+		require.Len(t, output, 1)
+		require.Contains(t, output[0], "Delete failed")
+
+		output, err = listFilesInAllocation(t, configPath, createParams(map[string]interface{}{
+			"allocation": allocationID,
+			"remotepath": remotepath,
+			"json":       "",
+		}), false)
+		require.Nil(t, err, "List files failed", err, strings.Join(output, "\n"))
+		require.Len(t, output, 1)
+		require.NotEqual(t, "null", output[0], strings.Join(output, "\n"))
 	})
 }
