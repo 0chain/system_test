56a26ba268c8945921f7688c67dcb70043a2e59f
Kishan-Dhakan <kishandhakan17@gmail.com>
2022-01-06 18:47:33 +0530

    add minerUpdate function

---
+++ tests/cli_tests/zwalletcli_miner_update_settings_test.go
@@ -0,0 +1,27 @@
+package cli_tests
+
+import (
+	"fmt"
+	"os"
+	"testing"
+	"time"
+
+	cliutils "github.com/0chain/system_test/internal/cli/util"
+)
+
+func TestMinerUpdateSettings(t *testing.T) {
+	t.Parallel()
+
+	if _, err := os.Stat("./config/" + minerNodeDelegateWallet + "_wallet.json"); err != nil {
+		t.Skipf("blobber owner wallet located at %s is missing", "./config/"+minerNodeDelegateWallet+"_wallet.json")
+	}
+}
+
+func minerUpdateSettings(t *testing.T, cliConfigFilename, params string) ([]string, error) {
+	return minerUpdateSettingsForWallet(t, cliConfigFilename, params, minerNodeDelegateWallet)
+}
+
+func minerUpdateSettingsForWallet(t *testing.T, cliConfigFilename, params, wallet string) ([]string, error) {
+	t.Log("Updating miner settings...")
+	return cliutils.RunCommand(t, fmt.Sprintf("./zbox mn-update-settings %s --silent --wallet %s_wallet.json --configDir ./config --config %s", params, wallet, cliConfigFilename), 3, time.Second*2)
+}
