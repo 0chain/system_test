62d5569f0fbe95f4d8d4cba1606ebbae16ad3814
Kishan-Dhakan <kishandhakan17@gmail.com>
2022-01-07 02:43:32 +0530

    min_stake less than global min_stake should fail

--- tests/cli_tests/zwalletcli_miner_update_settings_test.go
+++ tests/cli_tests/zwalletcli_miner_update_settings_test.go
@@ -4,6 +4,7 @@ import (
 	"encoding/json"
 	"fmt"
 	"os"
+	"strings"
 	"testing"
 	"time"
 
@@ -18,6 +19,8 @@ func TestMinerUpdateSettings(t *testing.T) {
 	if _, err := os.Stat("./config/" + minerNodeDelegateWallet + "_wallet.json"); err != nil {
 		t.Skipf("blobber owner wallet located at %s is missing", "./config/"+minerNodeDelegateWallet+"_wallet.json")
 	}
+
+	mnConfig := getMinerSCConfiguration(t)
 
 	t.Run("Miner update min_stake by delegate wallet should work", func(t *testing.T) {
 		t.Parallel()
@@ -141,6 +144,46 @@ func TestMinerUpdateSettings(t *testing.T) {
 		require.Nil(t, err, "error updating max_stake in miner node")
 		require.Len(t, output, 1)
 		require.Equal(t, "settings updated", output[0])
+	})
+
+	t.Run("Miner update min_stake with less than global min stake should fail", func(t *testing.T) {
+		t.Parallel()
+
+		output, err := listMiners(t, configPath, "--json")
+		require.Nil(t, err, "error listing miners")
+		require.Len(t, output, 1)
+
+		var miners climodel.MinerSCNodes
+		err = json.Unmarshal([]byte(output[0]), &miners)
+		require.Nil(t, err, "error unmarshalling ls-miners json output")
+
+		miner := miners.Nodes[2]
+		output, err = minerInfo(t, configPath, createParams(map[string]interface{}{
+			"id": miner.ID,
+		}))
+		require.Nil(t, err, "error fetching miner node info")
+		require.Len(t, output, 1)
+
+		var oldMinerInfo climodel.SimpleNode
+		err = json.Unmarshal([]byte(output[0]), &oldMinerInfo)
+		require.Nil(t, err, "error unmarshalling miner info")
+
+		output, err = minerUpdateSettings(t, configPath, createParams(map[string]interface{}{
+			"id":        miner.ID,
+			"min_stake": mnConfig["min_stake"] - 1e-9,
+		}), false)
+		defer func() {
+			output, err = minerUpdateSettings(t, configPath, createParams(map[string]interface{}{
+				"id":        miner.ID,
+				"min_stake": oldMinerInfo.MinStake,
+			}), true)
+			require.Nil(t, err, "error reverting miner node settings after test")
+			require.Len(t, output, 1)
+			require.Equal(t, "settings updated", output[0])
+		}()
+		require.NotNil(t, err, "expected error when updating min_stake less than global min_stake but got output:", strings.Join(output, "\n"))
+		require.Len(t, output, 1)
+		require.Equal(t, `fatal:{"error": "verify transaction failed"}`, output[0])
 	})
 }
 
