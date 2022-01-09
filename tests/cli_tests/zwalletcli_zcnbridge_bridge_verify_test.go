c0a8cc97026a3b8260feff26c2fea2404045bdf3
DmitryD <dima.dyachkov@gmail.com>
2022-01-10 02:34:06 +0300

    Refactoring

--- tests/cli_tests/zwalletcli_zcnbridge_bridge_verify_test.go
+++ tests/cli_tests/zwalletcli_zcnbridge_bridge_verify_test.go
@@ -1,6 +1,7 @@
 package cli_tests
 
 import (
+	"fmt"
 	"strings"
 	"testing"
 
@@ -14,7 +15,15 @@ func TestBridgeVerify(t *testing.T) {
 	t.Run("Verify ethereum transaction", func(t *testing.T) {
 		t.Parallel()
 
-		output, err := bridgeVerify(t, "0x31925839586949a96e72cacf25fed7f47de5faff78adc20946183daf3c4cf230")
+		const (
+			cmd = "bridge-verify"
+		)
+
+		output, err := bridgeCmd(
+			t,
+			cmd,
+			"0x31925839586949a96e72cacf25fed7f47de5faff78adc20946183daf3c4cf230",
+		)
 
 		require.Nil(t, err, "error trying to verify transaction", strings.Join(output, "\n"))
 		require.Equal(t, "Transaction verification success: 0x31925839586949a96e72cacf25fed7f47de5faff78adc20946183daf3c4cf230", output[len(output)-1])
@@ -22,11 +31,8 @@ func TestBridgeVerify(t *testing.T) {
 }
 
 // bridge-verify
-func bridgeVerify(t *testing.T, hash string) ([]string, error) {
+func bridgeCmd(t *testing.T, cmd, hash string) ([]string, error) {
 	t.Logf("Verify ethereum transaction for " + hash)
-
-	cmd := "./zwallet bridge-verify" +
-		" --hash " + hash
-
-	return cliutils.RunCommandWithoutRetry(cmd)
+	run := fmt.Sprintf("./zwallet %s --hash %s", cmd, hash)
+	return cliutils.RunCommandWithoutRetry(run)
 }
