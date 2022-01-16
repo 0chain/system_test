e708d607945dfbc876a8cffe01f6158f4ce9372e
Kishan-Dhakan <kishandhakan17@gmail.com>
2022-01-17 01:01:07 +0530

    add teardowns

--- tests/cli_tests/zwalletcli_minersc_pool_info_test.go
+++ tests/cli_tests/zwalletcli_minersc_pool_info_test.go
@@ -65,6 +65,15 @@ func TestMinerSCUserPoolInfo(t *testing.T) {
 		require.Len(t, poolsInfo.Pools["miner"][minerNodeDelegateWallet.ClientID], 1)
 		require.Equal(t, poolId, poolsInfo.Pools["miner"][minerNodeDelegateWallet.ClientID][0].ID)
 		require.Equal(t, float64(1), intToZCN(poolsInfo.Pools["miner"][minerNodeDelegateWallet.ClientID][0].Balance))
+
+		// teardown
+		_, err = minerOrSharderUnlock(t, configPath, createParams(map[string]interface{}{
+			"id":      minerNodeDelegateWallet.ClientID,
+			"pool_id": poolId,
+		}), true)
+		if err != nil {
+			t.Log("error unlocking tokens after test: ", t.Name())
+		}
 	})
 
 	t.Run("Getting MinerSC Stake pools of a wallet before and after locking against a sharder should work", func(t *testing.T) {
@@ -105,6 +114,15 @@ func TestMinerSCUserPoolInfo(t *testing.T) {
 		require.Len(t, poolsInfo.Pools["sharder"][sharderNodeDelegateWallet.ClientID], 1)
 		require.Equal(t, poolId, poolsInfo.Pools["sharder"][sharderNodeDelegateWallet.ClientID][0].ID)
 		require.Equal(t, float64(1), intToZCN(poolsInfo.Pools["sharder"][sharderNodeDelegateWallet.ClientID][0].Balance))
+
+		// teardown
+		_, err = minerOrSharderUnlock(t, configPath, createParams(map[string]interface{}{
+			"id": sharderNodeDelegateWallet.ClientID,
+			"pool_id": poolId,
+		}), true)
+		if err != nil {
+			t.Log("error unlocking tokens after test: ", t.Name())
+		}
 	})
 
 	t.Run("Getting MinerSC pools info for a different client id than wallet owner should work", func(t *testing.T) {
@@ -158,6 +176,23 @@ func TestMinerSCUserPoolInfo(t *testing.T) {
 		require.Len(t, poolsInfo.Pools["sharder"][sharderNodeDelegateWallet.ClientID], 1)
 		require.Equal(t, sharderPoolId, poolsInfo.Pools["sharder"][sharderNodeDelegateWallet.ClientID][0].ID)
 		require.Equal(t, float64(1), intToZCN(poolsInfo.Pools["sharder"][sharderNodeDelegateWallet.ClientID][0].Balance))
+
+		// teardown
+		_, err = minerOrSharderUnlock(t, configPath, createParams(map[string]interface{}{
+			"id": minerNodeDelegateWallet.ClientID,
+			"pool_id": minerPoolId,
+		}), true)
+		if err != nil {
+			t.Log("error unlocking tokens after test: ", t.Name())
+		}
+
+		_, err = minerOrSharderUnlock(t, configPath, createParams(map[string]interface{}{
+			"id": sharderNodeDelegateWallet.ClientID,
+			"pool_id": sharderPoolId,
+		}), true)
+		if err != nil {
+			t.Log("error unlocking tokens after test: ", t.Name())
+		}
 	})
 }
 
@@ -184,5 +219,18 @@ func minerOrSharderLockForWallet(t *testing.T, cliConfigFilename, params, wallet
 		return cliutils.RunCommand(t, fmt.Sprintf("./zwallet mn-lock %s --silent --wallet %s_wallet.json --configDir ./config --config %s", params, wallet, cliConfigFilename), 3, time.Second)
 	} else {
 		return cliutils.RunCommandWithoutRetry(fmt.Sprintf("./zwallet mn-lock %s --silent --wallet %s_wallet.json --configDir ./config --config %s", params, wallet, cliConfigFilename))
+	}
+}
+
+func minerOrSharderUnlock(t *testing.T, cliConfigFilename, params string, retry bool) ([]string, error) {
+	return minerOrSharderUnlockForWallet(t, cliConfigFilename, params, escapedTestName(t), retry)
+}
+
+func minerOrSharderUnlockForWallet(t *testing.T, cliConfigFilename, params, wallet string, retry bool) ([]string, error) {
+	t.Log("unlocking tokens from miner/sharder pool...")
+	if retry {
+		return cliutils.RunCommand(t, fmt.Sprintf("./zwallet mn-unlock %s --silent --wallet %s_wallet.json --configDir ./config --config %s", params, wallet, cliConfigFilename), 3, time.Second)
+	} else {
+		return cliutils.RunCommandWithoutRetry(fmt.Sprintf("./zwallet mn-unlock %s --silent --wallet %s_wallet.json --configDir ./config --config %s", params, wallet, cliConfigFilename))
 	}
 }
