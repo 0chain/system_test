08f7e87c34f3f1d504c596c3e9e4e98e629e2092
Kishan-Dhakan <kishandhakan17@gmail.com>
2022-01-16 11:39:51 +0530

    add logging, verify id and balance against miner

--- tests/cli_tests/zwalletcli_minersc_pool_info_test.go
+++ tests/cli_tests/zwalletcli_minersc_pool_info_test.go
@@ -4,6 +4,7 @@ import (
 	"encoding/json"
 	"fmt"
 	"os"
+	"regexp"
 	"strings"
 	"testing"
 	"time"
@@ -23,11 +24,14 @@ func TestMinerSCUserPoolInfo(t *testing.T) {
 	minerNodeDelegateWallet, err := getWalletForName(t, configPath, minerNodeDelegateWalletName)
 	require.Nil(t, err, "error fetching wallet")
 
-	t.Run("Getting MinerSC Stake pools of a wallet before and after making a pool should work", func(t *testing.T) {
+	t.Run("Getting MinerSC Stake pools of a wallet before and after locking against a miner should work", func(t *testing.T) {
 		t.Parallel()
 
 		output, err := registerWallet(t, configPath)
 		require.Nil(t, err, "error registering wallet", strings.Join(output, "\n"))
+
+		output, err = executeFaucetWithTokens(t, configPath, 2.0)
+		require.Nil(t, err, "error executing faucet", strings.Join(output, "\n"))
 
 		// before locking tokens against a miner
 		output, err = stakePoolsInMinerSCInfo(t, configPath, "", true)
@@ -40,9 +44,24 @@ func TestMinerSCUserPoolInfo(t *testing.T) {
 		require.Empty(t, pools)
 
 		output, err = minerOrSharderLock(t, configPath, createParams(map[string]interface{}{
-			"id": minerNodeDelegateWallet.ClientID,
+			"id":     minerNodeDelegateWallet.ClientID,
+			"tokens": 1,
 		}), true)
-		require.Nil(t, err, "error ")
+		require.Nil(t, err, "error staking tokens against node")
+		require.Regexp(t, regexp.MustCompile("locked with: [a-z0-9]{64}"), output[0])
+		poolId := strings.Fields(output[0])[2]
+
+		// after locking tokens against a miner
+		output, err = stakePoolsInMinerSCInfo(t, configPath, "", true)
+		require.Nil(t, err, "error fetching stake pools")
+		require.Len(t, output, 1)
+
+		var poolsAfterLock climodel.MinerSCUserPoolsInfo
+		err = json.Unmarshal([]byte(output[0]), &poolsAfterLock)
+		require.Nil(t, err, "error unmarshalling Miner SC User Pool")
+		require.Len(t, poolsAfterLock.Pools["miner"][minerNodeDelegateWallet.ClientID], 1)
+		require.Equal(t, poolId, poolsAfterLock.Pools["miner"][minerNodeDelegateWallet.ClientID][0].ID)
+		require.Equal(t, float64(1), intToZCN(poolsAfterLock.Pools["miner"][minerNodeDelegateWallet.ClientID][0].Balance))
 	})
 }
 
@@ -51,6 +70,7 @@ func stakePoolsInMinerSCInfo(t *testing.T, cliConfigFilename, params string, ret
 }
 
 func stakePoolsInMinerSCInfoForWallet(t *testing.T, cliConfigFilename, params, wallet string, retry bool) ([]string, error) {
+	t.Log("fetching mn-user-info...")
 	if retry {
 		return cliutils.RunCommand(t, fmt.Sprintf("./zwallet mn-user-info %s --json --silent --wallet %s_wallet.json --configDir ./config --config %s", params, wallet, cliConfigFilename), 3, time.Second)
 	} else {
@@ -63,6 +83,7 @@ func minerOrSharderLock(t *testing.T, cliConfigFilename, params string, retry bo
 }
 
 func minerOrSharderLockForWallet(t *testing.T, cliConfigFilename, params, wallet string, retry bool) ([]string, error) {
+	t.Log("locking tokens against miner/sharder...")
 	if retry {
 		return cliutils.RunCommand(t, fmt.Sprintf("./zwallet mn-lock %s --silent --wallet %s_wallet.json --configDir ./config --config %s", params, wallet, cliConfigFilename), 3, time.Second)
 	} else {
