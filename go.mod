50291120937ff7250ff8a33a50ae8d0d997aba27
Kishan-Dhakan <kishandhakan17@gmail.com>
2022-01-22 00:43:18 +0530

    challenge in allocation before and after update

--- go.mod
+++ go.mod
@@ -3,13 +3,13 @@ module github.com/0chain/system_test
 go 1.16
 
 require (
+	github.com/0chain/gosdk v1.4.2
 	github.com/herumi/bls-go-binary v1.0.1-0.20210830012634-a8e769d3b872
 	github.com/kr/pretty v0.3.0 // indirect
-	github.com/rogpeppe/go-internal v1.8.0 // indirect
 	github.com/sirupsen/logrus v1.8.1
 	github.com/stretchr/testify v1.7.0
-	golang.org/x/crypto v0.0.0-20211117183948-ae814b36b871
-	golang.org/x/sys v0.0.0-20211124211545-fe61309f8881 // indirect
-	gopkg.in/check.v1 v1.0.0-20201130134442-10cb98267c6c // indirect
-	gopkg.in/yaml.v3 v3.0.0-20210107192922-496545a6307b // indirect
+	golang.org/x/crypto v0.0.0-20211209193657-4570a0811e8b
 )
+
+// temporary, for development
+replace github.com/0chain/gosdk => ../gosdk
---
+++ tests/cli_tests/blobber_challenge_test.go
@@ -0,0 +1,91 @@
+package cli_tests
+
+import (
+	"encoding/json"
+	"os"
+	"path/filepath"
+	"strings"
+	"testing"
+	"time"
+
+	"github.com/0chain/gosdk/zboxcore/sdk"
+	climodel "github.com/0chain/system_test/internal/cli/model"
+	cliutils "github.com/0chain/system_test/internal/cli/util"
+	"github.com/stretchr/testify/require"
+)
+
+func TestBlobberChallenge(t *testing.T) {
+	t.Parallel()
+
+	t.Run("Creating challenge against an allocation of used size > 1 MB should work", func(t *testing.T) {
+		t.Parallel()
+
+		allocationId := setupAllocationAndReadLock(t, configPath, map[string]interface{}{
+			"size":   10 * MB,
+			"tokens": 1,
+		})
+
+		remotepath := "/"
+		filesize := 2 * MB
+		filename := generateRandomTestFileName(t)
+
+		err := createFileWithSize(filename, int64(filesize))
+		require.Nil(t, err)
+
+		// Upload parameters
+		output, err := uploadFile(t, configPath, map[string]interface{}{
+			"allocation": allocationId,
+			"remotepath": remotepath,
+			"localpath":  filename,
+			"commit":     true,
+		}, true)
+		require.Nil(t, err, "error uploading file", strings.Join(output, "\n"))
+
+		err = os.Remove(filename)
+		require.Nil(t, err)
+
+		remoteFilepath := remotepath + filepath.Base(filename)
+
+		cliutils.Wait(t, 30*time.Second)
+
+		output, err = getFileStats(t, configPath, createParams(map[string]interface{}{
+			"allocation": allocationId,
+			"remotepath": remoteFilepath,
+			"json":       "",
+		}), true)
+		require.Nil(t, err, "error getting file stats")
+		require.Len(t, output, 1)
+
+		var stats map[string]climodel.FileStats
+		err = json.Unmarshal([]byte(output[0]), &stats)
+		require.Nil(t, err, "error unmarshalling file stats json")
+
+		output, err = downloadFile(t, configPath, createParams(map[string]interface{}{
+			"allocation": allocationId,
+			"remotepath": remoteFilepath,
+			"localpath":  os.TempDir() + string(os.PathSeparator),
+		}), true)
+		require.Nil(t, err, "error downloading file", strings.Join(output, "\n"))
+
+		err = sdk.GenerateChallenges(0)
+		require.Nil(t, err, "error calling Generate Challenges")
+
+		output, err = getFileStats(t, configPath, createParams(map[string]interface{}{
+			"allocation": allocationId,
+			"remotepath": remoteFilepath,
+			"json":       "",
+		}), true)
+		require.Nil(t, err, "error getting file stats")
+		require.Len(t, output, 1)
+
+		err = json.Unmarshal([]byte(output[0]), &stats)
+		require.Nil(t, err, "error unmarshalling file stats json")
+
+		var challenge int
+		for _, stat := range stats {
+			challenge += int(stat.NumOfChallenges)
+		}
+
+		require.Greater(t, challenge, 0)
+	})
+}
