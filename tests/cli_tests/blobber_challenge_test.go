6b1f1b96062be5fe08e0c5cfb35af2752c02dfa6
Kishan-Dhakan <kishandhakan17@gmail.com>
2022-01-26 13:37:24 +0530

    refactor

--- tests/cli_tests/blobber_challenge_test.go
+++ tests/cli_tests/blobber_challenge_test.go
@@ -21,7 +21,27 @@ import (
 func TestBlobberChallenge(t *testing.T) {
 	t.Parallel()
 
-	t.Run("Creating challenge against an allocation of used size > 1 MB should work", func(t *testing.T) {
+	output, err := registerWallet(t, configPath)
+	require.Nil(t, err, "error registering wallet", strings.Join(output, "\n"))
+
+	// Get sharder list.
+	output, err = getSharders(t, configPath)
+	require.Nil(t, err, "get sharders failed", strings.Join(output, "\n"))
+	require.Greater(t, len(output), 1)
+	require.Equal(t, "MagicBlock Sharders", output[0])
+
+	var sharders map[string]climodel.Sharder
+	err = json.Unmarshal([]byte(strings.Join(output[1:], "")), &sharders)
+	require.Nil(t, err, "Error deserializing JSON string `%s`: %v", strings.Join(output[1:], "\n"), err)
+	require.NotEmpty(t, sharders, "No sharders found: %v", strings.Join(output[1:], "\n"))
+
+	// Use first sharder from map.
+	sharder := sharders[reflect.ValueOf(sharders).MapKeys()[0].String()]
+
+	// Get base URL for API calls.
+	sharderBaseUrl := getNodeBaseURL(sharder.Host, sharder.Port)
+
+	t.Run("Downloading a file greater than 1 MB should generate randomized challenges", func(t *testing.T) {
 		t.Parallel()
 
 		allocationId := setupAllocationAndReadLock(t, configPath, map[string]interface{}{
@@ -67,28 +87,12 @@ func TestBlobberChallenge(t *testing.T) {
 			blobberId = stat.BlobberID
 		}
 
-		// Get sharder list.
-		output, err = getSharders(t, configPath)
-		require.Nil(t, err, "get sharders failed", strings.Join(output, "\n"))
-		require.Greater(t, len(output), 1)
-		require.Equal(t, "MagicBlock Sharders", output[0])
+		res, err := apiGetOpenChallenges(sharderBaseUrl, blobberId)
+		require.Nil(t, err, "error getting challenges", res)
+		require.True(t, res.StatusCode >= 200 && res.StatusCode < 300, "Failed API request to get open challenges for blobber id: %s", blobberId)
+		require.NotNil(t, res.Body, "Open challenges API response must not be nil")
 
-		var sharders map[string]climodel.Sharder
-		err = json.Unmarshal([]byte(strings.Join(output[1:], "")), &sharders)
-		require.Nil(t, err, "Error deserializing JSON string `%s`: %v", strings.Join(output[1:], "\n"), err)
-		require.NotEmpty(t, sharders, "No sharders found: %v", strings.Join(output[1:], "\n"))
-
-		// Use first sharder from map.
-		sharder := sharders[reflect.ValueOf(sharders).MapKeys()[0].String()]
-
-		// Get base URL for API calls.
-		sharderBaseUrl := getNodeBaseURL(sharder.Host, sharder.Port)
-		res1, err := apiGetOpenChallenges(sharderBaseUrl, blobberId)
-		require.Nil(t, err, "error getting challenges", res1)
-		require.True(t, res1.StatusCode >= 200 && res1.StatusCode < 300, "Failed API request to get open challenges for blobber id: %s", blobberId)
-		require.NotNil(t, res1.Body, "Open challenges API response must not be nil")
-
-		res1Body, err := ioutil.ReadAll(res1.Body)
+		res1Body, err := ioutil.ReadAll(res.Body)
 		require.Nil(t, err, "Error reading response body")
 		var openChallengesBefore apimodel.BlobberChallenge
 		err = json.Unmarshal(res1Body, &openChallengesBefore)
@@ -103,12 +107,12 @@ func TestBlobberChallenge(t *testing.T) {
 
 		cliutils.Wait(t, 30*time.Second)
 
-		res2, err := apiGetOpenChallenges(sharderBaseUrl, blobberId)
-		require.Nil(t, err, "error getting challenges", res2)
-		require.True(t, res2.StatusCode >= 200 && res2.StatusCode < 300, "Failed API request to get open challenges for blobber id: %s", blobberId)
-		require.NotNil(t, res1.Body, "Open challenges API response must not be nil")
+		res, err = apiGetOpenChallenges(sharderBaseUrl, blobberId)
+		require.Nil(t, err, "error getting challenges", res)
+		require.True(t, res.StatusCode >= 200 && res.StatusCode < 300, "Failed API request to get open challenges for blobber id: %s", blobberId)
+		require.NotNil(t, res.Body, "Open challenges API response must not be nil")
 
-		res2Body, err := ioutil.ReadAll(res2.Body)
+		res2Body, err := ioutil.ReadAll(res.Body)
 		require.Nil(t, err, "Error reading response body")
 		var openChallengesAfter apimodel.BlobberChallenge
 		err = json.Unmarshal(res2Body, &openChallengesAfter)
