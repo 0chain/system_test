ae3ce7c543afc4b0fe675e73db20e790a08e6691
Kishan-Dhakan <kishandhakan17@gmail.com>
2022-01-27 12:23:52 +0530

    challenge generation on move file

--- tests/cli_tests/blobber_challenge_test.go
+++ tests/cli_tests/blobber_challenge_test.go
@@ -19,8 +19,6 @@ import (
 )
 
 func TestBlobberChallenge(t *testing.T) {
-	t.Parallel()
-
 	output, err := registerWallet(t, configPath)
 	require.Nil(t, err, "error registering wallet", strings.Join(output, "\n"))
 
@@ -42,8 +40,6 @@ func TestBlobberChallenge(t *testing.T) {
 	sharderBaseUrl := getNodeBaseURL(sharder.Host, sharder.Port)
 
 	t.Run("Downloading a file greater than 1 MB should generate randomized challenges", func(t *testing.T) {
-		t.Parallel()
-
 		allocationId := setupAllocationAndReadLock(t, configPath, map[string]interface{}{
 			"size":   10 * MB,
 			"tokens": 1,
@@ -119,6 +115,82 @@ func TestBlobberChallenge(t *testing.T) {
 		require.Nil(t, err, "error unmarshalling response body")
 
 		// New challenges must have been created after a download request
+		require.Greater(t, len(openChallengesAfter.Challenges), len(openChallengesBefore.Challenges))
+	})
+
+	t.Run("Moving a file greater than 1 MB should generate randomized challenges", func(t *testing.T) {
+		allocationId := setupAllocationAndReadLock(t, configPath, map[string]interface{}{
+			"size":   10 * MB,
+			"tokens": 1,
+		})
+
+		remotepath := "/dir/"
+		filesize := 2 * MB
+		filename := generateRandomTestFileName(t)
+
+		err := createFileWithSize(filename, int64(filesize))
+		require.Nil(t, err, "error creating file")
+
+		output, err := uploadFile(t, configPath, map[string]interface{}{
+			"allocation": allocationId,
+			"remotepath": remotepath + filepath.Base(filename),
+			"localpath":  filename,
+		}, true)
+		require.Nil(t, err, "error uploading file", strings.Join(output, "\n"))
+
+		err = os.Remove(filename)
+		require.Nil(t, err)
+
+		remoteFilepath := remotepath + filepath.Base(filename)
+
+		output, err = getFileStats(t, configPath, createParams(map[string]interface{}{
+			"allocation": allocationId,
+			"remotepath": remoteFilepath,
+			"json":       "",
+		}), true)
+		require.Nil(t, err, "error getting file stats")
+		require.Len(t, output, 1)
+
+		var stats map[string]climodel.FileStats
+		err = json.Unmarshal([]byte(output[0]), &stats)
+		require.Nil(t, err, "error unmarshalling file stats json")
+
+		var blobberId string
+		for _, stat := range stats {
+			blobberId = stat.BlobberID
+		}
+
+		res, err := apiGetOpenChallenges(sharderBaseUrl, blobberId)
+		require.Nil(t, err, "error getting challenges", res)
+		require.True(t, res.StatusCode >= 200 && res.StatusCode < 300, "Failed API request to get open challenges for blobber id: %s", blobberId)
+		require.NotNil(t, res.Body, "Open challenges API response must not be nil")
+
+		resBody, err := io.ReadAll(res.Body)
+		require.Nil(t, err, "error reading response body")
+		var openChallengesBefore apimodel.BlobberChallenge
+		err = json.Unmarshal(resBody, &openChallengesBefore)
+		require.Nil(t, err, "error unmarshalling response body")
+
+		output, err = moveFile(t, configPath, map[string]interface{}{
+			"allocation": allocationId,
+			"remotepath": remoteFilepath,
+			"destpath":   "/dest/",
+		}, true)
+		require.Nil(t, err, "error moving file", strings.Join(output, "\n"))
+
+		cliutils.Wait(t, 30*time.Second)
+
+		res, err = apiGetOpenChallenges(sharderBaseUrl, blobberId)
+		require.Nil(t, err, "error getting challenges", res)
+		require.True(t, res.StatusCode >= 200 && res.StatusCode < 300, "Failed API request to get open challenges for blobber id: %s", blobberId)
+		require.NotNil(t, res.Body, "Open challenges API response must not be nil")
+
+		resBody, err = io.ReadAll(res.Body)
+		require.Nil(t, err, "error reading response body")
+		var openChallengesAfter apimodel.BlobberChallenge
+		err = json.Unmarshal([]byte(resBody), &openChallengesAfter)
+		require.Nil(t, err, "error unmarshalling response body")
+
 		require.Greater(t, len(openChallengesAfter.Challenges), len(openChallengesBefore.Challenges))
 	})
 }
