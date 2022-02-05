3aad7e671b29417409095e672095b979cbce629b
dmdv <dimos-d@yandex.ru>
2022-02-06 02:23:55 +0300

    WZCN bridge system-tests (#158)
    * bridge-owner-init
    
    * linting
    
    * golint
    
    * Client Bridge Config
    
    * eth-register-account
    
    * linting
    
    * linting
    
    * List Ethereum accounts
    
    * bridge-verify
    
    * Refactoring
    
    * Refactoring
    
    * bridge-list-auth
    
    * bridge-burn-eth
    
    * bridge-burn-zcn
    
    * bridge-get-eth-burn
    
    * refactoring
    
    * bridge-mint-zcn
    bridge-mint-wzcn
    
    * renaming
    
    * renaming
    
    * Get ZCN burn ticket
    
    * Squashing similar tests
    
    * Refactoring
    
    * Refactoring
    
    * Refactoring
    
    * Refactoring, deleted file
    
    * Added retries
    
    * fix
    
    * Refactoring, new tests
    
    * Fixed configuration tests
    
    * Fixed some commands, updated to the latest zwallet version
    
    * Skipped some tests until deploy/chain issues is solved
    
    * Fixed linting
    
    * testing failing tests
    
    * go mod tidy
    
    * Removed config.yaml
    
    * remove abs
    
    * change getConfigDir acc. to system-tests
    
    * add config and configDir flags in commands
    
    * correct defaultpath
    
    * fix bridgeburn test
    
    Co-authored-by: Dmitry <dmitry@centralex.io>
    Co-authored-by: Kishan-Dhakan <kishandhakan17@gmail.com>

---
+++ tests/cli_tests/zwalletcli_zcnbridge_mint_test.go
@@ -0,0 +1,53 @@
+package cli_tests
+
+import (
+	"fmt"
+	"strings"
+	"testing"
+	"time"
+
+	cliutils "github.com/0chain/system_test/internal/cli/util"
+	"github.com/stretchr/testify/require"
+)
+
+const (
+	TransactionHash = "0x607abfece03c42afb446c77ffc81783f2d8fb614774d3fe241eb54cb52943f95"
+)
+
+func TestBridgeMint(t *testing.T) {
+	t.Parallel()
+
+	var zwallet = func(cmd, hash, help string) ([]string, error) {
+		t.Logf("%s, hash: %s", help, hash)
+		run := fmt.Sprintf("./zwallet %s --hash %s", cmd, hash)
+		return cliutils.RunCommand(t, run, 3, time.Second*5)
+	}
+
+	t.Run("Mint WZCN tokens", func(t *testing.T) {
+		t.Skip("Skipping due to deployment issue")
+		t.Parallel()
+
+		output, err := zwallet(
+			"bridge-mint-wzcn",
+			TransactionHash,
+			"Mint WZCN tokens using ZCN burn ticket",
+		)
+
+		require.Nil(t, err, "error: %s", strings.Join(output, "\n"))
+		require.Contains(t, output[len(output)-1], "Verification [OK]")
+	})
+
+	t.Run("Mint ZCN tokens", func(t *testing.T) {
+		t.Skip("Skipping due to deployment issue")
+		t.Parallel()
+
+		output, err := zwallet(
+			"bridge-mint-zcn",
+			TransactionHash,
+			"Mint ZCN tokens using WZCN burn ticket",
+		)
+
+		require.Nil(t, err, "error: %s", strings.Join(output, "\n"))
+		require.Contains(t, output[len(output)-1], "Verification [OK]")
+	})
+}
