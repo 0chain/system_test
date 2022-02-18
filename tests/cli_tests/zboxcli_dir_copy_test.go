c989e26fd5b91d2d6335997d8737cfceabd3223b
Mohsen Mirghafouri <mohsen.no1@gmail.com>
2022-02-18 21:04:05 +0200

    Copy directory test

--- tests/cli_tests/zboxcli_dir_copy_test.go
+++ tests/cli_tests/zboxcli_dir_copy_test.go
@@ -10,10 +10,10 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
-func TestMoveDir(t *testing.T) {
+func TestCopyDir(t *testing.T) {
 	t.Parallel()
 
-	t.Run("move root dir", func(t *testing.T) {
+	t.Run("copy root dir", func(t *testing.T) {
 		t.Parallel()
 
 		allocationID := setupAllocation(t, configPath)
@@ -24,12 +24,6 @@ func TestMoveDir(t *testing.T) {
 		require.Len(t, output, 1)
 		require.Equal(t, dirname+" directory created", output[0])
 
-		dirname2 := "/rootdir2"
-		output, err = createDir(t, configPath, allocationID, dirname2, true)
-		require.Nil(t, err, "Unexpected create dir failure %s", strings.Join(output, "\n"))
-		require.Len(t, output, 1)
-		require.Equal(t, dirname2+" directory created", output[0])
-
 		output, err = listAll(t, configPath, allocationID, true)
 		require.Nil(t, err, "Unexpected list all failure %s", strings.Join(output, "\n"))
 		require.Len(t, output, 1)
@@ -38,18 +32,17 @@ func TestMoveDir(t *testing.T) {
 		err = json.Unmarshal([]byte(output[0]), &directories)
 		require.Nil(t, err, "Error deserializing JSON string `%s`: %v", strings.Join(output, "\n"), err)
 
-		require.Len(t, directories, 2, "Expecting directories created. Possibly `createdir` failed to create on blobbers (error suppressed) or unable to `list-all` from 3/4 blobbers")
+		require.Len(t, directories, 1, "Expecting rootdir created. Possibly `createdir` failed to create on blobbers (error suppressed) or unable to `list-all` from 3/4 blobbers")
 		require.Contains(t, directories, climodel.AllocationFile{Name: "rootdir", Path: "/rootdir", Type: "d"}, "rootdir expected to be created")
-		require.Contains(t, directories, climodel.AllocationFile{Name: "rootdir2", Path: "/rootdir2", Type: "d"}, "rootdir2 expected to be created")
 
-		output, err = moveFile(t, configPath, map[string]interface{}{
+		output, err = copyFile(t, configPath, map[string]interface{}{
 			"allocation": allocationID,
 			"remotepath": dirname,
-			"destpath":   dirname2,
+			"destpath":   "/rootdir_copy",
 		}, true)
 		require.Nil(t, err, strings.Join(output, "\n"))
 		require.Len(t, output, 1)
-		require.Equal(t, fmt.Sprintf(dirname+" moved"), output[0])
+		require.Equal(t, fmt.Sprintf(dirname+" copied"), output[0])
 
 		output, err = listAll(t, configPath, allocationID, true)
 		require.Nil(t, err, "Unexpected list all failure %s", strings.Join(output, "\n"))
@@ -58,8 +51,8 @@ func TestMoveDir(t *testing.T) {
 		err = json.Unmarshal([]byte(output[0]), &directories)
 		require.Nil(t, err, "Error deserializing JSON string `%s`: %v", strings.Join(output, "\n"), err)
 
-		require.Len(t, directories, 1, "Expecting directories created. Possibly `createdir` failed to create on blobbers (error suppressed) or unable to `list-all` from 3/4 blobbers")
-		require.Contains(t, directories, climodel.AllocationFile{Name: "rootdir", Path: "/rootdir2/rootdir", Type: "d"}, "rootdir expected to be moved to rootdir2 directory")
-		require.NotContains(t, directories, climodel.AllocationFile{Name: "rootdir", Path: "/rootdir", Type: "d"}, "rootdir expected to be moved to rootdir2 directory")
+		require.Len(t, directories, 2, "Expecting directories created. Possibly `createdir` failed to create on blobbers (error suppressed) or unable to `list-all` from 3/4 blobbers")
+		require.Contains(t, directories, climodel.AllocationFile{Name: "rootdir", Path: "/rootdir", Type: "d"})
+		require.Contains(t, directories, climodel.AllocationFile{Name: "rootdir_copy", Path: "/rootdir_copy", Type: "d"})
 	})
 }
