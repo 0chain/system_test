64da24910a5795839cd2e7a31ec19281d6f1f3c6
Kishan-Dhakan <kishandhakan17@gmail.com>
2022-02-25 00:53:48 +0530

    stake blobbers in TestMain

--- tests/cli_tests/main_test.go
+++ tests/cli_tests/main_test.go
@@ -1,11 +1,14 @@
 package cli_tests
 
 import (
+	"encoding/json"
+	"fmt"
 	"os"
 	"path/filepath"
 	"strings"
 	"testing"
 
+	climodel "github.com/0chain/system_test/internal/cli/model"
 	cliutils "github.com/0chain/system_test/internal/cli/util"
 )
 
@@ -71,6 +74,72 @@ func TestMain(m *testing.M) {
 		}
 	}
 
+	// Blobber-stake
+	walletName := "blobber-stake-wallet_wallet.json"
+	_, err := cliutils.RunCommandWithoutRetry("./zbox register --silent " + "--wallet " + walletName + " --configDir ./config --config " + configPath)
+	ExitWithError(err)
+
+	blobbers := []climodel.BlobberInfo{}
+	output, err := cliutils.RunCommandWithoutRetry(fmt.Sprintf("./zbox ls-blobbers --json --silent --wallet %s_wallet.json --configDir ./config --config %s", walletName, configPath))
+	ExitWithError(err)
+
+	err = json.Unmarshal([]byte(output[0]), &blobbers)
+	ExitWithError(err)
+
+	if len(blobbers) <= 0 {
+		cliutils.Logger.Error("No blobbers found in blobber list")
+		os.Exit(1)
+	}
+
+	if len(blobbers) < 10 {
+		_, err = cliutils.RunCommandWithoutRetry(fmt.Sprintf("./zwallet faucet --methodName pour --tokens %d --input {} --silent --wallet %s --configDir ./config --config %s",
+			len(blobbers),
+			walletName,
+			configPath))
+		ExitWithError(err)
+	} else {
+		for i := 0; i < len(blobbers); i++ {
+			_, err = cliutils.RunCommandWithoutRetry(fmt.Sprintf("./zwallet faucet --methodName pour --tokens %d --input {} --silent --wallet %s --configDir ./config --config %s",
+				1,
+				walletName,
+				configPath))
+			ExitWithError(err)
+		}
+	}
+
+	cliutils.Logger.Info("Enough balance. Moving to staking now...")
+
+	done := 0
+	for done < len(blobbers) {
+		blobber := blobbers[done]
+		cliutils.Logger.Info("Getting stake pool info for :", blobber.Id)
+		output, err = cliutils.RunCommandWithoutRetry(fmt.Sprintf("./zbox sp-info %s --silent --wallet %s_wallet.json --configDir ./config --config %s", "--blobber_id "+blobber.Id+" --json", walletName, configPath))
+		ExitWithError(err)
+
+		stakePool := climodel.StakePoolInfo{}
+		err = json.Unmarshal([]byte(output[0]), &stakePool)
+		ExitWithError(err)
+
+		if stakePool.Balance < 1 {
+			output, err = cliutils.RunCommandWithoutRetry(fmt.Sprintf("./zbox sp-lock %s --silent --wallet %s_wallet.json --configDir ./config --config %s", " --blobber_id "+blobber.Id+" --json ", walletName, configPath))
+			if err != nil {
+				ExitWithError(err)
+			}
+			cliutils.Logger.Info("Successfully staked for blobber ", blobber.Id, " with output ", output)
+			done++
+		} else {
+			fmt.Println("Blobber already have stake pool, URL : ", blobber.Id, " poolID : ", stakePool.ID, " amount : ", stakePool.Balance)
+			done++
+		}
+	}
+
 	exitRun := m.Run()
 	os.Exit(exitRun)
+}
+
+func ExitWithError(err error) {
+	if err != nil {
+		cliutils.Logger.Error(err)
+		os.Exit(1)
+	}
 }
