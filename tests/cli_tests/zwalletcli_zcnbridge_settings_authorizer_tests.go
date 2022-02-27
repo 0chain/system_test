0960b73f3a24459cc4984b2a4193f04a8e9b863d
DmitryD <dima.dyachkov@gmail.com>
2022-02-28 02:36:40 +0300

    lint fix

--- tests/cli_tests/zwalletcli_zcnbridge_settings_authorizer_tests.go
+++ tests/cli_tests/zwalletcli_zcnbridge_settings_authorizer_tests.go
@@ -1,19 +1,14 @@
 package cli_tests
 
-import (
-	"fmt"
-	"strings"
-)
-
-func createParams(params map[string]interface{}) string {
-	var builder strings.Builder
-
-	for k, v := range params {
-		if v == nil {
-			_, _ = builder.WriteString(fmt.Sprintf("--%s ", k))
-		} else {
-			_, _ = builder.WriteString(fmt.Sprintf("--%s %v ", k, v))
-		}
-	}
-	return strings.TrimSpace(builder.String())
-}
+//func createParams(params map[string]interface{}) string {
+//	var builder strings.Builder
+//
+//	for k, v := range params {
+//		if v == nil {
+//			_, _ = builder.WriteString(fmt.Sprintf("--%s ", k))
+//		} else {
+//			_, _ = builder.WriteString(fmt.Sprintf("--%s %v ", k, v))
+//		}
+//	}
+//	return strings.TrimSpace(builder.String())
+//}
