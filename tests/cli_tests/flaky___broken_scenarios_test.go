ee43bead40e5701aa9ab473813ca6b5617b1de00
Kishan-Dhakan <kishandhakan17@gmail.com>
2022-04-24 22:26:51 +0530

    move owner update to broken cases

--- tests/cli_tests/flaky___broken_scenarios_test.go
+++ tests/cli_tests/flaky___broken_scenarios_test.go
@@ -642,6 +642,50 @@ func Test___FlakyBrokenScenarios(t *testing.T) {
 		// require.Equal(t, newOwnerWallet.ClientID, cfgAfter[ownerKey], "new value [%s] for owner was not set", newOwnerWallet.ClientID)
 	})
 
+	t.Run("should allow update of owner", func(t *testing.T) {
+		t.Parallel()
+
+		if _, err := os.Stat("./config/" + scOwnerWallet + "_wallet.json"); err != nil {
+			t.Skipf("SC owner wallet located at %s is missing", "./config/"+scOwnerWallet+"_wallet.json")
+		}
+
+		// unused wallet, just added to avoid having the creating new wallet outputs
+		output, err := registerWallet(t, configPath)
+		require.Nil(t, err, "Failed to register wallet", strings.Join(output, "\n"))
+
+		// register SC owner wallet
+		output, err = registerWalletForName(t, configPath, scOwnerWallet)
+		require.Nil(t, err, "Failed to register wallet", strings.Join(output, "\n"))
+
+		ownerKey := "owner_id"
+		newOwner := "22e412a350036944f9762a3d6b5687ee4f64d20d2cf6faf2571a490defd10f17"
+		oldOwner := "1746b06bb09f55ee01b33b5e2e055d6cc7a900cb57c0a3a5eaabb8a0e7745802"
+
+		output, err = updateFaucetSCConfig(t, scOwnerWallet, map[string]interface{}{
+			"keys":   ownerKey,
+			"values": newOwner,
+		}, true)
+		require.Nil(t, err, strings.Join(output, "\n"))
+		require.Len(t, output, 2, strings.Join(output, "\n"))
+		require.Equal(t, "faucet smart contract settings updated", output[0], strings.Join(output, "\n"))
+
+		output, err = getFaucetSCConfig(t, configPath, true)
+		require.Nil(t, err, strings.Join(output, "\n"))
+		require.Greater(t, len(output), 0, strings.Join(output, "\n"))
+
+		cfgAfter, _ := keyValuePairStringToMap(t, output)
+
+		require.Equal(t, newOwner, cfgAfter[ownerKey], "new value [%s] for owner was not set", newOwner)
+
+		output, err = updateFaucetSCConfig(t, scOwnerWallet, map[string]interface{}{
+			"keys":   ownerKey,
+			"values": oldOwner,
+		}, true)
+		require.Nil(t, err, strings.Join(output, "\n"))
+		require.Len(t, output, 2, strings.Join(output, "\n"))
+		require.Equal(t, "faucet smart contract settings updated", output[0], strings.Join(output, "\n"))
+	})
+
 	// FIXME: Commented out because these cases hang the broken test suite till timeout
 
 	// FIXME: add param validation
