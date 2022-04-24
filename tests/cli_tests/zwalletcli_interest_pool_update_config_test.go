d883e37d8cc2c2fcc1ab6ad148ae1ab76f2cbe8e
Kishan-Dhakan <kishandhakan17@gmail.com>
2022-04-25 04:55:19 +0530

    move owner update to broken cases

--- tests/cli_tests/zwalletcli_interest_pool_update_config_test.go
+++ tests/cli_tests/zwalletcli_interest_pool_update_config_test.go
@@ -89,50 +89,6 @@ func TestInterestPoolUpdateConfig(t *testing.T) {
 		require.Nil(t, err, "faucet execution failed", strings.Join(output, "\n"))
 	})
 
-	t.Run("should allow update of owner", func(t *testing.T) {
-		t.Parallel()
-
-		if _, err := os.Stat("./config/" + scOwnerWallet + "_wallet.json"); err != nil {
-			t.Skipf("SC owner wallet located at %s is missing", "./config/"+scOwnerWallet+"_wallet.json")
-		}
-
-		ownerKey := "owner_id"
-		newOwner := "22e412a350036944f9762a3d6b5687ee4f64d20d2cf6faf2571a490defd10f17"
-		oldOwner := "1746b06bb09f55ee01b33b5e2e055d6cc7a900cb57c0a3a5eaabb8a0e7745802"
-
-		// unused wallet, just added to avoid having the creating new wallet outputs
-		output, err := registerWallet(t, configPath)
-		require.Nil(t, err, "Failed to register wallet", strings.Join(output, "\n"))
-
-		// register SC owner wallet
-		output, err = registerWalletForName(t, configPath, scOwnerWallet)
-		require.Nil(t, err, "Failed to register wallet", strings.Join(output, "\n"))
-
-		output, err = updateInterestPoolSCConfig(t, scOwnerWallet, map[string]interface{}{
-			"keys":   ownerKey,
-			"values": newOwner,
-		}, true)
-		require.Nil(t, err, strings.Join(output, "\n"))
-		require.Len(t, output, 2, strings.Join(output, "\n"))
-		require.Equal(t, "interest pool smart contract settings updated", output[0], strings.Join(output, "\n"))
-
-		output, err = getInterestPoolSCConfig(t, configPath, true)
-		require.Nil(t, err, strings.Join(output, "\n"))
-		require.Greater(t, len(output), 0, strings.Join(output, "\n"))
-
-		cfgAfter, _ := keyValuePairStringToMap(t, output)
-
-		require.Equal(t, newOwner, cfgAfter[ownerKey], "new value [%s] for owner was not set", newOwner)
-
-		output, err = updateInterestPoolSCConfig(t, scOwnerWallet, map[string]interface{}{
-			"keys":   ownerKey,
-			"values": oldOwner,
-		}, true)
-		require.Nil(t, err, strings.Join(output, "\n"))
-		require.Len(t, output, 2, strings.Join(output, "\n"))
-		require.Equal(t, "interest pool smart contract settings updated", output[0], strings.Join(output, "\n"))
-	})
-
 	// FIXME should fail given config key value is not valid and not actually updated
 	t.Run("update min_lock to invalid value should fail", func(t *testing.T) {
 		t.Parallel()
@@ -158,54 +114,6 @@ func TestInterestPoolUpdateConfig(t *testing.T) {
 		}, false)
 		require.Error(t, err)
 		require.True(t, strings.Contains(output[0], "failed to update variables: cannot conver key min_lock"), output[0])
-	})
-
-	// FIXME should fail given config key value is not valid and not actually updated
-	t.Run("update owner and update min_lock after with old owner should fail", func(t *testing.T) {
-		t.Parallel()
-
-		if _, err := os.Stat("./config/" + scOwnerWallet + "_wallet.json"); err != nil {
-			t.Skipf("SC owner wallet located at %s is missing", "./config/"+scOwnerWallet+"_wallet.json")
-		}
-
-		ownerKey := "owner_id"
-		newOwner := "22e412a350036944f9762a3d6b5687ee4f64d20d2cf6faf2571a490defd10f17"
-		oldOwner := "1746b06bb09f55ee01b33b5e2e055d6cc7a900cb57c0a3a5eaabb8a0e7745802"
-
-		configKey := "min_lock"
-		newValue := "8"
-
-		// unused wallet, just added to avoid having the creating new wallet outputs
-		output, err := registerWallet(t, configPath)
-		require.Nil(t, err, "Failed to register wallet", strings.Join(output, "\n"))
-
-		// register SC owner wallet
-		output, err = registerWalletForName(t, configPath, scOwnerWallet)
-		require.Nil(t, err, "Failed to register wallet", strings.Join(output, "\n"))
-
-		output, err = updateInterestPoolSCConfig(t, scOwnerWallet, map[string]interface{}{
-			"keys":   ownerKey,
-			"values": newOwner,
-		}, false)
-		require.Nil(t, err, strings.Join(output, "\n"))
-		require.Len(t, output, 2, strings.Join(output, "\n"))
-		require.Equal(t, "interest pool smart contract settings updated", output[0], strings.Join(output, "\n"))
-
-		output, err = updateInterestPoolSCConfig(t, scOwnerWallet, map[string]interface{}{
-			"keys":   configKey,
-			"values": newValue,
-		}, false)
-		require.Nil(t, err, strings.Join(output, "\n"))
-		require.Len(t, output, 1, strings.Join(output, "\n"))
-		require.Equal(t, "fatal:{\"error\": \"verify transaction failed\"}", output[0], strings.Join(output, "\n"))
-
-		output, err = updateInterestPoolSCConfig(t, scOwnerWallet, map[string]interface{}{
-			"keys":   ownerKey,
-			"values": oldOwner,
-		}, false)
-		require.Nil(t, err, strings.Join(output, "\n"))
-		require.Len(t, output, 2, strings.Join(output, "\n"))
-		require.Equal(t, "interest pool smart contract settings updated", output[0], strings.Join(output, "\n"))
 	})
 
 	t.Run("update by non-smartcontract owner should fail", func(t *testing.T) {
