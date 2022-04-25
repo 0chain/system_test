e34c43058bca7f415bd537d055850ee1faff0f12
Kishan-Dhakan <kishandhakan17@gmail.com>
2022-04-25 06:06:54 +0530

    invalid configkey cases

--- tests/cli_tests/flaky___broken_scenarios_test.go
+++ tests/cli_tests/flaky___broken_scenarios_test.go
@@ -960,6 +960,96 @@ func Test___FlakyBrokenScenarios(t *testing.T) {
 		require.Equal(t, "interest pool smart contract settings updated", output[0], strings.Join(output, "\n"))
 	})
 
+	// FIXME: interest rate is invalid config key
+	t.Run("should allow update of interest_rate", func(t *testing.T) {
+		t.Parallel()
+
+		if _, err := os.Stat("./config/" + scOwnerWallet + "_wallet.json"); err != nil {
+			t.Skipf("SC owner wallet located at %s is missing", "./config/"+scOwnerWallet+"_wallet.json")
+		}
+
+		configKey := "interest_rate"
+		newValue := "0.1"
+
+		// unused wallet, just added to avoid having the creating new wallet outputs
+		output, err := registerWallet(t, configPath)
+		require.Nil(t, err, "Failed to register wallet", strings.Join(output, "\n"))
+
+		// register SC owner wallet
+		output, err = registerWalletForName(t, configPath, scOwnerWallet)
+		require.Nil(t, err, "Failed to register wallet", strings.Join(output, "\n"))
+
+		output, err = getMinerSCConfig(t, configPath, true)
+		require.Nil(t, err, strings.Join(output, "\n"))
+		require.Greater(t, len(output), 0, strings.Join(output, "\n"))
+
+		cfgBefore, _ := keyValuePairStringToMap(t, output)
+
+		// ensure revert in config is run regardless of test result
+		defer func() {
+			oldValue := cfgBefore[configKey]
+			output, err = updateMinerSCConfig(t, scOwnerWallet, map[string]interface{}{
+				"keys":   configKey,
+				"values": oldValue,
+			}, true)
+			require.Nil(t, err, strings.Join(output, "\n"))
+			require.Len(t, output, 2, strings.Join(output, "\n"))
+			// FIXME should say miner instead of storagesc
+			require.Equal(t, "storagesc smart contract settings updated", output[0], strings.Join(output, "\n"))
+			require.Regexp(t, `Hash: [0-9a-f]+`, output[1], strings.Join(output, "\n"))
+		}()
+
+		output, err = updateMinerSCConfig(t, scOwnerWallet, map[string]interface{}{
+			"keys":   configKey,
+			"values": newValue,
+		}, true)
+		require.Nil(t, err, strings.Join(output, "\n"))
+		require.Len(t, output, 2, strings.Join(output, "\n"))
+		// FIXME should say miner instead of storagesc
+		require.Equal(t, "storagesc smart contract settings updated", output[0], strings.Join(output, "\n"))
+		require.Regexp(t, `Hash: [0-9a-f]+`, output[1], strings.Join(output, "\n"))
+
+		output, err = getMinerSCConfig(t, configPath, true)
+		require.Nil(t, err, strings.Join(output, "\n"))
+		require.Greater(t, len(output), 0, strings.Join(output, "\n"))
+
+		cfgAfter, _ := keyValuePairStringToMap(t, output)
+
+		require.Equal(t, newValue, cfgAfter[configKey], "new value %s for config was not set", newValue, configKey)
+
+		// test transaction to verify chain is still working
+		output, err = executeFaucetWithTokens(t, configPath, 1)
+		require.Nil(t, err, "faucet execution failed", strings.Join(output, "\n"))
+	})
+
+	// FIXME: interest rate is not a valid configkey
+	t.Run("update interest_rate to invalid value should fail", func(t *testing.T) {
+		t.Parallel()
+
+		if _, err := os.Stat("./config/" + scOwnerWallet + "_wallet.json"); err != nil {
+			t.Skipf("SC owner wallet located at %s is missing", "./config/"+scOwnerWallet+"_wallet.json")
+		}
+
+		configKey := "interest_rate"
+		newValue := "x"
+
+		// unused wallet, just added to avoid having the creating new wallet outputs
+		output, err := registerWallet(t, configPath)
+		require.Nil(t, err, "Failed to register wallet", strings.Join(output, "\n"))
+
+		// register SC owner wallet
+		output, err = registerWalletForName(t, configPath, scOwnerWallet)
+		require.Nil(t, err, "Failed to register wallet", strings.Join(output, "\n"))
+
+		output, err = updateMinerSCConfig(t, scOwnerWallet, map[string]interface{}{
+			"keys":   configKey,
+			"values": newValue,
+		}, false)
+		require.NotNil(t, err, strings.Join(output, "\n"))
+		require.Len(t, output, 1, strings.Join(output, "\n"))
+		require.Equal(t, "update_settings: unsupported key interest_rate", output[0], strings.Join(output, "\n"))
+	})
+
 	// FIXME: Commented out because these cases hang the broken test suite till timeout
 
 	// FIXME: add param validation
