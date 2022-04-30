ae3032a5ab997f257f97c2655d302eb9571efb3a
Piers Shepperson <piers.shepperson@btinternet.com>
2022-04-30 12:22:26 +0100

    unskip

--- tests/cli_tests/block_rewards_test.go
+++ tests/cli_tests/block_rewards_test.go
@@ -173,7 +173,6 @@ func TestBlockRewards(t *testing.T) { // nolint:gocyclo // team preference is to
 	})
 
 	t.Run("Sharder share on block fees and rewards", func(t *testing.T) {
-		t.Skip("piers")
 		output, err := registerWallet(t, configPath)
 		require.Nil(t, err, "registering wallet failed", strings.Join(output, "\n"))
 
--- tests/cli_tests/zwalletcli_miner_stake_test.go
+++ tests/cli_tests/zwalletcli_miner_stake_test.go
@@ -89,7 +89,6 @@ func TestMinerStake(t *testing.T) {
 	})
 
 	t.Run("Multiple stakes against a miner should create multiple pools", func(t *testing.T) {
-		//t.Skip("piers")
 		output, err := registerWallet(t, configPath)
 		require.Nil(t, err, "error registering wallet", strings.Join(output, "\n"))
 
