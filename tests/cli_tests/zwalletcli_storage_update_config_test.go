746f280a9cc6c35ccd35a8ff0007052d023da344
Kishan-Dhakan <kishandhakan17@gmail.com>
2022-05-01 23:17:09 +0530

    add cleanup

--- tests/cli_tests/zwalletcli_storage_update_config_test.go
+++ tests/cli_tests/zwalletcli_storage_update_config_test.go
@@ -17,6 +17,52 @@ func TestStorageUpdateConfig(t *testing.T) {
 		t.Skipf("SC owner wallet located at %s is missing", "./config/"+scOwnerWallet+"_wallet.json")
 	}
 
+	t.Run("should allow update of owner", func(t *testing.T) {
+		ownerKey := "owner_id"
+
+		//nolint:revive
+		// newOwnerWallet := `{"client_id":"4b92987d710b7d2c12c5ad31bef72bb91f6ad9e8aab3c14b6acad843dc0085df","client_key":"e39e41b5ca69dda58f2e06aa8bfdad5c14b750504e03c71defadd031acff3a042f9c136158894284537918ae12a0c7699883254a16fc50c79207ff3b1cc3e798","keys":[{"public_key":"e39e41b5ca69dda58f2e06aa8bfdad5c14b750504e03c71defadd031acff3a042f9c136158894284537918ae12a0c7699883254a16fc50c79207ff3b1cc3e798","private_key":"7074efffda1faa7b9ce0a79adbca18ba182dafdb50fe7ff0ee83ac5ee2abcc0b"}],"mnemonics":"next concert hedgehog code tip unfair crunch valid episode shiver label object motion maid slam chef alter any kingdom ten search fortune test stem","version":"1.0","date_created":"2022-03-31T13:57:30+05:30"}`
+		//nolint:revive
+		// oldOwnerWallet := `{"client_id":"1746b06bb09f55ee01b33b5e2e055d6cc7a900cb57c0a3a5eaabb8a0e7745802","client_key":"7b630ba670dac2f22d43c2399b70eff378689a53ee03ea20957bb7e73df016200fea410ba5102558b0c39617e5afd2c1843b161a1dedec15e1ab40543a78a518","keys":[{"public_key":"7b630ba670dac2f22d43c2399b70eff378689a53ee03ea20957bb7e73df016200fea410ba5102558b0c39617e5afd2c1843b161a1dedec15e1ab40543a78a518","private_key":"c06b6f6945ba02d5a3be86b8779deca63bb636ce7e46804a479c50e53c864915"}],"mnemonics":"cactus panther essence ability copper fox wise actual need cousin boat uncover ride diamond group jacket anchor current float rely tragic omit child payment","version":"1.0","date_created":"2021-08-04 18:53:56.949069945 +0100 BST m=+0.018986002"}`
+		// newOwner := "4b92987d710b7d2c12c5ad31bef72bb91f6ad9e8aab3c14b6acad843dc0085df"
+		oldOwner := "1746b06bb09f55ee01b33b5e2e055d6cc7a900cb57c0a3a5eaabb8a0e7745802"
+
+		output, err := registerWallet(t, configPath)
+		require.Nil(t, err, "Failed to register wallet", strings.Join(output, "\n"))
+
+		newOwnerWallet, err := getWallet(t, configPath)
+		require.Nil(t, err, "error fetching wallet")
+
+		// register SC owner wallet
+		output, err = registerWalletForName(t, configPath, scOwnerWallet)
+		require.Nil(t, err, "Failed to register wallet", strings.Join(output, "\n"))
+
+		output, err = updateStorageSCConfig(t, scOwnerWallet, map[string]interface{}{
+			"keys":   ownerKey,
+			"values": newOwnerWallet.ClientID,
+		}, true)
+		require.Nil(t, err, strings.Join(output, "\n"))
+		require.Len(t, output, 2, strings.Join(output, "\n"))
+		require.Equal(t, "storagesc smart contract settings updated", output[0], strings.Join(output, "\n"))
+
+		cliutils.Wait(t, 15*time.Second)
+
+		output, err = getStorageSCConfig(t, configPath, true)
+		require.Nil(t, err, strings.Join(output, "\n"))
+		require.Greater(t, len(output), 0, strings.Join(output, "\n"))
+		cfgAfter, _ := keyValuePairStringToMap(t, output)
+		require.Equal(t, newOwnerWallet.ClientID, cfgAfter[ownerKey], "new value [%s] for owner was not set", newOwnerWallet.ClientID)
+
+		t.Cleanup(func() {
+			output, err := updateStorageSCConfig(t, escapedTestName(t), map[string]interface{}{
+				"keys":   ownerKey,
+				"values": oldOwner,
+			}, true)
+			require.Nil(t, err, strings.Join(output, "\n"))
+			require.Len(t, output, 2, strings.Join(output, "\n"))
+		})
+	})
+
 	t.Run("update by non-smartcontract owner should fail", func(t *testing.T) {
 		configKey := "max_read_price"
 		newValue := "110"
