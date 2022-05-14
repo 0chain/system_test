4ad7a13503579e69dc4b4b078883c29ffb0d8103
Kishan-Dhakan <kishandhakan17@gmail.com>
2022-05-14 14:34:27 +0530

    replace read write price with inepsilon

--- tests/cli_tests/zboxcli_blobber_config_update_test.go
+++ tests/cli_tests/zboxcli_blobber_config_update_test.go
@@ -302,7 +302,7 @@ func TestBlobberConfigUpdate(t *testing.T) {
 		err = json.Unmarshal([]byte(output[0]), &finalBlobberInfo)
 		require.Nil(t, err, strings.Join(output, "\n"))
 
-		require.Equal(t, newReadPrice, intToZCN(finalBlobberInfo.Terms.Read_price))
+		require.InEpsilon(t, newReadPrice, intToZCN(finalBlobberInfo.Terms.Read_price), 1e-10)
 	})
 
 	t.Run("update blobber write price should work", func(t *testing.T) {
@@ -325,7 +325,7 @@ func TestBlobberConfigUpdate(t *testing.T) {
 		err = json.Unmarshal([]byte(output[0]), &finalBlobberInfo)
 		require.Nil(t, err, strings.Join(output, "\n"))
 
-		require.Equal(t, newWritePrice, intToZCN(finalBlobberInfo.Terms.Write_price))
+		require.InEpsilon(t, newWritePrice, intToZCN(finalBlobberInfo.Terms.Write_price), 1e-10)
 	})
 
 	t.Run("update all params at once should work", func(t *testing.T) {
@@ -356,9 +356,9 @@ func TestBlobberConfigUpdate(t *testing.T) {
 		err = json.Unmarshal([]byte(output[0]), &finalBlobberInfo)
 		require.Nil(t, err, strings.Join(output, "\n"))
 
-		require.Equal(t, newWritePrice, intToZCN(finalBlobberInfo.Terms.Write_price))
+		require.InEpsilon(t, newWritePrice, intToZCN(finalBlobberInfo.Terms.Write_price), 1e-10)
 		require.Equal(t, newServiceCharge, finalBlobberInfo.StakePoolSettings.ServiceCharge)
-		require.Equal(t, newReadPrice, intToZCN(finalBlobberInfo.Terms.Read_price))
+		require.InEpsilon(t, newReadPrice, intToZCN(finalBlobberInfo.Terms.Read_price), 1e-10)
 		require.Equal(t, newNumberOfDelegates, finalBlobberInfo.StakePoolSettings.NumDelegates)
 		require.Equal(t, newMaxOfferDuration, finalBlobberInfo.Terms.Max_offer_duration)
 		require.Equal(t, newCapacity, finalBlobberInfo.Capacity)
