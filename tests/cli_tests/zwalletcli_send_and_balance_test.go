4593589ec628afbc3bd847b976be64ef4df4733a
Lz <imlangzi@qq.com>
2022-05-25 17:10:59 +0800

    fix(zwallet/send): fixec assertion on send cmd (#271)
    * fix(zwallet/send): fixec assertion on send cmd
    
    * fix(zwallet/send): fixec assertion on send cmd
    
    * revert config
    
    * remove self assignment
    
    * add send txn model
    
    * add json test case
    
    * replace contains with regex
    
    Co-authored-by: Kishan-Dhakan <kishandhakan17@gmail.com>

--- tests/cli_tests/zwalletcli_send_and_balance_test.go
+++ tests/cli_tests/zwalletcli_send_and_balance_test.go
@@ -40,12 +40,43 @@ func TestSendAndBalance(t *testing.T) {
 		output, err = executeFaucetWithTokens(t, configPath, 1)
 		require.Nil(t, err, "Unexpected faucet failure", strings.Join(output, "\n"))
 
-		output, err = sendZCN(t, configPath, target.ClientID, "1", "rich description", true)
+		output, err = sendZCN(t, configPath, target.ClientID, "1", "rich description", createParams(map[string]interface{}{}), true)
 		require.Nil(t, err, "Unexpected send failure", strings.Join(output, "\n"))
 
 		require.Len(t, output, 1)
-		require.Equal(t, "Send tokens success", output[0])
+		require.Regexp(t, regexp.MustCompile("Send tokens success:  [a-f0-9]{64}"), output[0])
 		// cannot verify transaction payload at this moment due to transaction hash not being printed.
+	})
+
+	t.Run("Send with json flag", func(t *testing.T) {
+		t.Parallel()
+
+		targetWallet := escapedTestName(t) + "_TARGET"
+
+		output, err := registerWallet(t, configPath)
+		require.Nil(t, err, "Unexpected register wallet failure", strings.Join(output, "\n"))
+
+		output, err = registerWalletForName(t, configPath, targetWallet)
+		require.Nil(t, err, "Unexpected register wallet failure", strings.Join(output, "\n"))
+
+		target, err := getWalletForName(t, configPath, targetWallet)
+		require.Nil(t, err, "Error occurred when retrieving target wallet")
+
+		output, err = executeFaucetWithTokens(t, configPath, 1)
+		require.Nil(t, err, "Unexpected faucet failure", strings.Join(output, "\n"))
+
+		output, err = sendZCN(t, configPath, target.ClientID, "1", "rich description", createParams(map[string]interface{}{
+			"json": "",
+		}), true)
+		require.Nil(t, err, "Unexpected send failure", strings.Join(output, "\n"))
+		require.Len(t, output, 1)
+
+		sendTxnOutput := &climodel.SendTransaction{}
+		err = json.Unmarshal([]byte(output[0]), sendTxnOutput)
+		require.Nil(t, err, "error unmarshalling send txn json response")
+		require.Equal(t, "success", sendTxnOutput.Status)
+		require.NotEmpty(t, sendTxnOutput.Txn)
+		require.NotEmpty(t, sendTxnOutput.Nonce)
 	})
 
 	t.Run("Balance checks before and after ZCN sent", func(t *testing.T) {
@@ -78,11 +109,11 @@ func TestSendAndBalance(t *testing.T) {
 		ensureZeroBalance(t, output, err)
 
 		// Transfer ZCN from sender to target wallet
-		output, err = sendZCN(t, configPath, target.ClientID, "1", "", true)
+		output, err = sendZCN(t, configPath, target.ClientID, "1", "", createParams(map[string]interface{}{}), true)
 		require.Nil(t, err, "Unexpected send failure", strings.Join(output, "\n"))
 
 		require.Len(t, output, 1)
-		require.Equal(t, "Send tokens success", output[0])
+		require.Regexp(t, regexp.MustCompile("Send tokens success:  [a-f0-9]{64}"), output[0])
 
 		// After send balance checks
 		output, err = getBalance(t, configPath)
@@ -128,7 +159,7 @@ func TestSendAndBalance(t *testing.T) {
 
 		wantFailureMsg := "Send tokens failed. {\"error\": \"verify transaction failed\"}"
 
-		output, err = sendZCN(t, configPath, target.ClientID, "1", "", false)
+		output, err = sendZCN(t, configPath, target.ClientID, "1", "", createParams(map[string]interface{}{}), false)
 		require.NotNil(t, err, "Expected send to fail", strings.Join(output, "\n"))
 
 		require.Len(t, output, 1)
@@ -148,7 +179,7 @@ func TestSendAndBalance(t *testing.T) {
 		wantFailureMsg := "Send tokens failed. submit transaction failed. {\"code\":\"invalid_request\"," +
 			"\"error\":\"invalid_request: Invalid request (to client id must be a hexadecimal hash)\"}"
 
-		output, err = sendZCN(t, configPath, invalidClientID, "1", "", false)
+		output, err = sendZCN(t, configPath, invalidClientID, "1", "", createParams(map[string]interface{}{}), false)
 		require.NotNil(t, err, "Expected send to fail", strings.Join(output, "\n"))
 
 		require.Len(t, output, 1)
@@ -177,11 +208,11 @@ func TestSendAndBalance(t *testing.T) {
 		require.Nil(t, err, "Unexpected faucet failure", strings.Join(output, "\n"))
 
 		//FIXME: This passes when fees are disabled but should be rejected once they are enabled
-		output, err = sendZCN(t, configPath, target.ClientID, "0", "", true)
+		output, err = sendZCN(t, configPath, target.ClientID, "0", "", createParams(map[string]interface{}{}), true)
 		require.Nil(t, err, "Unexpected send failure", strings.Join(output, "\n"))
 
 		require.Len(t, output, 1)
-		require.Equal(t, "Send tokens success", output[0])
+		require.Regexp(t, regexp.MustCompile("Send tokens success:  [a-f0-9]{64}"), output[0])
 	})
 
 	t.Run("Send attempt to exceeding balance should fail", func(t *testing.T) {
@@ -203,7 +234,7 @@ func TestSendAndBalance(t *testing.T) {
 
 		wantFailureMsg := "Send tokens failed. {\"error\": \"verify transaction failed\"}"
 
-		output, err = sendZCN(t, configPath, target.ClientID, "1.5", "", false)
+		output, err = sendZCN(t, configPath, target.ClientID, "1.5", "", createParams(map[string]interface{}{}), false)
 		require.NotNil(t, err, "Expected send to fail", strings.Join(output, "\n"))
 
 		require.Len(t, output, 1)
@@ -230,7 +261,7 @@ func TestSendAndBalance(t *testing.T) {
 		wantFailureMsg := "Send tokens failed. submit transaction failed. {\"code\":\"invalid_request\"," +
 			"\"error\":\"invalid_request: Invalid request (value must be greater than or equal to zero)\"}"
 
-		output, err = sendZCN(t, configPath, target.ClientID, "-1", "", false)
+		output, err = sendZCN(t, configPath, target.ClientID, "-1", "", createParams(map[string]interface{}{}), false)
 		require.NotNil(t, err, "Expected send to fail", strings.Join(output, "\n"))
 
 		require.Len(t, output, 1)
@@ -263,7 +294,7 @@ func TestSendAndBalance(t *testing.T) {
 		wantFailureMsg := "Send tokens failed. submit transaction failed. {\"code\":\"txn_exceed_max_payload\"," +
 			"\"error\":\"txn_exceed_max_payload: transaction payload exceeds the max payload (98304)\"}"
 
-		output, err = sendZCN(t, configPath, target.ClientID, "1", longDesc, false)
+		output, err = sendZCN(t, configPath, target.ClientID, "1", longDesc, createParams(map[string]interface{}{}), false)
 		require.NotNil(t, err, "Expected send to fail", strings.Join(output, "\n"))
 
 		require.Len(t, output, 1)
@@ -285,7 +316,7 @@ func TestSendAndBalance(t *testing.T) {
 		wantFailureMsg := "Send tokens failed. submit transaction failed. {\"code\":\"invalid_request\"," +
 			"\"error\":\"invalid_request: Invalid request (from and to client should be different)\"}"
 
-		output, err = sendZCN(t, configPath, wallet.ClientID, "1", "", false)
+		output, err = sendZCN(t, configPath, wallet.ClientID, "1", "", createParams(map[string]interface{}{}), false)
 		require.NotNil(t, err, "Expected send to fail", strings.Join(output, "\n"))
 
 		require.Len(t, output, 1)
@@ -293,11 +324,11 @@ func TestSendAndBalance(t *testing.T) {
 	})
 }
 
-func sendZCN(t *testing.T, cliConfigFilename, toClientID, tokens, desc string, retry bool) ([]string, error) {
+func sendZCN(t *testing.T, cliConfigFilename, toClientID, tokens, desc, params string, retry bool) ([]string, error) {
 	t.Logf("Sending ZCN...")
 	cmd := "./zwallet send --silent --tokens " + tokens +
 		" --desc \"" + desc + "\"" +
-		" --to_client_id " + toClientID +
+		" --to_client_id " + toClientID + " " + params +
 		" --wallet " + escapedTestName(t) + "_wallet.json --configDir ./config --config " + cliConfigFilename
 	if retry {
 		return cliutils.RunCommand(t, cmd, 3, time.Second*2)
@@ -362,10 +393,9 @@ func getNodeBalanceFromASharder(t *testing.T, client_id string) *apimodel.Balanc
 	resBody, err := io.ReadAll(res.Body)
 	require.Nil(t, err, "Error reading response body")
 	strBody := string(resBody)
-	strBody = strBody
 	var startBalance apimodel.Balance
 	err = json.Unmarshal(resBody, &startBalance)
-	require.Nil(t, err, "Error deserializing JSON string `%s`: %v", string(resBody), err)
+	require.Nil(t, err, "Error deserializing JSON string `%s`: %v", strBody, err)
 
 	return &startBalance
 }
