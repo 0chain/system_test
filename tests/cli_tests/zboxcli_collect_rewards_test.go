444d07fee6e5136a96e83cef765719bd5b526264
shrhawk <shrhawk88@gmail.com>
2022-06-08 18:20:26 +0500

    get rewards from poolDelegateInfo for rewards comparison

--- tests/cli_tests/zboxcli_collect_rewards_test.go
+++ tests/cli_tests/zboxcli_collect_rewards_test.go
@@ -27,13 +27,11 @@ func TestCollectRewards(t *testing.T) {
 		output, err = listBlobbers(t, configPath, "--json")
 		require.Nil(t, err, "Error listing blobbers", strings.Join(output, "\n"))
 		require.Len(t, output, 1)
-
 		err = json.Unmarshal([]byte(output[0]), &blobbers)
 		require.Nil(t, err, "Error unmarshalling blobber list", strings.Join(output, "\n"))
 		require.True(t, len(blobbers) > 0, "No blobbers found in blobber list")
 
-		// Pick a random blobber
-		blobber := blobbers[time.Now().Unix()%int64(len(blobbers))]
+		blobber := blobbers[0]
 
 		// Stake tokens against this blobber
 		output, err = stakeTokens(t, configPath, createParams(map[string]interface{}{
@@ -46,32 +44,45 @@ func TestCollectRewards(t *testing.T) {
 		stakePoolID := strings.Fields(output[0])[4]
 		require.Nil(t, err, "Error extracting pool Id from sp-lock output", strings.Join(output, "\n"))
 
-		balanceBefore := getBalanceFromSharders(t, wallet.ClientID)
+		stackPoolOutputBefore, err := stakePoolInfo(t, configPath, createParams(map[string]interface{}{
+			"blobber_id": blobber.Id,
+			"json":       "",
+		}))
+		stakePoolBefore := climodel.StakePoolInfo{}
+		err = json.Unmarshal([]byte(stackPoolOutputBefore[0]), &stakePoolBefore)
+		require.Nil(t, err, "Error unmarshalling stake pool info", strings.Join(stackPoolOutputBefore, "\n"))
+		require.NotEmpty(t, stakePoolBefore)
 
-		cliutils.Wait(t, 2*time.Minute)
+		rewardsBefore := int64(0)
+		for _, poolDelegateInfo := range stakePoolBefore.Delegate {
+			if poolDelegateInfo.DelegateID == wallet.ClientID {
+				rewardsBefore = poolDelegateInfo.TotalReward
+				break
+			}
+		}
+
 		output, err = collectRewards(t, configPath, stakePoolID, true)
 		require.Equal(t, "transferred reward tokens", output[0])
 		require.Nil(t, err, "Error collecting rewards", strings.Join(output, "\n"))
 
-		balanceAfter := getBalanceFromSharders(t, wallet.ClientID)
-		require.Greater(t, balanceAfter, balanceBefore)
+		cliutils.Wait(t, 30*time.Second)
+		stackPoolOutputAfter, err := stakePoolInfo(t, configPath, createParams(map[string]interface{}{
+			"blobber_id": blobber.Id,
+			"json":       "",
+		}))
+		stakePoolAfter := climodel.StakePoolInfo{}
+		err = json.Unmarshal([]byte(stackPoolOutputAfter[0]), &stakePoolAfter)
+		require.Nil(t, err, "Error unmarshalling stake pool info", strings.Join(stackPoolOutputAfter, "\n"))
+		require.NotEmpty(t, stakePoolAfter)
 
-		//// Wallet balance should decrease by locked amount
-		//output, err = getBalance(t, configPath)
-		//require.Nil(t, err, "Error fetching balance", strings.Join(output, "\n"))
-		//require.Len(t, output, 1)
-		//require.Regexp(t, regexp.MustCompile(`Balance: 500.00\d mZCN \(\d*\.?\d+ USD\)$`), output[0])
-
-		//cliutils.Wait(t, 20*time.Second)
-		//output, err = stakePoolInfo(t, configPath, createParams(map[string]interface{}{
-		//	"blobber_id": blobber.Id,
-		//	"json":       "",
-		//}))
-		//stakePoolAfter := climodel.StakePoolInfo{}
-		//err = json.Unmarshal([]byte(output[0]), &stakePoolAfter)
-		//require.Nil(t, err, "Error unmarshalling stake pool info", strings.Join(output, "\n"))
-		//require.NotEmpty(t, stakePoolAfter)
-		//require.Greater(t, stakePoolAfter.Rewards, stakePoolBefore.Rewards)
+		rewardsAfter := int64(0)
+		for _, poolDelegateInfo := range stakePoolAfter.Delegate {
+			if poolDelegateInfo.DelegateID == wallet.ClientID {
+				rewardsAfter = poolDelegateInfo.TotalReward
+				break
+			}
+		}
+		require.Greater(t, rewardsAfter, rewardsBefore)
 	})
 }
 
