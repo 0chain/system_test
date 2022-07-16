fa9b991317c52a451ddeb602499fdc7bd3c02e7c
Kishan-Dhakan <kishandhakan17@gmail.com>
2022-07-16 22:48:48 +0530

    add test boilerplate

---
+++ tests/cli_tests/zboxcli_blobber_shutdown_test.go
@@ -0,0 +1,30 @@
+package cli_tests
+
+import (
+	"fmt"
+	"testing"
+	"time"
+
+	cliutils "github.com/0chain/system_test/internal/cli/util"
+	"github.com/stretchr/testify/require"
+)
+
+func TestShutDownBlobber(t *testing.T) {
+	t.Parallel()
+
+	t.Run("shutting down blobber by blobber owner should work", func(t *testing.T) {
+		output, err := shutdownBlobberForWallet(t, configPath, "", blobberOwnerWallet)
+		require.Nil(t, err)
+		require.Len(t, output, 1)
+		require.Equal(t, "shut down blobber", output[0])
+	})
+}
+
+func shutdownBlobber(t *testing.T, cliConfigFilename, params string) ([]string, error) {
+	return shutdownBlobberForWallet(t, cliConfigFilename, params, escapedTestName(t))
+}
+
+func shutdownBlobberForWallet(t *testing.T, cliConfigFilename, params, wallet string) ([]string, error) {
+	t.Log("Requesting blobber info...")
+	return cliutils.RunCommand(t, fmt.Sprintf("./zbox shut-down-blobber %s --silent --wallet %s_wallet.json --configDir ./config --config %s", params, wallet, cliConfigFilename), 3, time.Second*2)
+}
