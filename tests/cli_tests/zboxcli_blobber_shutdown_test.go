40645b2f613453bd574b81e99f7f06bcea35c407
Kishan-Dhakan <kishandhakan17@gmail.com>
2022-07-20 00:57:21 +0530

    refactor with blobber id

--- tests/cli_tests/zboxcli_blobber_shutdown_test.go
+++ tests/cli_tests/zboxcli_blobber_shutdown_test.go
@@ -1,17 +1,36 @@
 package cli_tests
 
 import (
+	"encoding/json"
 	"fmt"
+	"strings"
 	"testing"
 	"time"
 
+	climodel "github.com/0chain/system_test/internal/cli/model"
 	cliutils "github.com/0chain/system_test/internal/cli/util"
 	"github.com/stretchr/testify/require"
 )
 
 func TestShutDownBlobber(t *testing.T) {
+	output, err := registerWallet(t, configPath)
+	require.Nil(t, err, "registering wallet failed", strings.Join(output, "\n"))
+
+	blobbers := []climodel.BlobberInfo{}
+	output, err = listBlobbers(t, configPath, "--json")
+	require.Nil(t, err, "Error listing blobbers", strings.Join(output, "\n"))
+	require.Len(t, output, 1)
+	err = json.Unmarshal([]byte(output[0]), &blobbers)
+	require.Nil(t, err, "Error unmarshalling blobber list", strings.Join(output, "\n"))
+	require.True(t, len(blobbers) > 0, "No blobbers found in blobber list")
+
+	// Pick a random blobber to shutdown
+	blobber := blobbers[time.Now().Unix()%int64(len(blobbers))]
+
 	t.Run("shutting down blobber by blobber owner should work", func(t *testing.T) {
-		output, err := shutdownBlobberForWallet(t, configPath, "", "wallets/blobber02_owner_dev3")
+		output, err := shutdownBlobberForWallet(t, configPath, createParams(map[string]interface{}{
+			"blobber_id": blobber.Id,
+		}), blobberOwnerWallet)
 		require.Nil(t, err)
 		require.Len(t, output, 1)
 		require.Equal(t, "shut down blobber", output[0])
