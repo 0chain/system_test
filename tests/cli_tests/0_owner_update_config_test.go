45a04a8042ba6b8a0ab089fab1b1ce777e700588
Sumit <103603196+sum-it08@users.noreply.github.com>
2022-07-27 23:06:49 +0530

    - fauscetsc config update (#343)

--- tests/cli_tests/0_owner_update_config_test.go
+++ tests/cli_tests/0_owner_update_config_test.go
@@ -1,12 +1,11 @@
 package cli_tests
 
 import (
+	"github.com/stretchr/testify/require"
 	"os"
 	"strings"
 	"testing"
 	"time"
-
-	"github.com/stretchr/testify/require"
 )
 
 func TestOwnerUpdate(t *testing.T) {
@@ -164,12 +163,6 @@ func TestOwnerUpdate(t *testing.T) {
 	})
 
 	t.Run("should allow update of owner: FaucetSC", func(t *testing.T) {
-		t.Skip("Skip till fixed")
-		output, err := registerWallet(t, configPath)
-		require.Nil(t, err, "Failed to register wallet", strings.Join(output, "\n"))
-
-		newOwnerWallet, err := getWallet(t, configPath)
-		require.Nil(t, err, "error getting wallet")
 
 		ownerKey := "owner_id"
 		oldOwner := "1746b06bb09f55ee01b33b5e2e055d6cc7a900cb57c0a3a5eaabb8a0e7745802"
