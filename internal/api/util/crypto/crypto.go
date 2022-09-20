cedf9e8a61b88efe0a2844265e23e7b57d960ad1
stewartie4 <ryanstewart456@gmail.com>
2022-09-20 23:14:33 +0100

    add synchronisation to txn hash, handle panic, removed unused functions

--- internal/api/util/crypto/crypto.go
+++ internal/api/util/crypto/crypto.go
@@ -2,26 +2,19 @@ package crypto
 
 import (
 	"bytes"
-	"crypto/sha1"
-	"crypto/sha256"
 	_ "crypto/sha256"
 	"encoding/hex"
 	"fmt"
 	"golang.org/x/crypto/sha3"
-	"io"
-	"os"
 	"sync"
 	"testing"
 
 	"github.com/0chain/system_test/internal/api/model"
 	"github.com/herumi/bls-go-binary/bls"
-	"github.com/lithammer/shortuuid/v3" //nolint
-	"github.com/tyler-smith/go-bip39"   //nolint
+	"github.com/tyler-smith/go-bip39" //nolint
 )
 
 var blsLock sync.Mutex
-
-const BLS0Chain = "bls0chain"
 
 func GenerateMnemonic(t *testing.T) string {
 	entropy, _ := bip39.NewEntropy(256)       //nolint
@@ -32,11 +25,7 @@ func GenerateMnemonic(t *testing.T) string {
 }
 
 func GenerateKeys(t *testing.T, mnemonic string) *model.KeyPair {
-	defer func() {
-		if err := recover(); err != nil {
-			t.Errorf("panic occurred: ", err)
-		}
-	}()
+	defer handlePanic(t)
 	blsLock.Lock()
 	defer func() {
 		blsLock.Unlock()
@@ -60,32 +49,13 @@ func GenerateKeys(t *testing.T, mnemonic string) *model.KeyPair {
 	return &model.KeyPair{PublicKey: *publicKey, PrivateKey: secretKey}
 }
 
-func NewConnectionID() string {
-	return shortuuid.New() //nolint
-}
+func SignTransaction(t *testing.T, request *model.Transaction, pair *model.KeyPair) {
+	defer handlePanic(t)
+	blsLock.Lock()
+	defer blsLock.Unlock()
 
-func HashOfFileSHA1(src *os.File) (string, error) {
-	h := sha1.New()
-	if _, err := io.Copy(h, src); err != nil {
-		return "", err
-	}
-	if _, err := src.Seek(0, io.SeekStart); err != nil {
-		return "", err
-	}
-
-	return hex.EncodeToString(h.Sum(nil)), nil
-}
-
-func HashOfFileSHA256(src *os.File) (string, error) {
-	h := sha256.New()
-	if _, err := io.Copy(h, src); err != nil {
-		return "", err
-	}
-	if _, err := src.Seek(0, io.SeekStart); err != nil {
-		return "", err
-	}
-
-	return hex.EncodeToString(h.Sum(nil)), nil
+	hashToSign, _ := hex.DecodeString(request.Hash)
+	request.Signature = pair.PrivateKey.Sign(string(hashToSign)).SerializeToHexStr()
 }
 
 func HashTransaction(request *model.Transaction) {
@@ -107,14 +77,15 @@ func Sha3256(publicKeyBytes []byte) string {
 	return clientId
 }
 
-func SignTransaction(request *model.Transaction, pair *model.KeyPair) {
-	hashToSign, _ := hex.DecodeString(request.Hash)
-	request.Signature = pair.PrivateKey.Sign(string(hashToSign)).SerializeToHexStr()
-}
-
 func blankIfNil(obj interface{}) string {
 	if obj == nil {
 		return ""
 	}
 	return fmt.Sprintf("%v", obj)
+}
+
+func handlePanic(t *testing.T) {
+	if err := recover(); err != nil {
+		t.Errorf("panic occurred: ", err)
+	}
 }
