e04f7490b0e1e33e5a1a76d25680fc148bb1d0e9
killbotXD <sonawane.1@iitj.ac.in>
2022-09-27 00:26:41 +0530

    added more test cases and fix pipeline

--- tests/cli_tests/zboxcli_blobber_sp_lock_unlock_test.go
+++ tests/cli_tests/zboxcli_blobber_sp_lock_unlock_test.go
@@ -57,7 +57,9 @@ func TestBlobberStakePoolLockUnlock(t *testing.T) {
 		require.Nil(t, err, strings.Join(output, "\n"))
 		require.True(t, len(output) > 0, "expected output length be at least 1")
 		require.Regexp(t, regexp.MustCompile("^Allocation created: [0-9a-fA-F]{64}$"), output[0], strings.Join(output, "\n"))
-		allocationID := strings.Fields(output[0])[2]
+		allocation := climodel.Allocation{}
+		err = json.Unmarshal([]byte(output[0]), &allocation)
+		allocationID := allocation.ID
 
 		// Wallet balance should decrease by locked amount
 		output, err = getBalance(t, configPath)
@@ -71,7 +73,10 @@ func TestBlobberStakePoolLockUnlock(t *testing.T) {
 			"json":       "",
 		}))
 		require.Nil(t, err, "Error fetching stake pool info", strings.Join(output, "\n"))
-		require.Len(t, output, 2)
+		require.Len(t, output, 1)
+		spInfo := climodel.StakePoolInfo{}
+		err = json.Unmarshal([]byte(output[0]), &spInfo)
+		require.Len(t, spInfo.Delegate, 2)
 
 		stakePool := climodel.StakePoolInfo{}
 		err = json.Unmarshal([]byte(output[0]), &stakePool)
@@ -137,4 +142,95 @@ func TestBlobberStakePoolLockUnlock(t *testing.T) {
 
 	})
 
+	t.Run("Unlock tokens that are offered for allocation should not work", func(t *testing.T) {
+		t.Parallel()
+		output, err := registerWallet(t, configPath)
+		require.Nil(t, err, "registering wallet failed", strings.Join(output, "\n"))
+
+		output, err = executeFaucetWithTokens(t, configPath, 1.9)
+		require.Nil(t, err, "faucet execution failed", strings.Join(output, "\n"))
+
+		blobbers := []climodel.BlobberInfo{}
+		output, err = listBlobbers(t, configPath, "--json")
+		require.Nil(t, err, "Error listing blobbers", strings.Join(output, "\n"))
+		require.Len(t, output, 1)
+
+		err = json.Unmarshal([]byte(output[0]), &blobbers)
+		require.Nil(t, err, "Error unmarshalling blobber list", strings.Join(output, "\n"))
+		require.True(t, len(blobbers) > 0, "No blobbers found in blobber list")
+
+		// Pick a random blobber
+		blobber := blobbers[time.Now().Unix()%int64(len(blobbers))]
+
+		// Stake tokens against this blobber
+		output, err = stakeTokens(t, configPath, createParams(map[string]interface{}{
+			"blobber_id": blobber.Id,
+			"tokens":     0.8,
+		}), true)
+		require.Nil(t, err, "Error staking tokens", strings.Join(output, "\n"))
+		var stakePool = climodel.StakePoolInfo{}
+		err = json.Unmarshal([]byte(output[0]), &stakePool)
+		require.Len(t, output, 1)
+		require.Regexp(t, regexp.MustCompile("tokens locked, txn hash: ([a-f0-9]{64})"), output[0])
+
+		// open allocation make sure to use most of the blobber stake
+		name := cliutils.RandomAlphaNumericString(10)
+
+		options := map[string]interface{}{
+			"lock": 0.7,
+			"name": name,
+		}
+		output, err = createNewAllocation(t, configPath, createParams(options))
+		require.Nil(t, err, strings.Join(output, "\n"))
+		require.True(t, len(output) > 0, "expected output length be at least 1")
+		require.Regexp(t, regexp.MustCompile("^Allocation created: [0-9a-fA-F]{64}$"), output[0], strings.Join(output, "\n"))
+
+		//unlock tokens for offered tokens should fail
+		output, err = unstakeTokens(t, configPath, createParams(map[string]interface{}{
+			"blobber_id": blobber.Id,
+			"pool_id":    stakePool.ID,
+		}))
+		require.Equal(t, err, "Error unstaking tokens from stake pool", strings.Join(output, "\n"))
+
+	})
+
+	t.Run("Locked tokens that are not offered for allocation should unlock okay", func(t *testing.T) {
+		t.Parallel()
+		output, err := registerWallet(t, configPath)
+		require.Nil(t, err, "registering wallet failed", strings.Join(output, "\n"))
+
+		output, err = executeFaucetWithTokens(t, configPath, 1.9)
+		require.Nil(t, err, "faucet execution failed", strings.Join(output, "\n"))
+
+		blobbers := []climodel.BlobberInfo{}
+		output, err = listBlobbers(t, configPath, "--json")
+		require.Nil(t, err, "Error listing blobbers", strings.Join(output, "\n"))
+		require.Len(t, output, 1)
+
+		err = json.Unmarshal([]byte(output[0]), &blobbers)
+		require.Nil(t, err, "Error unmarshalling blobber list", strings.Join(output, "\n"))
+		require.True(t, len(blobbers) > 0, "No blobbers found in blobber list")
+
+		// Pick a random blobber
+		blobber := blobbers[time.Now().Unix()%int64(len(blobbers))]
+
+		// Stake tokens against this blobber
+		output, err = stakeTokens(t, configPath, createParams(map[string]interface{}{
+			"blobber_id": blobber.Id,
+			"tokens":     0.8,
+		}), true)
+		require.Nil(t, err, "Error staking tokens", strings.Join(output, "\n"))
+		var stakePool = climodel.StakePoolInfo{}
+		err = json.Unmarshal([]byte(output[0]), &stakePool)
+		require.Len(t, output, 1)
+		require.Regexp(t, regexp.MustCompile("tokens locked, txn hash: ([a-f0-9]{64})"), output[0])
+
+		output, err = unstakeTokens(t, configPath, createParams(map[string]interface{}{
+			"blobber_id": blobber.Id,
+			"pool_id":    stakePool.ID,
+		}))
+		require.Nil(t, err, "Error unstaking tokens from stake pool", strings.Join(output, "\n"))
+		require.Len(t, output, 1)
+		require.Equal(t, "tokens unlocked, pool deleted", output[0])
+	})
 }
