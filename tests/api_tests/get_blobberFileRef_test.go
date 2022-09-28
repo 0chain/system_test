7a3675e952de5504f04582119a116845dfa03170
nlok5923 <nlokhande5923@gmail.com>
2022-09-28 11:06:50 +0530

    using cli for operations

--- tests/api_tests/get_blobberFileRef_test.go
+++ tests/api_tests/get_blobberFileRef_test.go
@@ -2,9 +2,12 @@ package api_tests
 
 import (
 	"crypto/rand"
+	"encoding/json"
 	"fmt"
+	"io"
 	"math/big"
 	"os"
+	"reflect"
 	"strings"
 	"testing"
 	"time"
@@ -12,33 +15,59 @@ import (
 	"github.com/0chain/gosdk/core/sys"
 	"github.com/0chain/gosdk/core/zcncrypto"
 	"github.com/0chain/system_test/internal/api/model"
+	"github.com/0chain/system_test/internal/api/util/crypto"
+	"github.com/0chain/system_test/internal/api/util/endpoint"
+	climodel "github.com/0chain/system_test/internal/cli/model"
+	cliutils "github.com/0chain/system_test/internal/cli/util"
 	"github.com/stretchr/testify/require"
 	//nolint
 )
 
 func TestGetBlobberFileRefs(t *testing.T) {
 	t.Parallel()
-
+	configPath := "./api_tests_config.yaml"
 	t.Run("Get file ref with allocation id, remote path and ref type should work", func(t *testing.T) {
 		t.Parallel()
-		registeredWallet, keyPair := registerWallet(t)
-		_, blobberRequirements := getBlobbersMatchingRequirements(t, registeredWallet, keyPair, 147483648, 2, 2, time.Minute*2)
-		transactionResponse, _ := createAllocation(t, registeredWallet, keyPair, blobberRequirements)
-		// making request at blobber01
+
+		_, err := registerWalletViaCli(t, configPath)
+		require.Nil(t, err)
+		wallet := readWalletFile(t, "./config/"+escapedTestName(t)+"_wallet.json")
+		registeredWallet := &model.Wallet{
+			ClientID:  wallet.ClientID,
+			ClientKey: wallet.ClientKey,
+			Nonce:     0,
+		}
+		keyPair := crypto.GenerateKeys(t, wallet.Mnemonic)
+		executeFaucetTransactionResponse, confirmation := executeFaucet(t, registeredWallet, keyPair)
+		require.NotNil(t, executeFaucetTransactionResponse)
+		require.Equal(t, endpoint.TxSuccessfulStatus, confirmation.Status, confirmation.Transaction.TransactionOutput)
+
+		availableBlobbers, blobberRequirements := getBlobbersMatchingRequirements(t, registeredWallet, keyPair, 10000, 2, 1, time.Minute*20)
+		require.NotNil(t, availableBlobbers)
+		require.NotNil(t, blobberRequirements)
+
+		blobberRequirements.Blobbers = availableBlobbers
+
+		createAllocationTransactionResponse, confirmation := createAllocation(t, registeredWallet, keyPair, blobberRequirements)
+		require.NotNil(t, createAllocationTransactionResponse)
+		require.Equal(t, endpoint.TxSuccessfulStatus, confirmation.Status, confirmation.Transaction.TransactionOutput)
+
+		allocation := getAllocation(t, createAllocationTransactionResponse.Entity.Hash)
+		t.Logf((" this is allocation id %s"), allocation.ID)
+		require.NotNil(t, allocation)
+
 		sysKeyPair := []sys.KeyPair{sys.KeyPair{
 			PrivateKey: keyPair.PrivateKey.SerializeToHexStr(),
 			PublicKey:  keyPair.PublicKey.SerializeToHexStr(),
 		}}
-		// v1BlobberFileUpload
+
 		url := "http://dev.0chain.net/blobber01"
-		clientSignature, _ := SignHash(transactionResponse.Entity.Hash, "bls0chain", sysKeyPair)
-		allocationId := transactionResponse.Entity.Hash // allocation id
-		refType := "f"                                  // or can be "d"
+		clientSignature, _ := SignHash(createAllocationTransactionResponse.Entity.Hash, "bls0chain", sysKeyPair)
+		// // require.NotNil(t, allocation)
+		allocationId := allocation.ID
+		refType := "f" // or can be "d"
 		getBlobberFileUploadRequest(t, url, registeredWallet, keyPair, allocationId, refType, clientSignature)
-		blobberFileRefRequest := getBlobberFileRefRequest(t, url, registeredWallet, keyPair, allocationId, refType, clientSignature)
-		_, _, httpError := v1BlobberGetFileRefs(t, blobberFileRefRequest)
-		require.NotNil(t, httpError)
-		require.Nil(t, httpError)
+		// blobberFileRefRequest := getBlobberFileRefRequest(t, url, registeredWallet, keyPair, allocationId, refType, clientSignature)
 	})
 }
 
@@ -81,30 +110,17 @@ func getBlobberFileUploadRequest(t *testing.T, url string, registeredWallet *mod
 
 	// generating random file
 	filename := generateRandomTestFileName(t)
-	file, err := createFileWithSize(filename, 1024)
+	_, err := createFileWithSize(filename, 1024)
 	require.Nil(t, err)
 
-	blobberUploadFileMeta := model.BlobberUploadFileMeta{
-		ConnectionID: "connection_id", // need to find the connectionId
-		FileName:     filename,
-		FilePath:     "/",
-		ActualHash:   "actual_hash",  // need actual_hash
-		ContentHash:  "content_hash", // need to calculate content_hash
-		MimeType:     "text/plain",
-		ActualSize:   1024,
-		IsFinal:      true,
-	}
-	blobberFileUploadFileRequest := model.BlobberUploadFileRequest{
-		URL:             url,
-		ClientID:        registeredWallet.ClientID,
-		ClientKey:       registeredWallet.ClientKey,
-		ClientSignature: clientSignature,
-		AllocationID:    allocationId,
-		File:            file,
-		Meta:            blobberUploadFileMeta,
-	}
-	fileUploadResponse, _, _ := v1BlobberFileUpload(t, blobberFileUploadFileRequest)
-	require.NotNil(t, fileUploadResponse)
+	configPath := "./api_tests_config.yaml"
+	output, err := uploadFileForWallet(t, escapedTestName(t), configPath, map[string]interface{}{
+		"allocation": allocationId,
+		"remotepath": "/",
+		"localpath":  filename,
+	}, true)
+	require.Nil(t, err, strings.Join(output, "\n"))
+	require.Len(t, output, 2)
 }
 
 func generateRandomTestFileName(t *testing.T) string {
@@ -126,4 +142,69 @@ func RandomAlphaNumericString(n int) string {
 		ret[i] = letters[num.Int64()]
 	}
 	return string(ret)
+}
+
+func uploadFileForWallet(t *testing.T, wallet, cliConfigFilename string, param map[string]interface{}, retry bool) ([]string, error) {
+	t.Logf("Uploading file...")
+
+	p := createParams(param)
+	cmd := fmt.Sprintf(
+		"./zbox upload %s --silent --wallet %s_wallet.json --configDir ./config --config %s",
+		p,
+		wallet,
+		cliConfigFilename,
+	)
+
+	if retry {
+		return cliutils.RunCommand(t, cmd, 3, time.Second*40)
+	} else {
+		return cliutils.RunCommandWithoutRetry(cmd)
+	}
+}
+
+func createParams(params map[string]interface{}) string {
+	var builder strings.Builder
+
+	for k, v := range params {
+		if v == nil {
+			_, _ = builder.WriteString(fmt.Sprintf("--%s ", k))
+		} else if reflect.TypeOf(v).String() == "bool" {
+			_, _ = builder.WriteString(fmt.Sprintf("--%s=%v ", k, v))
+		} else {
+			_, _ = builder.WriteString(fmt.Sprintf("--%s %v ", k, v))
+		}
+	}
+	return strings.TrimSpace(builder.String())
+}
+
+func escapedTestName(t *testing.T) string {
+	replacer := strings.NewReplacer("/", "-", "\"", "-", ":", "-", "(", "-",
+		")", "-", "<", "LESS_THAN", ">", "GREATER_THAN", "|", "-", "*", "-",
+		"?", "-")
+	return replacer.Replace(t.Name())
+}
+
+func registerWalletViaCli(t *testing.T, cliConfigFilename string) ([]string, error) {
+	return registerWalletViaCliForName(t, cliConfigFilename, escapedTestName(t))
+}
+
+func registerWalletViaCliForName(t *testing.T, cliConfigFilename, name string) ([]string, error) {
+	t.Logf("Registering wallet...")
+	return cliutils.RunCommand(t, "./zbox register --silent "+
+		"--wallet "+name+"_wallet.json"+" --configDir ./config --config "+cliConfigFilename, 3, time.Second*2)
+}
+
+func readWalletFile(t *testing.T, file string) *climodel.WalletFile {
+	wallet := &climodel.WalletFile{}
+
+	f, err := os.Open(file)
+	require.Nil(t, err, "wallet file %s not found", file)
+
+	ownerWalletBytes, err := io.ReadAll(f)
+	require.Nil(t, err, "error reading wallet file %s", file)
+
+	err = json.Unmarshal(ownerWalletBytes, wallet)
+	require.Nil(t, err, "error marshaling wallet content")
+
+	return wallet
 }
