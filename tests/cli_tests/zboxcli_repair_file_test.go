f964adf8a451ccfdbace348be918d8f95703d067
Naman-1234 <namankalrabhiwani54@gmail.com>
2022-10-11 00:05:51 +0530

    feat: fixing repair test

--- tests/cli_tests/zboxcli_repair_file_test.go
+++ tests/cli_tests/zboxcli_repair_file_test.go
@@ -24,6 +24,94 @@ import (
 func TestRepairFile(t *testing.T) {
 	t.Parallel()
 
+	t.Run("Attempt the file repair on multiple files that needs repair", func(t *testing.T) {
+		t.Parallel()
+		output, err := registerWallet(t, configPath)
+		require.Nil(t, err, "Unexpected register wallet failure", strings.Join(output, "\n"))
+
+		output, err = executeFaucetWithTokens(t, configPath, 1)
+		require.Nil(t, err, "Unexpected faucet failure", strings.Join(output, "\n"))
+
+		wallet, err := getWallet(t, configPath)
+
+		walletOwner := escapedTestName(t)
+		require.Nil(t, err, "Error occurred when retrieving wallet")
+
+		// first uploading the file
+		allocSize := int64(1 * MB)
+		fileSize := int64(512 * KB)
+
+		allocationID := setupAllocation(t, configPath, map[string]interface{}{
+			"size":   allocSize,
+			"parity": 2,
+			"data":   2,
+		})
+
+		filename1 := generateRandomTestFileName(t)
+		err = createFileWithSize(filename1, fileSize)
+		require.Nil(t, err)
+
+		filename2 := generateRandomTestFileName(t)
+		err = createFileWithSize(filename1, fileSize)
+		require.Nil(t, err)
+
+		files := []string{filename1, filename2}
+
+		output, err = uploadFile(t, configPath, map[string]interface{}{
+			"allocation": allocationID,
+			"remotepath": "/",
+			"localpath":  filename,
+		}, true)
+
+		require.Nil(t, err, strings.Join(output, "\n"))
+		require.Len(t, output, 2)
+		require.Equal(t, fmt.Sprintf("Status completed callback. Type = application/octet-stream. Name = %s", filepath.Base(filename)), output[1])
+
+		allocation := getAllocation(t, allocationID)
+		require.Len(t, allocation.Blobbers, 4)
+
+		// Make API call to delete file from a single blobber
+		connectionID := shortuuid.New()
+
+		blobbers := getBlobbersList(t)
+		blobberUrl := blobbers[0].Url
+
+		keyPair := crypto.GenerateKeys(wallet.Mnemonics)
+		sign, err := crypto.SignHash(allocation.ID, keyPair)
+		require.Nil(t, err)
+
+		blobberDeleteConnectionRequest := &climodel.BlobberDeleteConnectionRequest{
+			ClientKey:       wallet.ClientPublicKey,
+			ClientSignature: sign,
+			ClientID:        wallet.ClientID,
+			ConnectionID:    connectionID,
+			AllocationID:    allocationID,
+			Path:            "/" + filepath.Base(filename),
+			URL:             blobberUrl,
+		}
+		require.NotNil(t, blobberDeleteConnectionRequest)
+		deleteBlobberFile(t, blobberDeleteConnectionRequest)
+		fmt.Printf("%+v\n", blobberDeleteConnectionRequest)
+		// require.Nil(t, err)
+		// require.NotNil(t, resp)
+
+		// now we will try to repair the file and will create another folder to keep the same
+		err = os.MkdirAll(os.TempDir(), os.ModePerm)
+		require.Nil(t, err)
+
+		params := createParams(map[string]interface{}{
+			"allocation": allocationID,
+			"repairpath": "/",
+			"rootpath":   os.TempDir(),
+		})
+
+		output, _ = repairAllocation(t, walletOwner, configPath, params, false)
+		require.Len(t, output, 1)
+		require.Equal(t, fmt.Sprintf("Repair file completed, Total files repaired: %s", "2"), output[len(output)-1])
+
+	})
+	return
+
 	t.Run("Attempt file repair on the single file that needs repair", func(t *testing.T) {
 		t.Parallel()
 
@@ -81,7 +169,7 @@ func TestRepairFile(t *testing.T) {
 
 		blobberDeleteConnectionRequest := &climodel.BlobberDeleteConnectionRequest{
 			ClientKey:       wallet.ClientPublicKey,
-			ClientSignature: string(sign),
+			ClientSignature: sign,
 			ClientID:        wallet.ClientID,
 			ConnectionID:    connectionID,
 			AllocationID:    allocationID,
@@ -89,7 +177,8 @@ func TestRepairFile(t *testing.T) {
 			URL:             blobberUrl,
 		}
 		require.NotNil(t, blobberDeleteConnectionRequest)
-		// deleteBlobberFile(t, blobberDeleteConnectionRequest)
+		deleteBlobberFile(t, blobberDeleteConnectionRequest)
+		fmt.Printf("%+v\n", blobberDeleteConnectionRequest)
 		// require.Nil(t, err)
 		// require.NotNil(t, resp)
 
@@ -105,7 +194,7 @@ func TestRepairFile(t *testing.T) {
 
 		output, _ = repairAllocation(t, walletOwner, configPath, params, false)
 		require.Len(t, output, 1)
-		require.Equal(t, fmt.Sprintf("Repair file completed, Total files repaired: %s", "2"), output[len(output)-1])
+		require.Equal(t, fmt.Sprintf("Repair file completed, Total files repaired: %s", "1"), output[len(output)-1])
 	})
 
 	return
@@ -175,7 +264,7 @@ func repairAllocation(t *testing.T, wallet, cliConfigFilename, params string, re
 	}
 }
 
-func deleteBlobberFile(t *testing.T, blobberDeleteConnectionRequest *climodel.BlobberDeleteConnectionRequest) {
+func deleteBlobberFile(t *testing.T, blobberDeleteConnectionRequest *climodel.BlobberDeleteConnectionRequest) (*http.Response, error) {
 
 	// Forming the body of the request
 	formData := map[string]string{
@@ -188,8 +277,12 @@ func deleteBlobberFile(t *testing.T, blobberDeleteConnectionRequest *climodel.Bl
 	require.NotNil(t, formData)
 
 	url := blobberDeleteConnectionRequest.URL + "/v1/file/upload/" + blobberDeleteConnectionRequest.AllocationID
-	req, _ := http.NewRequest(http.MethodDelete, url, bytes.NewBuffer(formDataByteArray))
+	req, err := http.NewRequest(http.MethodDelete, url, bytes.NewBuffer(formDataByteArray))
 
+	if err != nil {
+		fmt.Println(err)
+		return nil, err
+	}
 	// Setting the request headers
 	req.Header.Set("X-App-Client-Id", blobberDeleteConnectionRequest.ClientID)
 	req.Header.Set("X-App-Client-Key", blobberDeleteConnectionRequest.ClientKey)
@@ -204,4 +297,13 @@ func deleteBlobberFile(t *testing.T, blobberDeleteConnectionRequest *climodel.Bl
 	fmt.Println("err from client", err)
 	require.Nil(t, err)
 	require.NotNil(t, resp)
+
+	return resp, nil
 }
+
+// func uploadAndDeleteInSingleBlobber(t, files []string){
+
+// 	for _, file = range(files){
+
+// 	}
+// }
