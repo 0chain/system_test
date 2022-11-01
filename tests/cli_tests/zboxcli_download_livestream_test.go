ffb4b6ecf3aeee84f47de4ac73382bb3742faa05
Kishan-Dhakan <kishandhakan17@gmail.com>
2022-11-02 01:54:13 +0530

    lint fixes

--- tests/cli_tests/zboxcli_download_livestream_test.go
+++ tests/cli_tests/zboxcli_download_livestream_test.go
@@ -22,7 +22,7 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
-func TestLivestreamDownload(t *testing.T) {
+func TestLivestreamDownload(t *testing.T) { // nolint cyclomatic complexity 48
 	KillFFMPEG()
 
 	defer KillFFMPEG()
@@ -110,7 +110,7 @@ func TestLivestreamDownload(t *testing.T) {
 						return err
 					}
 
-					num = num + 1
+					num++
 
 					newStr := fmt.Sprintf("up%d.ts", num)
 					hashmap[newStr] = hex.EncodeToString(hash.Sum(nil))
@@ -262,7 +262,7 @@ func TestLivestreamDownload(t *testing.T) {
 						return err
 					}
 
-					num = num + 1
+					num++
 
 					newStr := fmt.Sprintf("up%d.ts", num)
 					hashmap[newStr] = hex.EncodeToString(hash.Sum(nil))
@@ -422,7 +422,7 @@ func TestLivestreamDownload(t *testing.T) {
 						return err
 					}
 
-					num = num + 1
+					num++
 
 					newStr := fmt.Sprintf("up%d.ts", num)
 					hashmap[newStr] = hex.EncodeToString(hash.Sum(nil))
