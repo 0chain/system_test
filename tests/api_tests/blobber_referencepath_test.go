f66a54d033c14fa34e900750235fe05e30a64ec3
Kishan-Dhakan <kishandhakan17@gmail.com>
2022-11-08 19:31:05 +0530

    add invalid alloc id case

--- tests/api_tests/blobber_referencepath_test.go
+++ tests/api_tests/blobber_referencepath_test.go
@@ -11,7 +11,7 @@ import (
 )
 
 func TestFileReferencePath(t *testing.T) {
-	t.Run("Get file ref with allocation id, remote path with reftype as regular or updated should work", func(t *testing.T) {
+	t.Run("Get file ref with allocation id, remote path should work", func(t *testing.T) {
 		apiClient.ExecuteFaucet(t, sdkWallet, client.TxSuccessfulStatus)
 
 		blobberRequirements := model.DefaultBlobberRequirements(sdkWallet.Id, sdkWallet.PublicKey)
@@ -29,27 +29,61 @@ func TestFileReferencePath(t *testing.T) {
 		blobber := apiClient.GetBlobber(t, blobberID, client.HttpOkStatus)
 		url := blobber.BaseURL
 		keyPair := crypto.GenerateKeys(t, sdkWalletMnemonics)
-		refType := "regular"
 		sign := encryption.Hash(allocation.Tx)
 
 		clientSignature := crypto.SignHexString(t, sign, &keyPair.PrivateKey)
 
-		blobberFileRefPathRequest := newBlobberFileRefPathRequest(url, sdkWallet, allocationID, refType, clientSignature, remoteFilePath)
+		blobberFileRefPathRequest := newBlobberFileRefPathRequest(url, sdkWallet, allocationID, clientSignature, remoteFilePath)
 		blobberFileRefsResponse, resp, err := apiClient.V1BlobberGetFileRefPaths(t, blobberFileRefPathRequest, client.HttpOkStatus)
 		require.Nil(t, err)
 		require.NotNil(t, blobberFileRefsResponse)
 		require.Equal(t, resp.StatusCode(), client.HttpOkStatus, resp)
+		require.Equal(t, blobberFileRefsResponse.Ref.Path, remoteFilePath)
+		require.Equal(t, blobberFileRefsResponse.Ref.Type, "f")
+
+		// TODO add more assertions once there blobber endpoints are documented
+	})
+
+	t.Run("Get file ref with invalid allocation id should fail", func(t *testing.T) {
+		apiClient.ExecuteFaucet(t, sdkWallet, client.TxSuccessfulStatus)
+
+		blobberRequirements := model.DefaultBlobberRequirements(sdkWallet.Id, sdkWallet.PublicKey)
+		allocationBlobbers := apiClient.GetAllocationBlobbers(t, sdkWallet, blobberRequirements, client.HttpOkStatus)
+		allocationID := apiClient.CreateAllocation(t, sdkWallet, allocationBlobbers, client.TxSuccessfulStatus)
+
+		allocation := apiClient.GetAllocation(t, allocationID, client.HttpOkStatus)
+
+		// TODO: replace with native "Upload API" call
+		remoteFilePath := sdkClient.UploadFile(t, allocationID)
+
+		blobberID := getFirstUsedStorageNodeID(allocationBlobbers.Blobbers, allocation.Blobbers)
+		require.NotZero(t, blobberID)
+
+		blobber := apiClient.GetBlobber(t, blobberID, client.HttpOkStatus)
+		blobberUrl := blobber.BaseURL
+		keyPair := crypto.GenerateKeys(t, sdkWalletMnemonics)
+		sign := encryption.Hash(allocation.Tx)
+
+		clientSignature := crypto.SignHexString(t, sign, &keyPair.PrivateKey)
+		blobberFileRefPathRequest := newBlobberFileRefPathRequest(blobberUrl, sdkWallet, "invalid_allocation_id", clientSignature, remoteFilePath)
+		blobberFileRefsResponse, resp, err := apiClient.V1BlobberGetFileRefPaths(t, blobberFileRefPathRequest, client.HttpOkStatus)
+		// FIXME: error should be returned
+		require.Nil(t, err)
+		require.Empty(t, blobberFileRefsResponse)
+		require.Equal(t, resp.StatusCode(), client.HttpBadRequestStatus)
+
+		// TODO add more assertions once there blobber endpoints are documented
 	})
 }
 
-func newBlobberFileRefPathRequest(url string, registeredsdkWallet *model.Wallet, allocationId string, refType string, clientSignature string, remotePath string) *model.BlobberFileRefPathRequest {
+func newBlobberFileRefPathRequest(url string, registeredsdkWallet *model.Wallet, allocationId, clientSignature, remotePath string) *model.BlobberFileRefPathRequest {
 	blobberFileRefPathRequest := model.BlobberFileRefPathRequest{
 		URL:             url,
 		ClientID:        registeredsdkWallet.Id,
 		ClientKey:       registeredsdkWallet.PublicKey,
 		ClientSignature: clientSignature,
 		AllocationID:    allocationId,
-		Path:      remotePath,
+		Path:            remotePath,
 	}
 	return &blobberFileRefPathRequest
 }
