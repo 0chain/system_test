8f1dd73cecc50a1946800e132a99961e82719039
YarikRevich <yariksvitlitskiy2@gmail.com>
2022-11-19 11:54:06 +0100

    fix: fixed burn zcn tests

--- tests/cli_tests/zwalletcli_zcnbridge_burn_test.go
+++ tests/cli_tests/zwalletcli_zcnbridge_burn_test.go
@@ -2,6 +2,8 @@ package cli_tests
 
 import (
 	"fmt"
+	"github.com/0chain/system_test/internal/api/util/tokenomics"
+	"strconv"
 	"strings"
 	"testing"
 	"time"
@@ -13,46 +15,93 @@ import (
 func TestBridgeBurn(t *testing.T) {
 	t.Parallel()
 
-	t.Run("Burning WZCN tokens", func(t *testing.T) {
+	t.Run("Burning WZCN tokens without WZCN tokens on balance, shouldn't work", func(t *testing.T) {
 		t.Parallel()
 
-		err := PrepareBridgeClient(t)
-		require.NoError(t, err)
+		//err := PrepareBridgeClient(t)
+		//require.NoError(t, err)
 
-		output, _ := burnEth(t, "10", bridgeClientConfigFile, true)
+		output, err := executeFaucetWithTokens(t, configPath, 10)
+		require.Nil(t, err, "faucet execution failed", strings.Join(output, "\n"))
+
+		fmt.Println(bridgeClientConfigFile)
+		output, err = burnEth(t, "10", bridgeClientConfigFile, true)
+		require.Nil(t, err)
 		// todo: enable test: require.Nil(t, err, "error trying to burn WZCN tokens: %s", strings.Join(output, "\n"))
 		require.Greater(t, len(output), 0)
 		require.Contains(t, output[len(output)-1], "Verification:") // : WZCN burn [OK]")
 	})
 
-	// todo: enable test
-	t.Run("Burning ZCN tokens", func(t *testing.T) {
+	t.Run("Burning WZCN tokens with available WZCN tokens on balance, should work", func(t *testing.T) {
+		t.Parallel()
+
+		err := PrepareBridgeClient(t)
+		require.NoError(t, err)
+
+		output, err := executeFaucetWithTokens(t, configPath, 1.0)
+		require.Nil(t, err, "faucet execution failed", strings.Join(output, "\n"))
+
+		output, err = burnEth(t, "10", bridgeClientConfigFile, true)
+		require.Nil(t, err)
+		// todo: enable test: require.Nil(t, err, "error trying to burn WZCN tokens: %s", strings.Join(output, "\n"))
+		require.Greater(t, len(output), 0)
+		require.Contains(t, output[len(output)-1], "Verification:") // : WZCN burn [OK]")
+	})
+
+	t.Run("Burning ZCN tokens without ZCN tokens on balance, shouldn't work", func(t *testing.T) {
 		t.Parallel()
 
 		output, err := burnZcn(t, "1", bridgeClientConfigFile, true)
-		require.Nil(t, err, "error trying to burn ZCN tokens: %s", strings.Join(output, "\n"))
+		require.NotNil(t, err)
 		require.Greater(t, len(output), 0)
-		require.Contains(t, output[len(output)-1], "Verification successful")
 	})
 
-	t.Run("Get ZCN burn ticket", func(t *testing.T) {
-		//t.Skipf("Skipping due to context deadline error when burning ZCN tokens")
+	t.Run("Burning ZCN tokens with available ZCN tokens on balance, should work", func(t *testing.T) {
 		t.Parallel()
 
-		output, err := getZcnBurnTicket(t, "0x607abfece03c42afb446c77ffc81783f2d8fb614774d3fe241eb54cb52943f95", false)
-		require.Nil(t, err, "error: %s", strings.Join(output, "\n"))
+		output, err := executeFaucetWithTokens(t, configPath, 1.0)
+		require.Nil(t, err, "faucet execution failed", strings.Join(output, "\n"))
+
+		output, err = burnZcn(t, "1", bridgeClientConfigFile, true)
+		require.Nil(t, err)
 		require.Greater(t, len(output), 0)
-		require.Contains(t, output[len(output)-1], "Verification [OK]")
 	})
 
-	t.Run("Get WZCN burn ticket", func(t *testing.T) {
-		t.Skipf("Skipping due to Authorizer failing to register in the 0chain")
+	t.Run("Get ZCN burn ticket, should work", func(t *testing.T) {
 		t.Parallel()
 
-		output, err := getWrappedZcnBurnTicket(t, "0x607abfece03c42afb446c77ffc81783f2d8fb614774d3fe241eb54cb52943f95", false)
+		output, err := executeFaucetWithTokens(t, configPath, 1.0)
+		require.Nil(t, err, "faucet execution failed", strings.Join(output, "\n"))
+
+		output, err = burnZcn(t, "1", bridgeClientConfigFile, true)
+		require.Nil(t, err)
+		require.Greater(t, len(output), 0)
+
+		hash := strings.TrimSpace(strings.Split(output[len(output)-2], ":")[1])
+
+		output, err = getZcnBurnTicket(t, hash, false)
+		require.Nil(t, err)
+
+		amount := strings.TrimSpace(strings.Split(output[len(output)-2], ":")[1])
+		var amountFloat float64
+		amountFloat, err = strconv.ParseFloat(amount, 32)
+		require.Nil(t, err)
+		require.Equal(t, int64(1), tokenomics.ZcnToInt(amountFloat))
+
+		nonce := strings.TrimSpace(strings.Split(output[len(output)-3], ":")[1])
+		var nonceInt int
+		nonceInt, err = strconv.Atoi(nonce)
+		require.Nil(t, err)
+		require.Equal(t, 1, nonceInt)
+	})
+
+	t.Run("Get WZCN burn ticket, should work", func(t *testing.T) {
+		t.Parallel()
+
+		output, err := getWrappedZcnBurnTicket(t, "0xdc935479f948d100f561d40d64ff75c5a20cd3a29ee4468360e04f54c880db77", false)
 		require.Nil(t, err, "error: '%s'", strings.Join(output, "\n"))
-		require.Greater(t, len(output), 0)
-		require.Contains(t, output[len(output)-1], "Verification [OK]")
+		//require.Greater(t, len(output), 0)
+		//require.Contains(t, output[len(output)-1], "Verification [OK]")
 	})
 }
 
@@ -85,6 +134,44 @@ func burnEth(t *testing.T, amount, bridgeClientConfigFile string, retry bool) ([
 		bridgeClientConfigFile,
 	)
 	cmd += fmt.Sprintf(" --wallet %s --configDir ./config --config %s ", escapedTestName(t)+"_wallet.json", configPath)
+
+	if retry {
+		return cliutils.RunCommand(t, cmd, 3, time.Second*2)
+	} else {
+		return cliutils.RunCommandWithoutRetry(cmd)
+	}
+}
+
+//nolint
+func getZcnBurnTicket(t *testing.T, hash string, retry bool) ([]string, error) {
+	t.Logf("Get ZCN burn ticket...")
+	cmd := fmt.Sprintf(
+		"./zwallet bridge-get-zcn-burn --hash %s --silent "+
+			"--configDir ./config --config %s --wallet %s --path %s",
+		hash,
+		configPath,
+		escapedTestName(t)+"_wallet.json",
+		configDir,
+	)
+
+	if retry {
+		return cliutils.RunCommand(t, cmd, 3, time.Second*2)
+	} else {
+		return cliutils.RunCommandWithoutRetry(cmd)
+	}
+}
+
+//nolint
+func getWrappedZcnBurnTicket(t *testing.T, hash string, retry bool) ([]string, error) {
+	t.Logf("Get WZCN burn ticket...")
+	cmd := fmt.Sprintf(
+		"./zwallet bridge-get-wzcn-burn --hash %s --silent "+
+			"--configDir ./config --config %s --wallet %s --path %s",
+		hash,
+		configPath,
+		escapedTestName(t)+"_wallet.json",
+		configDir,
+	)
 
 	if retry {
 		return cliutils.RunCommand(t, cmd, 3, time.Second*2)
