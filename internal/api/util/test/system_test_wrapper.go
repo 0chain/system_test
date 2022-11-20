097b02c732eee1fa0b4a9eb319ad9b90fcae85a4
stewartie4 <ryanstewart456@gmail.com>
2022-11-20 21:55:10 +0000

    ensure test wrapper handles runtime.goExit()

--- internal/api/util/test/system_test_wrapper.go
+++ internal/api/util/test/system_test_wrapper.go
@@ -2,6 +2,7 @@ package test
 
 import (
 	"runtime/debug"
+	"sync"
 	"testing"
 	"time"
 )
@@ -12,36 +13,44 @@ type SystemTest struct {
 	*testing.T
 }
 
-type Void struct{}
-
-func executeTest(t *testing.T, testFunction func(t *testing.T)) (ret Void) {
-	defer func() {
-		if err := recover(); err != nil {
-			t.Errorf("Test case exited due to panic - [%v] with stack trace [%v]", err, string(debug.Stack()))
-			ret = Void{}
-		}
-	}()
-	testFunction(t)
-	return Void{}
-}
-
 func (w *SystemTest) RunWithCustomTimeout(name string, timeout time.Duration, f func(t *testing.T)) bool {
+	wg := sync.WaitGroup{}
+	wg.Add(1)
+
 	timeoutWrappedTestCase := func(t *testing.T) {
 		t.Logf("Test case [%s] start.", name)
-		result := make(chan Void, 1)
+
+		testCase := make(chan struct{}, 1)
+
 		go func() {
-			result <- executeTest(t, f)
+			go func() {
+				defer wg.Done()
+				executeTest(t, f)
+			}()
+			wg.Wait()
+			close(testCase)
 		}()
+
 		select {
 		case <-time.After(timeout):
 			t.Logf("Test case [%s] timed out after [%v]", name, timeout)
 			t.Fatalf("Test case [%s] end.", name)
-		case _ = <-result:
+		case _ = <-testCase:
 		}
+
 		t.Logf("Test case [%s] end.", name)
 	}
 
 	return w.T.Run(name, timeoutWrappedTestCase)
+}
+
+func executeTest(t *testing.T, testFunction func(t *testing.T)) {
+	defer func() {
+		if err := recover(); err != nil {
+			t.Errorf("Test case exited due to panic - [%v] with stack trace [%v]", err, string(debug.Stack()))
+		}
+	}()
+	testFunction(t)
 }
 
 func (w *SystemTest) Run(name string, testCaseFunction func(t *testing.T)) bool {
