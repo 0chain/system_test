71ae7315570fa9d5e8a749666b84b59b64019ab4
Ryan <ryanstewart456@gmail.com>
2022-12-17 23:54:31 +0000

    increase test timeout

--- tests/cli_tests/zboxcli_livestream_test.go
+++ tests/cli_tests/zboxcli_livestream_test.go
@@ -106,7 +106,7 @@ func TestStreamUploadDownload(testSetup *testing.T) {
 		}
 	})
 
-	t.RunSequentiallyWithTimeout("Upload from feed with delay flag must work", 2*time.Minute+30*time.Second, func(t *test.SystemTest) { // todo slow
+	t.RunSequentiallyWithTimeout("Upload from feed with delay flag must work", 4*time.Minute, func(t *test.SystemTest) { // todo slow
 		feed, ok := getFeed()
 
 		if !ok {
