fc6399d4161eed75ad03c3a9cad3a243654096d5
Kishan-Dhakan <kishandhakan17@gmail.com>
2023-01-12 02:57:36 +0530

    set data parity to 1 each

--- tests/cli_tests/SINGULAR_blobber_rewards_test.go
+++ tests/cli_tests/SINGULAR_blobber_rewards_test.go
@@ -20,11 +20,24 @@ func TestBlobberStorageRewards(testSetup *testing.T) {
 	t.RunWithTimeout("Finalize Expired Allocation Should Work", 5*time.Minute, func(t *test.SystemTest) {
 		//TODO: unacceptably slow
 
-		allocationID, _ := setupAndParseAllocation(t, configPath)
+		options := map[string]interface{}{
+			"expire": "1h",
+			"size":   "1024",
+			"parity": "1",
+			"lock":   "0.5",
+			"data":   "1",
+		}
+		output, err := createNewAllocation(t, configPath, createParams(options))
+		require.Nil(t, err, strings.Join(output, "\n"))
+		require.True(t, len(output) > 0, "expected output length be at least 1")
+		require.Regexp(t, regexp.MustCompile("^Allocation created: [0-9a-fA-F]{64}$"), output[0], strings.Join(output, "\n"))
+
+		allocationID, err := getAllocationID(output[0])
+		require.Nil(t, err, "could not get allocation ID", strings.Join(output, "\n"))
 
 		cliutils.Wait(t, 4*time.Minute)
 
-		output, err := finalizeAllocation(t, configPath, allocationID, false)
+		output, err = finalizeAllocation(t, configPath, allocationID, false)
 
 		require.Nil(t, err, "unexpected error updating allocation", strings.Join(output, "\n"))
 		require.True(t, len(output) > 0, "expected output length be at least 1", strings.Join(output, "\n"))
