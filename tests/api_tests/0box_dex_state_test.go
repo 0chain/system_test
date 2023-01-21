f1d37730d39fb9f7fc161f479bd222f58829f2b5
Kishan-Dhakan <kishandhakan17@gmail.com>
2023-01-21 22:05:57 +0530

    add more cases

--- tests/api_tests/0box_dex_state_test.go
+++ tests/api_tests/0box_dex_state_test.go
@@ -26,4 +26,28 @@ func TestDexState(testSetup *testing.T) {
 		require.Equal(t, 200, response.StatusCode())
 		require.NotNil(t, dexState)
 	})
+
+	t.RunSequentially("Create a DEX state with invalid phone number should fail", func(t *test.SystemTest) {
+		csrfToken := createCsrfToken(t, zboxClient.DefaultPhoneNumber)
+
+		dexState, response, err := zboxClient.PostDexState(t,
+			firebaseToken.IdToken,
+			csrfToken,
+			"123456789",
+		)
+		require.Error(t, err)
+		require.Equal(t, 400, response.StatusCode())
+		require.Nil(t, dexState)
+	})
+
+	t.RunSequentially("Create a DEX state with invalid csrf token should fail", func(t *test.SystemTest) {
+		dexState, response, err := zboxClient.PostDexState(t,
+			firebaseToken.IdToken,
+			"abcd",
+			zboxClient.DefaultPhoneNumber,
+		)
+		require.Error(t, err)
+		require.Equal(t, 400, response.StatusCode())
+		require.Nil(t, dexState)
+	})
 }
