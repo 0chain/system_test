51a341d2b2b23ca3e1180a3e6dad3e1c51eee06b
manali <manali@squareops.com>
2023-02-07 11:26:22 +0000

    uncomment nft info/state

--- tests/api_tests/0box_nft_info_state_test.go
+++ tests/api_tests/0box_nft_info_state_test.go
@@ -42,54 +42,54 @@ func TestNftInfoState(testSetup *testing.T) {
 		fmt.Println(string(response.Body()))
 	})
 
-	// t.RunSequentially("Posting NFT Info with valid form-data should work", func(t *test.SystemTest) {
-	// 	teardown(t, firebaseToken.IdToken, zboxClient.DefaultPhoneNumber)
-	// 	csrfToken := createCsrfToken(t, zboxClient.DefaultPhoneNumber)
-	// 	PostNftInfo, response, err := zboxClient.PostNftInfo(t,
-	// 		firebaseToken.IdToken,
-	// 		csrfToken,
-	// 		zboxClient.DefaultPhoneNumber,
-	// 	)
-	// 	require.NoError(t, err)
-	// 	require.Equal(t, 201, response.StatusCode())
-	// 	require.NotNil(t, PostNftInfo)
-	// })
+	t.RunSequentially("Posting NFT Info with valid form-data should work", func(t *test.SystemTest) {
+		teardown(t, firebaseToken.IdToken, zboxClient.DefaultPhoneNumber)
+		csrfToken := createCsrfToken(t, zboxClient.DefaultPhoneNumber)
+		PostNftInfo, response, err := zboxClient.PostNftInfo(t,
+			firebaseToken.IdToken,
+			csrfToken,
+			zboxClient.DefaultPhoneNumber,
+		)
+		require.NoError(t, err)
+		require.Equal(t, 201, response.StatusCode())
+		require.NotNil(t, PostNftInfo)
+	})
 
-	// t.RunSequentially("Putting NFT Info with valid form-data should work", func(t *test.SystemTest) {
-	// 	teardown(t, firebaseToken.IdToken, zboxClient.DefaultPhoneNumber)
-	// 	csrfToken := createCsrfToken(t, zboxClient.DefaultPhoneNumber)
-	// 	PutNftInfo, response, err := zboxClient.PutNftInfo(t,
-	// 		firebaseToken.IdToken,
-	// 		csrfToken,
-	// 		zboxClient.DefaultPhoneNumber,
-	// 	)
-	// 	require.NoError(t, err)
-	// 	require.Equal(t, 201, response.StatusCode())
-	// 	require.NotNil(t, PutNftInfo)
-	// })
+	t.RunSequentially("Putting NFT Info with valid form-data should work", func(t *test.SystemTest) {
+		teardown(t, firebaseToken.IdToken, zboxClient.DefaultPhoneNumber)
+		csrfToken := createCsrfToken(t, zboxClient.DefaultPhoneNumber)
+		PutNftInfo, response, err := zboxClient.PutNftInfo(t,
+			firebaseToken.IdToken,
+			csrfToken,
+			zboxClient.DefaultPhoneNumber,
+		)
+		require.NoError(t, err)
+		require.Equal(t, 201, response.StatusCode())
+		require.NotNil(t, PutNftInfo)
+	})
 
-	// t.RunSequentially("Putting NFT State with valid form-data should work", func(t *test.SystemTest) {
-	// 	teardown(t, firebaseToken.IdToken, zboxClient.DefaultPhoneNumber)
-	// 	csrfToken := createCsrfToken(t, zboxClient.DefaultPhoneNumber)
-	// 	PutNftState, response, err := zboxClient.PutNftState(t,
-	// 		firebaseToken.IdToken,
-	// 		csrfToken,
-	// 		zboxClient.DefaultPhoneNumber,
-	// 	)
-	// 	require.NoError(t, err)
-	// 	require.Equal(t, 201, response.StatusCode())
-	// 	require.NotNil(t, PutNftState)
-	// })
+	t.RunSequentially("Putting NFT State with valid form-data should work", func(t *test.SystemTest) {
+		teardown(t, firebaseToken.IdToken, zboxClient.DefaultPhoneNumber)
+		csrfToken := createCsrfToken(t, zboxClient.DefaultPhoneNumber)
+		PutNftState, response, err := zboxClient.PutNftState(t,
+			firebaseToken.IdToken,
+			csrfToken,
+			zboxClient.DefaultPhoneNumber,
+		)
+		require.NoError(t, err)
+		require.Equal(t, 201, response.StatusCode())
+		require.NotNil(t, PutNftState)
+	})
 
-	// t.RunSequentially("Getting NFT State with valid form-data should work", func(t *test.SystemTest) {
-	// 	teardown(t, firebaseToken.IdToken, zboxClient.DefaultPhoneNumber)
-	// 	csrfToken := createCsrfToken(t, zboxClient.DefaultPhoneNumber)
-	// 	response, err := zboxClient.GetNftState(t,
-	// 		firebaseToken.IdToken,
-	// 		csrfToken,
-	// 		zboxClient.DefaultPhoneNumber,
-	// 	)
-	// 	require.NoError(t, err)
-	// 	require.Equal(t, 200, response.StatusCode())
-	// })
+	t.RunSequentially("Getting NFT State with valid form-data should work", func(t *test.SystemTest) {
+		teardown(t, firebaseToken.IdToken, zboxClient.DefaultPhoneNumber)
+		csrfToken := createCsrfToken(t, zboxClient.DefaultPhoneNumber)
+		response, err := zboxClient.GetNftState(t,
+			firebaseToken.IdToken,
+			csrfToken,
+			zboxClient.DefaultPhoneNumber,
+		)
+		require.NoError(t, err)
+		require.Equal(t, 200, response.StatusCode())
+	})
 }
