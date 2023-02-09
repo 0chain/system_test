f3a368ac2294e24862d9e99db3945fda28e09bae
ahmedzidan <ahmedzidan_2015@hotmail.com>
2023-02-10 00:46:25 +0800

    add test for zs3server

---
+++ tests/api_tests/zs3_sever_test.go
@@ -0,0 +1,31 @@
+package api_tests
+
+import (
+	"testing"
+
+	"github.com/0chain/system_test/internal/api/util/client"
+	"github.com/0chain/system_test/internal/api/util/test"
+	"github.com/stretchr/testify/require"
+)
+
+func TestCreateBucket(testSetup *testing.T) {
+	t := test.NewSystemTest(testSetup)
+
+	t.Parallel()
+	t.Run("run createbucket test", func(t *test.SystemTest) {
+		resp, err := zs3Client.CreateBucket(t)
+		require.Nil(t, err)
+		require.Equal(t, resp.StatusCode(), client.HttpOkStatus)
+	})
+}
+
+func TestListBuckets(testSetup *testing.T) {
+	t := test.NewSystemTest(testSetup)
+
+	t.Parallel()
+	t.Run("run ListBuckets test", func(t *test.SystemTest) {
+		resp, err := zs3Client.ListBucket(t)
+		require.Nil(t, err)
+		require.Equal(t, resp.StatusCode(), client.HttpOkStatus)
+	})
+}
