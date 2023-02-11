00f913081b36d023c0c1c4216331a992987b89c1
Kishan-Dhakan <kishandhakan17@gmail.com>
2023-02-12 01:06:42 +0530

    replace regex with contains

--- tests/cli_tests/zboxcli_create_allocation_test.go
+++ tests/cli_tests/zboxcli_create_allocation_test.go
@@ -129,7 +129,7 @@ func TestCreateAllocation(testSetup *testing.T) {
 		output, err := createNewAllocationWithoutRetry(t, configPath, createParams(options))
 		require.NotNil(t, err, strings.Join(output, "\n"))
 		require.True(t, len(output) > 0, "expected output length be at least 1")
-		require.Regexp(t, regexp.MustCompile(`Error creating allocation: failed_get_allocation_blobbers: failed to get blobbers for allocation: allocation_creation_failed: Too many blobbers selected, max available \d*`), output[0], strings.Join(output, "\n"))
+		require.Contains(t, output[0], "Too many blobbers selected")
 	})
 
 	t.Run("Create allocation with too large data (Greater than the number of blobbers) Should Fail", func(t *test.SystemTest) {
@@ -139,7 +139,7 @@ func TestCreateAllocation(testSetup *testing.T) {
 		output, err := createNewAllocationWithoutRetry(t, configPath, createParams(options))
 		require.NotNil(t, err, strings.Join(output, "\n"))
 		require.True(t, len(output) > 0, "expected output length be at least 1")
-		require.Regexp(t, regexp.MustCompile(`Error creating allocation: failed_get_allocation_blobbers: failed to get blobbers for allocation: allocation_creation_failed: Too many blobbers selected, max available \d*`), output[0], strings.Join(output, "\n"))
+		require.Contains(t, output[0], "Too many blobbers selected")
 	})
 
 	t.Run("Create allocation with too large data and parity (Greater than the number of blobbers) Should Fail", func(t *test.SystemTest) {
@@ -149,7 +149,7 @@ func TestCreateAllocation(testSetup *testing.T) {
 		output, err := createNewAllocationWithoutRetry(t, configPath, createParams(options))
 		require.NotNil(t, err, strings.Join(output, "\n"))
 		require.True(t, len(output) > 0, "expected output length be at least 1")
-		require.Regexp(t, regexp.MustCompile(`Error creating allocation: failed_get_allocation_blobbers: failed to get blobbers for allocation: allocation_creation_failed: Too many blobbers selected, max available \d*`), output[0], strings.Join(output, "\n"))
+		require.Contains(t, output[0], "Too many blobbers selected")
 	})
 
 	t.Run("Create allocation with read price range 0-0 Should Fail", func(t *test.SystemTest) {
