6504a37c987b8c83f884c4d40b71962cb5ac594e
ahmedzidan <ahmedzidan_2015@hotmail.com>
2023-02-12 14:52:02 +0800

    add test for zs3server in separate files

--- internal/api/util/client/zs3_client.go
+++ internal/api/util/client/zs3_client.go
@@ -1,8 +1,6 @@
 package client
 
 import (
-	"fmt"
-
 	"github.com/0chain/system_test/internal/api/util/test"
 	"github.com/go-resty/resty/v2"
 )
@@ -24,101 +22,8 @@ func NewZS3Client(zs3ServerUrl string) *ZS3Client {
 	return zs3Client
 }
 
-func (c *ZS3Client) CreateBucket(t *test.SystemTest) (*resty.Response, error) {
-	queryParams := map[string]string{
-		"accessKey":       AccessKey,
-		"secretAccessKey": SecretAccessKey,
-		"action":          "createBucket",
-		"bucketName":      "system-test1",
-	}
-	resp, err := c.BaseHttpClient.HttpClient.R().SetQueryParams(queryParams).Get(c.zs3ServerUrl)
-	if err != nil {
-		t.Log(err)
-		return nil, err
-	}
-	t.Logf("%s returned %s with status %s", c.zs3ServerUrl, resp.String(), resp.Status())
-	return resp, nil
-}
-
-func (c *ZS3Client) ListBucket(t *test.SystemTest) (*resty.Response, error) {
-	queryParams := map[string]string{
-		"accessKey":       AccessKey,
-		"secretAccessKey": SecretAccessKey,
-		"action":          "listBuckets",
-	}
-	resp, err := c.BaseHttpClient.HttpClient.R().SetQueryParams(queryParams).Get(c.zs3ServerUrl)
-	if err != nil {
-		t.Log(err)
-		return nil, err
-	}
-	t.Logf("%s returned %s with status %s", c.zs3ServerUrl, resp.String(), resp.Status())
-	return resp, nil
-}
-
-func (c *ZS3Client) ListBucketsObjects(t *test.SystemTest) (*resty.Response, error) {
-	queryParams := map[string]string{
-		"accessKey":       AccessKey,
-		"secretAccessKey": SecretAccessKey,
-		"action":          "listBucketsObjects",
-	}
-	resp, err := c.BaseHttpClient.HttpClient.R().SetQueryParams(queryParams).Get(c.zs3ServerUrl)
-	if err != nil {
-		t.Log(err)
-		return nil, err
-	}
-	t.Logf("%s returned %s with status %s", c.zs3ServerUrl, resp.String(), resp.Status())
-	return resp, nil
-}
-
-func (c *ZS3Client) ListObjects(t *test.SystemTest) (*resty.Response, error) {
-	queryParams := map[string]string{
-		"accessKey":       AccessKey,
-		"secretAccessKey": SecretAccessKey,
-		"action":          "listObjects",
-		"bucketName":      "root",
-	}
-	resp, err := c.BaseHttpClient.HttpClient.R().SetQueryParams(queryParams).Get(c.zs3ServerUrl)
-	if err != nil {
-		t.Log(err)
-		return nil, err
-	}
-	t.Logf("%s returned %s with status %s", c.zs3ServerUrl, resp.String(), resp.Status())
-	return resp, nil
-}
-
-func (c *ZS3Client) PutObject(t *test.SystemTest) (*resty.Response, error) {
-	queryParams := map[string]string{
-		"accessKey":       AccessKey,
-		"secretAccessKey": SecretAccessKey,
-		"action":          "putObject",
-		"bucketName":      "root",
-	}
-
-	formData := map[string]string{
-		"file": "https://github.com/0chain/zs3server/blob/task/logapi/assets/main-struture.png",
-	}
-	resp, err := c.BaseHttpClient.HttpClient.R().SetFiles(formData).SetQueryParams(queryParams).Get(c.zs3ServerUrl)
-	if err != nil {
-		t.Log(err)
-		return nil, err
-	}
-	t.Logf("%s returned %s with status %s", c.zs3ServerUrl, resp.String(), resp.Status())
-	return resp, nil
-}
-
-func (c *ZS3Client) RemoveObject(t *test.SystemTest) (*resty.Response, error) {
-	queryParams := map[string]string{
-		"accessKey":       AccessKey,
-		"secretAccessKey": SecretAccessKey,
-		"action":          "removeObject",
-		"bucketName":      "root",
-		"objectName":      "main-struture.png",
-	}
-
-	formData := map[string]string{
-		"file": "https://github.com/0chain/zs3server/blob/task/logapi/assets/main-struture.png",
-	}
-	resp, err := c.BaseHttpClient.HttpClient.R().SetFiles(formData).SetQueryParams(queryParams).Get(c.zs3ServerUrl)
+func (c *ZS3Client) BucketOperation(t *test.SystemTest, queryParams map[string]string, formData map[string]string) (*resty.Response, error) {
+	resp, err := c.BaseHttpClient.HttpClient.R().SetFiles(formData).SetQueryParams(queryParams).Post(c.zs3ServerUrl)
 	if err != nil {
 		t.Log(err)
 		return nil, err
---
+++ tests/api_tests/list_objects_test.go
@@ -0,0 +1,24 @@
+package api_tests
+
+import (
+	"testing"
+
+	"github.com/0chain/system_test/internal/api/util/test"
+	"github.com/stretchr/testify/require"
+)
+
+func TestListObjects(testSetup *testing.T) {
+	t := test.NewSystemTest(testSetup)
+	t.Parallel()
+	t.Run("ListObjects should return 200 when all the parameters are correct", func(t *test.SystemTest) {
+		queryParams := map[string]string{
+			"accessKey":       AccessKey,
+			"secretAccessKey": SecretAccessKey,
+			"action":          "listObjects",
+			"bucketName":      "root",
+		}
+		resp, err := zs3Client.BucketOperation(t, queryParams, map[string]string{})
+		require.Nil(t, err)
+		require.Equal(t, 200, resp.StatusCode())
+	})
+}
