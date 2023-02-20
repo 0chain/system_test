8390d7aa357243900749c63909024bca1ff4a8c6
Kishan-Dhakan <kishandhakan17@gmail.com>
2023-02-21 01:05:25 +0530

    skip vesting sc tests

--- tests/cli_tests/zwallet_vesting_test.go
+++ tests/cli_tests/zwallet_vesting_test.go
@@ -24,6 +24,7 @@ const minDuration = "min_duration"
 const minLock = "min_lock"
 
 func TestVestingPoolAdd(testSetup *testing.T) {
+	testSetup.Skip("Enable post mainnet when vesting sc is enabled")
 	t := test.NewSystemTest(testSetup)
 
 	t.Parallel()
@@ -606,8 +607,8 @@ func TestVestingPoolAdd(testSetup *testing.T) {
 }
 
 func TestVestingPoolDelete(testSetup *testing.T) {
+	testSetup.Skip("Enable post mainnet when vesting sc is enabled")
 	t := test.NewSystemTest(testSetup)
-	t.Skip("Skip vesting pool delete tests, balance need to reduce the txn fee")
 
 	// get current valid vesting configs
 	output, err := registerWallet(t, configPath)
@@ -738,6 +739,7 @@ func vestingPoolDelete(t *test.SystemTest, cliConfigFilename, params string, ret
 }
 
 func TestVestingPoolInfo(testSetup *testing.T) {
+	testSetup.Skip("Enable post mainnet when vesting sc is enabled")
 	t := test.NewSystemTest(testSetup)
 
 	// get current valid vesting configs
@@ -944,6 +946,7 @@ func TestVestingPoolInfo(testSetup *testing.T) {
 }
 
 func TestVestingPoolStop(testSetup *testing.T) {
+	testSetup.Skip("Enable post mainnet when vesting sc is enabled")
 	t := test.NewSystemTest(testSetup)
 
 	// get current valid vesting configs
@@ -1186,6 +1189,7 @@ func TestVestingPoolStop(testSetup *testing.T) {
 }
 
 func TestVestingPoolTokenAccounting(testSetup *testing.T) {
+	testSetup.Skip("Enable post mainnet when vesting sc is enabled")
 	t := test.NewSystemTest(testSetup)
 
 	t.Run("Vesting pool with one destination should move some balance to pending which should be unlockable", func(t *test.SystemTest) {
@@ -1382,6 +1386,7 @@ func TestVestingPoolTokenAccounting(testSetup *testing.T) {
 }
 
 func TestVestingPoolTrigger(testSetup *testing.T) {
+	testSetup.Skip("Enable post mainnet when vesting sc is enabled")
 	t := test.NewSystemTest(testSetup)
 
 	// get current valid vesting configs
@@ -1572,6 +1577,7 @@ func TestVestingPoolTrigger(testSetup *testing.T) {
 }
 
 func TestVestingPoolUnlock(testSetup *testing.T) {
+	testSetup.Skip("Enable post mainnet when vesting sc is enabled")
 	t := test.NewSystemTest(testSetup)
 
 	// get current valid vesting configs
@@ -1783,6 +1789,7 @@ func TestVestingPoolUnlock(testSetup *testing.T) {
 }
 
 func TestVestingPoolUpdateConfig(testSetup *testing.T) {
+	testSetup.Skip("Enable post mainnet when vesting sc is enabled")
 	t := test.NewSystemTest(testSetup)
 	if _, err := os.Stat("./config/" + scOwnerWallet + "_wallet.json"); err != nil {
 		t.Skipf("SC owner wallet located at %s is missing", "./config/"+scOwnerWallet+"_wallet.json")
