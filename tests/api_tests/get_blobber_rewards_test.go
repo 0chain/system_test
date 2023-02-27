a101f71e9835b49f2cabd68085868b22ad87887d
Kishan-Dhakan <kishandhakan17@gmail.com>
2023-02-27 20:05:19 +0530

    unskip blobber rewards api test

--- tests/api_tests/get_blobber_rewards_test.go
+++ tests/api_tests/get_blobber_rewards_test.go
@@ -16,8 +16,6 @@ func TestBlobberRewards(testSetup *testing.T) {
 	t := test.NewSystemTest(testSetup)
 
 	t.RunSequentially("Check if blobber, which already exists in allocation as additional parity shard can receive rewards, should work", func(t *test.SystemTest) {
-		t.Skip("wait for reward fixes")
-
 		apiClient.ExecuteFaucet(t, sdkWallet, client.TxSuccessfulStatus)
 
 		blobberRequirements := model.DefaultBlobberRequirements(sdkWallet.Id, sdkWallet.PublicKey)
@@ -61,7 +59,6 @@ func TestBlobberRewards(testSetup *testing.T) {
 	})
 
 	t.RunSequentially("Check if the balance of the wallet has been changed without rewards being claimed, shouldn't work", func(t *test.SystemTest) {
-		t.Skip("wait for reward fixes")
 		apiClient.ExecuteFaucet(t, sdkWallet, client.TxSuccessfulStatus)
 
 		blobberRequirements := model.DefaultBlobberRequirements(sdkWallet.Id, sdkWallet.PublicKey)
