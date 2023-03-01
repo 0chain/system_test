cec41dcce8d9ef94634455dadb13efc4fafc2e6b
Kishan Dhakan <42718091+Kishan-Dhakan@users.noreply.github.com>
2023-03-01 19:09:35 +0530

    skip create bucket tests till zs3server issues are resolved

--- tests/api_tests/create_bucket_test.go
+++ tests/api_tests/create_bucket_test.go
@@ -12,6 +12,7 @@ func TestCreateBucket(testSetup *testing.T) {
 
 	t.Parallel()
 	t.Run("CreateBucket should return 200 when all the parameters are correct", func(t *test.SystemTest) {
+		t.Skip("skip till zs3server issues are resolved")
 		queryParams := map[string]string{
 			"accessKey":       AccessKey,
 			"secretAccessKey": SecretAccessKey,
@@ -24,6 +25,7 @@ func TestCreateBucket(testSetup *testing.T) {
 	})
 
 	t.Run("CreateBucket should return 500 when one of more required parameters are missing", func(t *test.SystemTest) {
+		t.Skip("skip till zs3server issues are resolved")
 		queryParams := map[string]string{
 			"accessKey":       AccessKey,
 			"secretAccessKey": SecretAccessKey,
