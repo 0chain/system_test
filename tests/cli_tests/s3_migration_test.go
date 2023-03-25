efc4a0c2ff7e8785acad1fe41501f6be65e573d0
ahmedzidan <ahmedzidan_2015@hotmail.com>
2023-03-25 17:02:47 +0800

    add s3 migration test

---
+++ tests/cli_tests/s3_migration_test.go
@@ -0,0 +1,37 @@
+package cli_tests
+
+import (
+	"fmt"
+	"strings"
+	"testing"
+	"time"
+
+	"github.com/0chain/system_test/internal/api/util/test"
+	cliutils "github.com/0chain/system_test/internal/cli/util"
+	"github.com/stretchr/testify/require"
+)
+
+func S3MigrationTest(testSetup *testing.T) {
+	t := test.NewSystemTest(testSetup)
+
+	t.Parallel()
+	t.Run("S3 migration should work fine when migration command run with the correct flags", func(t *test.SystemTest) {
+		output, err := S3Migrate(t, "", "", true)
+		require.NoError(t, err, "", strings.Join(output, "\n"))
+		require.Len(t, output, 1)
+	})
+}
+
+func S3Migrate(t *test.SystemTest, accessKey, secretKey string, retry bool) ([]string, error) {
+	t.Logf("Canceling allocation...")
+	cmd := fmt.Sprintf(
+		"./s3mgrt %s",
+		"help",
+	)
+
+	if retry {
+		return cliutils.RunCommand(t, cmd, 3, time.Second*2)
+	} else {
+		return cliutils.RunCommandWithoutRetry(cmd)
+	}
+}
