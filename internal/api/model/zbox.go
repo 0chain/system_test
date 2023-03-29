749fdb70cbf4d4e09b77e766d0fca530bb8c5b8d
peterlimg <peterlimg@protonmail.com>
2023-03-30 08:20:18 +1100

    Fix incorrect nft stage type

--- internal/api/model/zbox.go
+++ internal/api/model/zbox.go
@@ -140,7 +140,7 @@ type ZboxNft struct {
 	ClientId        string `json:"client_id"`
 	CollectionId    string `json:"collection_id"`
 	OwnedBy         string `json:"owned_by"`
-	Stage           string `json:"stage"`
+	Stage           int64  `json:"stage"`
 	Reference       string `json:"reference"`
 	NftActivity     string `json:"nft_activity"`
 	MetaData        string `json:"meta_data"`
