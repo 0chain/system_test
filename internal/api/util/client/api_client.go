e3a24c713a6041bd5942ee93fcb2ca05af103640
Kishan-Dhakan <kishandhakan17@gmail.com>
2023-04-10 01:02:09 +0530

    increase faucet

--- internal/api/util/client/api_client.go
+++ internal/api/util/client/api_client.go
@@ -618,6 +618,7 @@ func (c *APIClient) RegisterWalletForMnemonicWithoutAssertion(t *test.SystemTest
 // ExecuteFaucet provides basic assertions
 func (c *APIClient) ExecuteFaucet(t *test.SystemTest, wallet *model.Wallet, requiredTransactionStatus int) {
 	c.ExecuteFaucetWithTokens(t, wallet, 9.0, requiredTransactionStatus)
+	c.ExecuteFaucetWithTokens(t, wallet, 9.0, requiredTransactionStatus)
 }
 
 // ExecuteFaucet provides basic assertions
