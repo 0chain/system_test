39bf57c971f94b65a7a918556cbb184ca5eca339
Kishan-Dhakan <kishandhakan17@gmail.com>
2023-04-13 00:51:08 +0530

    fix stake unstake, share test

--- tests/cli_tests/zboxcli_stake_unstake_token_test.go
+++ tests/cli_tests/zboxcli_stake_unstake_token_test.go
@@ -224,6 +224,13 @@ func TestStakeUnstakeTokens(testSetup *testing.T) {
 		// Pick a random blobber
 		blobber := blobbers[time.Now().Unix()%int64(len(blobbers))]
 
+		// get min_stake
+		output, err = getMinerSCConfig(t, configPath, true)
+		require.Nil(t, err, strings.Join(output, "\n"))
+		require.Greater(t, len(output), 0, strings.Join(output, "\n"))
+
+		cfg, _ := keyValuePairStringToMap(output)
+
 		// Stake tokens against this blobber
 		output, err = stakeTokens(t, configPath, createParams(map[string]interface{}{
 			"blobber_id": blobber.Id,
@@ -232,7 +239,7 @@ func TestStakeUnstakeTokens(testSetup *testing.T) {
 		require.NotNil(t, err, "Expected error when staking 0 tokens than in stake pool", strings.Join(output, "\n"))
 		require.GreaterOrEqual(t, len(output), 1)
 		require.Equal(t, fmt.Sprintf("Failed to lock tokens in stake pool: stake_pool_lock_failed:"+
-			" too small stake to lock: 0 < %v", blobber.Stake_pool_settings.Min_stake), output[0])
+			" too small stake to lock: 0 < %v", cfg["min_stake"]), output[0])
 
 		// Wallet balance after staking tokens
 		output, err = getBalance(t, configPath)
