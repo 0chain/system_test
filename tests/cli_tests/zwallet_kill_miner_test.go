6a5d0c08e6571db9a55d5199f99986070d1bb063
Piers Shepperson <piers.shepperson@btinternet.com>
2023-04-12 15:44:05 +0100

    sharders

---
+++ tests/cli_tests/zwallet_kill_miner_test.go
@@ -0,0 +1,79 @@
+package cli_tests
+
+import (
+	"fmt"
+	"strings"
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/require"
+
+	"github.com/0chain/system_test/internal/api/util/test"
+	cliutil "github.com/0chain/system_test/internal/cli/util"
+	cliutils "github.com/0chain/system_test/internal/cli/util"
+)
+
+func TestKillMiner(testSetup *testing.T) { // nolint:gocyclo // team preference is to have codes all within test.
+	t := test.NewSystemTest(testSetup)
+
+	t.RunSequentiallyWithTimeout("Killed miner does not receive rewards", 200*time.Second, func(t *test.SystemTest) {
+		_ = initialiseTest(t, escapedTestName(t)+"_TARGET", true)
+
+		sharderUrl := getSharderUrl(t)
+		minerIds := getSortedMinerIds(t, sharderUrl)
+		require.True(t, len(minerIds) > 0, "no miners found")
+
+		startMiners := getNodes(t, minerIds, sharderUrl)
+		var minerToKill string
+		for i := range startMiners.Nodes {
+			if startMiners.Nodes[i].IsKilled {
+				minerToKill = startMiners.Nodes[i].ID
+				break
+			}
+		}
+		require.True(t, len(minerToKill) > 0, "no un-killed miners found")
+
+		output, err := killMiner(t, scOwnerWallet, configPath, createParams(map[string]interface{}{
+			"id": minerToKill,
+		}), true)
+		require.NoError(t, err, strings.Join(output, "\n"))
+		require.Len(t, output, 1)
+
+		cliutil.Wait(t, time.Second)
+
+		minerAfterKill := getMinersDetail(t, minerToKill)
+		require.True(t, minerAfterKill.IsKilled, "miner should be killed")
+
+		// ------------------------------------
+		cliutil.Wait(t, 10*time.Second)
+		// ------------------------------------
+
+		minerAfterRewardTest := getMinersDetail(t, minerToKill)
+		require.Equalf(t, minerAfterKill.TotalReward, minerAfterRewardTest.TotalReward,
+			"killed miner %s should not receive any more rewards", minerToKill)
+	})
+
+	t.RunSequentially("kill miner by non-smartcontract owner should fail", func(t *test.SystemTest) {
+		sharderUrl := getSharderUrl(t)
+		minerIds := getSortedMinerIds(t, sharderUrl)
+		require.True(t, len(minerIds) > 0, "no miners found")
+
+		output, err := killBlobber(t, escapedTestName(t), configPath, createParams(map[string]interface{}{
+			"id": minerIds[0],
+		}), true)
+		require.Error(t, err, "kill miner by non-smartcontract owner should fail")
+		require.Len(t, output, 1)
+		require.True(t, strings.Contains(output[0], "unauthorized access - only the owner can access"), "")
+	})
+}
+
+func killMiner(t *test.SystemTest, wallet, cliConfigFilename, params string, retry bool) ([]string, error) {
+	t.Log("kill miner...")
+	cmd := fmt.Sprintf("./zbox mn-kill %s --silent --wallet %s_wallet.json --configDir ./config --config %s",
+		params, wallet, cliConfigFilename)
+	if retry {
+		return cliutils.RunCommand(t, cmd, 3, time.Second*2)
+	} else {
+		return cliutils.RunCommandWithoutRetry(cmd)
+	}
+}
---
+++ tests/cli_tests/zwallet_kill_sharder_test.go
@@ -0,0 +1,79 @@
+package cli_tests
+
+import (
+	"fmt"
+	"strings"
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/require"
+
+	"github.com/0chain/system_test/internal/api/util/test"
+	cliutil "github.com/0chain/system_test/internal/cli/util"
+	cliutils "github.com/0chain/system_test/internal/cli/util"
+)
+
+func TestKillSharder(testSetup *testing.T) { // nolint:gocyclo // team preference is to have codes all within test.
+	t := test.NewSystemTest(testSetup)
+
+	t.RunSequentiallyWithTimeout("Killed sharder does not receive rewards", 200*time.Second, func(t *test.SystemTest) {
+		_ = initialiseTest(t, escapedTestName(t)+"_TARGET", true)
+
+		sharderUrl := getSharderUrl(t)
+		sharderIds := getSortedSharderIds(t, sharderUrl)
+		require.True(t, len(sharderIds) > 0, "no sharders found")
+
+		startSharders := getNodes(t, sharderIds, sharderUrl)
+		var sharderToKill string
+		for i := range startSharders.Nodes {
+			if startSharders.Nodes[i].IsKilled {
+				sharderToKill = startSharders.Nodes[i].ID
+				break
+			}
+		}
+		require.True(t, len(sharderToKill) > 0, "no un-killed sharders found")
+
+		output, err := killSharder(t, scOwnerWallet, configPath, createParams(map[string]interface{}{
+			"id": sharderToKill,
+		}), true)
+		require.NoError(t, err, strings.Join(output, "\n"))
+		require.Len(t, output, 1)
+
+		cliutil.Wait(t, time.Second)
+
+		sharderAfterKill := getMinersDetail(t, sharderToKill)
+		require.True(t, sharderAfterKill.IsKilled, "sharder should be killed")
+
+		// ------------------------------------
+		cliutil.Wait(t, 10*time.Second)
+		// ------------------------------------
+
+		sharderAfterRewardTest := getMinersDetail(t, sharderToKill)
+		require.Equalf(t, sharderAfterKill.TotalReward, sharderAfterRewardTest.TotalReward,
+			"killed sharder %s should not receive any more rewards", sharderToKill)
+	})
+
+	t.RunSequentially("kill sharder by non-smartcontract owner should fail", func(t *test.SystemTest) {
+		sharderUrl := getSharderUrl(t)
+		sharderIds := getSortedSharderIds(t, sharderUrl)
+		require.True(t, len(sharderIds) > 0, "no sharders found")
+
+		output, err := killBlobber(t, escapedTestName(t), configPath, createParams(map[string]interface{}{
+			"id": sharderIds[0],
+		}), true)
+		require.Error(t, err, "kill sharder by non-smartcontract owner should fail")
+		require.Len(t, output, 1)
+		require.True(t, strings.Contains(output[0], "unauthorized access - only the owner can access"), "")
+	})
+}
+
+func killSharder(t *test.SystemTest, wallet, cliConfigFilename, params string, retry bool) ([]string, error) {
+	t.Log("kill sharder...")
+	cmd := fmt.Sprintf("./zbox sh-kill %s --silent --wallet %s_wallet.json --configDir ./config --config %s",
+		params, wallet, cliConfigFilename)
+	if retry {
+		return cliutils.RunCommand(t, cmd, 3, time.Second*2)
+	} else {
+		return cliutils.RunCommandWithoutRetry(cmd)
+	}
+}
