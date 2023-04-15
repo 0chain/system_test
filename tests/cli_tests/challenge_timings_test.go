ee7c3d4216acf4d643af8bbc857c63c4fea90352
Jayash <jayash.satolia.min19@iitbhu.ac.in>
2023-04-15 20:29:58 +0530

    Update

--- tests/cli_tests/challenge_timings_test.go
+++ tests/cli_tests/challenge_timings_test.go
@@ -3,6 +3,7 @@ package cli_tests
 import (
 	"encoding/json"
 	"fmt"
+	"github.com/0chain/gosdk/core/common"
 	"github.com/0chain/system_test/internal/api/util/test"
 	climodel "github.com/0chain/system_test/internal/cli/model"
 	"github.com/stretchr/testify/require"
@@ -38,17 +39,14 @@ func TestChallengeTimings(testSetup *testing.T) {
 	require.Nil(t, err, "Error unmarshalling validator list", strings.Join(output, "\n"))
 	require.True(t, len(validatorList) > 0, "No validators found in validator list")
 
-	t.RunSequentiallyWithTimeout("Case 1: 1 allocation, 1mb each", (500*time.Minute)+(40*time.Second), func(t *test.SystemTest) {
-		stakeTokensToBlobbersAndValidators(t, blobberList, validatorList, configPath, []float64{
-			1, 1, 1, 1,
-		}, 1)
+	t.RunSequentiallyWithTimeout("Case 1: 1 10mb allocation, 1mb each", (500*time.Minute)+(40*time.Second), func(t *test.SystemTest) {
 
 		output, err := registerWallet(t, configPath)
 		require.Nil(t, err, "Error registering wallet", strings.Join(output, "\n"))
 
 		// 1. Create an allocation with 1 data shard and 1 parity shard.
 		allocationId := setupAllocationAndReadLock(t, configPath, map[string]interface{}{
-			"size":   1 * GB,
+			"size":   10 * MB,
 			"tokens": 1,
 			"data":   1,
 			"parity": 1,
@@ -58,55 +56,56 @@ func TestChallengeTimings(testSetup *testing.T) {
 
 		// Uploading 10% of allocation
 
-		for i := 0; i < 5; i++ {
-			remotepath := "/dir/"
-			filesize := 1 * MB
-			filename := generateRandomTestFileName(t)
+		remotepath := "/dir/"
+		filesize := 1 * MB
+		filename := generateRandomTestFileName(t)
 
-			err = createFileWithSize(filename, int64(filesize))
-			require.Nil(t, err)
+		err = createFileWithSize(filename, int64(filesize))
+		require.Nil(t, err)
 
-			output, err = uploadFile(t, configPath, map[string]interface{}{
-				// fetch the latest block in the chain
-				"allocation": allocationId,
-				"remotepath": remotepath + filepath.Base(filename),
-				"localpath":  filename,
-			}, true)
-			require.Nil(t, err, "error uploading file", strings.Join(output, "\n"))
-		}
+		output, err = uploadFile(t, configPath, map[string]interface{}{
+			// fetch the latest block in the chain
+			"allocation": allocationId,
+			"remotepath": remotepath + filepath.Base(filename),
+			"localpath":  filename,
+		}, true)
+		require.Nil(t, err, "error uploading file", strings.Join(output, "\n"))
 
 		time.Sleep(1 * time.Minute)
 
 		result := getChallengeTimings(t, blobberList, []string{allocationId})
 
-		proofGenTimes := result[0]
-		txnSubmissions := result[1]
-		txnVerifications := result[2]
+		fmt.Println("ProofGenTimes : ", result[0])
+		fmt.Println("TxnSubmissions : ", result[1])
+		fmt.Println("TxnVerifications : ", result[2])
 
-		for _, proofGenTime := range proofGenTimes {
-			require.True(t, proofGenTime < 10, "Proof generation time is more than 2 seconds")
-		}
+		//proofGenTimes := result[0]
+		//txnSubmissions := result[1]
+		//txnVerifications := result[2]
 
-		for _, txnSubmission := range txnSubmissions {
-			require.True(t, txnSubmission < 1680789860, "Transaction submission time is more than 2 seconds")
-		}
-
-		for _, txnVerification := range txnVerifications {
-			require.True(t, txnVerification < 1680789865, "Transaction verification time is more than 2 seconds")
-		}
+		//for _, proofGenTime := range proofGenTimes {
+		//	require.True(t, proofGenTime < 1681327150, "Proof generation time is more than 2 seconds")
+		//}
+		//
+		//for _, txnSubmission := range txnSubmissions {
+		//	require.True(t, txnSubmission == 0, "Transaction submission time is more than 2 seconds")
+		//}
+		//
+		//for _, txnVerification := range txnVerifications {
+		//	require.True(t, txnVerification < 18446744072028224700, "Transaction verification time is more than 2 seconds")
+		//}
 	})
 
-	t.RunSequentiallyWithTimeout("Case 2: 1 allocation, 10mb each", (500*time.Minute)+(40*time.Second), func(t *test.SystemTest) {
-		stakeTokensToBlobbersAndValidators(t, blobberList, validatorList, configPath, []float64{
-			1, 1, 1, 1,
-		}, 1)
+	t.Skip()
+
+	t.RunSequentiallyWithTimeout("Case 2: 1 100mb allocation, 10mb each", (500*time.Minute)+(40*time.Second), func(t *test.SystemTest) {
 
 		output, err := registerWallet(t, configPath)
 		require.Nil(t, err, "Error registering wallet", strings.Join(output, "\n"))
 
 		// 1. Create an allocation with 1 data shard and 1 parity shard.
 		allocationId := setupAllocationAndReadLock(t, configPath, map[string]interface{}{
-			"size":   1 * GB,
+			"size":   100 * MB,
 			"tokens": 1,
 			"data":   1,
 			"parity": 1,
@@ -116,22 +115,20 @@ func TestChallengeTimings(testSetup *testing.T) {
 
 		// Uploading 10% of allocation
 
-		for i := 0; i < 5; i++ {
-			remotepath := "/dir/"
-			filesize := 10 * MB
-			filename := generateRandomTestFileName(t)
+		remotepath := "/dir/"
+		filesize := 10 * MB
+		filename := generateRandomTestFileName(t)
 
-			err = createFileWithSize(filename, int64(filesize))
-			require.Nil(t, err)
+		err = createFileWithSize(filename, int64(filesize))
+		require.Nil(t, err)
 
-			output, err = uploadFile(t, configPath, map[string]interface{}{
-				// fetch the latest block in the chain
-				"allocation": allocationId,
-				"remotepath": remotepath + filepath.Base(filename),
-				"localpath":  filename,
-			}, true)
-			require.Nil(t, err, "error uploading file", strings.Join(output, "\n"))
-		}
+		output, err = uploadFile(t, configPath, map[string]interface{}{
+			// fetch the latest block in the chain
+			"allocation": allocationId,
+			"remotepath": remotepath + filepath.Base(filename),
+			"localpath":  filename,
+		}, true)
+		require.Nil(t, err, "error uploading file", strings.Join(output, "\n"))
 
 		time.Sleep(1 * time.Minute)
 
@@ -155,10 +152,9 @@ func TestChallengeTimings(testSetup *testing.T) {
 
 	})
 
-	t.RunSequentiallyWithTimeout("Case 3: 10 allocation, 10mb each", (500*time.Minute)+(40*time.Second), func(t *test.SystemTest) {
-		stakeTokensToBlobbersAndValidators(t, blobberList, validatorList, configPath, []float64{
-			1, 1, 1, 1,
-		}, 1)
+	t.Skip()
+
+	t.RunSequentiallyWithTimeout("Case 3: 10 100mb allocation, 10mb file each", (500*time.Minute)+(40*time.Second), func(t *test.SystemTest) {
 
 		var allocationIDs []string
 
@@ -170,7 +166,7 @@ func TestChallengeTimings(testSetup *testing.T) {
 
 			// 1. Create an allocation with 1 data shard and 1 parity shard.
 			allocationId := setupAllocationAndReadLock(t, configPath, map[string]interface{}{
-				"size":   1 * GB,
+				"size":   100 * MB,
 				"tokens": 1,
 				"data":   1,
 				"parity": 1,
@@ -221,10 +217,7 @@ func TestChallengeTimings(testSetup *testing.T) {
 
 	})
 
-	t.RunSequentiallyWithTimeout("Case 4: 10 allocation, 100mb each", (500*time.Minute)+(40*time.Second), func(t *test.SystemTest) {
-		stakeTokensToBlobbersAndValidators(t, blobberList, validatorList, configPath, []float64{
-			1, 1, 1, 1,
-		}, 1)
+	t.RunSequentiallyWithTimeout("Case 4: 10 1gb allocation, 100mb each", (500*time.Minute)+(40*time.Second), func(t *test.SystemTest) {
 
 		var allocationIDs []string
 
@@ -289,10 +282,7 @@ func TestChallengeTimings(testSetup *testing.T) {
 
 	t.Skip()
 
-	t.RunSequentiallyWithTimeout("Case 5: 10 allocation, 1gb each", (500*time.Minute)+(40*time.Second), func(t *test.SystemTest) {
-		stakeTokensToBlobbersAndValidators(t, blobberList, validatorList, configPath, []float64{
-			1, 1, 1, 1,
-		}, 1)
+	t.RunSequentiallyWithTimeout("Case 5: 10 10gb allocation, 1gb each", (500*time.Minute)+(40*time.Second), func(t *test.SystemTest) {
 
 		var allocationIDs []string
 
@@ -359,16 +349,16 @@ func TestChallengeTimings(testSetup *testing.T) {
 
 }
 
-func getChallengeTimings(t *test.SystemTest, blobbers []climodel.BlobberInfo, allocationIDs []string) [][]int64 {
+func getChallengeTimings(t *test.SystemTest, blobbers []climodel.BlobberInfo, allocationIDs []string) [][]common.Timestamp {
 
 	blobberUrls := []string{}
 	for _, blobber := range blobbers {
 		blobberUrls = append(blobberUrls, blobber.Url)
 	}
 
-	var proofGenTimes []int64
-	var txnSubmissions []int64
-	var txnVerifications []int64
+	var proofGenTimes []common.Timestamp
+	var txnSubmissions []common.Timestamp
+	var txnVerifications []common.Timestamp
 
 	for _, allocationID := range allocationIDs {
 		challenges, _ := getAllChallenges(t, allocationID)
@@ -386,15 +376,22 @@ func getChallengeTimings(t *test.SystemTest, blobbers []climodel.BlobberInfo, al
 
 				body, err := io.ReadAll(resp.Body)
 
-				var challengeTiming ChallengeTimings
+				var challengeTiming ChallengeTiming
 				err = json.Unmarshal(body, &challengeTiming)
 				if err != nil {
 					fmt.Println("Error while unmarshalling challenge timings : ", err)
 				}
 
-				proofGenTimes = append(proofGenTimes, challengeTiming.ProofGenTime)
-				txnSubmissions = append(txnSubmissions, challengeTiming.TxnSubmission)
-				txnVerifications = append(txnVerifications, challengeTiming.TxnVerification)
+				fmt.Println(challengeTiming.ProofGenTime)
+
+				challengeTiming.ProofGenTime = challengeTiming.ProofGenTime / 1000
+				proofGenTime := common.Timestamp(challengeTiming.ProofGenTime)
+
+				fmt.Println("Proof Gen Time : ", proofGenTime)
+
+				proofGenTimes = append(proofGenTimes, challengeTiming.CompleteValidation-proofGenTime-challengeTiming.CreatedAtBlobber)
+				txnSubmissions = append(txnSubmissions, challengeTiming.TxnSubmission-challengeTiming.CompleteValidation)
+				txnVerifications = append(txnVerifications, challengeTiming.TxnVerification-challengeTiming.TxnSubmission)
 			}
 		}
 	}
@@ -404,7 +401,7 @@ func getChallengeTimings(t *test.SystemTest, blobbers []climodel.BlobberInfo, al
 	fmt.Println("Txn Verifications : ", txnVerifications)
 
 	// max values from all the lists
-	var maxProofGenTime, maxTxnSubmission, maxTxnVerification int64
+	var maxProofGenTime, maxTxnSubmission, maxTxnVerification common.Timestamp
 
 	for _, proofGenTime := range proofGenTimes {
 		if proofGenTime > maxProofGenTime {
@@ -428,7 +425,7 @@ func getChallengeTimings(t *test.SystemTest, blobbers []climodel.BlobberInfo, al
 	fmt.Println("Max Txn Submission : ", maxTxnSubmission)
 	fmt.Println("Max Txn Verification : ", maxTxnVerification)
 
-	var result [][]int64
+	var result [][]common.Timestamp
 	result = append(result, proofGenTimes)
 	result = append(result, txnSubmissions)
 	result = append(result, txnVerifications)
@@ -436,17 +433,26 @@ func getChallengeTimings(t *test.SystemTest, blobbers []climodel.BlobberInfo, al
 	return result
 }
 
-type ChallengeTimings struct {
-	Id                 string `json:"id"`
-	CreatedAtChain     int    `json:"created_at_chain"`
-	CreatedAtBlobber   int    `json:"created_at_blobber"`
-	FileSize           int    `json:"file_size"`
-	ProofGenTime       int64  `json:"proof_gen_time"`
-	CompleteValidation int    `json:"complete_validation"`
-	TxnSubmission      int64  `json:"txn_submission"`
-	TxnVerification    int64  `json:"txn_verification"`
-	Cancelled          int    `json:"cancelled"`
-	Expiration         int    `json:"expiration"`
-	Closed             int    `json:"closed"`
-	Updated            int    `json:"updated"`
+type ChallengeTiming struct {
+	// ChallengeID is the challenge ID generated on blockchain.
+	ChallengeID string `json:"id"`
+
+	// CreatedAtChain is when generated on blockchain.
+	CreatedAtChain common.Timestamp `json:"created_at_chain"`
+	// CreatedAtBlobber is when synchronized and created at blobber.
+	CreatedAtBlobber common.Timestamp `json:"created_at_blobber"`
+	// FileSize is size of file that was randomly selected for challenge
+	FileSize int64 `json:"file_size"`
+	// ProofGenTime is the time taken in millisecond to generate challenge proof for the file
+	ProofGenTime int64 `json:"proof_gen_time"`
+	// CompleteValidation is when all validation tickets are all received.
+	CompleteValidation common.Timestamp `json:"complete_validation"`
+	// TxnSubmission is when challenge response is first sent to blockchain.
+	TxnSubmission common.Timestamp `json:"txn_submission"`
+	// TxnVerification is when challenge response is verified on blockchain.
+	TxnVerification common.Timestamp `json:"txn_verification"`
+	// Cancelled is when challenge is cancelled by blobber due to expiration or bad challenge data (eg. invalid ref or not a file) which is impossible to validate.
+	Cancelled common.Timestamp `json:"cancelled"`
+	// Expiration is when challenge is marked as expired by blobber.
+	Expiration common.Timestamp `json:"expiration"`
 }
