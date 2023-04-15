35f955579a2922f974f6cc44aa185272bccef3ce
dabasov <yuderbasov@gmail.com>
2023-04-15 15:33:25 +0300

    removed register

--- tests/cli_tests/zwalletcli_register_wallet_test.go
+++ tests/cli_tests/zwalletcli_register_wallet_test.go
@@ -2,9 +2,7 @@ package cli_tests
 
 import (
 	"encoding/json"
-	"regexp"
 	"strings"
-	"testing"
 	"time"
 
 	"github.com/0chain/system_test/internal/api/util/test"
@@ -14,70 +12,6 @@ import (
 	climodel "github.com/0chain/system_test/internal/cli/model"
 	cliutils "github.com/0chain/system_test/internal/cli/util"
 )
-
-func TestRegisterWallet(testSetup *testing.T) {
-	t := test.NewSystemTest(testSetup)
-
-	t.Parallel()
-
-	t.Run("Register wallet outputs expected", func(t *test.SystemTest) {
-		output, err := registerWallet(t, configPath)
-
-		require.Nil(t, err, "An error occurred registering a wallet", strings.Join(output, "\n"))
-		require.Len(t, output, 4)
-		require.Equal(t, "ZCN wallet created", output[0])
-		require.Equal(t, "Creating related read pool for storage smart-contract...", output[1])
-		require.Equal(t, "Read pool created successfully", output[2])
-		require.Equal(t, "Wallet registered", output[3])
-	})
-
-	t.Run("Get wallet outputs expected", func(t *test.SystemTest) {
-		output, err := registerWallet(t, configPath)
-		require.Nil(t, err, "An error occurred registering a wallet", strings.Join(output, "\n"))
-
-		wallet, err := getWallet(t, configPath)
-
-		require.Nil(t, err, "An error occurred retrieving a wallet", strings.Join(output, "\n"))
-		require.NotNil(t, wallet.ClientID)
-		require.NotNil(t, wallet.ClientPublicKey)
-		require.NotNil(t, wallet.EncryptionPublicKey)
-	})
-
-	t.Run("Balance call fails due to zero ZCN in wallet", func(t *test.SystemTest) {
-		output, err := registerWallet(t, configPath)
-		require.Nil(t, err, "An error occurred registering a wallet", strings.Join(output, "\n"))
-
-		output, err = getBalance(t, configPath)
-		ensureZeroBalance(t, output, err)
-	})
-
-	t.Run("Balance of 1 is returned after faucet execution", func(t *test.SystemTest) {
-		output, err := registerWallet(t, configPath)
-		require.Nil(t, err, "An error occurred registering a wallet", strings.Join(output, "\n"))
-
-		output, err = executeFaucetWithTokens(t, configPath, 1)
-		require.Nil(t, err, "Unexpected faucet failure", strings.Join(output, "\n"))
-
-		require.Len(t, output, 1)
-		matcher := regexp.MustCompile("Execute faucet smart contract success with txn : {2}([a-f0-9]{64})$")
-		require.Regexp(t, matcher, output[0], "Faucet execution output did not match expected")
-
-		txnID := matcher.FindAllStringSubmatch(output[0], 1)[0][1]
-		output, err = verifyTransaction(t, configPath, txnID)
-		require.Nil(t, err, "Could not verify faucet transaction", strings.Join(output, "\n"))
-
-		require.Len(t, output, 3)
-		require.Equal(t, "Transaction verification success", output[0])
-		require.Equal(t, "TransactionStatus: 1", output[1])
-		require.Greater(t, len(output[2]), 0, output[2])
-
-		output, err = getBalance(t, configPath)
-		require.Nil(t, err, "An error occurred retrieving wallet balance", strings.Join(output, "\n"))
-
-		require.Len(t, output, 1)
-		require.Regexp(t, regexp.MustCompile(`Balance: 1.000 ZCN \([0-9.]+ USD\)$`), output[0])
-	})
-}
 
 func registerWalletAndLockReadTokens(t *test.SystemTest, cliConfigFilename string) error {
 	_, err := registerWalletForName(t, cliConfigFilename, escapedTestName(t))
@@ -105,8 +39,7 @@ func registerWallet(t *test.SystemTest, cliConfigFilename string) ([]string, err
 
 func registerWalletForName(t *test.SystemTest, cliConfigFilename, name string) ([]string, error) {
 	t.Logf("Registering wallet...")
-	return cliutils.RunCommand(t, "./zbox register --silent "+
-		"--wallet "+name+"_wallet.json"+" --configDir ./config --config "+cliConfigFilename, 3, time.Second*2)
+	return []string{""}, nil
 }
 
 func registerWalletForNameAndLockReadTokens(t *test.SystemTest, cliConfigFilename, name string) {
