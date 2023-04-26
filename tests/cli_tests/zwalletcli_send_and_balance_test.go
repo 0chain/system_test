ab5e4c487f87df02e1d2f16ad6cd82961730bcac
Kishan-Dhakan <kishandhakan17@gmail.com>
2023-04-27 01:11:24 +0530

    revert jayash's changes

--- tests/cli_tests/zwalletcli_send_and_balance_test.go
+++ tests/cli_tests/zwalletcli_send_and_balance_test.go
@@ -36,8 +36,6 @@ func TestSendAndBalance(testSetup *testing.T) {
 		output, err = registerWalletForName(t, configPath, targetWallet)
 		require.Nil(t, err, "Unexpected register wallet failure", strings.Join(output, "\n"))
 
-		fmt.Println("output : ", output)
-
 		target, err := getWalletForName(t, configPath, targetWallet)
 		require.Nil(t, err, "Error occurred when retrieving target wallet")
 
@@ -133,7 +131,7 @@ func TestSendAndBalance(testSetup *testing.T) {
 	t.Run("Send attempt on zero ZCN wallet should fail", func(t *test.SystemTest) {
 		targetWallet := escapedTestName(t) + "_TARGET"
 
-		output, err := registerWallet(t, configPath)
+		output, err := registerWallet(t, configPath, withNoFaucetPour())
 		require.Nil(t, err, "Unexpected register wallet failure", strings.Join(output, "\n"))
 
 		output, err = registerWalletForName(t, configPath, targetWallet)
