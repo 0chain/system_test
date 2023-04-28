7b6e562a001c8c299b4e80b54082888842013fc7
Hitenjain14 <hitenjain146@gmail.com>
2023-04-28 14:45:42 +0530

    rmv alloc change

--- tests/api_tests/get_blobber_rewards_test.go
+++ tests/api_tests/get_blobber_rewards_test.go
@@ -106,7 +106,7 @@ func TestBlobberRewards(testSetup *testing.T) {
 
 	t.RunSequentiallyWithTimeout("Check if a new added blobber as additional parity shard to allocation can receive rewards, should work", 3*time.Minute, func(t *test.SystemTest) {
 		apiClient.ExecuteFaucetWithTokens(t, sdkWallet, 9.0, client.TxSuccessfulStatus)
-		apiClient.ExecuteFaucetWithTokens(t, sdkWallet, 18.0, client.TxSuccessfulStatus) // Needs more tokens
+		apiClient.ExecuteFaucetWithTokens(t, sdkWallet, 9.0, client.TxSuccessfulStatus) // Needs more tokens
 
 		blobberRequirements := model.DefaultBlobberRequirements(sdkWallet.Id, sdkWallet.PublicKey)
 		allocationBlobbers := apiClient.GetAllocationBlobbers(t, sdkWallet, &blobberRequirements, client.HttpOkStatus)
