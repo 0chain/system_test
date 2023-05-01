0ba966882e13b095e8237e20e1b4dbfd1731a651
Hitenjain14 <hitenjain146@gmail.com>
2023-05-01 14:34:13 +0530

    added delete test for rollback

--- tests/cli_tests/zboxcli_rollback_test.go
+++ tests/cli_tests/zboxcli_rollback_test.go
@@ -3,6 +3,7 @@ package cli_tests
 import (
 	"fmt"
 	"os"
+	"path"
 	"path/filepath"
 	"strconv"
 	"strings"
@@ -16,11 +17,11 @@ import (
 
 func TestRollbackAllocation(testSetup *testing.T) {
 	t := test.NewSystemTest(testSetup)
-
+	t.Parallel()
 	err := os.MkdirAll("tmp", os.ModePerm)
 	require.Nil(t, err)
 
-	t.RunSequentially("rollback allocation", func(t *test.SystemTest) {
+	t.Run("rollback allocation after updating a file should work", func(t *test.SystemTest) {
 
 		allocationID := setupAllocationAndReadLock(t, configPath, map[string]interface{}{
 			"size":   4 * MB,
@@ -80,6 +81,56 @@ func TestRollbackAllocation(testSetup *testing.T) {
 		require.Equal(t, originalFileChecksum, downloadedFileChecksum)
 
 		createAllocationTestTeardown(t, allocationID)
+	})
+
+	t.Run("rollback allocation after deleting a file should work", func(t *test.SystemTest) {
+		allocationID := setupAllocationAndReadLock(t, configPath, map[string]interface{}{
+			"size":   4 * MB,
+			"tokens": 9,
+		})
+
+		filesize := int64(0.5 * MB)
+		remotepath := "/"
+		localFilePath := generateFileAndUpload(t, allocationID, remotepath, filesize)
+		remoteFilePath := path.Join(remotepath, localFilePath)
+
+		originalFileChecksum := generateChecksum(t, localFilePath)
+
+		err := os.Remove(localFilePath)
+		require.Nil(t, err)
+
+		output, err := deleteFile(t, escapedTestName(t), createParams(map[string]interface{}{
+			"allocation": allocationID,
+			"remotepath": remoteFilePath,
+		}), true)
+		require.Nil(t, err, strings.Join(output, "\n"))
+		require.Len(t, output, 1)
+		require.Equal(t, fmt.Sprintf("%s deleted", remoteFilePath), output[0])
+
+		output, err = rollbackAllocation(t, escapedTestName(t), configPath, createParams(map[string]interface{}{
+			"allocation": allocationID,
+		}))
+		t.Log(strings.Join(output, "\n"))
+		require.NoError(t, err, strings.Join(output, "\n"))
+		require.Len(t, output, 1)
+
+		output, err = downloadFile(t, configPath, createParams(map[string]interface{}{
+			"allocation": allocationID,
+			"remotepath": remotepath + filepath.Base(localFilePath),
+			"localpath":  "tmp/",
+		}), true)
+		require.Nil(t, err, strings.Join(output, "\n"))
+		require.Len(t, output, 2)
+
+		require.Contains(t, output[1], StatusCompletedCB)
+		require.Contains(t, output[1], filepath.Base(localFilePath))
+
+		downloadedFileChecksum := generateChecksum(t, "tmp/"+filepath.Base(localFilePath))
+
+		require.Equal(t, originalFileChecksum, downloadedFileChecksum)
+
+		createAllocationTestTeardown(t, allocationID)
+
 	})
 
 }
