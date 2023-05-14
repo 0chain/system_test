ec7c6e84ea7d2f407f1b96bad07a33d86df12953
Kishan Dhakan <42718091+Kishan-Dhakan@users.noreply.github.com>
2023-05-14 18:49:13 +0530

    Add staked capacity test (#659)
    * add staked capacity test
    
    * lint fixex
    
    * use 2 data 2 parity
    
    * refactor
    
    * Debugging with logs
    
    * Fix type of unit to int64
    
    * Added logs for testing
    
    * Fix
    
    * Added more logs
    
    * Uncommented total offers
    
    * Added changes to make it compatible with the new time unit
    
    * Fix
    
    * Removed print statements
    
    * Fix lint
    
    * Cleaned code by adding functions and comments
    
    * Fix lint
    
    * Cleanup
    
    * Cleanup
    
    * Fix linting
    
    * Fix linting
    
    ---------
    
    Co-authored-by: Jayash <jayash.satolia.min19@iitbhu.ac.in>
    Co-authored-by: Jayash Satolia <73050737+Jayashsatolia403@users.noreply.github.com>

--- internal/cli/model/model.go
+++ internal/cli/model/model.go
@@ -164,13 +164,20 @@ type Settings struct {
 }
 
 type BlobberInfo struct {
-	Id                  string   `json:"id"`
-	Url                 string   `json:"url"`
-	Capacity            int      `json:"capacity"`
-	Last_health_check   int      `json:"last_health_check"`
-	Allocated           int      `json:"allocated"`
-	Terms               Terms    `json:"terms"`
-	Stake_pool_settings Settings `json:"stake_pool_settings"`
+	Id                       string            `json:"id"`
+	Url                      string            `json:"url"`
+	Terms                    Terms             `json:"terms"`
+	Capacity                 int64             `json:"capacity"`
+	Allocated                int64             `json:"allocated"`
+	LastHealthCheck          int64             `json:"last_health_check"`
+	StakePoolSettings        StakePoolSettings `json:"stake_pool_settings"`
+	TotalStake               int64             `json:"total_stake"`
+	UsedAllocation           int64             `json:"used_allocation"`
+	TotalOffers              int64             `json:"total_offers"`
+	TotalServiceCharge       int64             `json:"total_service_charge"`
+	UncollectedServiceCharge int64             `json:"uncollected_service_charge"`
+	IsKilled                 bool              `json:"is_killed"`
+	IsShutdown               bool              `json:"is_shutdown"`
 }
 
 type ChallengePoolInfo struct {
