054a9958181e2ce3f745767419d805c7d1a88d81
Dell <shalinik@iitbhilai.ac.in>
2023-05-17 02:04:39 +0530

    fix

--- tests/api_tests/zs3server_opertions_test.go
+++ tests/api_tests/zs3server_opertions_test.go
@@ -7,6 +7,7 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
+// adding comment to run ppipeline again
 func TestZs3ServerOpertions(testSetup *testing.T) {
 	t := test.NewSystemTest(testSetup)
 	t.Parallel()
