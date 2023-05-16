207eafd57972f92e9c7f5ad124344690efdc4690
din-mukhammed <dimash.r53@gmail.com>
2023-05-16 23:44:30 +0600

    fix expected rc count

--- tests/cli_tests/readmarker_test.go
+++ tests/cli_tests/readmarker_test.go
@@ -365,7 +365,7 @@ func TestReadMarker(testSetup *testing.T) {
 		require.Len(t, readMarkers, blobbersRequiredForDownload)
 
 		for _, rm := range readMarkers {
-			require.Equal(t, int64(7), rm.ReadCounter)
+			require.Equal(t, int64(6), rm.ReadCounter)
 		}
 
 		afterCount := CountReadMarkers(t, allocationID, sharderUrl)
