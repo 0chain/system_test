7fea80cbf8047599101cdc68c0aeeb877c1b5c85
stewartie4 <ryanstewart456@gmail.com>
2023-05-18 01:40:47 +0100

    fix error msg

--- tests/cli_tests/s3mgrt_migrate_test.go
+++ tests/cli_tests/s3mgrt_migrate_test.go
@@ -73,7 +73,7 @@ func Test0S3Migration(testSetup *testing.T) {
 
 		require.NotNil(t, err, "Expected a migration failure but got no error", strings.Join(output, "\n"))
 		require.Greater(t, len(output), 0, "More/Less output was returned than expected", strings.Join(output, "\n"))
-		require.Contains(t, output[0], "Error: after 3 attempts, last error: consensus_not_met: Upload commit failed. Required consensus atleast 3, got 0", "Output was not as expected", strings.Join(output, "\n")) //FIXME: typo in error message, message could also be more specific to allocation being too small
+		require.Contains(t, output[0], "Max size reached for the allocation with this blobber", "Output was not as expected", strings.Join(output, "\n"))
 	})
 
 	t.Run("Should fail when bucket does not exist", func(t *test.SystemTest) {
