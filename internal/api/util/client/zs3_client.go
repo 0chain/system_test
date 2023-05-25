af991e058d3e0b70a3e34d522e17b4e6d571d983
shalinikum <48850514+shalinikum@users.noreply.github.com>
2023-05-26 00:06:36 +0530

    Zs3server test (#634)
    * added new test case
    
    * test are done
    
    * removed the errors
    
    * fixing issue
    
    * added skip with issue link
    
    * fixed lint issue
    
    * added get object endpoint
    
    * added get endpoint
    
    * fix
    
    * lint fixed
    
    * lint fix
    
    * changed method
    
    * fixed
    
    * fix
    
    * resolved comments
    
    * merged file
    
    * revert api_tests_config.yaml
    
    ---------
    
    Co-authored-by: Ryan <ryanstewart456@gmail.com>
    Co-authored-by: Kishan Dhakan <42718091+Kishan-Dhakan@users.noreply.github.com>

--- internal/api/util/client/zs3_client.go
+++ internal/api/util/client/zs3_client.go
@@ -1,6 +1,12 @@
 package client
 
 import (
+	"bytes"
+	"io"
+	"mime/multipart"
+	"os"
+	"strings"
+
 	"github.com/0chain/system_test/internal/api/util/test"
 	resty "github.com/go-resty/resty/v2"
 )
@@ -24,6 +30,51 @@ func NewZS3Client(zs3ServerUrl string) *ZS3Client {
 
 func (c *ZS3Client) BucketOperation(t *test.SystemTest, queryParams, formData map[string]string) (*resty.Response, error) {
 	resp, err := c.BaseHttpClient.HttpClient.R().SetFiles(formData).SetQueryParams(queryParams).Post(c.zs3ServerUrl)
+	if err != nil {
+		t.Log(err)
+		return nil, err
+	}
+	t.Logf("%s returned %s with status %s", c.zs3ServerUrl, resp.String(), resp.Status())
+	return resp, nil
+}
+
+func createForm(form map[string]string) (string, io.Reader, error) {
+	body := new(bytes.Buffer)
+	mp := multipart.NewWriter(body)
+	defer mp.Close()
+	for key, val := range form {
+		if strings.HasPrefix(val, "@") {
+			val = val[1:]
+			file, err := os.Open(val)
+			if err != nil {
+				return "", nil, err
+			}
+			defer file.Close() //nolint:gocritic
+			part, err := mp.CreateFormFile(key, val)
+			if err != nil {
+				return "", nil, err
+			}
+			_, err = io.Copy(part, file)
+			if err != nil {
+				return "", nil, err
+			}
+		} else {
+			err := mp.WriteField(key, val)
+			if err != nil {
+				return "", nil, err
+			}
+		}
+	}
+	return mp.FormDataContentType(), body, nil
+}
+
+func (c *ZS3Client) PutObject(t *test.SystemTest, queryParams, formData map[string]string) (*resty.Response, error) {
+	ct, body, err := createForm(formData)
+	if err != nil {
+		t.Log(err)
+		return nil, err
+	}
+	resp, err := c.BaseHttpClient.HttpClient.R().SetBody(body).SetHeaders(map[string]string{"Content-Type": ct}).SetQueryParams(queryParams).Get(c.zs3ServerUrl)
 	if err != nil {
 		t.Log(err)
 		return nil, err
