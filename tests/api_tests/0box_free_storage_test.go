ffa804118a7ea11e7c81d33736be43af5f5a38cb
Dell <shalinik@iitbhilai.ac.in>
2023-05-28 01:57:06 +0530

    all testcase done

--- tests/api_tests/0box_free_storage_test.go
+++ tests/api_tests/0box_free_storage_test.go
@@ -7,6 +7,7 @@ import (
 	"testing"
 
 	"github.com/0chain/system_test/internal/api/model"
+	"gopkg.in/errgo.v2/errors"
 
 	"github.com/0chain/system_test/internal/api/util/test"
 	"github.com/stretchr/testify/require"
@@ -41,52 +42,84 @@ func Test0BoxFreeStorage(testSetup *testing.T) {
 		require.NotNil(t, zboxWallet)
 		require.Equal(t, walletName, zboxWallet.Name, "Wallet name does not match expected")
 
-		storageMarker, response, err := zboxClient.CreateFreeStorage(t, zboxClient.DefaultMnemonic, walletName, description, firebaseToken.IdToken, csrfToken, zboxClient.DefaultPhoneNumber, "blimp")
+		storageMarker, response, err := zboxClient.CreateFreeStorage(t,
+			zboxClient.DefaultMnemonic,
+			walletName,
+			description,
+			firebaseToken.IdToken,
+			csrfToken,
+			zboxClient.DefaultPhoneNumber,
+			"blimp")
 		require.NoError(t, err)
 		require.Equal(t, 200, response.StatusCode(), "Response status code does not match expected. Output: [%v]", response.String())
 
-		marker, markerResponse := UnmarshalData(storageMarker)
+		marker, markerResponse, err := UnmarshalMarkerData(storageMarker)
+		require.Nil(t, err)
 		require.Equal(t, marker.Assigner, zboxClient.DefaultRecieverId)
 		require.Equal(t, markerResponse.RecipientPublicKey, zboxClient.DefaultPhoneNumber)
 	})
+
+	t.RunSequentially("Create FreeStorage should not work more than once", func(t *test.SystemTest) {
+		teardown(t, firebaseToken.IdToken, zboxClient.DefaultPhoneNumber)
+		csrfToken := createCsrfToken(t, zboxClient.DefaultPhoneNumber)
+		description := "wallet created as part of " + t.Name()
+		walletName := "wallet_name"
+		zboxWallet, response, err := zboxClient.PostWallet(t,
+			zboxClient.DefaultMnemonic,
+			walletName,
+			description,
+			firebaseToken.IdToken,
+			csrfToken,
+			zboxClient.DefaultPhoneNumber,
+			"blimp",
+		)
+		require.NoError(t, err)
+		require.Equal(t, 201, response.StatusCode(), "Response status code does not match expected. Output: [%v]", response.String())
+		require.NotNil(t, zboxWallet)
+		require.Equal(t, walletName, zboxWallet.Name, "Wallet name does not match expected")
+
+		_, response, err = zboxClient.CreateFreeStorage(t,
+			zboxClient.DefaultMnemonic,
+			walletName,
+			description,
+			firebaseToken.IdToken,
+			csrfToken,
+			zboxClient.DefaultPhoneNumber,
+			"blimp")
+		require.NotNil(t, err)
+		require.Equal(t, 400, response.StatusCode(), "Response status code does not match expected. Output: [%v]", response.String())
+
+	})
+
 }
 
-func UnmarshalData(storage *model.ZboxFreeStorage) (model.ZboxFreeStorageMarker, model.ZboxFreeStorageMarkerResponse) {
-	decodedBytes, err := base64.StdEncoding.DecodeString(storageMarker.Marker)
+func UnmarshalMarkerData(storage *model.ZboxFreeStorage) (model.ZboxFreeStorageMarker, model.ZboxFreeStorageMarkerResponse, error) {
+	decodedBytes, err := base64.StdEncoding.DecodeString(storage.Marker)
 	if err != nil {
-		fmt.Println("Error decoding string:", err)
-		return
+		return model.ZboxFreeStorageMarker{}, model.ZboxFreeStorageMarkerResponse{}, errors.New("error decode free storage response")
 	}
 
 	// Converting bytes to string
 	decodedString := string(decodedBytes)
-	t.Logf("the decoded stringg is %v ", decodedString)
-	// Printing the decoded string
-	fmt.Println("Decoded string:", decodedString)
-
-	// Removing whitespace
-
-	// Unmarshaling into struct
 	var markerResponse model.ZboxFreeStorageMarkerResponse
 	err = json.Unmarshal([]byte(decodedString), &markerResponse)
 	if err != nil {
 		fmt.Println("Error unmarshaling JSON:", err)
-		return model.ZboxFreeStorageMarker{}, model.ZboxFreeStorageMarkerResponse{}
+		return model.ZboxFreeStorageMarker{}, model.ZboxFreeStorageMarkerResponse{}, errors.New("error unmarshaling storage response")
 	}
+
 	decodedBytes, err = base64.StdEncoding.DecodeString(markerResponse.Marker)
 	if err != nil {
 		fmt.Println("Error decoding string:", err)
-		return model.ZboxFreeStorageMarker{}, model.ZboxFreeStorageMarkerResponse{}
+		return model.ZboxFreeStorageMarker{}, model.ZboxFreeStorageMarkerResponse{}, errors.New("error decoding marker")
 	}
-
-	// Converting bytes to string
 	decodedString = string(decodedBytes)
 	var marker model.ZboxFreeStorageMarker
 	err = json.Unmarshal([]byte(decodedString), &marker)
 	if err != nil {
 		fmt.Println("Error unmarshaling JSON:", err)
-		return model.ZboxFreeStorageMarker{}, model.ZboxFreeStorageMarkerResponse{}
+		return model.ZboxFreeStorageMarker{}, model.ZboxFreeStorageMarkerResponse{}, errors.New("error unmarshling marker")
 	}
-	return marker, markerResponse
+	return marker, markerResponse, nil
 
 }
