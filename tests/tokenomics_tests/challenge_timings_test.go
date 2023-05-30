10bf61e63ae0f61a701feedab514cdc1f4773960
Jayash <jayash.satolia.min19@iitbhu.ac.in>
2023-05-31 00:46:34 +0530

    Updated challenge timings model

--- tests/tokenomics_tests/challenge_timings_test.go
+++ tests/tokenomics_tests/challenge_timings_test.go
@@ -443,28 +443,58 @@ func getChallengeTimings(t *test.SystemTest, blobbers []climodel.BlobberInfo, al
 	return result
 }
 
+//type ChallengeTiming struct {
+//	// ChallengeID is the challenge ID generated on blockchain.
+//	ChallengeID string `json:"id"`
+//
+//	// CreatedAtChain is when generated on blockchain.
+//	CreatedAtChain common.Timestamp `json:"created_at_chain"`
+//	// CreatedAtBlobber is when synchronized and created at blobber.
+//	CreatedAtBlobber common.Timestamp `json:"created_at_blobber"`
+//	// FileSize is size of file that was randomly selected for challenge
+//	FileSize int64 `json:"file_size"`
+//	// ProofGenTime is the time taken in millisecond to generate challenge proof for the file
+//	ProofGenTime int64 `json:"proof_gen_time"`
+//	// CompleteValidation is when all validation tickets are all received.
+//	CompleteValidation common.Timestamp `json:"complete_validation"`
+//	// TxnSubmission is when challenge response is first sent to blockchain.
+//	TxnSubmission common.Timestamp `json:"txn_submission"`
+//	// TxnVerification is when challenge response is verified on blockchain.
+//	TxnVerification common.Timestamp `json:"txn_verification"`
+//	// Cancelled is when challenge is cancelled by blobber due to expiration or bad challenge data (eg. invalid ref or not a file) which is impossible to validate.
+//	Cancelled common.Timestamp `json:"cancelled"`
+//	// Expiration is when challenge is marked as expired by blobber.
+//	Expiration common.Timestamp `json:"expiration"`
+//}
+
 type ChallengeTiming struct {
 	// ChallengeID is the challenge ID generated on blockchain.
-	ChallengeID string `json:"id"`
+	ChallengeID string `gorm:"column:challenge_id;size:64;primaryKey" json:"id"`
 
 	// CreatedAtChain is when generated on blockchain.
-	CreatedAtChain common.Timestamp `json:"created_at_chain"`
+	CreatedAtChain common.Timestamp `gorm:"created_at_chain" json:"created_at_chain"`
 	// CreatedAtBlobber is when synchronized and created at blobber.
-	CreatedAtBlobber common.Timestamp `json:"created_at_blobber"`
+	CreatedAtBlobber common.Timestamp `gorm:"created_at_blobber" json:"created_at_blobber"`
 	// FileSize is size of file that was randomly selected for challenge
-	FileSize int64 `json:"file_size"`
+	FileSize int64 `gorm:"file_size" json:"file_size"`
 	// ProofGenTime is the time taken in millisecond to generate challenge proof for the file
-	ProofGenTime int64 `json:"proof_gen_time"`
+	ProofGenTime int64 `gorm:"proof_gen_time" json:"proof_gen_time"`
 	// CompleteValidation is when all validation tickets are all received.
-	CompleteValidation common.Timestamp `json:"complete_validation"`
+	CompleteValidation common.Timestamp `gorm:"complete_validation" json:"complete_validation"`
 	// TxnSubmission is when challenge response is first sent to blockchain.
-	TxnSubmission common.Timestamp `json:"txn_submission"`
+	TxnSubmission common.Timestamp `gorm:"txn_submission" json:"txn_submission"`
 	// TxnVerification is when challenge response is verified on blockchain.
-	TxnVerification common.Timestamp `json:"txn_verification"`
+	TxnVerification common.Timestamp `gorm:"txn_verification" json:"txn_verification"`
 	// Cancelled is when challenge is cancelled by blobber due to expiration or bad challenge data (eg. invalid ref or not a file) which is impossible to validate.
-	Cancelled common.Timestamp `json:"cancelled"`
+	Cancelled common.Timestamp `gorm:"cancelled" json:"cancelled"`
 	// Expiration is when challenge is marked as expired by blobber.
-	Expiration common.Timestamp `json:"expiration"`
+	Expiration common.Timestamp `gorm:"expiration" json:"expiration"`
+
+	// ClosedAt is when challenge is closed (eg. expired, cancelled, or completed/verified).
+	ClosedAt common.Timestamp `gorm:"column:closed_at;index:idx_closed_at,sort:desc;" json:"closed"`
+
+	// UpdatedAt is when row is last updated.
+	UpdatedAt common.Timestamp `gorm:"column:updated_at;index:idx_updated_at,sort:desc;" json:"updated"`
 }
 
 func getAllChallenges(t *test.SystemTest, allocationID string) ([]Challenge, error) {
