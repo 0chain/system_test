7b4dc3733a86426551a27ca8c869d3b0d74a3d41
Jayash <jayash.satolia.min19@iitbhu.ac.in>
2023-06-05 18:47:29 +0530

    Update

--- tests/cli_tests/flaky___broken_scenarios_test.go
+++ tests/cli_tests/flaky___broken_scenarios_test.go
@@ -7,9 +7,9 @@ import (
 	"os"
 	"path/filepath"
 	"regexp"
-	"strconv"
 	"strings"
 	"testing"
+	"time"
 
 	"github.com/0chain/system_test/internal/api/util/test"
 
@@ -32,6 +32,12 @@ func Test___FlakyBrokenScenarios(testSetup *testing.T) {
 	require.Nil(t, err)
 
 	t.Parallel()
+
+	// jsonPrint challengePool
+	printJson := func(message string, v interface{}) {
+		b, _ := json.MarshalIndent(v, "", "  ")
+		fmt.Println(message, string(b))
+	}
 
 	// FIXME The test is failing due to sync function inability to detect the file changes in local folder see https://github.com/0chain/zboxcli/issues/250
 	t.Run("Sync path to non-empty allocation - locally updated files (in root) must be updated in allocation", func(t *test.SystemTest) {
@@ -294,6 +300,7 @@ func Test___FlakyBrokenScenarios(testSetup *testing.T) {
 		var differences []climodel.FileDiff
 		err = json.Unmarshal([]byte(output[0]), &differences)
 		require.Nil(t, err, "Error deserializing JSON string `%s`: %v", strings.Join(output, "\n"), err)
+
 		require.Len(t, differences, 2, "Since we added a file and we updated 2 files (1 excluded) we expect 2 differences but we got %v", len(differences))
 
 		output, err = syncFolder(t, configPath, map[string]interface{}{
@@ -330,7 +337,7 @@ func Test___FlakyBrokenScenarios(testSetup *testing.T) {
 	})
 
 	// FIXME: WRITEPOOL TOKEN ACCOUNTING
-	t.Run("Tokens should move from write pool balance to challenge pool acc. to expected upload cost", func(t *test.SystemTest) {
+	t.RunWithTimeout("Tokens should move from write pool balance to challenge pool acc. to expected upload cost", 10*time.Minute, func(t *test.SystemTest) {
 		output, err := createWallet(t, configPath)
 		require.Nil(t, err, "Failed to create wallet", strings.Join(output, "\n"))
 
@@ -340,7 +347,7 @@ func Test___FlakyBrokenScenarios(testSetup *testing.T) {
 		allocParam := createParams(map[string]interface{}{
 			"lock":   balance,
 			"size":   10485760,
-			"expire": "1h",
+			"expire": "10m",
 		})
 		output, err = createNewAllocation(t, configPath, allocParam)
 		require.Nil(t, err, "Failed to create new allocation", strings.Join(output, "\n"))
@@ -355,23 +362,26 @@ func Test___FlakyBrokenScenarios(testSetup *testing.T) {
 		initialAllocation := getAllocation(t, allocationID)
 		require.Equal(t, 0.8, intToZCN(initialAllocation.WritePool))
 
+		// Get Challenge-Pool info after upload
+		output, err = challengePoolInfo(t, configPath, allocationID)
+		fmt.Println("Challenge Pool Info: ", output)
+		require.Nil(t, err, "Could not fetch challenge pool", strings.Join(output, "\n"))
+
+		challengePool := climodel.ChallengePoolInfo{}
+		err = json.Unmarshal([]byte(output[0]), &challengePool)
+		require.Nil(t, err, "Error unmarshalling challenge pool info", strings.Join(output, "\n"))
+
+		printJson("Challenge Pool: ", challengePool)
+
 		filename := generateRandomTestFileName(t)
-		err = createFileWithSize(filename, 1024*5)
+		err = createFileWithSize(filename, 1024*1024*0.5)
 		require.Nil(t, err, "error while generating file: ", err)
 
-		// Get expected upload cost
-		output, _ = getUploadCostInUnit(t, configPath, allocationID, filename)
+		// record time in minute
+		startTime := float64(time.Now().Second())
 
-		expectedUploadCostInZCN, err := strconv.ParseFloat(strings.Fields(output[0])[0], 64)
-		require.Nil(t, err, "Cost couldn't be parsed to float", strings.Join(output, "\n"))
+		fmt.Println("Start time: ", startTime)
 
-		unit := strings.Fields(output[0])[1]
-		expectedUploadCostInZCN = unitToZCN(expectedUploadCostInZCN, unit)
-
-		// Expected cost is given in "per 720 hours", we need 1 hour
-		// Expected cost takes into account data+parity, so we divide by that
-		actualExpectedUploadCostInZCN := expectedUploadCostInZCN / ((2 + 2) * 720)
-		fmt.Print(actualExpectedUploadCostInZCN)
 		// upload a dummy 5 MB file
 		uploadWithParam(t, configPath, map[string]interface{}{
 			"allocation": allocationID,
@@ -379,16 +389,33 @@ func Test___FlakyBrokenScenarios(testSetup *testing.T) {
 			"remotepath": "/",
 		})
 
+		// record time in minute
+		endTime := float64(time.Now().Second())
+
+		fmt.Println("End time: ", endTime)
+
+		minutesElapsed := endTime - startTime
+
+		minutesElapsed = 10 - minutesElapsed/60
+		fmt.Println("Minutes elapsed: ", minutesElapsed)
+
+		actualExpectedUploadCostInZCN := intToZCN(int64(minutesElapsed * 1e9 / 1024))
+
+		fmt.Println("Actual expected upload cost in ZCN: ", actualExpectedUploadCostInZCN)
+
 		finalAllocation := getAllocation(t, allocationID)
-		require.Equal(t, 0.8, intToZCN(finalAllocation.WritePool))
+		printJson("Final Allocation: ", finalAllocation)
 
 		// Get Challenge-Pool info after upload
 		output, err = challengePoolInfo(t, configPath, allocationID)
+		fmt.Println("Challenge Pool Info: ", output)
 		require.Nil(t, err, "Could not fetch challenge pool", strings.Join(output, "\n"))
 
-		challengePool := climodel.ChallengePoolInfo{}
+		challengePool = climodel.ChallengePoolInfo{}
 		err = json.Unmarshal([]byte(output[0]), &challengePool)
 		require.Nil(t, err, "Error unmarshalling challenge pool info", strings.Join(output, "\n"))
+
+		printJson("Challenge Pool: ", challengePool)
 
 		require.Regexp(t, regexp.MustCompile(fmt.Sprintf("([a-f0-9]{64}):challengepool:%s", allocationID)), challengePool.Id)
 		require.IsType(t, int64(1), challengePool.StartTime)
@@ -400,157 +427,164 @@ func Test___FlakyBrokenScenarios(testSetup *testing.T) {
 		// Blobber pool balance should reduce by (write price*filesize) for each blobber
 		totalChangeInWritePool := intToZCN(initialAllocation.WritePool - finalAllocation.WritePool)
 
-		require.Equal(t, actualExpectedUploadCostInZCN, totalChangeInWritePool, "expected write pool balance to decrease by [%v] but has actually decreased by [%v]", actualExpectedUploadCostInZCN, totalChangeInWritePool)
+		fmt.Println("Total Change in Write Pool: ", totalChangeInWritePool)
+		fmt.Println("Challenge Pool Balance: ", challengePool.Balance)
+		fmt.Println("Actual Expected Upload Cost in ZCN: ", actualExpectedUploadCostInZCN)
+
+		require.InEpsilon(t, actualExpectedUploadCostInZCN, totalChangeInWritePool, 0.15, "expected write pool balance to decrease by [%v] but has actually decreased by [%v]", actualExpectedUploadCostInZCN, totalChangeInWritePool)
 		require.Equal(t, totalChangeInWritePool, intToZCN(challengePool.Balance), "expected challenge pool balance to match deducted amount from write pool [%v] but balance was actually [%v]", totalChangeInWritePool, intToZCN(challengePool.Balance))
 	})
 
 	// FIXME: Commented out because these cases hang the broken test suite till timeout
 
 	// FIXME: add param validation
-	// t.Run("Upload from local webcam feed with a negative chunksize should fail", func(t *test.SystemTest) {
-	// 	output, err := createWallet(t, configPath)
-	// 	require.Nil(t, err, "Failed to create wallet", strings.Join(output, "\n"))
+	t.RunWithTimeout("Upload from local webcam feed with a negative chunksize should fail", 5*time.Minute, func(t *test.SystemTest) {
+		output, err := createWallet(t, configPath)
+		require.Nil(t, err, "Failed to create wallet", strings.Join(output, "\n"))
 
-	// 	output, err = executeFaucetWithTokens(t, configPath, 2.0)
-	// 	require.Nil(t, err, "faucet execution failed", strings.Join(output, "\n"))
+		output, err = executeFaucetWithTokens(t, configPath, 2.0)
+		require.Nil(t, err, "faucet execution failed", strings.Join(output, "\n"))
 
-	// 	output, err = createNewAllocation(t, configPath, createParams(map[string]interface{}{
-	// 		"lock": 1,
-	// 	}))
-	// 	require.Nil(t, err, "error creating allocation", strings.Join(output, "\n"))
-	// 	require.Len(t, output, 1)
-	// 	require.Regexp(t, regexp.MustCompile("Allocation created: ([a-f0-9]{64})"), output[0], "Allocation creation output did not match expected")
-	// 	allocationID := strings.Fields(output[0])[2]
+		output, err = createNewAllocation(t, configPath, createParams(map[string]interface{}{
+			"lock":   1,
+			"expire": "10m",
+		}))
+		require.Nil(t, err, "error creating allocation", strings.Join(output, "\n"))
+		require.Len(t, output, 1)
+		require.Regexp(t, regexp.MustCompile("Allocation created: ([a-f0-9]{64})"), output[0], "Allocation creation output did not match expected")
+		allocationID := strings.Fields(output[0])[2]
 
-	// 	remotepath := "/live/stream.m3u8"
-	// 	localfolder := filepath.Join(os.TempDir(), escapedTestName(t))
-	// 	localpath := filepath.Join(localfolder, "up.m3u8")
-	// 	err = os.MkdirAll(localpath, os.ModePerm)
-	// 	require.Nil(t, err, "Error in creating the folders", localpath)
-	// 	defer os.RemoveAll(localfolder)
+		remotepath := "/live/stream.m3u8"
+		localfolder := filepath.Join(os.TempDir(), escapedTestName(t))
+		localpath := filepath.Join(localfolder, "up.m3u8")
+		err = os.MkdirAll(localpath, os.ModePerm)
+		require.Nil(t, err, "Error in creating the folders", localpath)
+		defer os.RemoveAll(localfolder)
 
-	// 	chunksize := -655360
-	// 	// FIXME: negative chunksize works without error, after implementing fix change startUploadFeed to
-	// 	// runUploadFeed below
-	// 	err = startUploadFeed(t, configPath, createParams(map[string]interface{}{
-	// 		"allocation": allocationID,
-	// 		"localpath":  localpath,
-	// 		"remotepath": remotepath,
-	// 		"live":       "",
-	// 		"chunksize":  chunksize,
-	// 	}))
-	// 	require.Nil(t, err, "expected error when using negative chunksize")
-	// 	KillFFMPEG()
-	// })
-
-	// FIXME: add param validation
-	// t.Run("Upload from youtube feed with a negative chunksize should fail", func(t *test.SystemTest) {
-	//
-
-	// 	output, err := createWallet(t, configPath)
-	// 	require.Nil(t, err, "Failed to create wallet", strings.Join(output, "\n"))
-
-	// 	output, err = executeFaucetWithTokens(t, configPath, 2.0)
-	// 	require.Nil(t, err, "faucet execution failed", strings.Join(output, "\n"))
-
-	// 	output, err = createNewAllocation(t, configPath, createParams(map[string]interface{}{
-	// 		"lock": 1,
-	// 	}))
-	// 	require.Nil(t, err, "error creating allocation", strings.Join(output, "\n"))
-	// 	require.Len(t, output, 1)
-	// 	require.Regexp(t, regexp.MustCompile("Allocation created: ([a-f0-9]{64})"), output[0], "Allocation creation output did not match expected")
-	// 	allocationID := strings.Fields(output[0])[2]
-
-	// 	remotepath := "/live/stream.m3u8"
-	// 	localfolder := filepath.Join(os.TempDir(), escapedTestName(t))
-	// 	localpath := filepath.Join(localfolder, "up.m3u8")
-	// 	err = os.MkdirAll(localpath, os.ModePerm)
-	// 	require.Nil(t, err, "Error in creating the folders", localpath)
-	// 	defer os.RemoveAll(localfolder)
-
-	// 	chunksize := -655360
-	// 	// FIXME: negative chunksize works without error, after implementing fix change startUploadFeed to
-	// 	// runUploadFeed below
-	// 	err = startUploadFeed(t, configPath, createParams(map[string]interface{}{
-	// 		"allocation": allocationID,
-	// 		"localpath":  localpath,
-	// 		"remotepath": remotepath,
-	// 		"feed":       `https://www.youtube.com/watch?v=5qap5aO4i9A`,
-	// 		"sync":       "",
-	// 		"chunksize":  chunksize,
-	// 	}))
-	// 	require.Nil(t, err, "expected error when using negative chunksize")
-	// 	KillFFMPEG()
-	// })
+		chunksize := -655360
+		// FIXME: negative chunksize works without error, after implementing fix change startUploadFeed to
+		// runUploadFeed below
+		err = startUploadFeed(t, configPath, "feed", localfolder, createParams(map[string]interface{}{
+			"allocation": allocationID,
+			"localpath":  localpath,
+			"remotepath": remotepath,
+			"live":       "",
+			"chunksize":  chunksize,
+		}))
+		require.Nil(t, err, "expected error when using negative chunksize")
+		KillFFMPEG()
+	})
 
 	// FIXME: add param validation
-	// t.Run("Uploading youtube feed with negative delay should fail", func(t *test.SystemTest) {
-	// 	output, err := createWallet(t, configPath)
-	// 	require.Nil(t, err, "Failed to create wallet", strings.Join(output, "\n"))
+	t.RunWithTimeout("Upload from youtube feed with a negative chunksize should fail", 5*time.Minute, func(t *test.SystemTest) {
 
-	// 	output, err = executeFaucetWithTokens(t, configPath, 2.0)
-	// 	require.Nil(t, err, "faucet execution failed", strings.Join(output, "\n"))
+		output, err := createWallet(t, configPath)
+		require.Nil(t, err, "Failed to create wallet", strings.Join(output, "\n"))
 
-	// 	output, err = createNewAllocation(t, configPath, createParams(map[string]interface{}{
-	// 		"lock": 1,
-	// 	}))
-	// 	require.Nil(t, err, "error creating allocation", strings.Join(output, "\n"))
-	// 	require.Len(t, output, 1)
-	// 	require.Regexp(t, regexp.MustCompile("Allocation created: ([a-f0-9]{64})"), output[0], "Allocation creation output did not match expected")
-	// 	allocationID := strings.Fields(output[0])[2]
+		output, err = executeFaucetWithTokens(t, configPath, 2.0)
+		require.Nil(t, err, "faucet execution failed", strings.Join(output, "\n"))
 
-	// 	remotepath := "/live/stream.m3u8"
-	// 	localfolder := filepath.Join(os.TempDir(), escapedTestName(t))
-	// 	localpath := filepath.Join(localfolder, "up.m3u8")
-	// 	err = os.MkdirAll(localpath, os.ModePerm)
-	// 	require.Nil(t, err, "Error in creating the folders", localpath)
-	// 	defer os.RemoveAll(localfolder)
+		output, err = createNewAllocation(t, configPath, createParams(map[string]interface{}{
+			"lock":   1,
+			"expire": "10m",
+		}))
+		require.Nil(t, err, "error creating allocation", strings.Join(output, "\n"))
+		require.Len(t, output, 1)
+		require.Regexp(t, regexp.MustCompile("Allocation created: ([a-f0-9]{64})"), output[0], "Allocation creation output did not match expected")
+		allocationID := strings.Fields(output[0])[2]
 
-	// 	err = runUploadFeed(t, configPath, createParams(map[string]interface{}{
-	// 		"allocation": allocationID,
-	// 		"localpath":  localpath,
-	// 		"remotepath": remotepath,
-	// 		"feed":       `https://www.youtube.com/watch?v=5qap5aO4i9A`,
-	// 		"sync":       "",
-	// 		"delay":      -10,
-	// 	}))
-	// 	require.NotNil(t, err, "negative delay should fail")
-	// 	KillFFMPEG()
-	// })
+		remotepath := "/live/stream.m3u8"
+		localfolder := filepath.Join(os.TempDir(), escapedTestName(t))
+		localpath := filepath.Join(localfolder, "up.m3u8")
+		err = os.MkdirAll(localpath, os.ModePerm)
+		require.Nil(t, err, "Error in creating the folders", localpath)
+		defer os.RemoveAll(localfolder)
+
+		chunksize := -655360
+		// FIXME: negative chunksize works without error, after implementing fix change startUploadFeed to
+		// runUploadFeed below
+		err = startUploadFeed(t, configPath, "feed", localfolder, createParams(map[string]interface{}{
+			"allocation": allocationID,
+			"localpath":  localpath,
+			"remotepath": remotepath,
+			"feed":       `https://www.youtube.com/watch?v=5qap5aO4i9A`,
+			"sync":       "",
+			"chunksize":  chunksize,
+		}))
+		require.Nil(t, err, "expected error when using negative chunksize")
+		KillFFMPEG()
+	})
 
 	// FIXME: add param validation
-	// t.Run("Uploading local webcam feed with negative delay should fail", func(t *test.SystemTest) {
-	// 	output, err := createWallet(t, configPath)
-	// 	require.Nil(t, err, "Failed to create wallet", strings.Join(output, "\n"))
+	t.RunWithTimeout("Uploading youtube feed with negative delay should fail", 5*time.Minute, func(t *test.SystemTest) {
+		output, err := createWallet(t, configPath)
+		require.Nil(t, err, "Failed to create wallet", strings.Join(output, "\n"))
 
-	// 	output, err = executeFaucetWithTokens(t, configPath, 2.0)
-	// 	require.Nil(t, err, "faucet execution failed", strings.Join(output, "\n"))
+		output, err = executeFaucetWithTokens(t, configPath, 2.0)
+		require.Nil(t, err, "faucet execution failed", strings.Join(output, "\n"))
 
-	// 	output, err = createNewAllocation(t, configPath, createParams(map[string]interface{}{
-	// 		"lock": 1,
-	// 	}))
-	// 	require.Nil(t, err, "error creating allocation", strings.Join(output, "\n"))
-	// 	require.Len(t, output, 1)
-	// 	require.Regexp(t, regexp.MustCompile("Allocation created: ([a-f0-9]{64})"), output[0], "Allocation creation output did not match expected")
-	// 	allocationID := strings.Fields(output[0])[2]
+		output, err = createNewAllocation(t, configPath, createParams(map[string]interface{}{
+			"lock":   1,
+			"expire": "10m",
+		}))
+		require.Nil(t, err, "error creating allocation", strings.Join(output, "\n"))
+		require.Len(t, output, 1)
+		require.Regexp(t, regexp.MustCompile("Allocation created: ([a-f0-9]{64})"), output[0], "Allocation creation output did not match expected")
+		allocationID := strings.Fields(output[0])[2]
 
-	// 	remotepath := "/live/stream.m3u8"
-	// 	localfolder := filepath.Join(os.TempDir(), escapedTestName(t))
-	// 	localpath := filepath.Join(localfolder, "up.m3u8")
-	// 	err = os.MkdirAll(localpath, os.ModePerm)
-	// 	require.Nil(t, err, "Error in creating the folders", localpath)
-	// 	defer os.RemoveAll(localfolder)
+		remotepath := "/live/stream.m3u8"
+		localfolder := filepath.Join(os.TempDir(), escapedTestName(t))
+		localpath := filepath.Join(localfolder, "up.m3u8")
+		err = os.MkdirAll(localpath, os.ModePerm)
+		require.Nil(t, err, "Error in creating the folders", localpath)
+		defer os.RemoveAll(localfolder)
 
-	// 	err = runUploadFeed(t, configPath, createParams(map[string]interface{}{
-	// 		"allocation": allocationID,
-	// 		"localpath":  localpath,
-	// 		"remotepath": remotepath,
-	// 		"live":       "",
-	// 		"delay":      -10,
-	// 	}))
-	// 	require.NotNil(t, err, "negative delay should fail")
-	// 	KillFFMPEG()
-	// })
+		err = startUploadFeed(t, "feed", configPath, localfolder, createParams(map[string]interface{}{
+			"allocation": allocationID,
+			"localpath":  localpath,
+			"remotepath": remotepath,
+			"feed":       `https://www.youtube.com/watch?v=5qap5aO4i9A`,
+			"sync":       "",
+			"delay":      -10,
+		}))
+		require.NotNil(t, err, "negative delay should fail")
+		KillFFMPEG()
+	})
+
+	// FIXME: add param validation
+	t.RunWithTimeout("Uploading local webcam feed with negative delay should fail", 5*time.Minute, func(t *test.SystemTest) {
+		output, err := createWallet(t, configPath)
+		require.Nil(t, err, "Failed to create wallet", strings.Join(output, "\n"))
+
+		output, err = executeFaucetWithTokens(t, configPath, 2.0)
+		require.Nil(t, err, "faucet execution failed", strings.Join(output, "\n"))
+
+		output, err = createNewAllocation(t, configPath, createParams(map[string]interface{}{
+			"lock":   1,
+			"expire": "10m",
+		}))
+		require.Nil(t, err, "error creating allocation", strings.Join(output, "\n"))
+		require.Len(t, output, 1)
+		require.Regexp(t, regexp.MustCompile("Allocation created: ([a-f0-9]{64})"), output[0], "Allocation creation output did not match expected")
+		allocationID := strings.Fields(output[0])[2]
+
+		remotepath := "/live/stream.m3u8"
+		localfolder := filepath.Join(os.TempDir(), escapedTestName(t))
+		localpath := filepath.Join(localfolder, "up.m3u8")
+		err = os.MkdirAll(localpath, os.ModePerm)
+		require.Nil(t, err, "Error in creating the folders", localpath)
+		defer os.RemoveAll(localfolder)
+
+		err = startUploadFeed(t, configPath, "feed", localfolder, createParams(map[string]interface{}{
+			"allocation": allocationID,
+			"localpath":  localpath,
+			"remotepath": remotepath,
+			"live":       "",
+			"delay":      -10,
+		}))
+		require.NotNil(t, err, "negative delay should fail")
+		KillFFMPEG()
+	})
 }
 
 // func runUploadFeed(t *test.SystemTest, cliConfigFilename, params string) error {
