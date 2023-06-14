448c0632986432d65d0e7c7ba2939e1e08e3930b
Dell <shalinik@iitbhilai.ac.in>
2023-06-14 23:15:04 +0530

    added file stat method

--- tests/cli_tests/main_test.go
+++ tests/cli_tests/main_test.go
@@ -17,6 +17,10 @@ import (
 
 	cliutils "github.com/0chain/system_test/internal/cli/util"
 	"github.com/spf13/viper"
+
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/s3"
 )
 
 func setupDefaultConfig() {
@@ -93,6 +97,7 @@ var (
 	s3SecretKey     string
 	s3AccessKey     string
 	s3bucketName    string
+	S3Client 		*s3.S3
 )
 
 var (
@@ -151,6 +156,18 @@ func TestMain(m *testing.M) {
 	setupConfig()
 
 	tenderlyClient = tenderly.NewClient(ethereumNodeURL)
+
+	// Create a session with AWS
+	sess, err := session.NewSession(&aws.Config{
+		Region: aws.String("us-east-1"), // Replace with your desired AWS region
+	})
+	if err != nil {
+		fmt.Println("Failed to create AWS session:", err)
+		return
+	}
+
+	// Create an S3 client
+	S3Client := s3.New(sess)
 
 	snapshotHash, err := tenderlyClient.CreateSnapshot()
 	if err != nil {
