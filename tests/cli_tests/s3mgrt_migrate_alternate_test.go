837fd08c0ff3cbd09c363ebbdea171ae19d2b2e4
Dell <shalinik@iitbhilai.ac.in>
2023-06-14 23:15:04 +0530

    added file stat method

--- tests/cli_tests/s3mgrt_migrate_alternate_test.go
+++ tests/cli_tests/s3mgrt_migrate_alternate_test.go
@@ -1,16 +1,18 @@
 package cli_tests
 
 import (
+	"bytes"
 	"encoding/json"
 	"fmt"
 	"math"
 	"path"
-	"path/filepath"
 	"strings"
 	"testing"
 
 	"github.com/0chain/system_test/internal/api/util/test"
 	climodel "github.com/0chain/system_test/internal/cli/model"
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/service/s3"
 	"github.com/stretchr/testify/require"
 	"golang.org/x/crypto/sha3"
 )
@@ -35,47 +37,35 @@ how to verify --encrypt option I think you need to see the code
 // encrption need to figure out
 // --dup-suffix this can also be tested
 // newer than , prefix and older than can also be tested
+const chunksize = 64 * 1024
+
 func Test0S3MigrationAlternate(testSetup *testing.T) {
 	t := test.NewSystemTest(testSetup)
 
 	if s3SecretKey == "" || s3AccessKey == "" {
 		t.Skip("s3SecretKey or s3AccessKey was missing")
 	}
-	t.Parallel()
-	t.SetSmokeTests("Should migrate existing bucket successfully")
 
-	t.Run("Should migrate as copy bucket successfully ", func(t *test.SystemTest) {
-		allocSize := int64(50 * MB)
-		allocationID := setupAllocation(t, configPath, map[string]interface{}{
-			"size": allocSize,
-		})
+	// Specify the bucket name and file key
+	bucketName := "This bucket is created as a part of s3 migration test alternate flow"
+	fileKey := t.Name() + ".txt"
 
-		output, err := migrateFromS3(t, configPath, createParams(map[string]interface{}{
-			"access-key": s3AccessKey,
-			"secret-key": s3SecretKey,
-			"bucket":     s3bucketName,
-			"wallet":     escapedTestName(t) + "_wallet.json",
-			"allocation": allocationID,
-			"encrypt": "true",
-		}))
+	// Read file contents
+	fileContents := []byte("Hello, World!")
 
-		require.Nil(t, err, "Unexpected migration failure", strings.Join(output, "\n"))
-		require.Equal(t, len(output), 1, "More/Less output was returned than expected", strings.Join(output, "\n"))
-		require.Contains(t, "Migration completed successfully", output[0], "Output was not as expected", strings.Join(output, "\n"))
-
-		output, err = migrateFromS3(t, configPath, createParams(map[string]interface{}{
-			"access-key": s3AccessKey,
-			"secret-key": s3SecretKey,
-			"bucket":     s3bucketName,
-			"wallet":     escapedTestName(t) + "_wallet.json",
-			"allocation": allocationID,
-			"encrypt": "true",
-		}))
-
-		require.Nil(t, err, "Unexpected migration failure", strings.Join(output, "\n"))
-		require.Equal(t, len(output), 1, "More/Less output was returned than expected", strings.Join(output, "\n"))
-		require.Contains(t, "Migration completed successfully", output[0], "Output was not as expected", strings.Join(output, "\n"))
+	// Upload the file to S3
+	_, err := S3Client.PutObject(&s3.PutObjectInput{
+		Bucket: aws.String(bucketName),
+		Key:    aws.String(fileKey),
+		Body:   bytes.NewReader(fileContents),
 	})
+	if err != nil {
+		fmt.Println("Failed to create file in S3:", err)
+		t.Skip("S3 Bucket operatiion is not working properly")
+	}
+
+	//t.Parallel()
+	t.SetSmokeTests("Should migrate existing bucket successfully")
 
 	t.Run("Should migrate existing bucket to specified path successfully with encryption on", func(t *test.SystemTest) {
 		allocSize := int64(50 * MB)
@@ -89,15 +79,59 @@ func Test0S3MigrationAlternate(testSetup *testing.T) {
 			"bucket":     s3bucketName,
 			"wallet":     escapedTestName(t) + "_wallet.json",
 			"allocation": allocationID,
-			"migarate-to": "/root2",
+			//"migarate-to": "/root2",
 			"encrypt": "true",
 		}))
 
 		require.Nil(t, err, "Unexpected migration failure", strings.Join(output, "\n"))
 		require.Equal(t, len(output), 1, "More/Less output was returned than expected", strings.Join(output, "\n"))
 		require.Contains(t, "Migration completed successfully", output[0], "Output was not as expected", strings.Join(output, "\n"))
+
+		remotepath := "/"
+		fileKey = fileKey + "encrypt"
+		remoteFilePath := path.Join(remotepath, fileKey)
+		uploadStats := checkStats(t, remoteFilePath, fileKey, allocationID)
+		require.Equal(t, uploadStats, true, "The file migrated doesnot match with with required file")
 	})
 
+	t.Run("Should migrate as copy bucket successfully ", func(t *test.SystemTest) {
+		allocSize := int64(50 * MB)
+		allocationID := setupAllocation(t, configPath, map[string]interface{}{
+			"size": allocSize,
+		})
+
+		output, err := migrateFromS3(t, configPath, createParams(map[string]interface{}{
+			"access-key": s3AccessKey,
+			"secret-key": s3SecretKey,
+			"bucket":     bucketName,
+			"wallet":     escapedTestName(t) + "_wallet.json",
+			"allocation": allocationID,
+		}))
+
+		require.Nil(t, err, "Unexpected migration failure", strings.Join(output, "\n"))
+		require.Equal(t, len(output), 1, "More/Less output was returned than expected", strings.Join(output, "\n"))
+		require.Contains(t, "Migration completed successfully", output[0], "Output was not as expected", strings.Join(output, "\n"))
+
+		output, err = migrateFromS3(t, configPath, createParams(map[string]interface{}{
+			"access-key": s3AccessKey,
+			"secret-key": s3SecretKey,
+			"bucket":     s3bucketName,
+			"wallet":     escapedTestName(t) + "_wallet.json",
+			"allocation": allocationID,
+		}))
+
+		require.Nil(t, err, "Unexpected migration failure", strings.Join(output, "\n"))
+		require.Equal(t, len(output), 1, "More/Less output was returned than expected", strings.Join(output, "\n"))
+		require.Contains(t, "Migration completed successfully", output[0], "Output was not as expected", strings.Join(output, "\n"))
+
+		remotepath := "/"
+		fileKey = fileKey + "_copy"
+		remoteFilePath := path.Join(remotepath, fileKey)
+		uploadStats := checkStats(t, remoteFilePath, fileKey, allocationID)
+		require.Equal(t, uploadStats, true, "The file migrated doesnot match with with required file")
+	})
+
+	
 	t.Run("Should migrate existing bucket successfully with encryption on", func(t *test.SystemTest) {
 		allocSize := int64(50 * MB)
 		allocationID := setupAllocation(t, configPath, map[string]interface{}{
@@ -116,6 +150,12 @@ func Test0S3MigrationAlternate(testSetup *testing.T) {
 		require.Nil(t, err, "Unexpected migration failure", strings.Join(output, "\n"))
 		require.Equal(t, len(output), 1, "More/Less output was returned than expected", strings.Join(output, "\n"))
 		require.Contains(t, "Migration completed successfully", output[0], "Output was not as expected", strings.Join(output, "\n"))
+
+		remotepath := "/"
+		fileKey = fileKey + "_encrypt"
+		remoteFilePath := path.Join(remotepath, fileKey)
+		uploadStats := checkStats(t, remoteFilePath, fileKey, allocationID)
+		require.Equal(t, uploadStats, true, "The file migrated doesnot match with with required file")
 	})
 
 	t.Run("Should migrate existing bucket successfully with skip 0  and replace existing", func(t *test.SystemTest) {
@@ -136,6 +176,11 @@ func Test0S3MigrationAlternate(testSetup *testing.T) {
 		require.Nil(t, err, "Unexpected migration failure", strings.Join(output, "\n"))
 		require.Equal(t, len(output), 1, "More/Less output was returned than expected", strings.Join(output, "\n"))
 		require.Contains(t, "Migration completed successfully", output[0], "Output was not as expected", strings.Join(output, "\n"))
+
+		remotepath := "/"
+		remoteFilePath := path.Join(remotepath, fileKey)
+		uploadStats := checkStats(t, remoteFilePath, fileKey, allocationID)
+		require.Equal(t, uploadStats, true, "The file migrated doesnot match with with required file")
 	})
 
 	t.Run("Should skip migration with skip flag == 1", func(t *test.SystemTest) {
@@ -155,6 +200,7 @@ func Test0S3MigrationAlternate(testSetup *testing.T) {
 
 		require.Nil(t, err, "Unexpected migration failure", strings.Join(output, "\n"))
 		require.Equal(t, len(output), 1, "More/Less output was returned than expected", strings.Join(output, "\n"))
+		// change the massege please
 		require.Contains(t, "Migration completed successfully", output[0], "Output was not as expected", strings.Join(output, "\n"))
 	})
 
@@ -178,41 +224,59 @@ func Test0S3MigrationAlternate(testSetup *testing.T) {
 		require.Contains(t, "Migration completed successfully", output[0], "Output was not as expected", strings.Join(output, "\n"))
 
 		remotepath := "/"
-		filesize := int64(555)
-		filename := generateFileAndUpload(t, allocationID, remotepath, filesize)
-		fname := filepath.Base(filename)
-		remoteFilePath := path.Join(remotepath, fname)
-
-		output, err := getFileStats(t, configPath, createParams(map[string]interface{}{
-			"allocation": allocationID,
-			"remotepath": remoteFilePath,
-			"json":       "",
-		}), true)
-		require.Nil(t, err, strings.Join(output, "\n"))
-		require.Len(t, output, 1)
-
-		var stats map[string]climodel.FileStats
-
-		err = json.Unmarshal([]byte(output[0]), &stats)
-		require.Nil(t, err)
-
-		for _, data := range stats {
-			require.Equal(t, fname, data.Name)
-			require.Equal(t, remoteFilePath, data.Path)
-			require.Equal(t, fmt.Sprintf("%x", sha3.Sum256([]byte(allocationID+":"+remoteFilePath))), data.PathHash)
-			require.Equal(t, int64(0), data.NumOfBlockDownloads)
-			require.Equal(t, int64(1), data.NumOfUpdates)
-			require.Equal(t, float64(data.NumOfBlocks), math.Ceil(float64(data.Size)/float64(chunksize)))
-			if data.WriteMarkerTxn == "" {
-				require.Equal(t, false, data.BlockchainAware)
-			} else {
-				require.Equal(t, true, data.BlockchainAware)
-			}
-
-			// FIXME: POSSIBLE BUG: key name and blobberID in value should be same but this is not consistent for every run and happening randomly
-			// require.Equal(t, blobberID, data.BlobberID, "key name and blobberID in value should be same")
-		}
+		fileKey = fileKey + "_copy"
+		// add dupl prefix
+		remoteFilePath := path.Join(remotepath, fileKey)
+		uploadStats := checkStats(t, remoteFilePath, fileKey, allocationID)
+		require.Equal(t, uploadStats, true, "The file migrated doesnot match with with required file")
 	})
 
 
+}
+
+func checkStats(t *test.SystemTest, remoteFilePath, fname, allocationID string)(bool){
+
+	output, err := getFileStats(t, configPath, createParams(map[string]interface{}{
+		"allocation": allocationID,
+		"remotepath": remoteFilePath,
+		"json":       "",
+	}), true)
+	require.Nil(t, err, strings.Join(output, "\n"))
+	require.Len(t, output, 1)
+
+	var stats map[string]climodel.FileStats
+
+	err = json.Unmarshal([]byte(output[0]), &stats)
+	require.Nil(t, err)
+
+	for _, data := range stats {
+		if(fname != data.Name){
+			return false
+		}
+		if(remoteFilePath !=  data.Path){
+			return false
+		}
+		if( fmt.Sprintf("%x", sha3.Sum256([]byte(allocationID+":"+remoteFilePath))) !=  data.PathHash){
+			return false
+		}
+		if(int64(0) !=  data.NumOfBlockDownloads){
+			return false
+		}
+		if(int64(1) !=  data.NumOfUpdates){
+			return false
+		}
+		if(float64(data.NumOfBlocks) != math.Ceil(float64(data.Size)/float64(chunksize))){
+			return false
+		}
+		if data.WriteMarkerTxn == "" {
+			if( data.BlockchainAware != false){
+				return false;
+			}
+		}else {
+			if( data.BlockchainAware != true){
+				return false;
+			}
+		}
+	}
+	return true	
 }
