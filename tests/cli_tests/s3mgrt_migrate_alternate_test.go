2df3a4506a423775c289c4342a0a2abc7af07cc8
Dell <shalinik@iitbhilai.ac.in>
2023-06-16 02:16:33 +0530

    done

--- tests/cli_tests/s3mgrt_migrate_alternate_test.go
+++ tests/cli_tests/s3mgrt_migrate_alternate_test.go
@@ -386,3 +386,151 @@ func setupTest(t *test.SystemTest, bucketName, fileKey string) {
 	}
 
 }
+
+
+// allocation path and aws credential path also needs to be tested
+// if region is tested or not that we need to check
+// resume retry, first look into code then ask ryan
+func Test0S3MigrationAlternatePart2(testSetup *testing.T) {
+	t := test.NewSystemTest(testSetup)
+
+	if s3SecretKey == "" || s3AccessKey == "" {
+		t.Skip("s3SecretKey or s3AccessKey was missing")
+	}
+
+	// Specify the bucket name and file key
+	bucketName := "dummybucketfortestsmigration"
+	fileKey := t.Name() + ".txt"
+	setupTest(t, bucketName, fileKey)
+	
+	t.Cleanup(func() {
+		input := &s3.DeleteBucketInput{
+			Bucket: aws.String(bucketName),
+		}
+	
+		// Delete the bucket
+		_, err := S3Client.DeleteBucket(input)
+		if err != nil {
+			log.Info("Failed to delete bucket:", err)
+			return
+		}
+	
+		log.Info("Bucket", bucketName, "deleted successfully!")
+	})
+
+
+	// t.Parallel()
+	t.SetSmokeTests("Should migrate existing bucket successfully")
+
+	t.Run("Should migrate existing bucket successfully with concurrency==4 and working dir current dir", func(t *test.SystemTest) {
+		t.Logf("here")
+		allocSize := int64(50 * MB)
+		allocationID := setupAllocation(t, configPath, map[string]interface{}{
+			"size": allocSize,
+		})
+
+		workDir := "/"
+		output, err := migrateFromS3(t, configPath, createParams(map[string]interface{}{
+			"access-key": s3AccessKey,
+			"secret-key": s3SecretKey,
+			"bucket":     s3bucketName,
+			"wallet":     escapedTestName(t) + "_wallet.json",
+			"allocation": allocationID,
+			"concurrency" : 4,
+			"wd": workDir,
+
+		}))
+		// statFilePath := fmt.Sprintf("%v/%v.state", workDir, bucketName)
+		remotepath := "/"
+		require.Nil(t, err, "Unexpected migration failure", strings.Join(output, "\n"))
+		require.Equal(t, len(output), 1, "More/Less output was returned than expected", strings.Join(output, "\n"))
+		require.Contains(t, "Migration completed successfully", output[0], "Output was not as expected", strings.Join(output, "\n"))
+
+		remoteFilePath := path.Join(remotepath, fileKey)
+		uploadStats := checkStats(t, remoteFilePath, fileKey, allocationID)
+		require.Equal(t, uploadStats, true, "The file migrated doesnot match with with required file")
+	})
+
+	t.Run("Should migrate existing bucket successfully with newer than prefix", func(t *test.SystemTest) {
+		t.Logf("here")
+		allocSize := int64(50 * MB)
+		allocationID := setupAllocation(t, configPath, map[string]interface{}{
+			"size": allocSize,
+		})
+
+		//workDir := "/"
+		output, err := migrateFromS3(t, configPath, createParams(map[string]interface{}{
+			"access-key": s3AccessKey,
+			"secret-key": s3SecretKey,
+			"bucket":     s3bucketName,
+			"wallet":     escapedTestName(t) + "_wallet.json",
+			"allocation": allocationID,
+			"newer-than" : "10m",
+
+		}))
+		remotepath := "/"
+		require.Nil(t, err, "Unexpected migration failure", strings.Join(output, "\n"))
+		require.Equal(t, len(output), 1, "More/Less output was returned than expected", strings.Join(output, "\n"))
+		require.Contains(t, "Migration completed successfully", output[0], "Output was not as expected", strings.Join(output, "\n"))
+
+		remoteFilePath := path.Join(remotepath, fileKey)
+		uploadStats := checkStats(t, remoteFilePath, fileKey, allocationID)
+		require.Equal(t, uploadStats, true, "The file migrated doesnot match with with required file")
+	})
+
+	t.Run("Should migrate existing bucket successfully with older than prefix", func(t *test.SystemTest) {
+		t.Logf("here")
+		allocSize := int64(50 * MB)
+		allocationID := setupAllocation(t, configPath, map[string]interface{}{
+			"size": allocSize,
+		})
+
+		//workDir := "/"
+		output, err := migrateFromS3(t, configPath, createParams(map[string]interface{}{
+			"access-key": s3AccessKey,
+			"secret-key": s3SecretKey,
+			"bucket":     s3bucketName,
+			"wallet":     escapedTestName(t) + "_wallet.json",
+			"allocation": allocationID,
+			"older-than" : "10m",
+
+		}))
+		remotepath := "/"
+		require.Nil(t, err, "Unexpected migration failure", strings.Join(output, "\n"))
+		require.Equal(t, len(output), 1, "More/Less output was returned than expected", strings.Join(output, "\n"))
+		require.Contains(t, "Migration completed successfully", output[0], "Output was not as expected", strings.Join(output, "\n"))
+
+		remoteFilePath := path.Join(remotepath, fileKey)
+		uploadStats := checkStats(t, remoteFilePath, fileKey, allocationID)
+		require.Equal(t, uploadStats, true, "The file migrated doesnot match with with required file")
+	})
+
+	t.Run("Should migrate existing bucket successfully with files staring with given prefix", func(t *test.SystemTest) {
+		t.Logf("here")
+		allocSize := int64(50 * MB)
+		allocationID := setupAllocation(t, configPath, map[string]interface{}{
+			"size": allocSize,
+		})
+
+		//workDir := "/"
+		output, err := migrateFromS3(t, configPath, createParams(map[string]interface{}{
+			"access-key": s3AccessKey,
+			"secret-key": s3SecretKey,
+			"bucket":     s3bucketName,
+			"wallet":     escapedTestName(t) + "_wallet.json",
+			"allocation": allocationID,
+			"prefix" : "hello",
+
+		}))
+		remotepath := "/"
+		require.Nil(t, err, "Unexpected migration failure", strings.Join(output, "\n"))
+		require.Equal(t, len(output), 1, "More/Less output was returned than expected", strings.Join(output, "\n"))
+		require.Contains(t, "Migration completed successfully", output[0], "Output was not as expected", strings.Join(output, "\n"))
+
+		remoteFilePath := path.Join(remotepath, fileKey)
+		uploadStats := checkStats(t, remoteFilePath, fileKey, allocationID)
+		require.Equal(t, uploadStats, true, "The file migrated doesnot match with with required file")
+	})
+
+}
+
