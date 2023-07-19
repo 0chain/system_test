fc7913fdccf194f7f3907ded5e2ed094ada81a6f
Jayash Satolia <73050737+Jayashsatolia403@users.noreply.github.com>
2023-07-19 11:19:15 +0000

    Delete SUBSET_TEST_RUNNER_COMMAND.sh

--- SUBSET_TEST_RUNNER_COMMAND.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-set -o pipefail
-[ -z "$1" ] && TESTS_TO_RUN="-run ^\QTestAllocation\E$/^\QCreate_+_Upload_+_Cancel,_equal_read_price_0.1\E$ ./..." || TESTS_TO_RUN=$1
-CONFIG_PATH=./zbox_config.yaml go test -timeout=60m $TESTS_TO_RUN -json -count=1 | sed -r "/(=== (CONT|RUN|PAUSE).*)|(--- FAIL:.*)|(\"Test\":\".*\/[pP]arallel\")/d"
