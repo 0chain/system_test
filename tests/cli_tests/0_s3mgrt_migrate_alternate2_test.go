86c5cd63d8ef65da881eacda5c4ce7af68fd9b25
Kishan-Dhakan <kishandhakan17@gmail.com>
2023-07-25 02:05:53 +0530

    enable s3mgrt tests

--- tests/cli_tests/0_s3mgrt_migrate_alternate2_test.go
+++ tests/cli_tests/0_s3mgrt_migrate_alternate2_test.go
@@ -18,7 +18,6 @@ import (
 
 func Test0S3MigrationAlternatePart2(testSetup *testing.T) {
 	t := test.NewSystemTest(testSetup)
-	t.Skip("tests are broken - need to investigate")
 
 	if s3SecretKey == "" || s3AccessKey == "" {
 		t.Skip("s3SecretKey or s3AccessKey was missing")
