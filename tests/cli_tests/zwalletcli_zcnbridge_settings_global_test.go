a5177c90c83d257dae5c6f176fc4751efaa7b427
YarikRevich <yariksvitlitskiy2@gmail.com>
2023-08-02 20:44:31 +0200

    fix: fixed config recovery

--- tests/cli_tests/zwalletcli_zcnbridge_settings_global_test.go
+++ tests/cli_tests/zwalletcli_zcnbridge_settings_global_test.go
@@ -1,7 +1,9 @@
 package cli_tests
 
 import (
+	"encoding/json"
 	"fmt"
+	"regexp"
 	"strings"
 	"testing"
 	"time"
@@ -11,25 +13,49 @@ import (
 	"github.com/stretchr/testify/require"
 
 	cliutils "github.com/0chain/system_test/internal/cli/util"
+
+	"github.com/fatih/structs"
 )
+
+type BridgeConfig struct {
+	Fields struct {
+		BurnAddress          string `json:"burn_address"`
+		AddAuthorizerCost    string `json:"cost.add-authorizer"`
+		BurnCost             string `json:"cost.burn"`
+		DeleteAuthorizerCost string `json:"cost.delete-authorizer"`
+		MintCost             string `json:"cost.mint"`
+		MaxDelegates         string `json:"max_delegates"`
+		MaxFee               string `json:"max_fee"`
+		MaxStake             string `json:"max_stake"`
+		MinAuthorizers       string `json:"min_authorizers"`
+		MinBurn              string `json:"min_burn"`
+		MinLock              string `json:"min_lock"`
+		MinMint              string `json:"min_mint"`
+		MinStake             string `json:"min_stake"`
+		OwnerID              string `json:"owner_id"`
+		AuthorizersPercent   string `json:"percent_authorizers"`
+	} `json:"fields"`
+}
 
 func TestZCNBridgeGlobalSettings(testSetup *testing.T) {
 	t := test.NewSystemTest(testSetup)
 	t.SetSmokeTests("should allow update of min_mint_amount")
 
-	defaultConfig := getDefaultConfig(t)
-
-	t.Cleanup(func() {
-		output, err := updateZCNBridgeSCConfig(t, zcnscOwner, defaultConfig, true)
-
-		require.Nil(t, err, strings.Join(output, "\n"))
-		require.Len(t, output, 2, strings.Join(output, "\n"))
-		require.Equal(t, "faucet smart contract settings updated", output[0], strings.Join(output, "\n"))
-		require.Regexp(t, `Hash: [0-9a-f]+`, output[1], strings.Join(output, "\n"))
-	})
-
 	output, err := executeFaucetWithTokensForWallet(t, zcnscOwner, configPath, 10.0)
 	require.Nil(t, err, "faucet execution failed", strings.Join(output, "\n"))
+
+	defaultParams := getDefaultConfig(t)
+
+	t.Cleanup(func() {
+		for _, defaultParam := range defaultParams {
+			output, err := updateZCNBridgeSCConfig(t, zcnscOwner, createConfigParams(defaultParam), true)
+
+			require.Nil(t, err, strings.Join(output, "\n"))
+			require.Len(t, output, 2, strings.Join(output, "\n"))
+			require.Equal(t, "faucet smart contract settings updated", output[0], strings.Join(output, "\n"))
+			require.Regexp(t, `Hash: [0-9a-f]+`, output[1], strings.Join(output, "\n"))
+		}
+	})
 
 	t.RunSequentially("should allow update of min_mint_amount", func(t *test.SystemTest) {
 		testKey(t, "min_mint", "1")
@@ -64,16 +90,24 @@ func TestZCNBridgeGlobalSettings(testSetup *testing.T) {
 	})
 }
 
-func getDefaultConfig(t *test.SystemTest) map[string]interface{} {
+func getDefaultConfig(t *test.SystemTest) []map[string]string {
 	output, err := getZCNBridgeGlobalSCConfig(t, configPath, true)
 	require.Nil(t, err, strings.Join(output, "\n"))
 	require.Greater(t, len(output), 0, strings.Join(output, "\n"))
 
-	defaultConfig, _ := keyValuePairStringToMap(output)
+	match := regexp.MustCompile(`{"fields":\s*({.*?})}`).FindAllString(strings.Join(output, " "), 1)[0]
 
-	result := make(map[string]interface{})
-	for k, v := range defaultConfig {
-		result[k] = v
+	var resultRaw BridgeConfig
+	err = json.Unmarshal([]byte(match), &resultRaw)
+	require.Nil(t, err)
+
+	var result []map[string]string
+
+	fields := structs.Fields(resultRaw.Fields)
+	for _, field := range fields {
+		result = append(result, map[string]string{
+			field.Tag("json"): field.Value().(string),
+		})
 	}
 
 	return result
@@ -84,11 +118,28 @@ func testKey(t *test.SystemTest, key, value string) {
 	require.Equal(t, value, cfgAfter[key], "new value %s for config %s was not set", value, key)
 }
 
+func createConfigParams(params map[string]string) map[string]interface{} {
+	var (
+		keys   []string
+		values []string
+	)
+	for k, v := range params {
+		keys = append(keys, k)
+		values = append(values, v)
+	}
+
+	return map[string]interface{}{
+		"keys":   strings.Join(keys, ","),
+		"values": strings.Join(values, ","),
+	}
+}
+
 func updateAndVerify(t *test.SystemTest, key, value string) map[string]string {
-	output, err := updateZCNBridgeSCConfig(t, zcnscOwner, map[string]interface{}{
-		"keys":   key,
-		"values": value,
-	}, true)
+	params := createConfigParams(map[string]string{
+		key: value,
+	})
+
+	output, err := updateZCNBridgeSCConfig(t, zcnscOwner, params, true)
 
 	require.Nil(t, err, strings.Join(output, "\n"))
 	require.Len(t, output, 2, strings.Join(output, "\n"))
