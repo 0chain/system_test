bea1e63cecf38aaf0c8873832d37c441c8f4d2d1
Kishan-Dhakan <kishandhakan17@gmail.com>
2023-08-09 02:10:02 +0530

    rename test func to avoid regex issues

--- tests/cli_tests/0_challenge_protocol_test.go
+++ tests/cli_tests/0_challenge_protocol_test.go
@@ -19,7 +19,7 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
-func TestBlobberChallengesCount(testSetup *testing.T) {
+func TestChallengeProtocol(testSetup *testing.T) {
 	t := test.NewSystemTest(testSetup)
 	t.SetSmokeTests("Number of challenges between 2 blocks should be equal to the number of blocks (given that we have active allocations)")
 
