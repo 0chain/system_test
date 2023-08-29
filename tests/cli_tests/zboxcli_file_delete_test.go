8bcfa652eada78659eb4a76bffce7f2962b6a277
root <shalinik@iitbhilai.ac.in>
2023-08-29 20:09:22 +0200

    delete fixed

--- tests/cli_tests/zboxcli_file_delete_test.go
+++ tests/cli_tests/zboxcli_file_delete_test.go
@@ -23,7 +23,7 @@ func TestFileDelete(testSetup *testing.T) {
 	t.SetSmokeTests("delete existing file in root directory should work")
 
 	t.Parallel()
-	t.Run("delete directory with multiple existing file in root directory should work", func(t *test.SystemTest) {
+	t.Run("delete non root directory with multiple existing file present should work", func(t *test.SystemTest) {
 		allocationID := setupAllocation(t, configPath)
 		createAllocationTestTeardown(t, allocationID)
 
@@ -61,7 +61,7 @@ func TestFileDelete(testSetup *testing.T) {
 		require.Contains(t, strings.Join(output, "\n"), "Invalid path record not found")
 	})
 
-	t.Run("delete root directory with No existing file in root directory should work", func(t *test.SystemTest) {
+	t.Run("delete root directory with No existing file should not work", func(t *test.SystemTest) {
 		allocationID := setupAllocation(t, configPath)
 		createAllocationTestTeardown(t, allocationID)
 
@@ -71,19 +71,37 @@ func TestFileDelete(testSetup *testing.T) {
 			"allocation": allocationID,
 			"remotepath": "/",
 		}), true)
+		require.NotNil(t, err, strings.Join(output, "\n"))
+		require.Len(t, output, 1)
+		require.Equal(t, `Delete failed. consensus_not_met: Multioperation failed. Required consensus 3 got 0. Major error: delete_failed: Delete failed. response_error: unexpected response with status code 400, message: {"error":"file was deleted"}`, output[0])
+
+	})
+
+	t.Run("delete non-root directory with No existing file should work", func(t *test.SystemTest) {
+		allocationID := setupAllocation(t, configPath)
+		createAllocationTestTeardown(t, allocationID)
+		dirname := "/child"
+		output, err := createDir(t, configPath, allocationID, dirname, true)
+		require.Nil(t, err, "Unexpected create dir failure %s", strings.Join(output, "\n"))
+		require.Len(t, output, 1)
+		require.Equal(t, dirname+" directory created", output[0])
+
+		output, err = deleteFile(t, escapedTestName(t), createParams(map[string]interface{}{
+			"allocation": allocationID,
+			"remotepath": dirname,
+		}), true)
 		require.Nil(t, err, strings.Join(output, "\n"))
 		require.Len(t, output, 1)
-		require.Equal(t, fmt.Sprintf("%s deleted", remotepath), output[0])
+		require.Equal(t, fmt.Sprintf("%s deleted", dirname), output[0])
 
 		output, err = listFilesInAllocation(t, configPath, createParams(map[string]interface{}{
 			"allocation": allocationID,
-			"remotepath": remotepath,
+			"remotepath": dirname,
 			"json":       "",
 		}), true)
-		require.Nil(t, err, "List files failed", err, strings.Join(output, "\n"))
+		require.NotNil(t, err, "List files failed", err, strings.Join(output, "\n"))
 		require.Len(t, output, 1)
-		require.Equal(t, "null", output[0], strings.Join(output, "\n"))
-
+		require.Equal(t, `error from server list response: {"code":"invalid_parameters","error":"invalid_parameters: Invalid path record not found"}`, output[0], strings.Join(output, "\n"))
 	})
 
 	t.Run("delete existing file in root directory should work", func(t *test.SystemTest) {
@@ -182,7 +200,7 @@ func TestFileDelete(testSetup *testing.T) {
 		require.Equal(t, "null", output[0], strings.Join(output, "\n"))
 	})
 
-	t.Run("delete existing non-root directory should work", func(t *test.SystemTest) {
+	t.Run("delete existing non-root directory with file present should work", func(t *test.SystemTest) {
 		allocationID := setupAllocation(t, configPath)
 		createAllocationTestTeardown(t, allocationID)
 
@@ -243,7 +261,7 @@ func TestFileDelete(testSetup *testing.T) {
 		require.Equal(t, "null", output[0], strings.Join(output, "\n"))
 	})
 
-	t.Run("delete existing root directory should work", func(t *test.SystemTest) {
+	t.Run("delete existing root directory with files present should work", func(t *test.SystemTest) {
 		allocationID := setupAllocation(t, configPath)
 		createAllocationTestTeardown(t, allocationID)
 
