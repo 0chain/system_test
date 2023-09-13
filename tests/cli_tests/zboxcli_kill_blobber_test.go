82c3291a1f4610b751031624b1e629c89f50bdb7
Jayash Satolia <jayash.satolia.min19@iitbhu.ac.in>
2023-09-13 16:47:48 +0530

    Fix

--- tests/cli_tests/zboxcli_kill_blobber_test.go
+++ tests/cli_tests/zboxcli_kill_blobber_test.go
@@ -82,7 +82,7 @@ func TestKillBlobber(testSetup *testing.T) {
 			t.Log("spBefore", spBefore.Delegate[poolIndex].Balance)
 			t.Log("spAfter", spAfter.Delegate[poolIndex].Balance)
 			t.Log("killSlash", killSlash)
-			require.InDeltaf(t, float64(spBefore.Delegate[poolIndex].Balance)*killSlash, float64(spAfter.Delegate[poolIndex].Balance), delta,
+			require.InEpsilon(t, float64(spBefore.Delegate[poolIndex].Balance)*killSlash, float64(spAfter.Delegate[poolIndex].Balance), 0.05,
 				"stake pools should be slashed by %f", killSlash)
 		}
 
