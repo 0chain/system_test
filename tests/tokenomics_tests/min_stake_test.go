8ec480369777079bb0b4fd3c306906b38d0a1959
Jayash Satolia <jayash.satolia.min19@iitbhu.ac.in>
2023-10-07 23:40:13 +0530

    Fix

--- tests/tokenomics_tests/min_stake_test.go
+++ tests/tokenomics_tests/min_stake_test.go
@@ -1,1 +1,73 @@
 package tokenomics_tests
+
+import (
+	"encoding/json"
+	"github.com/0chain/system_test/internal/api/util/test"
+	climodel "github.com/0chain/system_test/internal/cli/model"
+	"github.com/0chain/system_test/tests/tokenomics_tests/utils"
+	"github.com/stretchr/testify/require"
+	"strings"
+	"testing"
+	"time"
+)
+
+func TestMinStakeForProviders(testSetup *testing.T) {
+	t := test.NewSystemTest(testSetup)
+
+	t.TestSetup("set storage config to use time_unit as 10 minutes", func() {
+		output, err := utils.UpdateStorageSCConfig(t, scOwnerWallet, map[string]string{
+			"time_unit": "10m",
+		}, true)
+		require.Nil(t, err, strings.Join(output, "\n"))
+	})
+
+	t.Cleanup(func() {
+		output, err := utils.UpdateStorageSCConfig(t, scOwnerWallet, map[string]string{
+			"time_unit": "1h",
+		}, true)
+		require.Nil(t, err, strings.Join(output, "\n"))
+	})
+
+	var blobberList []climodel.BlobberInfo
+	output, err := utils.ListBlobbers(t, configPath, "--json")
+	require.Nil(t, err, "Error listing blobbers", strings.Join(output, "\n"))
+	require.Len(t, output, 2)
+
+	err = json.Unmarshal([]byte(output[1]), &blobberList)
+	require.Nil(t, err, "Error unmarshalling blobber list", strings.Join(output, "\n"))
+	require.True(t, len(blobberList) > 0, "No blobbers found in blobber list")
+
+	var blobberListString []string
+	for _, blobber := range blobberList {
+		blobberListString = append(blobberListString, blobber.Id)
+	}
+
+	var validatorList []climodel.Validator
+	output, err = utils.ListValidators(t, configPath, "--json")
+	require.Nil(t, err, "Error listing validators", strings.Join(output, "\n"))
+	require.Len(t, output, 1)
+
+	err = json.Unmarshal([]byte(output[0]), &validatorList)
+	require.Nil(t, err, "Error unmarshalling validator list", strings.Join(output, "\n"))
+	require.True(t, len(validatorList) > 0, "No validators found in validator list")
+
+	var validatorListString []string
+	for _, validator := range validatorList {
+		validatorListString = append(validatorListString, validator.ID)
+	}
+
+	t.Log("Blobber List: ", blobberListString)
+	t.Log("Validator List: ", validatorListString)
+
+	t.RunWithTimeout("miner rewards", 10*time.Minute, func(t *test.SystemTest) {
+
+	})
+
+	t.RunWithTimeout("sharder rewards", 10*time.Minute, func(t *test.SystemTest) {
+
+	})
+
+	t.RunWithTimeout("blobber and validator rewards", 10*time.Minute, func(t *test.SystemTest) {
+
+	})
+}
