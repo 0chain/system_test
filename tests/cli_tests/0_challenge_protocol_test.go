759b2ec4219de0d887dd618093ad11ec611971b8
Jayash Satolia <jayash.satolia.min19@iitbhu.ac.in>
2023-10-13 05:55:58 +0530

    Ready to merge

--- tests/cli_tests/0_challenge_protocol_test.go
+++ tests/cli_tests/0_challenge_protocol_test.go
@@ -20,7 +20,6 @@ import (
 
 func TestProtocolChallenge(testSetup *testing.T) {
 	t := test.NewSystemTest(testSetup)
-	t.Skip("Skipping challenge protocol tests")
 	t.SetSmokeTests("Number of challenges between 2 blocks should be equal to the number of blocks (given that we have active allocations)")
 
 	var blobberList []climodel.BlobberInfo
