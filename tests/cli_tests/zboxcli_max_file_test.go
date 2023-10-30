ae36f8ba046db0b673d1d924ec5316fdd60d471c
Jayash Satolia <jayash.satolia.min19@iitbhu.ac.in>
2023-10-31 04:30:30 +0530

    Added max_file_size

---
+++ tests/cli_tests/zboxcli_max_file_test.go
@@ -0,0 +1,94 @@
+package cli_tests
+
+import (
+	"github.com/0chain/system_test/internal/api/util/test"
+	"github.com/0chain/system_test/tests/tokenomics_tests/utils"
+	"github.com/stretchr/testify/require"
+	"path/filepath"
+	"strings"
+	"testing"
+)
+
+func TestMaxFileSize(testSetup *testing.T) {
+	t := test.NewSystemTest(testSetup)
+
+	t.TestSetup("Create new owner wallet", func() {
+		output, err := updateStorageSCConfig(t, scOwnerWallet, map[string]string{
+			"max_file_size": "1024",
+		}, true)
+		require.Nil(t, err, strings.Join(output, "\n"))
+	})
+
+	t.Cleanup(func() {
+		output, err := updateStorageSCConfig(t, scOwnerWallet, map[string]string{
+			"max_file_size": "1000000000000",
+		}, true)
+		require.Nil(t, err, strings.Join(output, "\n"))
+	})
+
+	t.Run("should be able to upload files equal or smaller than max_file_size per blobber", func(t *test.SystemTest) {
+		output, err := utils.CreateWallet(t, configPath)
+		require.Nil(t, err, "Error registering wallet", strings.Join(output, "\n"))
+
+		_, err = utils.ExecuteFaucetWithTokens(t, configPath, 9)
+		require.Nil(t, err, "Error executing faucet", strings.Join(output, "\n"))
+
+		output, err = utils.CreateNewAllocation(t, configPath, utils.CreateParams(map[string]interface{}{
+			"size":   10 * MB,
+			"data":   1,
+			"lock":   2,
+			"parity": 1,
+		}))
+		require.Nil(t, err, "Error creating allocation", strings.Join(output, "\n"))
+
+		allocationId, err := utils.GetAllocationID(output[0])
+		require.Nil(t, err, "Error getting allocation ID", strings.Join(output, "\n"))
+
+		remotepath := "/dir/"
+		filesize := 1024
+		filename := utils.GenerateRandomTestFileName(t)
+
+		err = utils.CreateFileWithSize(filename, int64(filesize))
+		require.Nil(t, err)
+
+		output, err = utils.UploadFile(t, configPath, map[string]interface{}{
+			"allocation": allocationId,
+			"remotepath": remotepath + filepath.Base(filename),
+			"localpath":  filename,
+		}, true)
+		require.Nil(t, err, "error uploading file", strings.Join(output, "\n"))
+	})
+
+	t.Run("upload should fail on sizes higher than max_file_size per blobber", func(t *test.SystemTest) {
+		output, err := utils.CreateWallet(t, configPath)
+		require.Nil(t, err, "Error registering wallet", strings.Join(output, "\n"))
+
+		_, err = utils.ExecuteFaucetWithTokens(t, configPath, 9)
+		require.Nil(t, err, "Error executing faucet", strings.Join(output, "\n"))
+
+		output, err = utils.CreateNewAllocation(t, configPath, utils.CreateParams(map[string]interface{}{
+			"size":   10 * MB,
+			"data":   1,
+			"lock":   2,
+			"parity": 1,
+		}))
+		require.Nil(t, err, "Error creating allocation", strings.Join(output, "\n"))
+
+		allocationId, err := utils.GetAllocationID(output[0])
+		require.Nil(t, err, "Error getting allocation ID", strings.Join(output, "\n"))
+
+		remotepath := "/dir/"
+		filesize := 2048
+		filename := utils.GenerateRandomTestFileName(t)
+
+		err = utils.CreateFileWithSize(filename, int64(filesize))
+		require.Nil(t, err)
+
+		output, err = utils.UploadFile(t, configPath, map[string]interface{}{
+			"allocation": allocationId,
+			"remotepath": remotepath + filepath.Base(filename),
+			"localpath":  filename,
+		}, true)
+		require.Error(t, err, "Upload should fail")
+	})
+}
