ae092c0548eb03f52cc842d622ad330549c63855
zorro30 <gbp301195@gmail.com>
2023-12-31 13:26:51 +0530

    updated assertions & reduced size

--- tests/cli_tests/0_zboxcli_file_resume_upload_test.go
+++ tests/cli_tests/0_zboxcli_file_resume_upload_test.go
@@ -81,7 +81,7 @@ func TestResumeUpload(testSetup *testing.T) {
 		require.Equal(t, expected, output[1])
 	})
 
-	t.RunSequentiallyWithTimeout("Resume upload with diff file name", 10*time.Minute, func(t *test.SystemTest) {
+	t.RunSequentiallyWithTimeout("Resume upload with diff filename having same filesize (Negative)", 10*time.Minute, func(t *test.SystemTest) {
 		allocSize := int64(2 * GB)
 		fileSize := int64(100 * MB)
 
@@ -99,6 +99,7 @@ func TestResumeUpload(testSetup *testing.T) {
 		require.Nil(t, err)
 		defer func() {
 			os.Remove(filename) //nolint: errcheck
+			os.Remove(filename2) //nolint: errcheck
 		}()
 
 		param := map[string]interface{}{
@@ -137,7 +138,7 @@ func TestResumeUpload(testSetup *testing.T) {
 		require.Equal(t, expected, err.Error())
 	})
 
-	t.RunSequentiallyWithTimeout("Resume upload with diff file content", 10*time.Minute, func(t *test.SystemTest) {
+	t.RunSequentiallyWithTimeout("Resume upload with diff filesize having same filename (Negative)", 10*time.Minute, func(t *test.SystemTest) {
 		allocSize := int64(2 * GB)
 		fileSize := int64(100 * MB)
 		fileSize2 := int64(150 * MB)
@@ -184,6 +185,72 @@ func TestResumeUpload(testSetup *testing.T) {
 			"chunknumber": 500, // 64KB * 500 = 32M
 		}, false)
 
+		//asserting output
+		require.NotNil(t, err, strings.Join(output, "\n"))
+		expected := fmt.Sprintf(
+			"Error in file operation: consensus_not_met: Commit failed. Required consensus 3, got 0",
+		)
+		require.Equal(t, expected, output[1])
+		//asserting error
+		require.Error(t, err)
+		expected = fmt.Sprintf(
+			"exit status 1",
+		)
+		require.Equal(t, expected, err.Error())
+	})
+
+	t.RunSequentiallyWithTimeout("Resume upload with diff filesize and diff filename (Negative)", 10*time.Minute, func(t *test.SystemTest) {
+		allocSize := int64(2 * GB)
+		fileSize := int64(200 * MB)
+		fileSize2 := int64(300 * MB)
+
+		output, err := executeFaucetWithTokens(t, configPath, 100.0)
+		require.Nil(t, err, "error executing faucet", strings.Join(output, "\n"))
+
+		allocationID := setupAllocation(t, configPath, map[string]interface{}{
+			"size": allocSize,
+			"lock": 50,
+		})
+
+		filename := generateRandomTestFileName(t)
+		filename2 := generateRandomTestFileName(t)
+		err = createFileWithSize(filename, fileSize)
+		require.Nil(t, err)
+		defer func() {
+			os.Remove(filename) //nolint: errcheck
+			os.Remove(filename2) //nolint: errcheck
+		}()
+
+		param := map[string]interface{}{
+			"allocation":  allocationID,
+			"remotepath":  "/dummy",
+			"localpath":   filename,
+			"chunknumber": 500, // 64KB * 500 = 32M
+		}
+		upload_param := createParams(param)
+		command := fmt.Sprintf(
+			"./zbox upload %s --silent --wallet %s --configDir ./config --config %s",
+			upload_param,
+			escapedTestName(t)+"_wallet.json",
+			configPath,
+		)
+
+		cmd, _ := cliutils.StartCommandWithoutRetry(command)
+		uploaded := waitPartialUploadAndInterrupt(t, cmd)
+		t.Logf("the uploaded is %v ", uploaded)
+
+		// using diff filename & increasing the file size to test the negative flow
+		err = createFileWithSize(filename2, fileSize2)
+		output, err = uploadFile(t, configPath, map[string]interface{}{
+			"allocation":  allocationID,
+			"remotepath":  "/dummy",
+			"localpath":   filename2,
+			"chunknumber": 500, // 64KB * 500 = 32M
+		}, false)
+
+		fmt.Println("Output=======>>>>>>", output)
+		fmt.Println("Output0=======>>>>>>", output[0])
+		fmt.Println("Err===========>>>>>", err)
 		//asserting output
 		require.NotNil(t, err, strings.Join(output, "\n"))
 		expected := fmt.Sprintf(
