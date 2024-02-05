818751f21f3e02a6e17a91c6368e0a7f658788e3
Jayash Satolia <jayash.satolia.min19@iitbhu.ac.in>
2024-02-06 01:16:24 +0530

    Fix

--- tests/cli_tests/zboxcli_file_download_resume_test.go
+++ tests/cli_tests/zboxcli_file_download_resume_test.go
@@ -19,8 +19,6 @@ import (
 func TestResumeDownload(testSetup *testing.T) {
 	t := test.NewSystemTest(testSetup)
 
-	t.Parallel()
-
 	t.RunWithTimeout("Resume download should work", 10*time.Minute, func(t *test.SystemTest) {
 		allocSize := int64(600 * MB)
 		filesize := int64(300 * MB)
