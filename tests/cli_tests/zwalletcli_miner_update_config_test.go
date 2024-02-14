ea645f1fb2b732b470f99f7cdcf4c2500527bc54
CristalWilsonLobo <cristal.lobo3@gmail.com>
2024-02-13 19:58:03 -0500

    Initial TC suite #987

--- tests/cli_tests/zwalletcli_miner_update_config_test.go
+++ tests/cli_tests/zwalletcli_miner_update_config_test.go
@@ -84,6 +84,99 @@ func TestMinerUpdateConfig(testSetup *testing.T) {
 		require.Len(t, output, 1, strings.Join(output, "\n"))
 		require.Equal(t, "number keys must equal the number values", output[0], strings.Join(output, "\n"))
 	})
+
+	// Max Miner Count - Test case for updating max_n to the maximum allowed value
+	t.RunSequentially("successful update of max_n to maximum allowed value", func(t *test.SystemTest) {
+		configKey := "max_n"
+		maxValue := "100" // The maximum allowed value for max_n
+
+		output, err := updateMinerSCConfig(t, scOwnerWallet, map[string]interface{}{
+			"keys":   configKey,
+			"values": maxValue,
+		}, true)
+		require.Nil(t, err, strings.Join(output, "\n"))
+		require.True(t, isUpdateSuccess(output), "Update to max_n did not succeed with maximum value")
+	})
+
+	// Min Miner Count - Test case for updating min_n to the minimum allowed value
+	t.RunSequentially("successful update of min_n to minimum allowed value", func(t *test.SystemTest) {
+		configKey := "min_n"
+		minValue := "3" // The minimum allowed value for min_n
+
+		output, err := updateMinerSCConfig(t, scOwnerWallet, map[string]interface{}{
+			"keys":   configKey,
+			"values": minValue,
+		}, true)
+		require.Nil(t, err, strings.Join(output, "\n"))
+		require.True(t, isUpdateSuccess(output), "Update to min_n did not succeed with minimum value")
+	})
+
+	// Max Sharder Count - Test case for updating max_s to the maximum allowed value
+	t.RunSequentially("successful update of max_s to maximum allowed value", func(t *test.SystemTest) {
+		configKey := "max_s"
+		maxValue := "30" // The maximum allowed value for max_s
+
+		output, err := updateMinerSCConfig(t, scOwnerWallet, map[string]interface{}{
+			"keys":   configKey,
+			"values": maxValue,
+		}, true)
+		require.Nil(t, err, strings.Join(output, "\n"))
+		require.True(t, isUpdateSuccess(output), "Update to max_s did not succeed with maximum value")
+	})
+
+	// Min Sharder Count - Test case for updating min_s to the minimum allowed value
+	t.RunSequentially("successful update of min_s to minimum allowed value", func(t *test.SystemTest) {
+		configKey := "min_s"
+		minValue := "1" // The minimum allowed value for min_s
+
+		output, err := updateMinerSCConfig(t, scOwnerWallet, map[string]interface{}{
+			"keys":   configKey,
+			"values": minValue,
+		}, true)
+		require.Nil(t, err, strings.Join(output, "\n"))
+		require.True(t, isUpdateSuccess(output), "Update to min_s did not succeed with minimum value")
+	})
+
+	// Reward Rate - Test cases for updating reward_rate
+	t.RunSequentially("successful update of reward_rate to zero", func(t *test.SystemTest) {
+		configKey := "reward_rate"
+		newValue := "0" // Setting reward rate to zero to test open interval of range [0; 1)
+
+		output, err := updateMinerSCConfig(t, scOwnerWallet, map[string]interface{}{
+			"keys":   configKey,
+			"values": newValue,
+		}, true)
+		require.Nil(t, err, strings.Join(output, "\n"))
+		require.True(t, isUpdateSuccess(output), "Update to reward_rate did not succeed with value zero")
+	})
+
+	// Reward Rate - Test cases for updating reward_rate
+	t.RunSequentially("successful update of reward_rate to 0.5", func(t *test.SystemTest) {
+		configKey := "reward_rate"
+		newValue := "0.5" // Setting reward rate to 0.5  to test mid range [0; 1)
+
+		output, err := updateMinerSCConfig(t, scOwnerWallet, map[string]interface{}{
+			"keys":   configKey,
+			"values": newValue,
+		}, true)
+		require.Nil(t, err, strings.Join(output, "\n"))
+		require.True(t, isUpdateSuccess(output), "Update to reward_rate did not succeed with value 0.5")
+	})
+
+	// Block Reward - Test case for updating block_reward
+	t.RunSequentially("successful update of block_reward", func(t *test.SystemTest) {
+		configKey := "block_reward"
+		newValue := "0.9" // Flointing point value block reward
+
+		output, err := updateMinerSCConfig(t, scOwnerWallet, map[string]interface{}{
+			"keys":   configKey,
+			"values": newValue,
+		}, true)
+		require.Nil(t, err, strings.Join(output, "\n"))
+		require.True(t, isUpdateSuccess(output), "Update to block_reward did not succeed with specified value")
+	})
+       
+
 }
 
 func getMinerSCConfig(t *test.SystemTest, cliConfigFilename string, retry bool) ([]string, error) {
