c557d39f62be4d0e35f46406366823d157b1acc5
Jayash Satolia <jayash.satolia.min19@iitbhu.ac.in>
2024-02-15 22:02:34 +0530

    Cleanup deadcode

--- tests/cli_tests/zboxcli_kill_blobber_test.go
+++ tests/cli_tests/zboxcli_kill_blobber_test.go
@@ -361,9 +361,9 @@ func shutdownBlobber(t *test.SystemTest, wallet, cliConfigFilename, params strin
 	}
 }
 
-func collectRewards(t *test.SystemTest, cliConfigFilename, params string, retry bool) ([]string, error) {
-	return collectRewardsForWallet(t, cliConfigFilename, params, escapedTestName(t), retry)
-}
+//func collectRewards(t *test.SystemTest, cliConfigFilename, params string, retry bool) ([]string, error) {
+//	return collectRewardsForWallet(t, cliConfigFilename, params, escapedTestName(t), retry)
+//}
 
 func collectRewardsForWallet(t *test.SystemTest, cliConfigFilename, params, wallet string, retry bool) ([]string, error) {
 	t.Log("collecting rewards...")
