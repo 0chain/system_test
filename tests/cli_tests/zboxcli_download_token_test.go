1e9a0b747cae58fc90f7bc29a0322440a880f493
zorro30 <gbp301195@gmail.com>
2024-02-22 22:29:33 +0530

    removed tests related to download cost

--- tests/cli_tests/zboxcli_download_token_test.go
+++ tests/cli_tests/zboxcli_download_token_test.go
@@ -1,120 +1,13 @@
 package cli_tests
 
 import (
-	"encoding/json"
 	"fmt"
-	"os"
-	"path/filepath"
-	"strconv"
-	"strings"
-	"testing"
 	"time"
 
 	"github.com/0chain/system_test/internal/api/util/test"
 
-	climodel "github.com/0chain/system_test/internal/cli/model"
 	cliutils "github.com/0chain/system_test/internal/cli/util"
-	"github.com/stretchr/testify/require"
 )
-
-func TestFileDownloadTokenMovement(testSetup *testing.T) {
-	t := test.NewSystemTest(testSetup)
-	t.SetSmokeTests("Downloader's readpool balance should reduce by download cost")
-
-	t.Parallel()
-
-	t.RunWithTimeout("Downloader's readpool balance should reduce by download cost", 5*time.Minute, func(t *test.SystemTest) { //TODO: way too slow
-		walletOwner := escapedTestName(t)
-		allocSize := int64(50 * MB)
-		allocationID := setupAllocation(t, configPath, map[string]interface{}{
-			"size": allocSize,
-		})
-
-		file := generateRandomTestFileName(t)
-		remoteOwnerPath := "/" + filepath.Base(file)
-		fileSize := int64(10 * MB) // must upload bigger file to ensure has noticeable cost
-		err := createFileWithSize(file, fileSize)
-		require.Nil(t, err)
-
-		uploadParams := map[string]interface{}{
-			"allocation": allocationID,
-			"localpath":  file,
-			"remotepath": remoteOwnerPath,
-		}
-		output, err := uploadFile(t, configPath, uploadParams, true)
-		require.Nil(t, err, strings.Join(output, "\n"))
-		require.Len(t, output, 2)
-		require.Equal(t, fmt.Sprintf("Status completed callback. Type = application/octet-stream. Name = %s", filepath.Base(file)), output[1])
-
-		// locking 1 read tokens to readPool via wallet
-		createWalletForNameAndLockReadTokens(t, configPath, walletOwner)
-
-		output, err = readPoolInfoWithWallet(t, walletOwner, configPath)
-		require.Nil(t, err, "Error fetching read pool", strings.Join(output, "\n"))
-		require.Len(t, output, 1)
-
-		initialReadPool := climodel.ReadPoolInfo{}
-		err = json.Unmarshal([]byte(output[0]), &initialReadPool)
-		require.Nil(t, err, "Error unmarshalling read pool", strings.Join(output, "\n"))
-		require.NotEmpty(t, initialReadPool)
-
-		// staked a total of 1 ZCN in readpool
-		require.Equal(t, 1e10, float64(initialReadPool.Balance))
-
-		output, err = getDownloadCost(t, configPath, createParams(map[string]interface{}{
-			"allocation": allocationID,
-			"remotepath": remoteOwnerPath,
-		}), true)
-		require.Nil(t, err, "Could not get download cost", strings.Join(output, "\n"))
-		require.Len(t, output, 1)
-
-		expectedDownloadCost, err := strconv.ParseFloat(strings.Fields(output[0])[0], 64)
-		require.Nil(t, err, "Cost couldn't be parsed to float", strings.Join(output, "\n"))
-
-		unit := strings.Fields(output[0])[1]
-		expectedDownloadCostInSas := unitToZCN(expectedDownloadCost, unit) * 1e10
-		t.Logf("Download cost: %v sas", expectedDownloadCostInSas)
-
-		// Download the file (delete local copy first)
-		os.Remove(file)
-
-		downloadParams := createParams(map[string]interface{}{
-			"localpath":  file,
-			"allocation": allocationID,
-			"remotepath": remoteOwnerPath,
-		})
-
-		// downloading file for wallet
-		output, err = downloadFileForWallet(t, walletOwner, configPath, downloadParams, true)
-		require.Nil(t, err, strings.Join(output, "\n"))
-		require.Len(t, output, 2, "download file - Unexpected output", strings.Join(output, "\n"))
-		require.Contains(t, output[1], StatusCompletedCB)
-		require.Contains(t, output[1], filepath.Base(file))
-
-		// waiting 60 seconds for blobber to redeem tokens
-		cliutils.Wait(t, 60*time.Second)
-
-		output, err = readPoolInfoWithWallet(t, walletOwner, configPath)
-		require.Nil(t, err, "Error fetching read pool", strings.Join(output, "\n"))
-		require.Len(t, output, 1)
-
-		finalReadPool := climodel.ReadPoolInfo{}
-		err = json.Unmarshal([]byte(output[0]), &finalReadPool)
-		require.Nil(t, err, "Error unmarshalling read pool", strings.Join(output, "\n"))
-		require.NotEmpty(t, finalReadPool)
-
-		expectedRPBalance := float64(initialReadPool.Balance) - expectedDownloadCostInSas - 10 // because download cost is till 3 decimal point only and missing the 4th decimal digit
-		require.Nil(t, err, "Error fetching read pool", strings.Join(output, "\n"))
-
-		// getDownloadCost returns download cost when all the associated blobbers of an allocation are required
-		// In current enhancement/verify-download PR, it gets data from minimum blobbers possible.
-		// So the download cost will be in between initial balance and expected balance.
-		t.Logf("FinalReadPool.Balance:%d\nInitialReadPool.Balance:%d\nExpectedReadPool.Balance:%d\n", finalReadPool.Balance, initialReadPool.Balance, int64(expectedRPBalance))
-		require.Equal(t, true,
-			finalReadPool.Balance < initialReadPool.Balance &&
-				finalReadPool.Balance >= int64(expectedRPBalance))
-	})
-}
 
 func readPoolInfo(t *test.SystemTest, cliConfigFilename string) ([]string, error) {
 	return readPoolInfoWithWallet(t, escapedTestName(t), cliConfigFilename)
