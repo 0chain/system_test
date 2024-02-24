baa2d73f1df48ced627cb5918a756ea2cabb6276
Ash-KODES <choudharykumarashutosh1209@gmail.com>
2024-02-24 20:44:03 +0530

    Adding set split key wallet info test

---
+++ tests/api_tests/set_wallet_info_test.go
@@ -0,0 +1,31 @@
+package api_tests
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/0chain/gosdk/zcncore"
+	"github.com/0chain/system_test/internal/api/util/test"
+	"github.com/stretchr/testify/require"
+)
+
+func TestSetWalletInfo(testSetup *testing.T) {
+	t := test.NewSystemTest(testSetup)
+	t.SetSmokeTests("Check if setWalletInfo is setting splitkey wallet or not")
+
+	t.Run("Check if setWalletInfo is setting splitkey wallet or not", func(t *test.SystemTest) {
+		wallet := createWallet(t)
+		privateKey := wallet.Keys.PrivateKey
+		// this represents number of split keys made from private key
+		numSplit := 2
+		splitKeyWallet, err := zcncore.SplitKeys(privateKey, numSplit)
+		if err != nil {
+			fmt.Println("Error while spliting keys:", err)
+			return
+		}
+		// setwallet info takes a json string wallet (marshalled wallet)
+		err = zcncore.SetWalletInfo(splitKeyWallet, true)
+		require.Nil(t, err)
+		require.NotNil(t, splitKeyWallet)
+	})
+}
