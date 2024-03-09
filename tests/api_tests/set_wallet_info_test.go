d688ab0769352360b66e69d33b819ae319c49076
Ash-KODES <choudharykumarashutosh1209@gmail.com>
2024-03-09 22:29:22 +0530

    Serializing the private key

--- tests/api_tests/set_wallet_info_test.go
+++ tests/api_tests/set_wallet_info_test.go
@@ -3,7 +3,7 @@ package api_tests
 import (
 	"fmt"
 	"testing"
-
+	"encoding/base64"
 	"github.com/0chain/gosdk/zcncore"
 	"github.com/0chain/system_test/internal/api/util/test"
 	"github.com/stretchr/testify/require"
@@ -16,9 +16,11 @@ func TestSetWalletInfo(testSetup *testing.T) {
 	t.Run("Check if setWalletInfo is setting splitkey wallet or not", func(t *test.SystemTest) {
 		wallet := createWallet(t)
 		privateKey := wallet.Keys.PrivateKey
+		serializedPrivateKey := privateKey.Serialize()
+		stringPrivateKey := base64.StdEncoding.EncodeToString(serializedPrivateKey)
 		// this represents number of split keys made from private key
 		numSplit := 2
-		splitKeyWallet, err := zcncore.SplitKeys(privateKey, numSplit)
+		splitKeyWallet, err := zcncore.SplitKeys(stringPrivateKey, numSplit)
 		if err != nil {
 			fmt.Println("Error while spliting keys:", err)
 			return
--- tests/api_tests/split_key_test.go
+++ tests/api_tests/split_key_test.go
@@ -25,23 +25,6 @@ func TestSplitKey(testSetup *testing.T) {
 		numSplit := 2
 		wStr, err := zcncore.SplitKeys(stringPrivateKey, numSplit)
 		require.NoError(t, err, "failed to create split key wallet using private key")
-		/*
-			{
-				"client_id": "f2461679c2407f12a0cbe161b55f1367aeb7af9e196438effa39b9c29e147af8",
-				"client_key": "49bd9013d0ebee27ff16f4d4b6888db21c5e2db9c4f93c2a11d7124f86e7580fb2ccd2ad5ba2450ddf8c6f6b280fa4db6be48a8a9c276b8cd02be012fb5b4e21",
-				"keys": [
-					{
-					    "public_key": "f4caf190ffa8be2d1fd03f79d781ffe0d3fadc44708d3c0fad2eec23af367b11316debd2f27882ef41545f2855e14fefd62e795fd2386a792366aef6e8e0a71f",
-					    "private_key": "18aeedb37af04422ab3ed28b51bf006029083a79a77274074042a18364b43b16"
-					}
-				],
-
-				"mnemonics": "",
-				"version": "",
-				"date_created": "",
-				"nonce": 0
-			}
-		*/
 		var splitKeyWallet  zcncrypto.Wallet
 		err = json.Unmarshal([]byte(wStr), &splitKeyWallet)
 		require.NoError(t, err, "failed to deserialize wallet object", wStr)
