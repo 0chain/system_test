07ddc02b747634b127df8ede8e29aaf076498ee1
zorro30 <gbp301195@gmail.com>
2024-03-09 16:46:42 +0530

    added base_url test

--- tests/cli_tests/zboxcli_blobber_config_update_test.go
+++ tests/cli_tests/zboxcli_blobber_config_update_test.go
@@ -214,6 +214,7 @@ func TestBlobberConfigUpdate(testSetup *testing.T) {
 		newNumberOfDelegates := intialBlobberInfo.StakePoolSettings.MaxNumDelegates + 1
 		newCapacity := intialBlobberInfo.Capacity + 1
 		newNotAvailable := !intialBlobberInfo.NotAvailable
+		url := intialBlobberInfo.BaseURL
 
 		output, err := updateBlobberInfo(t, configPath, createParams(map[string]interface{}{
 			"blobber_id":     intialBlobberInfo.ID,
@@ -223,6 +224,7 @@ func TestBlobberConfigUpdate(testSetup *testing.T) {
 			"num_delegates":  newNumberOfDelegates,
 			"capacity":       newCapacity,
 			"not_available":  newNotAvailable,
+			"url": 			  url,
 		}))
 		require.Nil(t, err, strings.Join(output, "\n"))
 		require.Len(t, output, 1)
@@ -246,6 +248,7 @@ func TestBlobberConfigUpdate(testSetup *testing.T) {
 		require.Equal(t, newNumberOfDelegates, finalBlobberInfo.StakePoolSettings.MaxNumDelegates)
 		require.Equal(t, newCapacity, finalBlobberInfo.Capacity)
 		require.Equal(t, newNotAvailable, finalBlobberInfo.NotAvailable)
+		require.Equal(t, url, finalBlobberInfo.BaseURL)
 	})
 }
 
