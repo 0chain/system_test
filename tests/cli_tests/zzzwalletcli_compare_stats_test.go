6723b921e4e897e53c74c4518f4d526a7ad7c671
CristalWilsonLobo <cristal.lobo3@gmail.com>
2024-04-19 21:40:11 -0400

    fetch provider specific node details

--- tests/cli_tests/zzzwalletcli_compare_stats_test.go
+++ tests/cli_tests/zzzwalletcli_compare_stats_test.go
@@ -39,8 +39,9 @@ func TestCompareMPTAndEventsDBData(testSetup *testing.T) {
 	t.Log(sharders[0].Host)
 */
 	//var resp *model.SCRestGetBlobbersResponse
-	scStateGetResponse, resp, err := fetchMPTdata(testSetup, StorageScAddress)
+	scStateGetResponse, resp, err := fetchMPTdata(t, StorageScAddress)
 	t.Log(scStateGetResponse)
+	t.Log(err)
 
 	t.RunSequentially("Compare data in MPT with events DB for blobbers", func(t *test.SystemTest) {
 
@@ -84,17 +85,56 @@ func TestCompareMPTAndEventsDBData(testSetup *testing.T) {
 	//sharder := sharders[reflect.ValueOf(sharders).MapKeys()[0].String()]
 
 }
-
-func fetchAndCompareProviderData(t *testing.T, block, provider , providerType string) {
-	t.Logf("Fetch state of provider : %s ", providerType)
+	
+func fetchAndLogProviderData(t *testing.T, baseURL, StorageScAddress, providerType string) {
+	/*
+		t.Logf("Fetch state of provider : %s ", providerType)
 
 	if(providerType == "blobber") {
 		var scRestGetBlobbersResponse *model.SCRestGetBlobbersResponse
 		scRestGetBlobbersResponse = provider
 		t.Log(scRestGetBlobbersResponse)
 	}
+	*/
+
+	// Fetch the list of provider IDs
+	url := fmt.Sprintf("%s/v1/screst/%s/%s_ids", baseURL, StorageScAddress, providerType)
+	resp, err := http.Get(url)
+	if err != nil {
+		t.Fatalf("Failed to fetch %s list: %v", providerType, err)
+	}
+	defer resp.Body.Close()
+
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		t.Fatalf("Failed to read response body for %s list: %v", providerType, err)
+	}
+
+	// Split the response body to get individual provider IDs
+	providerIDs := strings.Split(string(body), ",")
+	for _, id := range providerIDs {
+		fetchAndLogProviderDetails(t, baseURL, providerType, id)
+	}
 }
-	
+
+func fetchAndLogProviderDetails(t *testing.T, baseURL, providerType, id string) {
+	// URL to fetch details for the specific provider
+	detailURL := fmt.Sprintf("%s/%s/%s", baseURL, providerType, id)
+	t.Log("Request URL: ", detailURL)
+
+	resp, err := http.Get(detailURL)
+	if err != nil {
+		t.Fatalf("Failed to fetch data for %s %s: %v", providerType, id, err)
+	}
+	defer resp.Body.Close()
+
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		t.Fatalf("Failed to read response for %s %s: %v", providerType, id, err)
+	}
+
+	t.Logf("Response for %s %s: %s", providerType, id, string(body))
+}
 
 func fetchMPTdata(testSetup *test.SystemTest, StorageScAddress string) (*model.SCStateGetResponse, *resty.Response, error){
 
