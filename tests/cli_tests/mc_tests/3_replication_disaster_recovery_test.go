c6920b8b324692e0cf1215704227183b90fd99fe
pewssh <pewssh@gmail.com>
2024-06-21 15:18:30 +0545

    Test for mc and zs3server

---
+++ tests/cli_tests/mc_tests/3_replication_disaster_recovery_test.go
@@ -0,0 +1,99 @@
+package cli_tests
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"os/exec"
+	"strconv"
+	"testing"
+	"time"
+
+	test "github.com/0chain/system_test/internal/api/util/test"
+	cli_utils "github.com/0chain/system_test/internal/cli/util"
+
+	"github.com/stretchr/testify/assert"
+	"gopkg.in/yaml.v3"
+)
+
+func read_file_mc(testSetup *testing.T) (string, string, string, string, string, string, string) {
+	file, err := os.Open("mc_hosts.yaml")
+	if err != nil {
+		testSetup.Fatalf("Error opening hosts.yaml file: %v\n", err)
+	}
+	defer file.Close()
+
+	decoder := yaml.NewDecoder(file)
+	var hosts map[string]interface{}
+	err = decoder.Decode(&hosts)
+	if err != nil {
+		testSetup.Fatalf("Error decoding mc_hosts.yaml file: %v\n", err)
+	}
+
+	accessKey := hosts["access_key"].(string)
+	secretKey := hosts["secret_key"].(string)
+	port := hosts["port"].(int)
+	concurrent := hosts["concurrent"].(int)
+	server := hosts["server"].(string)
+	secondary_server := hosts["secondary_server"].(string)
+	s_port := hosts["secondary_port"].(int)
+
+	host := strconv.FormatInt(int64(port), 10)
+	secondary_port := strconv.FormatInt(int64(s_port), 10)
+	concurrent_no := strconv.FormatInt(int64(concurrent), 10)
+	return server, host, accessKey, secretKey, concurrent_no, secondary_port, secondary_server
+
+}
+
+func TestZs3ServerReplication(testSetup *testing.T) {
+	t := test.NewSystemTest(testSetup)
+
+	server, port, accessKey, secretKey, _, s_port, s_server := read_file_mc(testSetup)
+	// run minio server
+	log.Print(s_port)
+	cmd := exec.Command("./minio", "gateway", "zcn", "--console-address", ":8000")
+
+	err := cmd.Start()
+	if err != nil {
+		fmt.Println(err)
+	}
+	t.Logf("Minio server startted")
+
+	t.RunSequentially("Test for replication", func(t *test.SystemTest) {
+		// creating two server
+		t.Log(server, "server")
+		_, _ = cli_utils.RunCommand(t, "../mc alias set primary "+server+":"+port+" "+accessKey+" "+secretKey+" --api S3v2", 1, time.Hour*2)
+		_, _ = cli_utils.RunCommand(t, "../mc alias set secondary "+s_server+":"+port+" "+accessKey+" "+secretKey+" --api S3v2", 1, time.Hour*2)
+
+		// create bucket in primary
+		_, _ = cli_utils.RunCommand(t, "../mc mb primary/mybucket", 1, time.Hour*2)
+
+		// enable mirror in primary
+		output, _ := cli_utils.RunCommand(t, "../mc mirror --watch --force primary/mybucket secondary/mybucket", 1, time.Hour*2)
+
+		assert.NotContains(t, output, "error")
+	})
+
+	t.RunSequentially("Test for Disaster Recovery", func(t *test.SystemTest) {
+		// creating two server
+		_, _ = cli_utils.RunCommand(t, "../mc alias set primary "+server+":"+port+" "+accessKey+" "+secretKey+" --api S3v2", 1, time.Hour*2)
+		_, _ = cli_utils.RunCommand(t, "../mc alias set secondary "+s_server+":"+port+" "+accessKey+" "+secretKey+" --api S3v2", 1, time.Hour*2)
+
+		// create bucket in primary
+		_, _ = cli_utils.RunCommand(t, "../mc mb primary/mybucket", 1, time.Hour*2)
+
+		// enable mirror in primary
+		_, _ = cli_utils.RunCommand(t, "../mc mirror --watch --force primary/mybucket secondary/mybucket", 1, time.Hour*2)
+
+		// lets remove bucket from primary server and recover from secondary bucket
+
+		// remove bucket from primary
+		_, _ = cli_utils.RunCommand(t, "../mc rb primary/mybucket", 1, time.Hour*2)
+
+		// mirro from secondary bucket to primary bucket
+		output, _ := cli_utils.RunCommand(t, "../mc mirror --watch --force secondary/mybucket primary/mybucket", 1, time.Hour*2)
+
+		assert.NotContains(t, output, "error")
+	})
+
+}
---
+++ tests/cli_tests/zs3server_tests/setup_tear_down_test.go
@@ -0,0 +1,112 @@
+// create a tear down for all tests
+package zs3servertests
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"os/exec"
+	"path/filepath"
+	"regexp"
+	"testing"
+	"time"
+)
+
+var allocationId string
+
+func TestMain(m *testing.M) {
+	globalSetup()
+	timeout := time.Duration(60 * time.Minute)
+	os.Setenv("GO_TEST_TIMEOUT", timeout.String())
+	code := m.Run()
+	globalTearDown()
+	os.Exit(code)
+}
+
+func hasParentDir(path string) bool {
+	return path != filepath.Dir(path)
+}
+
+func globalSetup() {
+	currentDir, err := os.Getwd()
+	if err != nil {
+		log.Fatal("Failed to get current working directory:", err)
+	}
+
+	// Check if a parent directory exists
+	if !hasParentDir(currentDir) {
+		log.Fatal("Script must be run from a directory with a parent")
+	}
+
+	requiredCommands := map[string]string{
+		"mc":    "../mc",
+		"zbox":  "../zbox",
+		"minio": "../minio",
+		"warp":  "../warp",
+	}
+
+	for cmd, path := range requiredCommands {
+		if _, err := os.Stat(path); os.IsNotExist(err) {
+			log.Fatal(cmd + " is not installed")
+			os.Exit(1)
+		} else {
+			log.Fatal(cmd + " is  installed")
+		}
+
+		if requiredCommands[cmd] == requiredCommands["warp"] {
+			log.Print("All required commands are installed")
+		} else {
+			log.Print("Checking for next command")
+
+		}
+	}
+
+	// create allocation from allocation.yaml file
+	data, parity, lock := read_file_allocation()
+	cmd := exec.Command("../zbox", "newallocation", "--lock", lock, "--data", data, "--parity", parity)
+
+	// get the allocation id for created
+
+	output, err := cmd.CombinedOutput()
+
+	if err != nil {
+		log.Fatal("Error creating allocation: ", err)
+		os.Exit(1)
+	} else {
+		log.Fatal("Allocation created successfully")
+	}
+
+	// use regex
+	re := regexp.MustCompile(`Allocation created:\s*([a-f0-9]+)`)
+
+	match := re.FindStringSubmatch(string(output))
+	if len(match) > 1 {
+		allocationId = match[1]
+	}
+
+	os.Setenv("MINIO_ROOT_USER", "someminiouser")
+	os.Setenv("MINIO_ROOT_PASSWORD", "someminiopassword")
+
+	cmd = exec.Command("./minio", "gateway", "zcn", "--console-address", ":8000")
+
+	err = cmd.Start()
+	if err != nil {
+		fmt.Println(err)
+	}
+	log.Print("Minio server started")
+
+	println("Global setup code executed")
+
+}
+
+func globalTearDown() {
+	println("Global teardown code Executing .......")
+	err := exec.Command("../zbox", "delete", "--allocation", allocationId, "--remotepath", "/").Run()
+	if err != nil {
+		log.Fatal("Error deleting allocation: ", err)
+		os.Exit(1)
+	} else {
+		log.Print("Allocation deleted successfully")
+	}
+
+}
