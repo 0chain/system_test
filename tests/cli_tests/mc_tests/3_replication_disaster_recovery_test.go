7a6cccc9becd23e8d6670dd887727eb7e68dc15b
adarsharegmi <adarsharegmi121@gmail.com>
2024-06-23 13:29:51 +0545

    log

--- tests/cli_tests/mc_tests/3_replication_disaster_recovery_test.go
+++ tests/cli_tests/mc_tests/3_replication_disaster_recovery_test.go
@@ -76,8 +76,15 @@ func TestZs3ServerReplication(testSetup *testing.T) {
 
 	t.RunSequentially("Test for Disaster Recovery", func(t *test.SystemTest) {
 		// creating two server
-		_, _ = cli_utils.RunCommand(t, "../mc alias set primary "+server+":"+port+" "+accessKey+" "+secretKey+" --api S3v2", 1, time.Hour*2)
-		_, _ = cli_utils.RunCommand(t, "../mc alias set secondary "+s_server+":"+port+" "+accessKey+" "+secretKey+" --api S3v2", 1, time.Hour*2)
+		commandGenerated := "../mc alias set primary "+server+":"+port+" "+accessKey+" "+secretKey+" --api S3v2"
+		t.Log("command :: ", commandGenerated)
+
+		_, _ = cli_utils.RunCommand(t, commandGenerated, 1, time.Hour*2)
+
+		commandGenerated = "../mc alias set secondary "+s_server+":"+port+" "+accessKey+" "+secretKey+" --api S3v2"
+
+		t.Log("command :: ", commandGenerated)
+		_, _ = cli_utils.RunCommand(t, commandGenerated, 1, time.Hour*2)
 
 		// create bucket in primary
 		_, _ = cli_utils.RunCommand(t, "../mc mb primary/mybucket", 1, time.Hour*2)
