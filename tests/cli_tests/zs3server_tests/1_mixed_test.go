f54eb0f75e8d74c737bd5a7ba5d34bc6e3c02cbd
pewssh <pewssh@gmail.com>
2024-07-15 14:32:55 +0545

    warp change

--- tests/cli_tests/zs3server_tests/1_mixed_test.go
+++ tests/cli_tests/zs3server_tests/1_mixed_test.go
@@ -18,7 +18,7 @@ func TestZs3serverMixedWarpTests(testSetup *testing.T) {
 	t.Logf("Minio server Started")
 
 	t.RunSequentiallyWithTimeout("Warp Mixed Benchmark", 40*time.Minute, func(t *test.SystemTest) {
-		commandGenerated := "../warp mixed --host=" + config.Server + ":" + config.HostPort + " --access-key=" + config.AccessKey + " --secret-key=" + config.SecretKey + " --objects=" + "22" + " --duration=" + "30s" + "  --obj.size=" + "256B"
+		commandGenerated := "./warp mixed --host=" + config.Server + ":" + config.HostPort + " --access-key=" + config.AccessKey + " --secret-key=" + config.SecretKey + " --objects=" + "22" + " --duration=" + "30s" + "  --obj.size=" + "256B"
 		log.Println("Command Generated: ", commandGenerated)
 
 		output, err := cliutils.RunCommand(t, commandGenerated, 1, time.Hour*2)
