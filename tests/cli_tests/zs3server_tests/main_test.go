437a1a4fb49e733762e6581158f05e64fe47ce45
pewssh <pewssh@gmail.com>
2024-07-16 12:49:47 +0545

    Common server

--- tests/cli_tests/zs3server_tests/main_test.go
+++ tests/cli_tests/zs3server_tests/main_test.go
@@ -3,9 +3,13 @@ package zs3servertests
 import (
 	"os"
 	"testing"
+
+	cliutils "github.com/0chain/system_test/internal/cli/util"
 )
 
 func TestMain(m *testing.M) {
+	config := cliutils.ReadFile(nil)
+	_, _ = cliutils.RunMinioServer(config.AccessKey, config.SecretKey)
 	exitRun := m.Run()
 	os.Exit(exitRun)
 }
