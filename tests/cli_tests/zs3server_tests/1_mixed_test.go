4277ff1402b59bcea2dbfaef3b303b05fb5bdcfa
pewssh <pewssh@gmail.com>
2024-08-25 19:39:27 +0545

    Removed log

--- tests/cli_tests/zs3server_tests/1_mixed_test.go
+++ tests/cli_tests/zs3server_tests/1_mixed_test.go
@@ -1,7 +1,6 @@
 package zs3servertests
 
 import (
-	"log"
 	"strings"
 	"testing"
 	"time"
@@ -11,7 +10,6 @@ import (
 )
 
 func TestZs3serverMixedWarpTests(testSetup *testing.T) {
-	log.Println("Running Warp Mixed Benchmark...")
 	t := test.NewSystemTest(testSetup)
 	_, err := cliutils.RunMinioServer("rootroot", "rootroot")
 	if err != nil {
@@ -23,13 +21,11 @@ func TestZs3serverMixedWarpTests(testSetup *testing.T) {
 
 	t.RunSequentiallyWithTimeout("Warp Mixed Benchmark", 40*time.Minute, func(t *test.SystemTest) {
 		commandGenerated := "../warp mixed --host=" + config.Server + ":" + config.HostPort + " --access-key=" + config.AccessKey + " --secret-key=" + config.SecretKey + " --objects=" + "22" + " --duration=" + "30s" + "  --obj.size=" + "256B"
-		log.Println("Command Generated: ", commandGenerated)
 
 		output, err := cliutils.RunCommand(t, commandGenerated, 1, time.Hour*2)
 		if err != nil {
 			testSetup.Fatalf("Error running warp mixed: %v\nOutput: %s", err, output)
 		}
-		log.Println("Warp mixed Output:\n", output)
 		output_string := strings.Join(output, "\n")
 		err = cliutils.AppendToFile("warp-mixed_output.txt", output_string)
 
--- tests/cli_tests/zs3server_tests/2.1_put_test.go
+++ tests/cli_tests/zs3server_tests/2.1_put_test.go
@@ -1,7 +1,6 @@
 package zs3servertests
 
 import (
-	"log"
 	"strings"
 	"testing"
 	"time"
@@ -11,19 +10,16 @@ import (
 )
 
 func TestZs3serverPutWarpTests(testSetup *testing.T) {
-	log.Println("Running Warp Put Benchmark...")
 	t := test.NewSystemTest(testSetup)
 	config := cliutils.ReadFile(testSetup)
 
 	commandGenerated := "../warp put --host=" + config.Server + ":" + config.HostPort + " --access-key=" + config.AccessKey + " --secret-key=" + config.SecretKey + "  --concurrent " + config.Concurrent + " --duration 30s" + " --obj.size " + config.ObjectSize
-	log.Println("Command Generated: ", commandGenerated)
 
 	output, err := cliutils.RunCommand(t, commandGenerated, 1, time.Hour*2)
 
 	if err != nil {
 		testSetup.Fatalf("Error running warp put: %v\nOutput: %s", err, output)
 	}
-	log.Println("Warp Put Output:\n", output)
 	output_string := strings.Join(output, "\n")
 	output_string = strings.Split(output_string, "----------------------------------------")[1]
 
