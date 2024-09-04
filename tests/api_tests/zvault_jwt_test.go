467a975f49ee9205718c256b382fbccba572703e
Yaroslav Svitlytskyi <yariksvitlitskiy81@gmail.com>
2024-09-04 10:13:05 +0200

    feature: added zvault jwt tests

--- tests/api_tests/zvault_jwt_test.go
+++ tests/api_tests/zvault_jwt_test.go
@@ -22,7 +22,7 @@ func TestZvaultJWT(testSetup *testing.T) {
 		require.Equal(t, 401, response.StatusCode(), "Response status code does not match expected. Output: [%v]", response.String())
 	})
 
-	t.RunSequentially("Perform keys retrieval call with incorrect JWT token, which contains wrong client id", func(w *test.SystemTest) {
+	t.RunSequentially("Perform keys retrieval call with JWT token, containing user id, for which there are no keys", func(w *test.SystemTest) {
 		headers := zboxClient.NewZboxHeaders(client.X_APP_BLIMP)
 		Teardown(t, headers)
 
@@ -35,15 +35,43 @@ func TestZvaultJWT(testSetup *testing.T) {
 		require.NoError(t, err)
 		require.Equal(t, 200, response.StatusCode(), "Response status code does not match expected. Output: [%v]", response.String())
 
+		oldHeaders := zvaultClient.NewZvaultHeaders(jwtToken.JwtToken)
+
+		splitWallet, response, err := zvaultClient.GenerateSplitWallet(t, oldHeaders)
+		require.NoError(t, err)
+		require.Equal(t, 200, response.StatusCode(), "Response status code does not match expected. Output: [%v]", response.String())
+		require.NotNil(t, splitWallet)
+
+		headers = zboxClient.NewZboxHeaders(client.X_APP_BLIMP)
+		Teardown(t, headers)
+
+		headers["X-App-Client-ID"] = client.X_APP_CLIENT_ID_A
+		headers["X-App-User-ID"] = client.X_APP_USER_ID_A
+		headers["X-App-Client-Key"] = client.X_APP_CLIENT_KEY_A
+		headers["X-App-Client-Signature"] = client.X_APP_CLIENT_SIGNATURE_A
+
+		sessionID, response, err = zboxClient.CreateJwtSession(t, headers)
+		require.NoError(t, err)
+		require.Equal(t, 200, response.StatusCode(), "Response status code does not match expected. Output: [%v]", response.String())
+		require.NotEqual(t, int64(0), sessionID)
+
+		jwtToken, response, err = zboxClient.CreateJwtToken(t, sessionID, headers)
+		require.NoError(t, err)
+		require.Equal(t, 200, response.StatusCode(), "Response status code does not match expected. Output: [%v]", response.String())
+
 		headers = zvaultClient.NewZvaultHeaders(jwtToken.JwtToken)
 
-		keys, response, err := zvaultClient.GetKeys(t, client.X_APP_CLIENT_ID, headers)
+		keys, response, err := zvaultClient.GetKeys(t, splitWallet.ClientID, headers)
 		require.NoError(t, err)
 		require.Equal(t, 200, response.StatusCode(), "Response status code does not match expected. Output: [%v]", response.String())
 		require.Len(t, keys.Keys, 0)
+
+		response, err = zvaultClient.Delete(t, splitWallet.ClientID, oldHeaders)
+		require.NoError(t, err)
+		require.Equal(t, 200, response.StatusCode(), "Response status code does not match expected. Output: [%v]", response.String())
 	})
 
-	t.RunSequentially("Perform keys retrieval call with JWT token, which contains", func(w *test.SystemTest) {
+	t.RunSequentially("Perform keys retrieval call with JWT token, containing user id with present split key", func(w *test.SystemTest) {
 		headers := zboxClient.NewZboxHeaders(client.X_APP_BLIMP)
 		Teardown(t, headers)
 
@@ -58,9 +86,19 @@ func TestZvaultJWT(testSetup *testing.T) {
 
 		headers = zvaultClient.NewZvaultHeaders(jwtToken.JwtToken)
 
-		keys, response, err := zvaultClient.GetKeys(t, client.X_APP_CLIENT_ID, headers)
+		splitWallet, response, err := zvaultClient.GenerateSplitWallet(t, headers)
 		require.NoError(t, err)
 		require.Equal(t, 200, response.StatusCode(), "Response status code does not match expected. Output: [%v]", response.String())
-		require.Len(t, keys.Keys, 0)
+		require.NotNil(t, splitWallet)
+
+		keys, response, err := zvaultClient.GetKeys(t, splitWallet.ClientID, headers)
+		require.NoError(t, err)
+		require.Equal(t, 200, response.StatusCode(), "Response status code does not match expected. Output: [%v]", response.String())
+		require.Len(t, keys.Keys, 1)
+		require.Equal(t, keys.Keys[0].ClientID, splitWallet.ClientID)
+
+		response, err = zvaultClient.Delete(t, splitWallet.ClientID, headers)
+		require.NoError(t, err)
+		require.Equal(t, 200, response.StatusCode(), "Response status code does not match expected. Output: [%v]", response.String())
 	})
 }
