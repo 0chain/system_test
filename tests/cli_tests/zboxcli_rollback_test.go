0f2ea369074efe5818855ba779496b4580beef3d
hitenjain14 <hitenjain146@gmail.com>
2024-09-24 11:03:18 +0530

    skip rename rollback test

--- tests/cli_tests/zboxcli_rollback_test.go
+++ tests/cli_tests/zboxcli_rollback_test.go
@@ -311,6 +311,7 @@ func TestRollbackAllocation(testSetup *testing.T) {
 	})
 
 	t.Run("rollback allocation after renaming a file should work", func(t *test.SystemTest) {
+		t.Skip("rename is not atomic in v2")
 		allocSize := int64(64 * KB * 2)
 		fileSize := int64(256)
 
