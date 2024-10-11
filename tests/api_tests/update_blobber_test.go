14444327da08b9d59aa322b50ecd6b19766c878c
Akhilesh Mahajan <mahajanakhilesh53@gmail.com>
2024-10-11 10:19:30 +0530

    updated: added update blobber version in api test

--- tests/api_tests/update_blobber_test.go
+++ tests/api_tests/update_blobber_test.go
@@ -17,6 +17,47 @@ func TestUpdateBlobber(testSetup *testing.T) {
 
 	t.Parallel()
 
+	t.Run("update blobber version should work", func(t *test.SystemTest) {
+		wallet := createWallet(t)
+
+		blobberRequirements := model.DefaultBlobberRequirements(wallet.Id, wallet.PublicKey)
+		allocationBlobbers := apiClient.GetAllocationBlobbers(t, wallet, &blobberRequirements, client.HttpOkStatus)
+		allocationID := apiClient.CreateAllocation(t, wallet, allocationBlobbers, client.TxSuccessfulStatus)
+
+		allocation := apiClient.GetAllocation(t, allocationID, client.HttpOkStatus)
+
+		blobberID := getFirstUsedStorageNodeID(allocationBlobbers.Blobbers, allocation.Blobbers)
+		require.NotZero(t, blobberID)
+
+		blobber := apiClient.GetBlobber(t, blobberID, client.HttpOkStatus)
+		require.NotEqual(t, wallet.Id, blobber.StakePoolSettings.DelegateWallet)
+
+		blobber.StorageVersion = 2
+
+		apiClient.UpdateBlobber(t, wallet, blobber, client.TxUnsuccessfulStatus)
+	})
+
+	t.Run("update blobber: degrade version should not work", func(t *test.SystemTest) {
+		wallet := createWallet(t)
+
+		blobberRequirements := model.DefaultBlobberRequirements(wallet.Id, wallet.PublicKey)
+		allocationBlobbers := apiClient.GetAllocationBlobbers(t, wallet, &blobberRequirements, client.HttpOkStatus)
+
+		allocationID := apiClient.CreateAllocation(t, wallet, allocationBlobbers, client.TxSuccessfulStatus)
+
+		allocation := apiClient.GetAllocation(t, allocationID, client.HttpOkStatus)
+
+		blobberID := getFirstUsedStorageNodeID(allocationBlobbers.Blobbers, allocation.Blobbers)
+		require.NotZero(t, blobberID)
+
+		blobber := apiClient.GetBlobber(t, blobberID, client.HttpOkStatus)
+		require.NotEqual(t, wallet.Id, blobber.StakePoolSettings.DelegateWallet)
+
+		blobber.StorageVersion = 1
+
+		apiClient.UpdateBlobber(t, wallet, blobber, client.TxUnsuccessfulStatus)
+	})
+
 	t.Run("Update blobber in allocation without correct delegated client, shouldn't work", func(t *test.SystemTest) {
 		wallet := createWallet(t)
 
