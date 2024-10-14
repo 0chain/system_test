715d39e226017a4949f25cbdf0b63fe84833566f
Akhilesh Mahajan <mahajanakhilesh53@gmail.com>
2024-10-15 00:01:30 +0530

    updated: reveretd create wallet

--- tests/api_tests/main_test.go
+++ tests/api_tests/main_test.go
@@ -1,7 +1,6 @@
 package api_tests
 
 import (
-	"math/rand"
 	"encoding/json"
 	"log"
 	"os"
@@ -192,12 +191,8 @@ type WalletFile struct {
 
 func createWallet(t *test.SystemTest) *model.Wallet {
 	walletMutex.Lock()
-
-	// generate random number for wallet idx
-	walletIdx = int64(rand.Intn(len(initialisedWallets)))
-
 	wallet := initialisedWallets[walletIdx]
-	//walletIdx++
+	walletIdx++
 	balance := apiClient.GetWalletBalance(t, wallet, client.HttpOkStatus)
 	wallet.Nonce = int(balance.Nonce)
 	walletMutex.Unlock()
